90c440ab89facf4717e9aaa195b7833fa469b9b6
manthosh <manthosh@169.254.77.143>
2017-05-16 15:09:27 +0530

    Added DB Connection Pool using Tomcat connection pool and also added a ServletContextListener for this.

---
+++ src/main/java/com/facilio/transaction/FacilioConnectionPool.java
@@ -0,0 +1,47 @@
+package com.facilio.transaction;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public enum FacilioConnectionPool {
+	INSTANCE;
+	
+	private DataSource ds = null;
+	
+	private FacilioConnectionPool() {
+		Context initCtx = null;
+		Context envCtx = null;
+		try {
+			initCtx = new InitialContext();
+			envCtx = (Context) initCtx.lookup("java:comp/env");
+			ds = (DataSource) envCtx.lookup("jdbc/BmsDB");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new ExceptionInInitializerError("Unable to initialize DB connection pool due to the following error : \n"+e.getMessage());
+		}
+		finally {
+			if(initCtx != null) {
+				try {
+					initCtx.close();
+				} catch (NamingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static FacilioConnectionPool getInstance() {
+		return INSTANCE;
+	}
+	
+	public Connection getConnection() throws SQLException {
+		return ds.getConnection();
+	}
+}
