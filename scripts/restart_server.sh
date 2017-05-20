f64bc49172507ab5b29b3a8d0ecdcdcf8bb9911d
Logesh <logesht@zoho.com>
2017-05-20 07:42:05 +0530

    use tomcat 9

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,2 +1,2 @@
 echo "this will the script restarting the server"
-sudo sh /home/ubuntu/apache-tomcat-7.0.77/bin/catalina.sh start
+sudo sh /home/ubuntu/apache-tomcat-9.0.0.M21/bin/catalina.sh start
