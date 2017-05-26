3fb177ccf442122dd3f79f2878dd3681f4c405ba
shivaraj <shivaraj@192.168.0.39>
2017-05-26 16:30:16 +0530

    IOT Data sync

--- src/main/java/com/facilio/tasker/tasks/SyncIotDataJob.java
+++ src/main/java/com/facilio/tasker/tasks/SyncIotDataJob.java
@@ -34,17 +34,16 @@ public class SyncIotDataJob extends FacilioJob{
 	        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 	        String xAmzDate = dateFormat.format(new Date());
 	        
-	        //String payload = "{\n\"state\":{\n\"desired\":{\n\"attribute1\":\"a\",\n\"attribute2\":\"b\"\n},\n\"reported\":{\n\"attribute1\":\"ds\",\n\"attribute2\":\"string1\"\n}\n},\n\"clientToken\":\"shiva2\",\n\"version\":17\n}";
-	        String payload = "{\n\"state\":{\n\"desired\":" + AwsUtil.getDeviceData() + "\n},\n\"clientToken\":\"shiva2\",\n\"version\":"+counter+"\n}";
+	        String payload = "{\n\"state\":{\n\"desired\":" + AwsUtil.getDeviceData() + "\n},\n\"clientToken\":\"shiva2\",\n\"version\":" + counter + "\n}";
 	        System.out.println("payload :::" +payload);
 	        String signature = AwsUtil.getSignature(secretKey, dateStamp, regionName, serviceName, payload, xAmzDate);
-	        String url = "https://a2ak8t6zogzde5.iot.us-west-2.amazonaws.com/things/EM/shadow";
+	        String url = "https://" + AwsUtil.getConfig("host") + "/things/EM/shadow";
 	        
 	        Map<String, String> headers = new HashMap<String, String>();
 	        headers.put("Content-Type", "application/json");
-	        headers.put("Host", "a2ak8t6zogzde5.iot.us-west-2.amazonaws.com");
+	        headers.put("Host", AwsUtil.getConfig("host"));
 	        headers.put("X-Amz-Date", xAmzDate);
-	        headers.put("Authorization", "AWS4-HMAC-SHA256 Credential=AKIAJPM4FLBRC6DFAXIA/20170525/us-west-2/iotdata/aws4_request, SignedHeaders=content-type;host;x-amz-date, Signature="+signature);
+	        headers.put("Authorization", "AWS4-HMAC-SHA256 Credential=" + AwsUtil.getConfig("accessKeyId") + "/" + new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + AwsUtil.getConfig("region") + "/" + AwsUtil.AWS_IOT_SERVICE_NAME + "/aws4_request, SignedHeaders=content-type;host;x-amz-date, Signature=" + signature);
 	        
 	        AwsUtil.doHttpPost(url, headers, null, payload);
 	        counter++;
