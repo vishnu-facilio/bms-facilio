e776ad01eabadcb9997447a763e453bea4de611b
yoge <yogebabu@gmail.com>
2017-06-13 16:31:11 +0530

    compilation fix

---
+++ src/main/java/com/facilio/bmsconsole/util/OrgApi.java
@@ -0,0 +1,10 @@
+package com.facilio.bmsconsole.util;
+
+public class OrgApi {
+
+	public static long getOrgIdFromDomain(String orgDomain) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
