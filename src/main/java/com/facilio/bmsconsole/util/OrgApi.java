72b7284cfe6b97e69e235ba9a37bd4ef9c611c30
Manthosh <manthosh@facilio.co>
2017-06-14 14:34:06 +0530

    Added OrgApi as it was missed during SupportMailParser

--- src/main/java/com/facilio/bmsconsole/util/OrgApi.java
+++ src/main/java/com/facilio/bmsconsole/util/OrgApi.java
@@ -1,10 +1,39 @@
 package com.facilio.bmsconsole.util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.facilio.sql.DBUtil;
+import com.facilio.transaction.FacilioConnectionPool;
+
 public class OrgApi {
-
-	public static long getOrgIdFromDomain(String orgDomain) {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	public static long getOrgIdFromDomain(String domain) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = FacilioConnectionPool.INSTANCE.getConnection();
+			pstmt = conn.prepareStatement("SELECT ORGID FROM Organizations WHERE FACILIODOMAINNAME = ?");
+			
+			pstmt.setString(1, domain);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				return rs.getLong("ORGID");
+			}
+		}
+		catch (SQLException e) {
+			throw e;
+		}
+		finally {
+			DBUtil.closeAll(conn, pstmt, rs);
+		}
+		
+		return -1;
 	}
-
 }
