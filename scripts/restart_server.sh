c356c14cbf6d93027e02f96073f50a3dd8b6d4a2
Magesh <magesh@thingscient.com>
2017-06-14 19:20:59 +0530

    restart server script changes

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,2 +1,3 @@
 echo "this will the script restarting the server"
+sudo sh /home/ubuntu/apache-tomcat-9.0.0.M21/bin/catalina.sh stop
 sudo sh /home/ubuntu/apache-tomcat-9.0.0.M21/bin/catalina.sh start
