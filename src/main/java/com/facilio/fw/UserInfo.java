8cbca3264009e21faf5c6799eb814cdd63e74ae0
Magesh <magesh@thingscient.com>
2017-06-22 19:01:27 +0530

    changes in current thread local user object

--- src/main/java/com/facilio/fw/UserInfo.java
+++ src/main/java/com/facilio/fw/UserInfo.java
@@ -1,29 +1,129 @@
 package com.facilio.fw;
 
+import com.facilio.bmsconsole.context.UserContext;
+import com.facilio.bmsconsole.util.UserAPI;
+
+/**
+ * 
+ * Current user object.
+ *
+ * You can get current user object by UserInfo.getCurrentUser();
+ * 
+ * Also, you can check if the current user has 'admin' role by UserInfo.getCurrentUser().hasRole('admin')
+ * 
+ */
+
 public class UserInfo {
-	private long userid;
+
+	private long userId;
+	private long orgId;
+	private long orgUserId;
 	private String email;
-	private String cognitoid;
 	private String username;
+	private String name;
+	private int role;
+	private boolean isActive;
+
 	private static ThreadLocal<UserInfo> userlocal = new ThreadLocal<UserInfo>();
-	
-	public  UserInfo(String username)
+
+	public UserInfo(String username) throws Exception
 	{
 		this.username = username;
-		this.email = email;
+		UserContext context = UserAPI.getUser(username);
+		
+		setUserId(context.getUserId());
+		setOrgId(context.getOrgId());
+		setOrgUserId(context.getOrgUserId());
+		setEmail(context.getEmail());
+		setUsername(context.getEmail());
+		setName(context.getName());
+		setRole(context.getRole());
+		setActive(context.getInviteAcceptStatus());
 	}
 
-	public static  UserInfo getCurrentUser()
+	public static UserInfo getCurrentUser()
 	{
 		return userlocal.get();
 	}
+
 	public static  void setCurrentUser(UserInfo userinfo)
 	{
-		 userlocal.set(userinfo);
+		userlocal.set(userinfo);
 	}
+
 	public static void cleanup()
 	{
 		userlocal.remove();
 	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public long getOrgId() {
+		return orgId;
+	}
+
+	public void setOrgId(long orgId) {
+		this.orgId = orgId;
+	}
+
+	public long getOrgUserId() {
+		return orgUserId;
+	}
+
+	public void setOrgUserId(long orgUserId) {
+		this.orgUserId = orgUserId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRole() {
+		return role;
+	}
+
+	public void setRole(int role) {
+		this.role = role;
+	}
 	
+	public boolean hasRole(int role) {
+		if (this.role == role) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
 }
