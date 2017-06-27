85ba2e2dbf6dec29b5aabfd51fa2a53a40e5a70f
yoge <yogebabu@gmail.com>
2017-06-27 14:37:27 +0530

    compile issue due to merge

--- src/main/java/com/facilio/bmsconsole/commands/FacilioContext.java
+++ src/main/java/com/facilio/bmsconsole/commands/FacilioContext.java
@@ -13,21 +13,6 @@ public class FacilioContext extends ContextBase {
 	boolean isAutoCommit = false;
 	
 	
-	public Connection package com.facilio.bmsconsole.commands;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import org.apache.commons.chain.impl.ContextBase;
-
-import com.facilio.transaction.FacilioConnectionPool;
-
-public class FacilioContext extends ContextBase {
-	java.sql.Connection conn = null;
-	boolean isConnectionOpen = false;
-	boolean isAutoCommit = false;
-	
-	
 	public Connection getConnection() throws SQLException
 	{
 		if(conn==null || !isConnectionOpen)
@@ -51,38 +36,19 @@ public class FacilioContext extends ContextBase {
 		isConnectionOpen = false;
 	}
 
-}
-() throws SQLException
-	{
-		if(conn==null || !isConnectionOpen)
-		{
-			conn = FacilioConnectionPool.getInstance().getConnection();
-			isConnectionOpen = true;
-			isAutoCommit = false;
-			conn.setAutoCommit(false);
-		}
-		return conn;
-	}
-	
 	public void commit() throws Exception {
-		if(!isAutoCommit){
-			conn.commit();
-		}
-		cleanup();
-	}
-	
-	public void rollback() throws Exception {
-		if(!isAutoCommit){
-			conn.rollback();
-		}
-		cleanup();
-	}
-	
-	private void cleanup() throws Exception
-	{
-		conn.close();
-		conn = null;
-		isConnectionOpen = false;
-	}
-
+  		if(!isAutoCommit){
+  			conn.commit();
+  		}
+  		cleanup();
+  	}
+  	
+  	public void rollback() throws Exception {
+  		if(!isAutoCommit){
+  			conn.rollback();
+  		}
+  		cleanup();
+  	}
 }
+
+
