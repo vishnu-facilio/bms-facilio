0dd7643759853dc66d8928714994650cb4015ad2
Sureshbabu <suresh@facilio.co>
2017-09-14 13:02:52 +0530

    Leed Building Details page

---
+++ src/main/java/com/facilio/leed/actions/BuildingHome.java
@@ -0,0 +1,58 @@
+package com.facilio.leed.actions;
+
+import org.apache.commons.chain.Chain;
+
+import com.facilio.bmsconsole.commands.FacilioChainFactory;
+import com.facilio.bmsconsole.commands.FacilioContext;
+import com.facilio.bmsconsole.context.BuildingContext;
+import com.facilio.constants.FacilioConstants;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BuildingHome extends ActionSupport {
+
+	private BuildingContext building;
+	private long buildingId;
+	
+	public String execute() throws Exception
+	{
+		return null;
+	}
+	
+	public BuildingContext getBuilding() 
+	{
+		return building;
+	}
+	public void setBuilding(BuildingContext building) 
+	{
+		this.building = building;
+	}
+	
+	
+	public long getBuildingId() 
+	{
+		return buildingId;
+	}
+	public void setBuildingId(long buildingId) 
+	{
+		this.buildingId = buildingId;
+	}
+
+	public String detailspage() throws Exception
+	{
+		setBuildingId(1);
+		
+		FacilioContext context = new FacilioContext();
+		
+		context.put(FacilioConstants.ContextNames.ID, getBuildingId());
+		
+		Chain getBuildingChain = FacilioChainFactory.getBuildingDetailsChain();
+		
+		getBuildingChain.execute(context);
+		
+		setBuilding((BuildingContext) context.get(FacilioConstants.ContextNames.BUILDING));
+		
+		System.out.println(">>>>>>>>>> Comes Here >>>>>>>>>>>");
+		
+		return SUCCESS;
+	}
+}
