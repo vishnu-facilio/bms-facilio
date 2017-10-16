0cad552f862a7dfbbea8e82e8648c44ed86518fa
yoge <DELL@DESKTOP-593216G>
2017-10-16 21:54:11 +0530

    api code

--- src/main/java/com/facilio/bmsconsole/actions/ApiAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/ApiAction.java
@@ -1,5 +1,8 @@
 package com.facilio.bmsconsole.actions;
 
+import java.util.HashMap;
+
+import com.facilio.bmsconsole.commands.data.EventInfo;
 import com.facilio.fw.OrgInfo;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -9,8 +12,22 @@ public class ApiAction extends ActionSupport {
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Org Info is :"+OrgInfo.getCurrentOrgInfo());
+		EventInfo.save(events.toArray(new EventInfo[events.size()]));
 		return super.execute();
 	}
 	
 
+	public java.util.ArrayList<EventInfo> getEvents() {
+		return events;
+	}
+
+
+	public void setEvents(java.util.ArrayList<EventInfo> events) {
+		this.events = events;
+	}
+
+
+	private java.util.ArrayList<EventInfo> events = new java.util.ArrayList<EventInfo>();
+			
+	
 }
---
+++ src/main/java/com/facilio/bmsconsole/commands/data/EventInfo.java
@@ -0,0 +1,112 @@
+package com.facilio.bmsconsole.commands.data;
+
+import com.facilio.fw.OrgInfo;
+import com.facilio.transaction.FacilioConnectionPool;
+import java.sql.PreparedStatement;
+
+public class EventInfo {
+
+	public String source;
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getNode() {
+		return node;
+	}
+
+	public void setNode(String node) {
+		this.node = node;
+	}
+
+	public String getResource() {
+		return resource;
+	}
+
+	public void setResource(String resource) {
+		this.resource = resource;
+	}
+
+	public String getSeverity() {
+		return severity;
+	}
+
+	public void setSeverity(String severity) {
+		this.severity = severity;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAdditionInfo() {
+		return additionInfo;
+	}
+
+	public void setAdditionInfo(String additionInfo) {
+		this.additionInfo = additionInfo;
+	}
+
+	public String type;
+	public String node;
+	public String resource;
+	public String severity;
+	public String description;
+	public String additionInfo;
+	
+	public static void save(EventInfo[] events) throws Exception
+	{
+		java.sql.Connection con = FacilioConnectionPool.getInstance().getConnection();
+		
+		PreparedStatement ps=null;
+		try {
+			String pre_query = "insert into Event (ORGID,SOURCE,NODE,EVENT_TYPE,SEVERITY,DESCRIPTION,ADDITIONAL_INFO) values ("+OrgInfo.getCurrentOrgInfo()+" ,?,?,?,?,?,? ) ";
+			ps = con.prepareStatement(pre_query);
+			for(int i=0;i<events.length;i++)
+			{
+				EventInfo event = events[i];
+				ps.setString(1, event.getSource());
+				ps.setString(2, event.getNode());
+
+				ps.setString(3, event.getType());
+
+				ps.setString(4, event.getSeverity());
+
+				ps.setString(5, event.getDescription());
+
+				ps.setString(6, event.getAdditionInfo());
+				ps.addBatch();
+				
+
+			}
+			ps.executeBatch();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally
+		{
+			ps.close();
+			con.close();
+		}
+		
+		
+	}
+	
+}
