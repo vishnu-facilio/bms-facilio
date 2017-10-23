35cb233be0cc90930a6eaf827855a5be6cb53bf7
Magesh V <magesh@facilio.com>
2017-10-23 16:36:39 +0530

    build update changes

---
+++ scripts/replace_conf.sh
@@ -0,0 +1,25 @@
+echo "replacing conf files..."
+
+export APP_HOME="/home/ubuntu/apache-tomcat-9.0.0.M21"
+export CONF_DIR="$APP_HOME/webapps/bms/WEB-INF/classes/conf"
+
+CURRENT_HOSTNAME=$(hostname)
+
+API_SERVERS="ip-172-31-22-26"
+SCHEDULE_SERVERS="ip-172-31-18-204"
+
+for SCHEDULE_SERVER in $SCHEDULE_SERVERS
+do
+        if [ "$SCHEDULE_SERVER" == "$CURRENT_HOSTNAME" ]; then
+                sed -i'' "s%schedulerServer=.*%schedulerServer=true%g" $CONF_DIR/awsprops.properties
+        fi
+done
+
+for API_SERVER in $API_SERVERS
+do
+        if [ "$API_SERVER" == "$CURRENT_HOSTNAME" ]; then
+                sed -i'' "s%schedulerServer=.*%schedulerServer=false%g" $CONF_DIR/awsprops.properties
+        fi
+done
+
+echo "conf files replaced..."
