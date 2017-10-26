6a555e31191423bf54f52b1adc32cb539c9e7878
Mark Zuckerberg <yoge@facilio.com>
2017-10-26 19:04:27 +0530

    new file, compilation fix

---
+++ src/main/java/com/facilio/bmsconsole/criteria/FacilioModulePredicate.java
@@ -0,0 +1,17 @@
+package com.facilio.bmsconsole.criteria;
+
+import org.apache.commons.collections.Predicate;
+
+public class FacilioModulePredicate implements Predicate {
+
+	public FacilioModulePredicate(String name, Predicate predicate) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean evaluate(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
