50cb4b635facbd89f286d92a2e1de123bb4ef1c4
Shivaraj <shivaraj@facilio.com>
2017-11-07 15:35:37 +0530

    Alarm bug fix

--- src/main/java/com/facilio/bmsconsole/commands/LoadAllFieldsCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/LoadAllFieldsCommand.java
@@ -4,6 +4,7 @@ import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 
 import com.facilio.beans.ModuleBean;
+import com.facilio.bmsconsole.context.AlarmContext;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.fw.BeanFactory;
 
@@ -18,10 +19,17 @@ public class LoadAllFieldsCommand implements Command{
 		if(moduleName == null || moduleName.isEmpty()) {
 			throw new IllegalArgumentException("Module Name is not set for the module");
 		}
- 		
-		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
-		
-		context.put(FacilioConstants.ContextNames.EXISTING_FIELD_LIST, modBean.getAllFields(moduleName));
+		if(context.get("activityType") != null && "alarmFromEvent".equals(context.get("activityType")))
+		{
+			AlarmContext alarm = (AlarmContext) context.get(FacilioConstants.ContextNames.ALARM);
+			ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean", alarm.getOrgId());
+			context.put(FacilioConstants.ContextNames.EXISTING_FIELD_LIST, modBean.getAllFields(moduleName));
+		}
+		else
+		{
+			ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+			context.put(FacilioConstants.ContextNames.EXISTING_FIELD_LIST, modBean.getAllFields(moduleName));
+		}
 		
 		return false;
 	}
