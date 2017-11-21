062fe5132c51c6f699d5f14a79b9837e659e0469
simranjain1507 <simranjain068@gmail.com>
2017-11-21 14:34:40 +0530

    UPDATE NOTIFICATION

---
+++ src/main/java/com/facilio/bmsconsole/commands/UpdateNotificationSettings.java
@@ -0,0 +1,49 @@
+package com.facilio.bmsconsole.commands;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.chain.Command;
+import org.apache.commons.chain.Context;
+
+import com.facilio.bmsconsole.context.SupportEmailContext;
+import com.facilio.bmsconsole.modules.FacilioField;
+import com.facilio.bmsconsole.modules.FieldFactory;
+import com.facilio.bmsconsole.modules.FieldUtil;
+import com.facilio.bmsconsole.workflow.WorkflowRuleContext;
+import com.facilio.constants.FacilioConstants;
+import com.facilio.fw.OrgInfo;
+import com.facilio.sql.GenericUpdateRecordBuilder;
+import com.facilio.transaction.FacilioConnectionPool;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UpdateNotificationSettings implements Command {
+	@Override
+	public boolean execute(Context context) throws Exception {
+		WorkflowRuleContext workflow=(WorkflowRuleContext) context.get(FacilioConstants.ContextNames.WORKFLOW_UPDATE);
+		System.out.println("....UpdateNotification");
+
+		if(workflow != null){
+			System.out.println("....UpdateNotification condition");
+
+			workflow.setOrgId(OrgInfo.getCurrentOrgInfo().getOrgid());
+			Map<String, Object> workflowProps= FieldUtil.getAsProperties(workflow);
+			Connection con = FacilioConnectionPool.getInstance().getConnection();
+			try{
+				List<FacilioField> fields= FieldFactory.getWorkflowRuleFields();
+				GenericUpdateRecordBuilder builder=new GenericUpdateRecordBuilder().table("Workflow_Rule").fields(fields).andCustomWhere("ID = ?", workflow.getId());
+				builder.update(workflowProps);
+				context.put(FacilioConstants.ContextNames.RESULT, "success");
+			} catch(Exception e){
+				e.printStackTrace();
+				throw e;
+			}
+			finally{
+				con.close();
+			}
+		}
+		return false;
+	}
+}
