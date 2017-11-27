38742d8c0ab0c344e8a9ce60fb9f9afb97607bec
Magesh V <magesh@facilio.com>
2017-11-27 10:08:36 +0530

    delete alarm commmand

---
+++ src/main/java/com/facilio/bmsconsole/commands/DeleteAlarmCommand.java
@@ -0,0 +1,40 @@
+package com.facilio.bmsconsole.commands;
+
+import java.util.List;
+
+import org.apache.commons.chain.Command;
+import org.apache.commons.chain.Context;
+
+import com.facilio.constants.FacilioConstants;
+import com.facilio.sql.GenericDeleteRecordBuilder;
+
+public class DeleteAlarmCommand implements Command {
+
+	@Override
+	public boolean execute(Context context) throws Exception {
+		// TODO Auto-generated method stub
+		List<Long> recordIds = (List<Long>) context.get(FacilioConstants.ContextNames.RECORD_ID_LIST);
+		if(recordIds != null && !recordIds.isEmpty()) {
+			String moduleName = (String) context.get(FacilioConstants.ContextNames.MODULE_NAME);
+			String dataTableName = (String) context.get(FacilioConstants.ContextNames.MODULE_DATA_TABLE_NAME);
+			
+			String condition = "ID IN (";
+			for (int i=0; i< recordIds.size(); i++) {
+				if (i != 0) {
+					condition += ",";
+				}
+				condition += "?";
+			}
+			condition += ")";
+			
+			GenericDeleteRecordBuilder builder = new GenericDeleteRecordBuilder()
+					.connection(((FacilioContext)context).getConnectionWithTransaction())
+					.table(dataTableName)
+					.andCustomWhere(condition, recordIds.toArray());
+			
+			context.put(FacilioConstants.ContextNames.ROWS_UPDATED, builder.delete());
+		}
+		return false;
+	}
+	
+}
