88d01df7e0f6192e55a11eacd7b43d8c53d1c555
User <user@Karthiks-MacBook-Air-2.local>
2017-12-06 13:27:49 +0530

    ChangeStatus Issue fixed

--- src/main/java/com/facilio/accounts/dto/User.java
+++ src/main/java/com/facilio/accounts/dto/User.java
@@ -24,9 +24,6 @@ public class User {
 	private long ouid;
 	private long orgId;
 	private long invitedTime;
-	private boolean isDefaultOrg;
-	private boolean userStatus;
-	private boolean inviteAcceptStatus;
 	private long roleId;
 	private int userType;
 	
@@ -154,23 +151,46 @@ public class User {
 	public void setInvitedTime(long invitedTime) {
 		this.invitedTime = invitedTime;
 	}
-	public boolean getIsDefaultOrg() {
+	
+	private Boolean isDefaultOrg;
+	public Boolean getIsDefaultOrg() {
 		return isDefaultOrg;
 	}
-	public void setDefaultOrg(boolean isDefaultOrg) {
+	public void setDefaultOrg(Boolean isDefaultOrg) {
 		this.isDefaultOrg = isDefaultOrg;
 	}
-	public boolean getUserStatus() {
+	public boolean isDefaultOrg() {
+		if(isDefaultOrg != null) {
+			return isDefaultOrg.booleanValue();
+		}
+		return false;
+	}
+	
+	private Boolean userStatus;
+	public Boolean getUserStatus() {
 		return userStatus;
 	}
-	public void setUserStatus(boolean userStatus) {
+	public void setUserStatus(Boolean userStatus) {
 		this.userStatus = userStatus;
 	}
-	public boolean getInviteAcceptStatus() {
+	public boolean isActive() {
+		if(userStatus != null) {
+			return userStatus.booleanValue();
+		}
+		return false;
+	}
+	private Boolean inviteAcceptStatus;
+	public Boolean getInviteAcceptStatus() {
 		return inviteAcceptStatus;
 	}
-	public void setInviteAcceptStatus(boolean inviteAcceptStatus) {
+	public void setInviteAcceptStatus(Boolean inviteAcceptStatus) {
 		this.inviteAcceptStatus = inviteAcceptStatus;
+	}
+	public boolean isInviteAcceptStatus(){
+		if(inviteAcceptStatus!=null){
+			return inviteAcceptStatus.booleanValue();
+		}
+		return false;
 	}
 	public long getRoleId() {
 		return roleId;
