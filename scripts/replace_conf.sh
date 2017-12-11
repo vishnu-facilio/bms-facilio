acef35c49b0fb2fa40425f6fe937f5365b266602
bhuvaneswaran.g <bhuvaneswaran.g@facilio.com>
2017-12-11 20:38:48 +0530

    Added db specific files for stage and production setup.

--- scripts/replace_conf.sh
+++ scripts/replace_conf.sh
@@ -2,7 +2,23 @@
 echo "replacing conf files..."
 
 export APP_HOME="/home/ubuntu/apache-tomcat-9.0.0.M21"
-export CONF_DIR="$APP_HOME/webapps/bms/WEB-INF/classes/conf"
+export BMS_DIR="$APP_HOME/webapps/bms"
+export CONF_DIR="$BMS_DIR/WEB-INF/classes/conf"
+export META_INF_DIR="$BMS_DIR/META-INF"
+
+echo "copying $DEPLOYMENT_GROUP_NAME context file"
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "pre_production" ]
+then
+    cp $META_INF_DIR/context-stage.xml $META_INF_DIR/context.xml
+fi
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "production_deployment" ]
+then
+    cp $META_INF_DIR/context-production.xml $META_INF_DIR/context.xml
+fi
+
+echo "copied $DEPLOYMENT_GROUP_NAME context file"
 
 CURRENT_HOSTNAME=$(hostname)
 
