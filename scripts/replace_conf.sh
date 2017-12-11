074af8019eec6de7a622f05998d2b5146cfdcd7b
bhuvaneswaran.g <bhuvaneswaran.g@facilio.com>
2017-12-11 22:07:43 +0530

    Reverted changes for files connecting different db

--- scripts/replace_conf.sh
+++ scripts/replace_conf.sh
@@ -2,25 +2,7 @@
 echo "replacing conf files..."
 
 export APP_HOME="/home/ubuntu/apache-tomcat-9.0.0.M21"
-export BMS_DIR="$APP_HOME/webapps/bms"
-export CONF_DIR="$BMS_DIR/WEB-INF/classes/conf"
-export META_INF_DIR="$BMS_DIR/META-INF"
-
-echo "Deployment group is : $DEPLOYMENT_GROUP_NAME" >> /home/ubuntu/deployment.log
-echo "copying $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
-echo "======" >> /home/ubuntu/deployment.log
-printenv >> /home/ubuntu/deployment.log
-echo "======" >> /home/ubuntu/deployment.log
-
-if [ "$DEPLOYMENT_GROUP_NAME" == "pre_production" ]; then
-    sudo cp $META_INF_DIR/context-stage.xml $META_INF_DIR/context.xml
-fi
-
-if [ "$DEPLOYMENT_GROUP_NAME" == "production_deployment" ]; then
-    sudo cp $META_INF_DIR/context-production.xml $META_INF_DIR/context.xml
-fi
-
-echo "copied $DEPLOYMENT_GROUP_NAME context file"
+export CONF_DIR="$APP_HOME/webapps/bms/WEB-INF/classes/conf"
 
 CURRENT_HOSTNAME=$(hostname)
 
