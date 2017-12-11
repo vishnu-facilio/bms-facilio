573c92205f9a40d8d1bf1c9122aa15c410ab87a0
bhuvaneswaran.g <bhuvaneswaran.g@facilio.com>
2017-12-11 20:51:35 +0530

    Replaced '==' with '='

--- scripts/replace_conf.sh
+++ scripts/replace_conf.sh
@@ -8,12 +8,12 @@ export META_INF_DIR="$BMS_DIR/META-INF"
 
 echo "copying $DEPLOYMENT_GROUP_NAME context file"
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "pre_production" ]
+if [ "$DEPLOYMENT_GROUP_NAME" = "pre_production" ]
 then
     cp $META_INF_DIR/context-stage.xml $META_INF_DIR/context.xml
 fi
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "production_deployment" ]
+if [ "$DEPLOYMENT_GROUP_NAME" = "production_deployment" ]
 then
     cp $META_INF_DIR/context-production.xml $META_INF_DIR/context.xml
 fi
