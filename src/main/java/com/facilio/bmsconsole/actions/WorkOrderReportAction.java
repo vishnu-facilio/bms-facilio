5617f09f91495829f7da3aacbf38fb1a114cb255
Vaish <vaishnavi.lm@facilio.com>
2017-12-12 00:57:23 +0530

    Move Map Location Issue fix to BMTS Branch

--- src/main/java/com/facilio/bmsconsole/actions/WorkOrderReportAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/WorkOrderReportAction.java
@@ -251,7 +251,7 @@ public class WorkOrderReportAction extends ActionSupport {
 						location.put("lng",locationCtxt.getLng());
 						buildingObj.put("location", location);
 					}
-//					List<BaseSpaceContext> allSpaces = SpaceAPI.getBaseSpaceWithChildren(building.getId());
+					// List<BaseSpaceContext> allSpaces = SpaceAPI.getBaseSpaceWithChildren(building.getId());
 					String buildingCnd ="\""+FacilioConstants.Ticket.SPACE+"\":{\"module\":\""+FacilioConstants.ContextNames.WORK_ORDER+"\",\"operator\":\"building_is\",\"value\":[\""+building.getId()+"_\"]}";
 					buildingObj.put("stats", getOpenWorkOrderSummary(buildingCnd));
 					woStats.add(buildingObj);
