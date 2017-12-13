c0f49175aa0bdc011e591fef8a121421e14a2eb6
bhuvaneswaran.g <bhuvaneswaran.g@facilio.com>
2017-12-13 21:18:12 +0530

    Running commands using sudo instead of root user

--- scripts/change_conf.sh
+++ scripts/change_conf.sh
@@ -7,14 +7,13 @@ export META_INF_DIR="$BMS_DIR/META-INF"
 echo "Deployment group is : $DEPLOYMENT_GROUP_NAME" >> /home/ubuntu/deployment.log
 echo "copying $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
 echo "======" >> /home/ubuntu/deployment.log
-printenv >> /home/ubuntu/deployment.log
 
-unzip $APP_HOME/webapps/ROOT.war -d $APP_HOME/webapps/ROOT
+sudo unzip $APP_HOME/webapps/ROOT.war -d $APP_HOME/webapps/ROOT
 
 if [ "$DEPLOYMENT_GROUP_NAME" = "pre_production" ]; then
-    cp $META_INF_DIR/context-stage.xml $META_INF_DIR/context.xml
+    sudo cp $META_INF_DIR/context-stage.xml $META_INF_DIR/context.xml
 fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" = "production_deployment" ]; then
-    cp $META_INF_DIR/context-production.xml $META_INF_DIR/context.xml
+    sudo cp $META_INF_DIR/context-production.xml $META_INF_DIR/context.xml
 fi
