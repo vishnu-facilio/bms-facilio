972a236b15cd528c5b61824bb4cf919281fece58
Magesh V <magesh@facilio.com>
2017-12-13 12:45:33 +0530

    lead tracker added

---
+++ src/main/java/com/facilio/bmsconsole/actions/LeadTracker.java
@@ -0,0 +1,50 @@
+package com.facilio.bmsconsole.actions;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+
+import com.facilio.aws.util.AwsUtil;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LeadTracker extends ActionSupport {
+	
+	@Override
+	public String execute() throws Exception {
+		
+		createLead(getData());
+		
+		return SUCCESS;
+	}
+	
+	private String response = "success";
+	
+	private JSONObject data;
+	
+	public void setData(JSONObject data) {
+		this.data = data;
+	}
+	
+	public JSONObject getData() {
+		return this.data;
+	}
+	
+	private void createLead(JSONObject lead) throws IOException {
+		
+		if (lead == null || lead.size() == 0) {
+			return;
+		}
+		
+		JSONObject postData = new JSONObject();
+		postData.put("lead", lead);
+		
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type","application/json");
+		headers.put("Authorization", "Token token=i-hJNvjv8TRdVGlJmGHNmw");
+		 
+		String url = "https://facilio.freshsales.io/api/leads";
+		AwsUtil.doHttpPost(url, headers, null, postData.toJSONString());
+	}
+}
