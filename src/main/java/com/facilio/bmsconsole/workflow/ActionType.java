71348d54969c136feacf24b64991873d6fa9ff76
Manthosh <manthosh@facilio.com>
2017-12-16 18:19:35 +0530

    Sending push notification to all emails in email template

--- src/main/java/com/facilio/bmsconsole/workflow/ActionType.java
+++ src/main/java/com/facilio/bmsconsole/workflow/ActionType.java
@@ -1,6 +1,5 @@
 package com.facilio.bmsconsole.workflow;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -9,15 +8,25 @@ import java.util.Map;
 
 import org.apache.commons.chain.Chain;
 import org.apache.commons.chain.Context;
+import org.apache.commons.lang3.StringUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
+import com.facilio.accounts.util.AccountConstants;
+import com.facilio.accounts.util.AccountUtil;
 import com.facilio.aws.util.AwsUtil;
 import com.facilio.bmsconsole.commands.FacilioContext;
+import com.facilio.bmsconsole.criteria.Condition;
+import com.facilio.bmsconsole.criteria.CriteriaAPI;
+import com.facilio.bmsconsole.criteria.StringOperators;
+import com.facilio.bmsconsole.modules.FacilioField;
+import com.facilio.bmsconsole.modules.FacilioModule;
+import com.facilio.bmsconsole.modules.FieldType;
+import com.facilio.bmsconsole.modules.ModuleFactory;
 import com.facilio.bmsconsole.util.SMSUtil;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.events.constants.EventConstants;
+import com.facilio.sql.GenericSelectRecordBuilder;
 
 public enum ActionType {
 	EMAIL_NOTIFICATION(1) {
@@ -180,30 +189,114 @@ public enum ActionType {
 			// TODO Auto-generated method stub
 			try {
 				if(obj != null) {
-					JSONObject content = new JSONObject();
-					content.put("to", "exA12zxrItk:APA91bFzIR6XWcacYh24RgnTwtsyBDGa5oCs5DVM9h3AyBRk7GoWPmlZ51RLv4DxPt2Dq2J4HDTRxW6_j-RfxwAVl9RT9uf9-d9SzQchMO5DHCbJs7fLauLIuwA5XueDuk7p5P7k9PfV");
 					
-					JSONObject notification = new JSONObject();
-					notification.put("body", "Test");
-					notification.put("title", "Test");
-					notification.put("content_available", true);
-					notification.put("priority", "high");
-					content.put("notification", notification);
+					List<String> emails = getEmailAddresses();
 					
-					JSONObject data = new JSONObject();
-					data.put("id", 1253);
-					content.put("data", data);
-					
-					Map<String, String> headers = new HashMap<>();
-					headers.put("Content-Type","application/json");
-					String url = "https://fcm.googleapis.com/fcm/send";
-					
-					AwsUtil.doHttpPost(url, headers, null, content.toJSONString());
+					if(emails != null && !emails.isEmpty()) {
+						Map<String, String> emailToMobileId = getMobileInstanceIDs(emails);
+						
+						for(String email : emails) {
+							String mobileId = emailToMobileId.get(email);
+							if(mobileId != null) {
+								JSONObject content = new JSONObject();
+//								content.put("to", "exA12zxrItk:APA91bFzIR6XWcacYh24RgnTwtsyBDGa5oCs5DVM9h3AyBRk7GoWPmlZ51RLv4DxPt2Dq2J4HDTRxW6_j-RfxwAVl9RT9uf9-d9SzQchMO5DHCbJs7fLauLIuwA5XueDuk7p5P7k9PfV");
+								content.put("to", mobileId);
+								JSONObject notification = new JSONObject();
+								notification.put("body", obj.get("message"));
+								notification.put("title", "New Alarm");
+								notification.put("content_available", true);
+								notification.put("priority", "high");
+								content.put("notification", notification);
+								
+								JSONObject data = new JSONObject();
+								data.put("id", 1253);
+								content.put("data", data);
+								
+								Map<String, String> headers = new HashMap<>();
+								headers.put("Content-Type","application/json");
+								String url = "https://fcm.googleapis.com/fcm/send";
+								
+								AwsUtil.doHttpPost(url, headers, null, content.toJSONString());
+								System.out.println("Push notification sent");
+								System.out.println(content.toJSONString());
+							}
+						}
+					}
 				}
 			}
 			catch(Exception e) {
 				e.printStackTrace();
 			}
+		}
+		
+		private Map<String, String> getMobileInstanceIDs(List<String> emails) throws Exception {
+			Map<String, String> emailToMobileId = new HashMap<>();
+			List<FacilioField> fields = new ArrayList<>();
+			
+			FacilioField email = new FacilioField();
+			email.setName("email");
+			email.setColumnName("EMAIL");
+			email.setDataType(FieldType.STRING);
+			email.setModule(ModuleFactory.getUserModule());
+			fields.add(email);
+			
+			FacilioField mobileInstanceId = new FacilioField();
+			mobileInstanceId.setName("mobileInstanceId");
+			mobileInstanceId.setColumnName("MOBILE_INSTANCE_ID");
+			mobileInstanceId.setDataType(FieldType.STRING);
+			mobileInstanceId.setModule(AccountConstants.getUserMobileSettingModule());
+			fields.add(mobileInstanceId);
+			
+			Condition condition = CriteriaAPI.getCondition("EMAIL", "email", StringUtils.join(emails, ","), StringOperators.IS);
+			
+			GenericSelectRecordBuilder selectBuilder = new GenericSelectRecordBuilder()
+															.select(fields)
+															.table("Users")
+															.innerJoin("User_Mobile_Setting")
+															.on("Users.USERID = User_Mobile_Setting.USERID")
+															.andCondition(condition)
+															.orderBy("USER_MOBILE_SETTING_ID")
+															;
+			
+			List<Map<String, Object>> props = selectBuilder.get();
+			if(props != null && !props.isEmpty()) {
+				for(Map<String, Object> prop : props) {
+					emailToMobileId.put((String) prop.get("email"), (String) prop.get("mobileInstanceId"));
+				}
+			}
+			return emailToMobileId;
+		}
+		
+		private List<String> getEmailAddresses() throws Exception {
+			FacilioModule module = ModuleFactory.getEMailTemplatesModule();
+			FacilioField field = new FacilioField();
+			field.setName("toAddr");
+			field.setColumnName("TO_ADDR");
+			field.setDataType(FieldType.STRING);
+			field.setModule(module);
+			
+			GenericSelectRecordBuilder selectBuilder = new GenericSelectRecordBuilder()
+															.select(Collections.singletonList(field))
+															.table(module.getTableName())
+															.innerJoin("Templates")
+															.on("EMail_Templates.ID = Templates.ID")
+															.andCustomWhere("ORGID = ?", AccountUtil.getCurrentOrg().getId());
+			
+			List<Map<String, Object>> props = selectBuilder.get();
+			if(props != null && !props.isEmpty()) {
+				List<String> addresses = new ArrayList<>();
+				for(Map<String, Object> emailProp : props) {
+					String emails = (String) emailProp.get("toAddr");
+					String[] emailList = emails.trim().split("\\s*,\\s*");
+					if(emailList != null && emailList.length != 0) {
+						for(String email : emailList) {
+							addresses.add(email);
+						}
+					}
+				}
+				return addresses;
+			}
+			return null;
 		}
 	};
 	
