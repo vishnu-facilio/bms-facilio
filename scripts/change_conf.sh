4100febbf4040f400d013aa3cc2863d05192d271
Swami <swami@facilio.com>
2018-02-01 13:01:52 +0530

    Kinesis enabled in ScheduleServer

--- scripts/change_conf.sh
+++ scripts/change_conf.sh
@@ -17,7 +17,7 @@ if [ "$DEPLOYMENT_GROUP_NAME" = "pre_production" ]; then
     sed -i -e 's/localhost:8080/facilio.in/g' $CONF_DIR/awsprops.properties
     sed -i'' "s%environment=.*%environment=stage%g" $CONF_DIR/awsprops.properties
     sed -i'' "s%schedulerServer=.*%schedulerServer=true%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%enableKinesis=.*%enableKinesis=true%g" $CONF_DIR/awsprops.properties
+    sed -i'' "s%enable.kinesis=.*%enable.kinesis=true%g" $CONF_DIR/awsprops.properties
     sed -i'' "s%s3.bucket.name=.*%s3.bucket.name=facilio-in-data%g" $CONF_DIR/awsprops.properties
     sed -i'' "s%enableeventjob=.*%enableeventjob=false%g" $CONF_DIR/awsprops.properties
     sed -i'' "s%servername=.*%servername=pre-production-1766931799.us-west-2.elb.amazonaws.com%g" $CONF_DIR/awsprops.properties
@@ -28,7 +28,7 @@ fi
 if [ "$DEPLOYMENT_GROUP_NAME" = "production_deployment" ]; then
     cp $META_INF_DIR/context-production.xml $META_INF_DIR/context.xml
     sed -i'' "s%schedulerServer=.*%schedulerServer=false%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%enableKinesis=.*%enableKinesis=false%g" $CONF_DIR/awsprops.properties
+    sed -i'' "s%enable.kinesis=.*%enable.kinesis=false%g" $CONF_DIR/awsprops.properties
     sed -i -e 's/localhost:9090/app.facilio.ae/g' $CONF_DIR/awsprops.properties
     sed -i -e 's/localhost:8080/facilio.ae/g' $CONF_DIR/awsprops.properties
     sed -i'' "s%environment=.*%environment=production%g" $CONF_DIR/awsprops.properties
@@ -41,7 +41,7 @@ if [ "$DEPLOYMENT_GROUP_NAME" = "production-scheduler" ]; then
     sed -i -e 's/localhost:9090/app.facilio.ae/g' $CONF_DIR/awsprops.properties
     sed -i -e 's/localhost:8080/facilio.ae/g' $CONF_DIR/awsprops.properties
     sed -i'' "s%schedulerServer=.*%schedulerServer=true%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%enableKinesis=.*%enableKinesis=true%g" $CONF_DIR/awsprops.properties
+    sed -i'' "s%enable.kinesis=.*%enable.kinesis=true%g" $CONF_DIR/awsprops.properties
     sed -i'' "s%environment=.*%environment=production%g" $CONF_DIR/awsprops.properties
     sed -i'' "s%servername=.*%servername=stage-1555618498.us-west-2.elb.amazonaws.com%g" $CONF_DIR/awsprops.properties
     sed -i'' "s%websocket.url=.*%websocket.url=wss://api.facilio.ae/websocket%g" $CONF_DIR/awsprops.properties
