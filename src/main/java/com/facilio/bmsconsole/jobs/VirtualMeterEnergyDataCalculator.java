daa79fa3fecdac34a249dd6c70d0c9b1b8265b10
Manthosh <manthosh@facilio.com>
2018-02-05 13:08:13 +0530

    Doing VM calculation with a minimum of One minute delay

--- src/main/java/com/facilio/bmsconsole/jobs/VirtualMeterEnergyDataCalculator.java
+++ src/main/java/com/facilio/bmsconsole/jobs/VirtualMeterEnergyDataCalculator.java
@@ -10,6 +10,8 @@ import com.facilio.tasker.job.JobContext;
 
 public class VirtualMeterEnergyDataCalculator extends FacilioJob {
 
+	private static final int CALCULATION_DELAY = 60*1000; //One minute delay
+	
 	@Override
 	public void execute(JobContext jc) {
 		// TODO Auto-generated method stub
@@ -17,7 +19,7 @@ public class VirtualMeterEnergyDataCalculator extends FacilioJob {
 			List<EnergyMeterContext> virtualMeters = DeviceAPI.getAllVirtualMeters();
 			if(virtualMeters != null && !virtualMeters.isEmpty()) {
 				
-				long endTime = System.currentTimeMillis();
+				long endTime = System.currentTimeMillis() - CALCULATION_DELAY;
 				long startTime = endTime - (jc.getPeriod()*1000);
 				
 				ReportsUtil.insertVirtualMeterReadings(virtualMeters, startTime,endTime);
