f58d95e1540df2457cc5b6fbcb9984bd50c416f3
bhuvaneswaran.g <bhuvaneswaran.g@facilio.com>
2018-02-13 11:06:41 +0530

    Changed file name in for pdf file

--- scripts/kill_server.sh
+++ scripts/kill_server.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo "stopping the server..."
 
 export APP_HOME="/home/ubuntu/tomcat"
@@ -6,6 +7,9 @@ export CONF_DIR="$APP_HOME/webapps/ROOT/WEB-INF/classes/conf"
 cd $APP_HOME
 
 sh bin/shutdown.sh
+servername=`hostname`
+servername=`grep "api.servername" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
+sudo aws s3 mv --recursive $APP_HOME/logs/ s3://facilio-server-logs/$servername/
 
 rm -rf webapps/ROOT
 rm -rf webapps/ROOT.war
