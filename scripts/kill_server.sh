82a2bc3213223fc476f73ab6ed2400d92596085b
bhuvaneswaran.g <bhuvaneswaran.g@facilio.com>
2018-02-14 15:11:21 +0530

    Move logs files to S3 during restart.

--- scripts/kill_server.sh
+++ scripts/kill_server.sh
@@ -7,9 +7,7 @@ export CONF_DIR="$APP_HOME/webapps/ROOT/WEB-INF/classes/conf"
 cd $APP_HOME
 
 sh bin/shutdown.sh
-servername=`hostname`
-servername=`grep "api.servername" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
-sudo aws s3 mv --recursive $APP_HOME/logs/ s3://facilio-server-logs/$servername/
+sh /home/ubuntu/move_logs.sh
 
 rm -rf webapps/ROOT
 rm -rf webapps/ROOT.war
