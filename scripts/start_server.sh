886a54ed17dcd943ec5a605e8c494de066b8ac26
bhuvaneswaran.g <bhuvaneswaran.g@facilio.com>
2018-03-06 14:13:17 +0530

    Copy aspose jar to web-inf classes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,12 @@
 echo "starting the server..."
-
-export APP_HOME="/home/ubuntu/tomcat"
+export UBUNTU_HOME="/home/ubuntu"
+export APP_HOME="$UBUNTU_HOME/tomcat"
 export CONF_DIR="$APP_HOME/webapps/ROOT/WEB-INF/classes/conf"
 
 cd /home/ubuntu
 
 sudo killall -9 java
+cp $UBUNTU_HOME/*.jar $CONF_DIR/../
 sudo sh $APP_HOME/bin/catalina.sh start
 sudo chmod 644 $APP_HOME/logs/*
 
