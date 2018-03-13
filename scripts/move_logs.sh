71c3b3b71dd12ec17c8babb3e0d5ead14a530d57
bhuvaneswaran.g <bhuvaneswaran.g@facilio.com>
2018-03-13 20:09:14 +0530

    Moving logs to server ip inside logs bucket

--- scripts/move_logs.sh
+++ scripts/move_logs.sh
@@ -3,8 +3,10 @@
 export APP_HOME="/home/ubuntu/tomcat"
 export CONF_DIR="$APP_HOME/webapps/ROOT/WEB-INF/classes/conf"
 
-servername=`hostname`
+ipAddress=`hostname -I`
+logsBucket=facilio-server-logs
 servername=`grep "api.servername" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
+logsBucket=`grep "logs.bucket" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 
 today=`date +%F`
 for file in `ls $APP_HOME/logs`
@@ -12,7 +14,7 @@ do
     if [ "$file" != "catalina.out" -a "$file" != "serverlog" ]; then
         dateString=`echo $file | cut -d '.' -f 2`
         if [ $dateString != $today ]; then
-            sudo aws s3 mv $APP_HOME/logs/$file s3://facilio-server-logs/$servername/
+            sudo aws s3 mv $APP_HOME/logs/$file s3://$logsBucket/$servername/$ipAddress/
         fi
     fi
 done
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,7 +6,6 @@ export CONF_DIR="$APP_HOME/webapps/ROOT/WEB-INF/classes/conf"
 cd /home/ubuntu
 
 sudo killall -9 java
-cp $UBUNTU_HOME/*.jar $CONF_DIR/../
 sudo sh $APP_HOME/bin/catalina.sh start
 sudo chmod 644 $APP_HOME/logs/*
 
