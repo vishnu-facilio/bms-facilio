fa695065377142864ebd16279f511c814098bc7f
bhuvaneswaran.g <bhuvaneswaran.g@facilio.com>
2018-03-13 20:17:29 +0530

    logs bucket conf externalized to awsprops

--- scripts/kill_server.sh
+++ scripts/kill_server.sh
@@ -6,14 +6,14 @@ export CONF_DIR="$APP_HOME/webapps/ROOT/WEB-INF/classes/conf"
 
 ipAddress=`hostname -I`
 servername=`grep "api.servername" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
-
+logsBucket=`grep "logs.bucket" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 today=`date +%F-%H-%M-%S`
 
 cd $APP_HOME
 
 sh bin/shutdown.sh
 sh /home/ubuntu/move_logs.sh
-sudo aws s3 mv $APP_HOME/logs/catalina.out s3://facilio-server-logs/$servername/$ipAddress/catalinaout.$today.log
+sudo aws s3 mv $APP_HOME/logs/catalina.out s3://$logsBucket/$servername/$ipAddress/catalinaout.$today.log
 
 rm -rf webapps/ROOT
 rm -rf webapps/ROOT.war
