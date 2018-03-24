dc233c6d8d561e76633795073f83f1f956e78028
Magesh V <magesh@facilio.com>
2018-03-24 14:38:56 +0530

    tabular data check

--- src/main/java/com/facilio/bmsconsole/commands/UpdateReportColumnSequence.java
+++ src/main/java/com/facilio/bmsconsole/commands/UpdateReportColumnSequence.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
-import org.apache.commons.lang3.StringUtils;
 
+import com.facilio.bmsconsole.context.ReportColumnContext;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.transaction.FacilioConnectionPool;
 
@@ -17,28 +17,18 @@ public class UpdateReportColumnSequence implements Command {
 	@Override
 	public boolean execute(Context context) throws Exception {
 		// TODO Auto-generated method stub
-		List<Long> ids = (List<Long>) context.get(FacilioConstants.ContextNames.REPORT_COLUMN_LIST);
-		if (ids != null && !ids.isEmpty()) {
-			StringBuilder sql = new StringBuilder();
-			sql.append("UPDATE Report_Columns SET SEQUENCE = CASE ");
-			for (int i = 0; i < ids.size(); i++) {
-				sql.append("WHEN ID = ")
-					.append(ids.get(i))
-					.append(" THEN ")
-					.append(i+1)
-					.append(" ");
-			}
-			sql.append("ELSE SEQUENCE END WHERE ID IN (")
-				.append(StringUtils.join(ids,","))
-				.append(")");
-			
-			try (Connection conn = FacilioConnectionPool.INSTANCE.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql.toString());) {
-				int rowsUpdated = pstmt.executeUpdate();
-				context.put(FacilioConstants.ContextNames.RESULT, "success");
-			}
-			catch(SQLException e) {
-				e.printStackTrace();
-				throw e;
+		List<ReportColumnContext> columns = (List<ReportColumnContext>) context.get(FacilioConstants.ContextNames.REPORT_COLUMN_LIST);
+		if (columns != null && !columns.isEmpty()) {
+			for (int i = 0; i < columns.size(); i++) {
+				String sql = "UPDATE Report_Columns SET WIDTH = "+columns.get(i).getWidth()+", SEQUENCE = "+(i+1)+" where ID="+columns.get(i).getId();
+				
+				try (Connection conn = FacilioConnectionPool.INSTANCE.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql.toString());) {
+					int rowsUpdated = pstmt.executeUpdate();
+				}
+				catch(SQLException e) {
+					e.printStackTrace();
+					throw e;
+				}
 			}
 		}
 		else {
