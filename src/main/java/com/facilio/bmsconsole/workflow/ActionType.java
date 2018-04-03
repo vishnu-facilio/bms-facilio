73a89d02112703b171f276cf76d7c1f9e6b9faf5
Manthosh <manthosh@facilio.com>
2018-04-03 12:57:46 +0530

    Considering duration as seconds in SLA templates

--- src/main/java/com/facilio/bmsconsole/workflow/ActionType.java
+++ src/main/java/com/facilio/bmsconsole/workflow/ActionType.java
@@ -669,7 +669,7 @@ public enum ActionType {
 				long priorityId = Long.parseLong((String)slaPolicy.get("priority"));
 				if(priorityId == workorderpriority)
 				{
-					duration = Long.parseLong((String)slaPolicy.get("duration"));
+					duration = Long.parseLong((String)slaPolicy.get("duration")) * 1000;
 				}
 				
 			}
