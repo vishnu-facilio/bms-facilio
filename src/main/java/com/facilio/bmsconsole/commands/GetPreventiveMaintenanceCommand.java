35604522d2ce285d9a6dfb40bdf6e32f1199f207
benitta <benitta@facilio.com>
2018-06-06 18:52:37 +0530

    Alarm count list and task update list

--- src/main/java/com/facilio/bmsconsole/commands/GetPreventiveMaintenanceCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetPreventiveMaintenanceCommand.java
@@ -20,7 +20,8 @@ public class GetPreventiveMaintenanceCommand implements Command {
 		// TODO Auto-generated method stub
 		
 		Criteria criteria = new Criteria();
-		
+		String count = (String) context.get(FacilioConstants.ContextNames.PREVENTIVE_MAINTENANCE_COUNT);
+
 		FacilioView view = (FacilioView) context.get(FacilioConstants.ContextNames.CUSTOM_VIEW);
 		JSONObject filters = (JSONObject) context.get(FacilioConstants.ContextNames.FILTERS);
 		JSONObject serachQuery = (JSONObject) context.get(FacilioConstants.ContextNames.SEARCH);
