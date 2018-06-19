562cd1b3f0b77e475a71133d4b04a547ae0d13f2
Bhuvaneswaran.G <bhuvaneswaran.g@facilio.com>
2018-06-19 14:12:22 +0000

    Added timestamp to gc log

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -2,5 +2,5 @@ JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=300
 JMX_EXPORTER_OPTS="-javaagent:/home/ubuntu/facmon/prometheus/jmx_prometheus_javaagent-0.3.1.jar=9200:/home/ubuntu/facmon/prometheus/jmx-tomcat.yml"
 JVM_OPTS="-Xms1G -Xmx2G -XX:+UseG1GC -server -XX:+UseStringDeduplication -XX:ParallelGCThreads=2 -XX:MaxGCPauseMillis=30"
 DATE=`date +%Y-%m-%d`
-JAVA_GC_OPTS="-XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=10M -Xloggc:/home/ubuntu/tomcat/logs/gc.$DATE.log -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
+JAVA_GC_OPTS="-XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=10M -Xloggc:/home/ubuntu/tomcat/logs/gc.%t.log -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
 JAVA_OPTS="$JMX_OPTS $JMX_EXPORTER_OPTS $JVM_OPTS $JAVA_GC_OPTS"
