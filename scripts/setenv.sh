f7428d2c44245057a8c33e1404c5358c5cad59c1
Bhuvaneswaran.G <bhuvaneswaran.g@facilio.com>
2018-06-19 10:12:39 +0000

    Disabled initial JVM heap

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,5 +1,5 @@
 JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=3000 -Dcom.sun.management.jmxremote.rmi.port=3000 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 JMX_EXPORTER_OPTS="-javaagent:/home/ubuntu/facmon/prometheus/jmx_prometheus_javaagent-0.3.1.jar=9200:/home/ubuntu/facmon/prometheus/jmx-tomcat.yml"
-JVM_OPTS="-Xms=1G -Xmx=2G -XX:+UseG1GC -server -XX:+UseStringDeduplication -XX:ParallelGCThreads=2 -XX:MaxGCPauseMillis=30"
+JVM_OPTS="-XX:+UseG1GC -server -XX:+UseStringDeduplication -XX:ParallelGCThreads=2 -XX:MaxGCPauseMillis=30"
 JAVA_GC_OPTS="-XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=10M -Xloggc:/home/ubuntu/tomcat/logs/gc.log -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
 JAVA_OPTS="$JMX_OPTS $JMX_EXPORTER_OPTS $JVM_OPTS $JAVA_GC_OPTS"
