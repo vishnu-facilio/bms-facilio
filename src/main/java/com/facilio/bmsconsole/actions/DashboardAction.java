c6ad4106c1f9708a77e1b74aa1aa75ca4ab1abc7
Krishna <krishnan.e@facilio.com>
2018-06-21 08:06:55 +0530

    report fix

--- src/main/java/com/facilio/bmsconsole/actions/DashboardAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/DashboardAction.java
@@ -3055,6 +3055,15 @@ public class DashboardAction extends ActionSupport {
 				if ("service".equalsIgnoreCase(report.getReportSpaceFilterContext().getGroupBy())) {
 					
 					List<EnergyMeterContext> meters = DeviceAPI.getRootServiceMeters(report.getReportSpaceFilterContext().getBuildingId()+"");
+					if(report.getId() == 2957l || report.getId() == 2795l) {
+						List<EnergyMeterContext> omitmeters = new ArrayList<>();
+						for(EnergyMeterContext meter :meters) {
+							if(meter.getPurpose().getId() == 372l || meter.getPurpose().getId() == 373l) {
+								omitmeters.add(meter);
+							}
+						}
+						meters.removeAll(omitmeters);
+					}
 					if (meters != null && meters.size() > 0) {
 						List<Long> meterIds = new ArrayList<Long>();
 						for (EnergyMeterContext meter : meters) {
