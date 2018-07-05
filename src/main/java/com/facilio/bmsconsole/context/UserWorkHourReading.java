c5117e0f9c30c98ac8a47fdebb26e346c3a88066
Manthosh <manthosh@facilio.com>
2018-07-05 18:41:33 +0530

    Compilation fix

---
+++ src/main/java/com/facilio/bmsconsole/context/UserWorkHourReading.java
@@ -0,0 +1,22 @@
+package com.facilio.bmsconsole.context;
+
+public enum UserWorkHourReading {
+	START,
+	PAUSE,
+	RESUME,
+	SHIFT_PAUSE,
+	SHIFT_RESUME,
+	CLOSE
+	;
+	
+	public int getValue() {
+		return ordinal() + 1;
+	}
+	
+	public static UserWorkHourReading valueOf (int value) {
+		if (value > 0 && value <= values().length) {
+			return values()[value - 1];
+		}
+		return null;
+	}
+}
