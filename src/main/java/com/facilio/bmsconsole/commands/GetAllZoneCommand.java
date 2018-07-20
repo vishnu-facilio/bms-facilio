437f0b90925a77678a185d302c6090e0b454086c
Benitta <benitta@facilio.com>
2018-07-20 19:25:30 +0530

    Zone Location baseSpacesContext

--- src/main/java/com/facilio/bmsconsole/commands/GetAllZoneCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetAllZoneCommand.java
@@ -1,13 +1,18 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 
+import com.facilio.bmsconsole.context.BaseSpaceContext;
 import com.facilio.bmsconsole.context.ZoneContext;
 import com.facilio.bmsconsole.modules.FacilioField;
 import com.facilio.bmsconsole.modules.SelectRecordsBuilder;
+import com.facilio.bmsconsole.util.SpaceAPI;
 import com.facilio.constants.FacilioConstants;
 
 public class GetAllZoneCommand implements Command{
@@ -26,8 +31,34 @@ public class GetAllZoneCommand implements Command{
 				.beanClass(ZoneContext.class)
 				.select(fields)
 				.orderBy("ID");
-
+		List<Long> spaceId = new ArrayList<Long>();
 		List<ZoneContext> zones = builder.get();
+		for (ZoneContext zoeCont : zones) {
+			if (zoeCont.getFloorId() > 0) {
+				spaceId.add(zoeCont.getFloorId());
+			}
+			else if (zoeCont.getBuildingId()> 0) {
+				spaceId.add(zoeCont.getBuildingId());
+
+			}
+			else if (zoeCont.getSiteId() > 0) {
+				spaceId.add(zoeCont.getSiteId());
+			}
+		}
+		Map<Long, BaseSpaceContext> mapSpaces = SpaceAPI.getBaseSpaceMap(spaceId);
+		for (ZoneContext zoeCont : zones) {
+			if (zoeCont.getFloorId() > 0) {
+				zoeCont.setBaseSpaceContext(mapSpaces.get(zoeCont.getFloorId()));
+			}
+			else if (zoeCont.getBuildingId()> 0) {
+				zoeCont.setBaseSpaceContext(mapSpaces.get(zoeCont.getBuildingId()));
+
+			}
+			else if (zoeCont.getSiteId() > 0) {
+				zoeCont.setBaseSpaceContext(mapSpaces.get(zoeCont.getSiteId()));
+			}
+		}
+		
 		context.put(FacilioConstants.ContextNames.ZONE_LIST, zones);
 		
 		return false;
--- src/main/java/com/facilio/bmsconsole/context/ZoneContext.java
+++ src/main/java/com/facilio/bmsconsole/context/ZoneContext.java
@@ -11,4 +11,32 @@ public class ZoneContext extends BaseSpaceContext {
 	public void setBasespaces(List<BaseSpaceContext> basespaces) {
 		this.basespaces = basespaces;
 	}
+	private BaseSpaceContext baseSpaceContext;
+	public BaseSpaceContext getBaseSpaceContext() {
+		return baseSpaceContext;
+	}
+	public void setBaseSpaceContext(BaseSpaceContext baseSpaceContext) {
+		this.baseSpaceContext = baseSpaceContext;
+	}
+	private FloorContext floor ;
+	public FloorContext getFloor() {
+		return floor;
+	}
+	public void setFloor(FloorContext floor) {
+		this.floor = floor;
+	}
+	private SiteContext site;
+	public SiteContext getSite() {
+		return site;
+	}
+	public void setSite(SiteContext site) {
+		this.site = site;
+	}
+	private BuildingContext building;
+	public BuildingContext getBuilding() {
+		return building;
+	}
+	public void setBuilding(BuildingContext building) {
+		this.building = building;
+	}
 }
