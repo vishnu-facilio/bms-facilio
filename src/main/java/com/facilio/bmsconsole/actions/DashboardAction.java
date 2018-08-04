fef6b42c1bf861e87746158127849a64c50149ce
Krishna <krishnan.e@facilio.com>
2018-08-04 14:41:21 +0530

    fix

--- src/main/java/com/facilio/bmsconsole/actions/DashboardAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/DashboardAction.java
@@ -3690,6 +3690,185 @@ public class DashboardAction extends ActionSupport {
 				
 				return ticketData;
 			}
+			
+			if(report.getId() == 4068l) { // 2NB
+				
+				ticketData = new JSONArray();
+				ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+				
+				List<TicketCategoryContext> categories = TicketAPI.getCategories(AccountUtil.getCurrentOrg().getOrgId());
+				
+				if(report.getReportSpaceFilterContext() != null && report.getReportSpaceFilterContext().getBuildingId() != null) {
+					
+					buildingId = report.getReportSpaceFilterContext().getBuildingId();
+					
+				}
+				
+				DecimalFormat df = new DecimalFormat(".##");
+				
+				for(TicketCategoryContext category:categories) {
+					
+					if(!(category.getName().equals("Auditing"))) {
+						continue;
+					}
+					List<WorkOrderContext> workorders = WorkOrderAPI.getWorkOrders(category.getId());
+					
+					if(workorders.isEmpty()) {
+						continue;
+					}
+					for(WorkOrderContext workorder:workorders) {
+						
+						LOGGER.log(Level.SEVERE, "buildingId --- "+buildingId);
+						if(workorder.getResource() != null) {
+							LOGGER.log(Level.SEVERE, "workorder.getResource().getId() --- "+workorder.getResource().getId());
+						}
+						
+						if(buildingId != null && workorder.getResource() != null && workorder.getResource().getId() != buildingId) {
+							continue;
+						}
+						
+						if( !(workorder.getSubject().contains("Daily"))) {
+							continue;
+						}
+						LOGGER.log(Level.SEVERE, "dateFilter --- "+dateFilter);
+						LOGGER.log(Level.SEVERE, "workorder.getResource().getId() --- "+workorder.getCreatedTime());
+						
+						if(dateFilter != null && !((Long)dateFilter.get(0) < workorder.getCreatedTime() && workorder.getCreatedTime() < (Long)dateFilter.get(1))) {
+							continue;
+						}
+						
+						int criticalCount = 0, tatCount =0, pdCount = 0, fasCountdaily = 0;
+						Map<String,Double> daily = new HashMap<>();
+						
+						Command chain = FacilioChainFactory.getGetTasksOfTicketCommand();
+						FacilioContext context = new FacilioContext();
+						
+						context.put(FacilioConstants.ContextNames.ID, workorder.getId());
+						context.put(FacilioConstants.ContextNames.MODULE_NAME, FacilioConstants.ContextNames.TASK);
+						context.put(FacilioConstants.ContextNames.MODULE_DATA_TABLE_NAME,"Tasks");
+						context.put(FacilioConstants.ContextNames.EXISTING_FIELD_LIST, modBean.getAllFields(FacilioConstants.ContextNames.TASK));
+						chain.execute(context);
+						
+						Map<Long, List<TaskContext>> taksSectionMap = (Map<Long, List<TaskContext>>) context.get(FacilioConstants.ContextNames.TASK_MAP);
+						
+						for(Long sectionid :taksSectionMap.keySet()) {
+							
+							TaskSectionContext section = TicketAPI.getTaskSection(sectionid);
+							
+							for(TaskContext task : taksSectionMap.get(sectionid)) {
+								
+								if(task.getInputValue() != null) {
+									
+									if(task.getInputValue().equals("Met")) {
+										value = 5.0;
+									}
+									else if(task.getInputValue().equals("Not Met")) {
+										value = 0d;
+									}
+									
+									if(workorder.getSubject().contains("Daily")) {
+										
+										if(section.getName().contains("Critical Service")) {
+											
+											criticalCount++;
+											double value1 = 0;
+											if(daily.containsKey("Critical Service")) {
+												value1 = daily.get("Critical Service");
+											}
+											value1 = value1 + value;
+											daily.put("Critical Service", value1);
+										}
+										else if(section.getName().contains("TAT")) {
+											
+											tatCount++;
+											double value1 = 0;
+											if(daily.containsKey("TAT")) {
+												value1 = daily.get("TAT");
+											}
+											value1 = value1 + value;
+											daily.put("TAT", value1);
+										}
+										else if(section.getName().contains("PD&PSI")) {
+											
+											pdCount++;
+											double value1 = 0;
+											if(daily.containsKey("PD&PSI")) {
+												value1 = daily.get("PD&PSI");
+											}
+											value1 = value1 + value;
+											daily.put("PD&PSI", value1);
+										}
+										else if(section.getName().contains("Financials & Ops")) {
+											
+											fasCountdaily++;
+											double value1 = 0;
+											if(daily.containsKey("Financials & Ops")) {
+												value1 = daily.get("Financials & Ops");
+											}
+											value1 = value1 + value;
+											daily.put("Financials & Ops", value1);
+										}
+									}
+								}
+							}
+						}
+						
+						JSONArray resList = new JSONArray();
+						for(String key:daily.keySet()) {
+							
+							JSONObject json = new JSONObject();
+							
+							if(daily.containsKey("Critical Service")) {
+								value = daily.get("Critical Service");
+								if(value > 0) {
+									value = value / criticalCount;
+								}
+								json.put("label", "Critical Service");
+								json.put("value", df.format(value * (40.0d/100.0d)));
+							}
+							
+							if(daily.containsKey("TAT")) {
+								value = daily.get("TAT");
+								if(value > 0) {
+									value = value / tatCount;
+								}
+								
+								json.put("label", "TAT");
+								json.put("value", df.format(value * (25.0d/100.0d)));
+								
+							}
+							if(daily.containsKey("PD&PSI")) {
+								value = daily.get("PD&PSI");
+								if(value > 0) {
+									value = value / pdCount;
+								}
+								json.put("label", "PD&PSI");
+								json.put("value", df.format(value * (10.0d/100.0d)));
+								
+							}
+							
+							if(daily.containsKey("Financials & Ops")) {
+								value = daily.get("Financials & Ops");
+								if(value > 0) {
+									value = value / fasCountdaily;
+								}
+								json.put("label", "Financials & Ops");
+								json.put("value", df.format(value * (25.0d/100.0d)));
+							}
+							
+							resList.add(json);
+						}
+						
+						JSONObject finalRes1 = new JSONObject();
+						
+						finalRes1.put("label", workorder.getCreatedTime());
+						finalRes1.put("value", resList);
+						
+						ticketData.add(finalRes1);
+					}
+				}
+				return ticketData;
+			}
 		}
 		
 		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
