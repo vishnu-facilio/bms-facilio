56103599d151cb370123daeb28ec17d361ce7295
radhakrishnan <radhakrishnan@facilio.com>
2018-08-14 13:10:04 +0530

    removed blank

--- scripts/change_conf.sh
+++ scripts/change_conf.sh
@@ -33,7 +33,7 @@ if [ "$DEPLOYMENT_GROUP_NAME" = "pre_production" ]; then
     sed -i'' "s%log4j.appender.graylog2.graylogHost=.*%log4j.appender.graylog2.graylogHost=172.31.35.38%g" $CLASSES_DIR/log4j.properties
     sed -i'' "s%websocket.url=.*%websocket.url=wss://api.facilio.in/websocket%g" $CONF_DIR/awsprops.properties
     sed -i'' "s%cors.allowed.origins=.*%cors.allowed.origins=http://localhost:8080,http://localhost:9090,https://facilio.in%g" $CONF_DIR/awsprops.properties
-    sed -i -e 's/ localhost:7444/172.31.10.148:7444/g' $CONF_DIR/awsprops.properties
+    sed -i -e 's/localhost:7444/172.31.10.148:7444/g' $CONF_DIR/awsprops.properties
     sed -i -e 's/tmp/home\/ubuntu\/analytics\/temp/g' $CONF_DIR/awsprops.properties
     echo "copied $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
 fi
