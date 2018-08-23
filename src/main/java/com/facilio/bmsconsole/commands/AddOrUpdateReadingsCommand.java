f6395bb5d29cac4ecc403f50510bfbbb1694656c
Manthosh <manthosh@facilio.com>
2018-08-23 15:47:55 +0530

    Consuming exception from Workflow and formula execution for readings as they are not errors per se

--- src/main/java/com/facilio/bmsconsole/commands/AddOrUpdateReadingsCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/AddOrUpdateReadingsCommand.java
@@ -1,15 +1,24 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.chain.Chain;
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorCheckpointer;
 import com.amazonaws.services.kinesis.model.Record;
+import com.facilio.bmsconsole.commands.util.CommonCommandUtil;
+import com.facilio.bmsconsole.context.ReadingContext;
 import com.facilio.constants.FacilioConstants;
 
 public class AddOrUpdateReadingsCommand implements Command {
 
+	private static final Logger LOGGER = LogManager.getLogger(AddOrUpdateReadingsCommand.class.getName());
+	
 	@Override
 	public boolean execute(Context context) throws Exception {
 		// TODO Auto-generated method stub
@@ -24,11 +33,25 @@ public class AddOrUpdateReadingsCommand implements Command {
 			checkPointer.checkpoint(record);
 		}
 		
-		Chain executeWorkflowChain = FacilioChainFactory.executeWorkflowsForReadingChain();
-		executeWorkflowChain.execute(context);
+		try {
+			Chain executeWorkflowChain = FacilioChainFactory.executeWorkflowsForReadingChain();
+			executeWorkflowChain.execute(context);
+		}
+		catch (Exception e) {
+			Map<String, List<ReadingContext>> readingMap = (Map<String, List<ReadingContext>>) context.get(FacilioConstants.ContextNames.RECORD_MAP);
+			LOGGER.error("Error occurred during workflow execution of readings. \n"+readingMap, e);
+			CommonCommandUtil.emailException("AddOrUpdateReading", "Error occurred during workflow execution of readings.", String.valueOf(readingMap));
+		}
 		
-		Chain formulaChain = FacilioChainFactory.calculateFormulaChain();
-		formulaChain.execute(context);
+		try {
+			Chain formulaChain = FacilioChainFactory.calculateFormulaChain();
+			formulaChain.execute(context);
+		}
+		catch (Exception e) {
+			Map<String, List<ReadingContext>> readingMap = (Map<String, List<ReadingContext>>) context.get(FacilioConstants.ContextNames.RECORD_MAP);
+			LOGGER.error("Error occurred during formula calculation of readings. \n"+readingMap, e);
+			CommonCommandUtil.emailException("AddOrUpdateReading", "Error occurred during formula calculation of readings.", String.valueOf(readingMap));
+		}
 		
 		return false;
 	}
