e2713d24f0cfde7166fe3a690b74c6dbb41ab763
Manthosh <manthosh@facilio.com>
2018-08-24 14:14:40 +0530

    Printing query params for slow queries

--- src/main/java/com/facilio/sql/GenericSelectRecordBuilder.java
+++ src/main/java/com/facilio/sql/GenericSelectRecordBuilder.java
@@ -215,6 +215,7 @@ public class GenericSelectRecordBuilder implements SelectBuilderIfc<Map<String, 
 			
 			if (queryTime > QUERY_TIME_THRESHOLD) {
 				LOGGER.info("SQL : "+sql);
+				LOGGER.info("Values : "+whereValues);
 				LOGGER.info("Time taken to execute query in GenericSelectBuilder : "+queryTime);
 			}
 			
