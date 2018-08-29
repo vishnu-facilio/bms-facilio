c0fb936031dc9f4dea0266a2494e461cedf128aa
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2018-08-29 13:01:56 +0530

    Added event api command

--- src/main/java/com/facilio/bmsconsole/jobs/DeviceMonitorJob.java
+++ src/main/java/com/facilio/bmsconsole/jobs/DeviceMonitorJob.java
@@ -1,11 +1,15 @@
 package com.facilio.bmsconsole.jobs;
 
+import com.facilio.bmsconsole.commands.FacilioContext;
 import com.facilio.bmsconsole.criteria.Condition;
+import com.facilio.bmsconsole.criteria.CriteriaAPI;
 import com.facilio.bmsconsole.criteria.NumberOperators;
 import com.facilio.bmsconsole.modules.*;
+import com.facilio.events.constants.EventConstants;
 import com.facilio.sql.GenericSelectRecordBuilder;
 import com.facilio.tasker.job.FacilioJob;
 import com.facilio.tasker.job.JobContext;
+import org.apache.commons.chain.Chain;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
@@ -19,6 +23,7 @@ public class DeviceMonitorJob extends FacilioJob {
     private static final FacilioModule DEVICE_DETAILS_MODULE = ModuleFactory.getDeviceDetailsModule();
     private static final List<FacilioField> FIELDS = FieldFactory.getDeviceDetailsFields();
     private static final FacilioField ORG_ID_FIELD = FieldFactory.getOrgIdField(DEVICE_DETAILS_MODULE);
+    private static final Condition IN_USE = CriteriaAPI.getCondition("IN_USE", "IN_USE", String.valueOf(1), NumberOperators.EQUALS);
     private static final long DEFAULT_TIMEOUT = 30*60*100L;
 
     public void execute(JobContext jc) {
@@ -40,7 +45,7 @@ public class DeviceMonitorJob extends FacilioJob {
             lastUpdatedTimeCondition.setOperator(NumberOperators.LESS_THAN_EQUAL);
             lastUpdatedTimeCondition.setValue(String.valueOf((System.currentTimeMillis()- DEFAULT_TIMEOUT)));
 
-            GenericSelectRecordBuilder builder = new GenericSelectRecordBuilder().table(DEVICE_DETAILS_MODULE.getTableName()).andCondition(orgIdCondition).andCondition(lastUpdatedTimeCondition).select(FIELDS);
+            GenericSelectRecordBuilder builder = new GenericSelectRecordBuilder().table(DEVICE_DETAILS_MODULE.getTableName()).andCondition(orgIdCondition).andCondition(lastUpdatedTimeCondition).andCondition(IN_USE).select(FIELDS);
             try {
                 List<Map<String, Object>> data = builder.get();
                 for(Map<String, Object> obj : data) {
@@ -56,6 +61,8 @@ public class DeviceMonitorJob extends FacilioJob {
                             name = (String)deviceName;
                         }
                         jsonObject.put("deviceName", name);
+                        jsonObject.put("message","Data missing for controller " + name);
+                        jsonObject.put("severity","Major");
                         sendEvent(jsonObject);
                     }
                 }
@@ -68,5 +75,13 @@ public class DeviceMonitorJob extends FacilioJob {
 
     private void sendEvent(JSONObject jsonObject) {
         LOGGER.info("Device data missing " + jsonObject);
+        try {
+            FacilioContext addEventContext = new FacilioContext();
+            addEventContext.put(EventConstants.EventContextNames.EVENT_PAYLOAD, jsonObject);
+            Chain getAddEventChain = EventConstants.EventChainFactory.getAddEventChain();
+            getAddEventChain.execute(addEventContext);
+        } catch (Exception e) {
+            LOGGER.info("Exception while adding event");
+        }
     }
 }
