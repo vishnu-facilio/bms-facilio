41596c220ec025e511eafdc7ae74d34dcf17796b
shaan <shaan@facilio.com>
2018-09-02 21:22:34 +0530

    adding view count constant

--- src/main/java/com/facilio/constants/FacilioConstants.java
+++ src/main/java/com/facilio/constants/FacilioConstants.java
@@ -453,6 +453,7 @@ public class FacilioConstants {
 		public static final String VIEW_LIST = "views";
 		public static final String VIEWCOLUMNS = "viewColumns";
 		public static final String PARENT_VIEW = "parentView";
+		public static final String VIEW_COUNT = "viewCount";
 		public static final String SUB_VIEW = "subView";
 		public static final String SUB_VIEW_COUNT = "subViewCount";
 		public static final String RESOURCE_ID = "RESOURCE_ID";
