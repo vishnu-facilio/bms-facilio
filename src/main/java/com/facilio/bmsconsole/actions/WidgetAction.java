fc2419c43d9853c5f60b19aa9814612c1b81a934
Krishna <krishnan.e@facilio.com>
2018-09-14 13:26:24 +0530

    image card fix

--- src/main/java/com/facilio/bmsconsole/actions/WidgetAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/WidgetAction.java
@@ -1,13 +1,20 @@
 package com.facilio.bmsconsole.actions;
 
+import java.io.File;
 import java.util.List;
 
+import org.apache.commons.chain.Chain;
 import org.json.simple.JSONObject;
 
 import com.facilio.accounts.dto.User;
 import com.facilio.accounts.util.AccountUtil;
+import com.facilio.bmsconsole.commands.FacilioChainFactory;
+import com.facilio.bmsconsole.commands.FacilioContext;
+import com.facilio.bmsconsole.commands.TransactionChainFactory;
+import com.facilio.bmsconsole.context.PhotosContext;
 import com.facilio.bmsconsole.context.WorkOrderContext;
 import com.facilio.bmsconsole.util.WorkOrderAPI;
+import com.facilio.constants.FacilioConstants;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class WidgetAction extends ActionSupport {
@@ -40,6 +47,63 @@ public class WidgetAction extends ActionSupport {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+	
+	private List<File> file;
+	private List<String> fileContentType;
+	private List<String> fileFileName;
+	
+	public List<File> getFile() {
+		return file;
+	}
+
+	public void setFile(List<File> file) {
+		this.file = file;
+	}
+
+	public List<String> getFileContentType() {
+		return fileContentType;
+	}
+
+	public void setFileContentType(List<String> fileContentType) {
+		this.fileContentType = fileContentType;
+	}
+
+	public List<String> getFileFileName() {
+		return fileFileName;
+	}
+
+	public void setFileFileName(List<String> fileFileName) {
+		this.fileFileName = fileFileName;
+	}
+	PhotosContext photo;
+	
+	public PhotosContext getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(PhotosContext photo) {
+		this.photo = photo;
+	}
+
+	public String addPhoto() throws Exception {
+		
+		FacilioContext context = new FacilioContext();
+		
+		context.put(FacilioConstants.ContextNames.ATTACHMENT_FILE_LIST, this.file);
+ 		context.put(FacilioConstants.ContextNames.ATTACHMENT_FILE_NAME, this.fileFileName);
+ 		context.put(FacilioConstants.ContextNames.ATTACHMENT_CONTENT_TYPE, this.fileContentType);
+		
+		Chain addPhotosChain = TransactionChainFactory.getAddPhotoChain();
+		addPhotosChain.execute(context);
+		
+		List<PhotosContext> photos = (List<PhotosContext>) context.get(FacilioConstants.ContextNames.PHOTOS);
+		
+		if(photos != null && !photos.isEmpty()) {
+			photo = photos.get(0);
+		}
+		
+		return SUCCESS;
 	}
 
 	public String getUserCardData() throws Exception {
