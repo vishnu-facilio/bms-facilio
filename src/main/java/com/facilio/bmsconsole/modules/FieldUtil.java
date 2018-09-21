5b4ae78a1de4a0c0c43e41fe2b68393fc03a36c2
Aravind Chintalapalli <aravind@facilio.com>
2018-09-21 19:06:25 +0530

    Adds Site ID in Reading Alarm table

--- src/main/java/com/facilio/bmsconsole/modules/FieldUtil.java
+++ src/main/java/com/facilio/bmsconsole/modules/FieldUtil.java
@@ -499,7 +499,7 @@ public class FieldUtil {
 	}
 	
 	private static Set<String> ALLOWED_MODULES = Collections.unmodifiableSet(
-			new HashSet<>(Arrays.asList("resource", "asset", "building", "floor", "space", "zone", "alarm", "ticket", "workorder", "workorderrequest", "task")));
+			new HashSet<>(Arrays.asList("resource", "asset", "building", "floor", "space", "zone", "alarm", "ticket", "workorder", "workorderrequest", "task", "readingalarm")));
 	
 	public static boolean isSiteIdFieldPresent(FacilioModule module) {
 		return ALLOWED_MODULES.contains(module.getName()) || (module.getExtendModule() != null && module.getExtendModule().getName().equals("asset"));
