d04aaddabb802401c05afebffcc9f4d0d10c12d2
Krishna <krishnan.e@facilio.com>
2018-09-24 18:04:20 +0530

    mobile widget fix

--- src/main/java/com/facilio/bmsconsole/context/WidgetStaticContext.java
+++ src/main/java/com/facilio/bmsconsole/context/WidgetStaticContext.java
@@ -63,6 +63,7 @@ public class WidgetStaticContext extends DashboardWidgetContext {
 		
 		resultJson.put("id", getId());
 		resultJson.put("type", getWidgetType().getName());
+		
 		JSONObject layoutJson = new JSONObject();
 		layoutJson.put("height", getLayoutHeight());
 		layoutJson.put("width", getLayoutWidth());
@@ -71,6 +72,15 @@ public class WidgetStaticContext extends DashboardWidgetContext {
 		layoutJson.put("position", getLayoutPosition());
 		
 		resultJson.put("layout", layoutJson);
+		
+		JSONObject mlayoutJson = new JSONObject();
+		mlayoutJson.put("height", getmLayoutHeight());
+		mlayoutJson.put("width", getmLayoutWidth());
+		mlayoutJson.put("x", getmXPosition());
+		mlayoutJson.put("y", getmYPosition());
+		mlayoutJson.put("position", getmLayoutPosition());
+		
+		resultJson.put("mLayout", mlayoutJson);
 		
 		JSONObject headerJson = new JSONObject();
 		headerJson.put("title", getHeaderText());
