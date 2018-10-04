ff1f4e2ae098ef27767220c59bb86de69b67fdc3
Manthosh <manthosh@facilio.com>
2018-10-04 11:39:24 +0530

    Fix NPE in SendEmail

--- src/main/java/com/facilio/aws/util/AwsUtil.java
+++ src/main/java/com/facilio/aws/util/AwsUtil.java
@@ -427,7 +427,7 @@ public class AwsUtil
 			Message message = new Message().withSubject(subjectContent).withBody(body);
 
 			try {
-				if (AccountUtil.getCurrentOrg().getId() == 104) {
+				if (AccountUtil.getCurrentOrg() != null && AccountUtil.getCurrentOrg().getId() == 104) {
 					LOGGER.info("Sending email : "+mailJson.toJSONString());
 				}
 				SendEmailRequest request = new SendEmailRequest().withSource((String) mailJson.get("sender"))
@@ -436,7 +436,7 @@ public class AwsUtil
 						.withRegion(Regions.US_WEST_2).withCredentials(getAWSCredentialsProvider()).build();
 				client.sendEmail(request);
 				LOGGER.info("Email sent!");
-				if (AccountUtil.getCurrentOrg().getId() == 104) {
+				if (AccountUtil.getCurrentOrg() != null && AccountUtil.getCurrentOrg().getId() == 104) {
 					LOGGER.info(mailJson.toJSONString());
 				}
 			} catch (Exception ex) {
