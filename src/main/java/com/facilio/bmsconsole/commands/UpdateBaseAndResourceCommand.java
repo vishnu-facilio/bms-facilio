311b639e9588b0c11a321d6cce328040f1b2d303
Prashanth <prashanth.balaji@facilio.com>
2018-10-05 19:02:34 +0530

    import subspace space_id fix

--- src/main/java/com/facilio/bmsconsole/commands/UpdateBaseAndResourceCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/UpdateBaseAndResourceCommand.java
@@ -11,8 +11,12 @@ import java.util.logging.Logger;
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 
+import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.context.ReadingContext;
+import com.facilio.bmsconsole.modules.FacilioModule;
 import com.facilio.bmsconsole.util.ImportAPI;
+import com.facilio.constants.FacilioConstants;
+import com.facilio.fw.BeanFactory;
 import com.facilio.transaction.FacilioConnectionPool;
 import com.google.common.collect.ArrayListMultimap;
 
@@ -22,8 +26,12 @@ public class UpdateBaseAndResourceCommand implements Command,Serializable {
 	@Override
 	public boolean execute(Context context) throws Exception {
 		ArrayListMultimap<String, ReadingContext> groupedContext = (ArrayListMultimap<String, ReadingContext>) context.get(ImportAPI.ImportProcessConstants.GROUPED_READING_CONTEXT);
-		for(String module : groupedContext.keySet()) {
-			List<ReadingContext> readingsList = groupedContext.get(module);
+		ArrayListMultimap<String, Long> recordsList = (ArrayListMultimap<String, Long>) context.get(FacilioConstants.ContextNames.RECORD_LIST);
+		
+		for(String module : recordsList.keySet()) {
+			ModuleBean modBean = (ModuleBean)BeanFactory.lookup("ModuleBean");
+			FacilioModule facilioModule = modBean.getModule(module);
+			List<Long> readingsList = recordsList.get(module);
 			String updateBaseQueryColumn = null;
 			switch(module) {
 				case "site":
@@ -43,13 +51,13 @@ public class UpdateBaseAndResourceCommand implements Command,Serializable {
 					break;
 				}
 			}
-			if(updateBaseQueryColumn != null) {
+			if(facilioModule.getExtendModule().getName().equals(FacilioConstants.ContextNames.BASE_SPACE)) {
 				try { Connection con = FacilioConnectionPool.INSTANCE.getConnection();
 				for(int done= 0 ;done< readingsList.size();) {
 					String updateResourceQuery = "UPDATE Resources SET SPACE_ID = CASE ID";
 					String updateBaseSpaceQuery = "UPDATE BaseSpace SET "+ updateBaseQueryColumn +" = CASE ID";
 					
-					List<ReadingContext> tempList = new ArrayList<>();
+					List<Long> tempList = new ArrayList<>();
 					int remaining  = readingsList.size() - done;
 					if(remaining > 1000) {
 						tempList = readingsList.subList(done,done+1000);
@@ -58,25 +66,29 @@ public class UpdateBaseAndResourceCommand implements Command,Serializable {
 						tempList = readingsList.subList(done, done+ remaining);
 					}
 					for(int temp1=0; temp1 < tempList.size() ; temp1++) {
-						updateResourceQuery = updateResourceQuery + " WHEN " + readingsList.get(temp1).getId() + " THEN " + readingsList.get(temp1).getId();
-						updateBaseSpaceQuery = updateBaseSpaceQuery + " WHEN " + readingsList.get(temp1).getId() + " THEN " + readingsList.get(temp1).getId();
+						updateResourceQuery = updateResourceQuery + " WHEN " + readingsList.get(temp1) + " THEN " + readingsList.get(temp1);
+						updateBaseSpaceQuery = updateBaseSpaceQuery + " WHEN " + readingsList.get(temp1) + " THEN " + readingsList.get(temp1);
 					}
 					
 					updateResourceQuery = updateResourceQuery + " else SPACE_ID end;";
 					PreparedStatement pstmt = con.prepareStatement(updateResourceQuery, Statement.RETURN_GENERATED_KEYS);
 					pstmt.executeUpdate();
-					updateBaseSpaceQuery = updateBaseSpaceQuery + " else " + updateBaseQueryColumn + " end;";
-					PreparedStatement basepstmt = con.prepareStatement(updateBaseSpaceQuery, Statement.RETURN_GENERATED_KEYS);
-					basepstmt.executeUpdate();
+					if(updateBaseQueryColumn != null) {
+						updateBaseSpaceQuery = updateBaseSpaceQuery + " else " + updateBaseQueryColumn + " end;";
+						PreparedStatement basepstmt = con.prepareStatement(updateBaseSpaceQuery, Statement.RETURN_GENERATED_KEYS);
+						basepstmt.executeUpdate();
+					}
+					
 					
 					done = done + tempList.size();
-					LOGGER.severe("UPDATED BASE and RESOURCE");
 				}
 				}catch(Exception e) {
 					LOGGER.severe(e.toString());
 				}
+				LOGGER.severe("UPDATED BASE and RESOURCE");
 			}
-		}
+			}
+				
 		return false;
 	}
 
