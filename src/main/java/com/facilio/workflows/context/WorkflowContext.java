767a196006260921eb27a9d9e2528286b10309a1
Manthosh <manthosh@facilio.com>
2018-10-07 01:45:42 +0530

    More debug

--- src/main/java/com/facilio/workflows/context/WorkflowContext.java
+++ src/main/java/com/facilio/workflows/context/WorkflowContext.java
@@ -242,13 +242,13 @@ public class WorkflowContext implements Serializable {
 			return 0;
 		}
 		
-		if (AccountUtil.getCurrentOrg().getId() == 135 || (id != null && (id == 2834 || id == 2835 || id == 2820))) {
-			LOGGER.finer("variableToExpresionMap --- "+variableResultMap+" \n\n"+"expString --- "+getResultEvaluator());
+		if (AccountUtil.getCurrentOrg().getId() == 135 || (AccountUtil.getCurrentOrg().getId() == 88 && (getResultEvaluator().equals("(a)*(c-d)") || getResultEvaluator().equals("(a)*(c-d)*(e)*(f)") || getResultEvaluator().equals("(a)*(c-d)*(e)*(f)")))) {
+			LOGGER.info("variableToExpresionMap --- "+variableResultMap+" \n\n"+"expString --- "+getResultEvaluator());
 		}
 		
 		result =  WorkflowUtil.evaluateExpression(getResultEvaluator(),variableResultMap, ignoreNullValues);
-		if (AccountUtil.getCurrentOrg().getId() == 135 || (id != null && (id == 2834 || id == 2835 || id == 2820))) {
-			LOGGER.finer("result --- "+result);
+		if (AccountUtil.getCurrentOrg().getId() == 135 || (AccountUtil.getCurrentOrg().getId() == 88)) {
+			LOGGER.info("result --- "+result);
 		}
 		return result;
 	}
