1129b525c427d6a94607455f05c6acd82a1edc4c
Magesh V <magesh@facilio.com>
2018-10-08 14:16:11 +0530

    chartWidth change for pdf export

--- src/main/java/com/facilio/bmsconsole/commands/GetExportReportDataCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetExportReportDataCommand.java
@@ -294,7 +294,7 @@ public class GetExportReportDataCommand implements Command {
 			url.append("/show");
 		}
 		if(fileFormat == FileFormat.PDF) {
-			url.append("?chartWidth=540");
+			url.append("?chartWidth=1000");
 		}
 		return url;
 	}
