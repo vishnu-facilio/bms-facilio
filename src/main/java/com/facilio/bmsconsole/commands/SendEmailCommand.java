6a022b56e432c100dfb7c06e90f0ac4eb0c44ab7
Krishna <krishnan.e@facilio.com>
2018-10-13 11:28:21 +0530

    fix

--- src/main/java/com/facilio/bmsconsole/commands/SendEmailCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/SendEmailCommand.java
@@ -6,6 +6,7 @@ import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
 import org.json.simple.JSONObject;
 
 import com.facilio.accounts.dto.User;
@@ -23,24 +24,30 @@ public class SendEmailCommand implements Command,Serializable{
 	
 	@Override
 	public boolean execute(Context c) throws Exception{
-		ImportProcessContext importProcessContext = (ImportProcessContext) c.get(ImportAPI.ImportProcessConstants.IMPORT_PROCESS_CONTEXT);
-		ImportProcessContext updatedContext = ImportAPI.getImportProcessContext(importProcessContext.getId());
-		StringBuilder emailMessage = (StringBuilder) c.get(ImportAPI.ImportProcessConstants.EMAIL_MESSAGE);
-		FileStore fs = FileStoreFactory.getInstance().getFileStore();
-		Integer mailSetting = updatedContext.getMailSetting();
+		try {
+			ImportProcessContext importProcessContext = (ImportProcessContext) c.get(ImportAPI.ImportProcessConstants.IMPORT_PROCESS_CONTEXT);
+			ImportProcessContext updatedContext = ImportAPI.getImportProcessContext(importProcessContext.getId());
+			StringBuilder emailMessage = (StringBuilder) c.get(ImportAPI.ImportProcessConstants.EMAIL_MESSAGE);
+			FileStore fs = FileStoreFactory.getInstance().getFileStore();
+			Integer mailSetting = updatedContext.getMailSetting();
+			
+			if (mailSetting != null) {
+				User user = AccountUtil.getCurrentUser();
+				FileInfo fileInfo = fs.getFileInfo(importProcessContext.getFileId());
+				JSONObject email = new JSONObject();
+				email.put("to", user.getEmail());
+				email.put("subject", "Import of " + fileInfo.getFileName());
+				email.put("message",emailMessage.toString());
+				AwsUtil.sendEmail(email);
+			}
+			else {
+				emailMessage.delete(0, emailMessage.length());
+			}
+		}
+		catch(Exception e) {
+			LOGGER.log(Priority.ERROR, e);
+		}
 		
-		if (mailSetting != null) {
-			User user = AccountUtil.getCurrentUser();
-			FileInfo fileInfo = fs.getFileInfo(importProcessContext.getFileId());
-			JSONObject email = new JSONObject();
-			email.put("to", user.getEmail());
-			email.put("subject", "Import of " + fileInfo.getFileName());
-			email.put("message",emailMessage.toString());
-			AwsUtil.sendEmail(email);
-		}
-		else {
-			emailMessage.delete(0, emailMessage.length());
-		}
 		return false;
 		
 	}
