9c390afacce9f3740f7041384bbf169bf149205d
Aravind Chintalapalli <aravind@facilio.com>
2018-10-22 17:23:11 +0530

    Fixes reading rule update

--- src/main/java/com/facilio/bmsconsole/commands/DeleterOldRuleActionsCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/DeleterOldRuleActionsCommand.java
@@ -1,13 +1,11 @@
 package com.facilio.bmsconsole.commands;
 
 import java.util.Collections;
-import java.util.List;
 
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 
 import com.facilio.bmsconsole.util.ActionAPI;
-import com.facilio.bmsconsole.workflow.rule.ActionContext;
 import com.facilio.bmsconsole.workflow.rule.WorkflowRuleContext;
 import com.facilio.constants.FacilioConstants;
 
@@ -15,11 +13,8 @@ public class DeleterOldRuleActionsCommand implements Command {
 
 	@Override
 	public boolean execute(Context context) throws Exception {
-		// TODO Auto-generated method stub
-		List<ActionContext> actions = (List<ActionContext>) context.get(FacilioConstants.ContextNames.WORKFLOW_ACTION);
 		WorkflowRuleContext rule = (WorkflowRuleContext) context.get(FacilioConstants.ContextNames.WORKFLOW_RULE);
-		
-		if (actions != null) {
+		if (rule != null && rule.getId() != -1) {
 			ActionAPI.deleteAllActionsFromWorkflowRules(Collections.singletonList(rule.getId()));
 		}
 		return false;
