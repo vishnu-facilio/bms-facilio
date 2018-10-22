b2315bedeb68c2377710b320665423377dac35f5
Manthosh <manthosh@facilio.com>
2018-10-22 20:12:40 +0530

    Populating approval and rejection form objects during fetching of Approval rule for approval workorders

--- src/main/java/com/facilio/bmsconsole/commands/FetchApprovalRulesCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/FetchApprovalRulesCommand.java
@@ -2,14 +2,16 @@ package com.facilio.bmsconsole.commands;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 
 import com.facilio.bmsconsole.context.WorkOrderContext;
+import com.facilio.bmsconsole.forms.FacilioForm;
+import com.facilio.bmsconsole.util.FormsAPI;
 import com.facilio.bmsconsole.util.WorkflowRuleAPI;
 import com.facilio.bmsconsole.workflow.rule.ApprovalRuleContext;
 import com.facilio.bmsconsole.workflow.rule.ApprovalState;
@@ -39,8 +41,37 @@ public class FetchApprovalRulesCommand implements Command {
 			
 			if (!ruleIds.isEmpty()) {
 				List<WorkflowRuleContext> rules = WorkflowRuleAPI.getWorkflowRules(ruleIds);
-				Map<Long, ApprovalRuleContext> ruleMap = rules.stream()
-														.collect(Collectors.toMap(WorkflowRuleContext::getId, r -> (ApprovalRuleContext) r));
+				Map<Long, ApprovalRuleContext> ruleMap = new HashMap<>();
+				List<Long> formIds = new ArrayList<>();
+				for (WorkflowRuleContext rule : rules) {
+					ApprovalRuleContext approvalRule = (ApprovalRuleContext) rule;
+					ruleMap.put(rule.getId(), approvalRule);
+					if (approvalRule.getApprovalFormId() != -1) {
+						formIds.add(approvalRule.getApprovalFormId());
+					}
+					if (approvalRule.getRejectionFormId() != -1) {
+						formIds.add(approvalRule.getRejectionFormId());
+					}
+				}
+				
+				if (!formIds.isEmpty()) {
+					Map<Long, FacilioForm> forms = FormsAPI.getFormsAsMap(formIds);
+					for (ApprovalRuleContext approvalRule : ruleMap.values()) {
+						if (approvalRule.getApprovalFormId() != -1) {
+							approvalRule.setApprovalForm(forms.get(approvalRule.getApprovalFormId()));
+							if (approvalRule.getApprovalForm() == null) {
+								throw new IllegalArgumentException("Invalid approval rule with corrupted approval form id");
+							}
+						}
+						
+						if (approvalRule.getRejectionFormId() != -1) {
+							approvalRule.setRejectionForm(forms.get(approvalRule.getRejectionFormId()));
+							if (approvalRule.getRejectionForm() == null) {
+								throw new IllegalArgumentException("Invalid approval rule with corrupted rejection form id");
+							}
+						}
+					}
+				}
 				
 				for (WorkOrderContext wo : workOrders) {
 					if (wo.getApprovalStateEnum() == ApprovalState.REQUESTED) {
