e9ce65353d570024d2dad20436cb01f3faa471b9
Krishna <krishnan.e@facilio.com>
2018-10-25 13:11:57 +0530

    last value in variance

--- src/main/java/com/facilio/bmsconsole/commands/CalculateVarianceCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/CalculateVarianceCommand.java
@@ -1,7 +1,10 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.chain.Command;
@@ -31,7 +34,7 @@ public class CalculateVarianceCommand implements Command {
 					
 					Map<Object, Object> data = aggregateReportData.get(aggregateResult);
 					
-					JSONObject variance = getStandardVariance(data.values());
+					JSONObject variance = getStandardVariance(data);
 					
 					res.put(aggregateResult, variance);
 				}
@@ -45,9 +48,10 @@ public class CalculateVarianceCommand implements Command {
 		return false;
 	}
 
-	public JSONObject getStandardVariance(Collection<Object> collection) {
+	public JSONObject getStandardVariance(Map<Object, Object> data) {
 		
 		JSONObject variance = new JSONObject();
+		Collection<Object> collection = data.values();
 		try {
 			Double min = null ,max = null,avg = null,sum = (double) 0;
 			
@@ -71,6 +75,13 @@ public class CalculateVarianceCommand implements Command {
 			if(sum > 0 && collection.size() > 0) {
 				avg = sum / collection.size();
 			}
+			
+			List keys = new ArrayList<>(data.keySet());
+			if(keys != null && !keys.isEmpty()) {
+				Collections.sort(keys);
+				variance.put("lastValue", data.get(keys.get(keys.size()-1)));
+			}
+			
 			variance.put("min", FacilioUtil.decimalClientFormat(min));
 			variance.put("max", FacilioUtil.decimalClientFormat(max));
 			variance.put("avg", FacilioUtil.decimalClientFormat(avg));
