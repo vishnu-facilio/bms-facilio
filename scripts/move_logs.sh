871fef3a5fece258985a8bb57126fc1556b2078c
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2018-10-29 17:51:12 +0530

    move logs year month and date

--- scripts/move_logs.sh
+++ scripts/move_logs.sh
@@ -8,13 +8,14 @@ logsBucket=facilio-server-logs
 servername=`grep "api.servername" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 logsBucket=`grep "logs.bucket" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 
-today=`date +%Y/%m/%d`
+today=`date +%F`
+logDir=`date +%Y/%m/%d`
 for file in `ls $APP_HOME/logs`
 do
     if [ "$file" != "catalina.out" -a "$file" != "serverlog" -a "$file" != "resultset" -a "$file" != "l4jaccesslog" ]; then
         dateString=`echo $file | cut -d '.' -f 2 | cut -d '_' -f 1`
         if [ $dateString != $today ]; then
-            sudo aws s3 mv $APP_HOME/logs/$file s3://$logsBucket/$servername/$dateString/$ipAddress/
+            sudo aws s3 mv $APP_HOME/logs/$file s3://$logsBucket/$servername/$logDir/$ipAddress/
         fi
     fi
 done
