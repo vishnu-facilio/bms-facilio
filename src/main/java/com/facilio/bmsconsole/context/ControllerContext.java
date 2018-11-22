035a06298607b2464b0641c2ba2f8d32b53cc588
Swami <swami@facilio.com>
2018-11-22 13:20:21 +0530

    ControllerContext setter, getter methods added

--- src/main/java/com/facilio/bmsconsole/context/ControllerContext.java
+++ src/main/java/com/facilio/bmsconsole/context/ControllerContext.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.apache.struts2.json.annotations.JSON;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import com.facilio.time.SecondsChronoUnit;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -101,6 +104,68 @@ public class ControllerContext implements Serializable {
 			return active.booleanValue();
 		}
 		return false;
+	}
+	
+	
+	private String controllerType;
+	public String getControllerType() {
+		return controllerType;
+	}
+	public void setControllerType(String controllerType) {
+		this.controllerType = controllerType;
+	}
+	
+	private String destinationId;
+	public String getDestinationId() {
+		return destinationId;
+	}
+	public void setDestinationId(String destinationId) {
+		this.destinationId = destinationId;
+	}
+	
+	private String broadcastIp;
+	public String getBroadcastIp() {
+		return broadcastIp;
+	}
+	public void setBroadcastIp(String broadcastIp) {
+		this.broadcastIp = broadcastIp;
+	}
+	
+	private long instanceNumber = -1;
+	public void setInstanceNumber(long instanceNumber) {
+		this.instanceNumber = instanceNumber;
+	}
+	public long getInstanceNumber() {
+		return this.instanceNumber;
+	}
+	
+	
+	private JSONObject controllerProps;
+	
+	public JSONObject getAdditionInfo() {
+		return controllerProps;
+	}
+	public void setAdditionInfo(JSONObject additionInfo) {
+		this.controllerProps = additionInfo;
+	}
+	public void addAdditionInfo(String key, Object value) {
+		if(this.controllerProps == null) {
+			this.controllerProps =  new JSONObject();
+		}
+		this.controllerProps.put(key,value);
+	}
+	
+	
+	public String getControllerPropsJsonStr() {
+		
+		if(controllerProps != null) {
+			return controllerProps.toJSONString();
+		}
+		return null;
+	}
+	public void setControllerPropsJsonStr(String jsonStr) throws ParseException {
+		JSONParser parser = new JSONParser();
+		controllerProps = (JSONObject) parser.parse(jsonStr);
 	}
 	
 	@Override
