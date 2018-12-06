70ea4d310b066e6d052d5cb43ae9156d9a3e6241
Manthosh <manthosh@facilio.com>
2018-12-06 17:41:14 +0530

    More debug

--- src/main/java/com/facilio/bmsconsole/commands/FetchReportExtraMeta.java
+++ src/main/java/com/facilio/bmsconsole/commands/FetchReportExtraMeta.java
@@ -91,6 +91,11 @@ public class FetchReportExtraMeta implements Command {
 							}
 						}
 					}
+					
+					if (AccountUtil.getCurrentOrg().getId() == 88) {
+						LOGGER.info("Fetching extra meta - parentIds : "+parentIds);
+					}
+					
 					if(report.getDateRange() != null && parentIds != null && !parentIds.isEmpty()) {
 						
 						long alarmId = -1;
@@ -123,6 +128,9 @@ public class FetchReportExtraMeta implements Command {
 									}
 								}
 							}
+							if (AccountUtil.getCurrentOrg().getId() == 88) {
+								LOGGER.info("Fetching extra meta - alarmProps : "+alarmProps);
+							}
 						}
 						alarmsMap.put(dataPoint.getName(), alarmProps);
 					}
@@ -130,6 +138,10 @@ public class FetchReportExtraMeta implements Command {
 			}
 			
 			List<ReportAlarmContext> reportAlarmContextList = getReportAlarms(allAlarms,report.getDateRange());
+			
+			if (AccountUtil.getCurrentOrg().getId() == 88) {
+				LOGGER.info("Fetching extra meta - alapmsMap : "+alarmsMap);
+			}
 			
 			context.put(FacilioConstants.ContextNames.REPORT_SAFE_LIMIT, safeLimit);
 			context.put(FacilioConstants.ContextNames.REPORT_ALARMS, alarmsMap);
