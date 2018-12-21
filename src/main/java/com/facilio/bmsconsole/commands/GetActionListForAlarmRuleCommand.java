2144fd0587aa1136603168c1da1a1a3918fce1ff
Krishna <krishnan.e@facilio.com>
2018-12-21 18:18:05 +0530

    new Alarm rule Fetch

---
+++ src/main/java/com/facilio/bmsconsole/commands/GetActionListForAlarmRuleCommand.java
@@ -0,0 +1,33 @@
+package com.facilio.bmsconsole.commands;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.chain.Command;
+import org.apache.commons.chain.Context;
+
+import com.facilio.accounts.util.AccountUtil;
+import com.facilio.bmsconsole.util.ActionAPI;
+import com.facilio.bmsconsole.workflow.rule.ActionContext;
+import com.facilio.bmsconsole.workflow.rule.AlarmRuleContext;
+import com.facilio.bmsconsole.workflow.rule.ReadingRuleContext;
+import com.facilio.bmsconsole.workflow.rule.WorkflowRuleContext;
+import com.facilio.constants.FacilioConstants;
+
+public class GetActionListForAlarmRuleCommand implements Command {
+
+	@Override
+	public boolean execute(Context context) throws Exception {
+		AlarmRuleContext alarmRule = (AlarmRuleContext) context.get(FacilioConstants.ContextNames.ALARM_RULE);
+		
+		List<ReadingRuleContext> rules = alarmRule.getAllRuleList();
+		if(rules != null && !rules.isEmpty()) {
+			for (WorkflowRuleContext rule : rules) {
+				List<ActionContext> actions = ActionAPI.getAllActionsFromWorkflowRule(AccountUtil.getCurrentOrg().getId(), rule.getId());
+				rule.setActions(actions);
+			}
+		}
+		return false;
+	}
+
+}
