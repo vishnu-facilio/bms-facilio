4bf832175e38219fbf89b03c032753242d0d91fc
aravindc26 <aravindc26@gmail.com>
2019-01-03 16:21:10 +0530

    Adds Task field in form

--- src/main/java/com/facilio/bmsconsole/forms/FormFactory.java
+++ src/main/java/com/facilio/bmsconsole/forms/FormFactory.java
@@ -226,6 +226,7 @@ public class FormFactory {
 		fields.add(new FormField("type",FieldDisplayType.LOOKUP_SIMPLE,"Maintenance Type", Required.OPTIONAL, "tickettype", 5, 1));
 		fields.add(new FormField("groups",FieldDisplayType.LOOKUP_SIMPLE,"Team", Required.OPTIONAL, "groups", 6, 1));
 		fields.add(new FormField("attachedFiles", FieldDisplayType.ATTACHMENT, "Attachments", Required.OPTIONAL, "attachment", 8, 1));
+		fields.add(new FormField("tasks", FieldDisplayType.TASKS, "Tasks", Required.OPTIONAL, 9, 1));
 		return fields;
 	}
 	
