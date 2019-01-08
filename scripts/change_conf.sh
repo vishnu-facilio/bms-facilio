82ec65e1a21d3e350af672cc236ac52abdc326ef
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2019-01-08 15:41:32 +0530

    build changes

--- scripts/change_conf.sh
+++ scripts/change_conf.sh
@@ -14,42 +14,16 @@ unzip $APP_HOME/webapps/ROOT.war -d $APP_HOME/webapps/ROOT
 
 if [ "$DEPLOYMENT_GROUP_NAME" = "pre_production" ]; then
     echo "copying $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
-    cp $META_INF_DIR/context-stage.xml $META_INF_DIR/context.xml
+    cp $UBUNTU_HOME/deployment-files/context-stage.xml $META_INF_DIR/context.xml
     cp $UBUNTU_HOME/setenv.sh $APP_HOME/bin/setenv.sh
-    sed -i -e 's/localhost:9090/app.facilio.in/g' $CONF_DIR/awsprops.properties
-    sed -i -e 's/localhost:8080/facilio.in/g' $CONF_DIR/awsprops.properties
-    sed -i'' "s%environment=.*%environment=stage%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%chargebee.site=.*%chargebee.site=payfacilio-test%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%chargebee.api=.*%chargebee.api=test_AcdMBlnceZzwYhGeAX6dkxzocvglIkJjL%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%kinesisServer=.*%kinesisServer=true%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%disable.csp=.*%disable.csp=true%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%schedulerServer=.*%schedulerServer=true%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%enable.transaction=.*%enable.transaction=true%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%enable.kinesis=.*%enable.kinesis=true%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%s3.bucket.name=.*%s3.bucket.name=facilio-stage1-data%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%enableeventjob=.*%enableeventjob=false%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%redis.enabled=.*%redis.enabled=true%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%redis.db=.*%redis.db=1%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%servername=.*%servername=stage-330328973.us-west-2.elb.amazonaws.com%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%api.servername=.*%api.servername=app.facilio.in%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%portal.domain=.*%portal.domain=facilstack.com%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%log4j.appender.graylog2.facility=.*%log4j.appender.graylog2.facility=stage%g" $CLASSES_DIR/log4j.properties
-    sed -i'' "s%log4j.appender.graylog2.graylogHost=.*%log4j.appender.graylog2.graylogHost=172.31.35.38%g" $CLASSES_DIR/log4j.properties
-    sed -i'' "s%websocket.url=.*%websocket.url=wss://app.facilio.in/websocket%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%static.url=.*%static.url=https://static.facilio.in%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%files.url=.*%files.url=files.facilio.in%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%portal.domain=.*%portal.domain=facilstack.com%g" $CONF_DIR/awsprops.properties
-    sed -i'' "s%cors.allowed.origins=.*%cors.allowed.origins=http://localhost:8080,http://localhost:9090,localhost:8080,localhost:9090,https://facilio.in,https://facilstack.com,https://facilstack.com:9090%g" $CONF_DIR/awsprops.properties
-    sed -i -e 's/localhost:7444/172.31.38.25:7444/g' $CONF_DIR/awsprops.properties
-    sed -i -e 's/tmp/home\/ubuntu\/analytics\/temp/g' $CONF_DIR/awsprops.properties
-    sed -i -e 's/anomalyRefreshWaitTimeInSeconds=10/anomalyRefreshWaitTimeInSeconds=1/g' $CONF_DIR/awsprops.properties
-    sed -i -e 's/anomalyDetectWaitTimeInSeconds=3/anomalyDetectWaitTimeInSeconds=0/g' $CONF_DIR/awsprops.properties 
+    cp $UBUNTU_HOME/deployment-files/awsprops-stage.properties $CONF_DIR/awsprops.properties
+    cp $UBUNTU_HOME/deployment-files/log4j-stage.properties $CLASSES_DIR/log4j.properties
     echo "copied $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
 fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" = "production_deployment" ]; then
     echo "copying $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
-    cp $META_INF_DIR/context-production.xml $META_INF_DIR/context.xml
+    cp $UBUNTU_HOME/deployment-files/context-production.xml $META_INF_DIR/context.xml
     cp $UBUNTU_HOME/setenv.sh $APP_HOME/bin/setenv.sh
     sed -i'' "s%schedulerServer=.*%schedulerServer=false%g" $CONF_DIR/awsprops.properties
     sed -i'' "s%chargebee.site=.*%chargebee.site=payfacilio%g" $CONF_DIR/awsprops.properties
@@ -79,7 +53,7 @@ fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" = "production-scheduler" ]; then
     echo "copying $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
-    cp $META_INF_DIR/context-production.xml $META_INF_DIR/context.xml
+    cp $UBUNTU_HOME/deployment-files/context-production.xml $META_INF_DIR/context.xml
     cp $UBUNTU_HOME/setenv.sh $APP_HOME/bin/setenv.sh
     sed -i -e 's/localhost:9090/app.facilio.com/g' $CONF_DIR/awsprops.properties
     sed -i -e 's/localhost:8080/facilio.com/g' $CONF_DIR/awsprops.properties
