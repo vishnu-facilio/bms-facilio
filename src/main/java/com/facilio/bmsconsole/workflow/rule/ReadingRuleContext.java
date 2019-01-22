87f7bb045d85174fb3a1ea892c9060bffc15b6fd
Krishna <krishnan.e@facilio.com>
2019-01-22 14:39:12 +0530

    fix

--- src/main/java/com/facilio/bmsconsole/workflow/rule/ReadingRuleContext.java
+++ src/main/java/com/facilio/bmsconsole/workflow/rule/ReadingRuleContext.java
@@ -641,6 +641,9 @@ public class ReadingRuleContext extends WorkflowRuleContext {
 	
 	public void executeTrueActions(Object record, Context context, Map<String, Object> placeHolders) throws Exception {
 		long ruleId = getId();
+//		if (clearAlarm()) {
+//			addClearEvent(record, context, placeHolders, (ReadingContext) record, this);
+//		}
 		List<ActionContext>	actions = ActionAPI.getActiveActionsFromWorkflowRule(ruleId);
 		if(actions != null) {
 			for(ActionContext action : actions) {
@@ -661,38 +664,41 @@ public class ReadingRuleContext extends WorkflowRuleContext {
 		ReadingContext reading = (ReadingContext) record;
 		if (getMetric(reading) != null) {
 			if (clearAlarm()) {
-				Map<Long, ReadingRuleAlarmMeta> alarmMetaMap = (Map<Long, ReadingRuleAlarmMeta>) context.get(FacilioConstants.ContextNames.READING_RULE_ALARM_META);
-				boolean isHistorical = true;
-				if (alarmMetaMap == null) {
-					alarmMetaMap = this.alarmMetaMap;
-					isHistorical = false;
-				}
-				
-				ReadingRuleAlarmMeta alarmMeta = alarmMetaMap != null ? alarmMetaMap.get(reading.getParentId()) : null;
-				if (isHistorical) {/*if (AccountUtil.getCurrentOrg().getId() == 135 || AccountUtil.getCurrentOrg().getId() == 75 || AccountUtil.getCurrentOrg().getId() == 88) {*/
-					LOGGER.info("Alarm meta for rule : "+getId()+" for resource : "+reading.getParentId()+" at time : "+reading.getTtime()+"::"+alarmMeta);
-				}
-				if (alarmMeta != null && !alarmMeta.isClear()) {
-					alarmMeta.setClear(true);
-					AlarmContext alarm = AlarmAPI.getAlarm(alarmMeta.getAlarmId());
-					
-					JSONObject json = AlarmAPI.constructClearEvent(alarm, "System auto cleared Alarm because associated rule executed false for the associated resource", reading.getTtime());
-					json.put("readingDataId", reading.getId());
-					json.put("readingVal", reading.getReading(getReadingField().getName()));
-					
-					if (isHistorical) {/*if (AccountUtil.getCurrentOrg().getId() == 135 || AccountUtil.getCurrentOrg().getId() == 75 || AccountUtil.getCurrentOrg().getId() == 88) {*/
-						LOGGER.info("Clearing alarm for rule : "+getId()+" for resource : "+reading.getParentId());
-					}
-					
-					FacilioContext addEventContext = new FacilioContext();
-					addEventContext.put(EventConstants.EventContextNames.EVENT_PAYLOAD, json);
-					Chain getAddEventChain = EventConstants.EventChainFactory.getAddEventChain();
-					getAddEventChain.execute(addEventContext);
-					
-				}
+				addClearEvent(record, context, placeHolders, reading, this);
 			}
 			
 			super.executeFalseActions(record, context, placeHolders);
+		}
+	}
+	
+	public void addClearEvent(Object record, Context context, Map<String, Object> placeHolders,ReadingContext reading,ReadingRuleContext readingRuleContext) throws Exception {
+		Map<Long, ReadingRuleAlarmMeta> alarmMetaMap = (Map<Long, ReadingRuleAlarmMeta>) context.get(FacilioConstants.ContextNames.READING_RULE_ALARM_META);
+		boolean isHistorical = true;
+		if (alarmMetaMap == null) {
+			alarmMetaMap = readingRuleContext.getAlarmMetaMap();
+			isHistorical = false;
+		}
+		
+		ReadingRuleAlarmMeta alarmMeta = alarmMetaMap != null ? alarmMetaMap.get(reading.getParentId()) : null;
+		if (isHistorical) {
+			LOGGER.info("Alarm meta for rule : "+readingRuleContext.getId()+" for resource : "+reading.getParentId()+" at time : "+reading.getTtime()+"::"+alarmMeta);
+		}
+		if (alarmMeta != null && !alarmMeta.isClear()) {
+			alarmMeta.setClear(true);
+			AlarmContext alarm = AlarmAPI.getAlarm(alarmMeta.getAlarmId());
+			
+			JSONObject json = AlarmAPI.constructClearEvent(alarm, "System auto cleared Alarm because associated rule executed false for the associated resource", reading.getTtime());
+			json.put("readingDataId", reading.getId());
+			json.put("readingVal", reading.getReading(readingRuleContext.getReadingField().getName()));
+			
+			if (isHistorical) {
+				LOGGER.info("Clearing alarm for rule : "+readingRuleContext.getId()+" for resource : "+reading.getParentId());
+			}
+			
+			FacilioContext addEventContext = new FacilioContext();
+			addEventContext.put(EventConstants.EventContextNames.EVENT_PAYLOAD, json);
+			Chain getAddEventChain = EventConstants.EventChainFactory.getAddEventChain();
+			getAddEventChain.execute(addEventContext);
 		}
 	}
 }
