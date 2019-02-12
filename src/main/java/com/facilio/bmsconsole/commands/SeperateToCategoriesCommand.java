ae4420fde0d89a620c68eed3d24ff892b312f7c5
Prashanth <prashanth.balaji@facilio.com>
2019-02-12 11:51:00 +0530

    Logger for seperate categories command

--- src/main/java/com/facilio/bmsconsole/commands/SeperateToCategoriesCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/SeperateToCategoriesCommand.java
@@ -2,25 +2,39 @@ package com.facilio.bmsconsole.commands;
 
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 
+import com.facilio.bmsconsole.actions.ImportProcessContext;
+import com.facilio.bmsconsole.commands.data.PopulateImportProcessCommand;
 import com.facilio.bmsconsole.context.ReadingContext;
+import com.facilio.bmsconsole.exceptions.importExceptions.ImportParseException;
 import com.facilio.bmsconsole.util.ImportAPI;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 
 public class SeperateToCategoriesCommand implements Command{
 	
+	private static Logger LOGGER = Logger.getLogger(SeperateToCategoriesCommand.class.getName());
 	@Override
 	public boolean execute(Context context) throws Exception{
 		
 		List<ReadingContext> readingsList = (List<ReadingContext>) context.get(ImportAPI.ImportProcessConstants.READINGS_LIST);
+		ImportProcessContext importProcessContext = (ImportProcessContext) context.get(ImportAPI.ImportProcessConstants.IMPORT_PROCESS_CONTEXT);
 		context.get(ImportAPI.ImportProcessConstants.FIELDS_MAPPING);
 		Multimap<String, ReadingContext> categoryBasedAsset = ArrayListMultimap.create();
+		Map<String, Object> category = null;
 		for(int i =0;i< readingsList.size();i++) {
-			Map<String,Object> category= (Map<String,Object>) readingsList.get(i).getData().get(ImportAPI.ImportProcessConstants.CATEGORY_FROM_CONTEXT);
+			try {
+			LOGGER.severe("Resolving Categories for row : " + i);
+			LOGGER.severe(readingsList.get(i).getData().get(ImportAPI.ImportProcessConstants.CATEGORY_FROM_CONTEXT).toString());
+			category= (Map<String,Object>) readingsList.get(i).getData().get(ImportAPI.ImportProcessConstants.CATEGORY_FROM_CONTEXT);
+			}catch(Exception e) {
+				ImportParseException importException = new ImportParseException(i,importProcessContext.getFieldMapping().get("asset__category"), e);
+				throw importException;
+			}
 			if(category == null) {
 				String CategoryName = ImportAPI.ImportProcessConstants.NO_CATEGORY_DEFINED;
 				categoryBasedAsset.put(CategoryName, readingsList.get(i));
