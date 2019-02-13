5382cf34c029d9e0f0b82a2c8ee4065ac5085366
Manthosh <manthosh@facilio.com>
2019-02-13 15:09:50 +0530

    ADding debug statment to check null condition in rdm props

--- src/main/java/com/facilio/timeseries/TimeSeriesAPI.java
+++ src/main/java/com/facilio/timeseries/TimeSeriesAPI.java
@@ -604,6 +604,9 @@ public class TimeSeriesAPI {
 		}
 
 		 List<Map<String, Object>> props =  builder.get();
+		 
+		 LOGGER.info("Query in getting instances : "+builder);
+		 
 		 if (props != null && !props.isEmpty() && !fetchCount) {
 			 return props.stream().map(prop -> {
 				 if (prop.get("instanceType") != null) {
