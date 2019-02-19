6172b8b3eb54862448c32e918ad1bdfb942079b7
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2019-02-19 15:28:48 +0530

    running xvfb script in background

--- scripts/start_xvfb.sh
+++ scripts/start_xvfb.sh
@@ -4,5 +4,5 @@ xvfb=`ps auxx | grep Xvfb | grep -v "grep"`
 
 if [ "xvfb$xvfb" = "xvfb" ]; then
     export DISPLAY=:99
-    Xvfb $DISPLAY -screen 0 1024x768x16 &
+    Xvfb $DISPLAY -screen 0 1024x768x16
 fi
