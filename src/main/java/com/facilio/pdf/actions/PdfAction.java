6feccc033b47b13c29385812b4feb78e46018cc2
shaan <shaan@facilio.com>
2019-02-22 01:57:51 +0530

    integeation v2 api and pdf create changes

--- src/main/java/com/facilio/pdf/actions/PdfAction.java
+++ src/main/java/com/facilio/pdf/actions/PdfAction.java
@@ -2,26 +2,23 @@ package com.facilio.pdf.actions;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import com.facilio.accounts.util.AccountUtil;
+import com.facilio.bmsconsole.actions.FacilioAction;
 import com.facilio.pdf.PdfUtil;
-import com.opensymphony.xwork2.Action;
-import com.opensymphony.xwork2.ActionSupport;
 
-public class PdfAction extends ActionSupport {
+public class PdfAction extends FacilioAction {
 
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private String url;
-    private long orgId;
-    private String username;
-    private InputStream fileInputStream;
+    private InputStream downloadStream;
 
     private static Logger log = LogManager.getLogger(PdfAction.class.getName());
 
@@ -33,40 +30,18 @@ public class PdfAction extends ActionSupport {
         this.url = url;
     }
 
-    public long getOrgId() {
-        return orgId;
+    public InputStream getDownloadStream() {
+        return downloadStream;
     }
 
-    public void setOrgId(long orgId) {
-        this.orgId = orgId;
+    public void setDownloadStream(InputStream downloadStream) {
+        this.downloadStream = downloadStream;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public InputStream getFileInputStream() {
-        return fileInputStream;
-    }
-
-    public void setFileInputStream(InputStream fileInputStream) {
-        this.fileInputStream = fileInputStream;
-    }
-
-    public String createPdf() {
-        String fileName  = PdfUtil.convertUrlToPdf(getOrgId(), getUsername(), getUrl(), null);
-        if(fileName != null){
-            try {
-                fileInputStream = new FileInputStream(new File(fileName));
-            } catch (FileNotFoundException e) {
-                log.info("Exception occurred ", e);
-            }
-            return Action.SUCCESS;
-        }
-        return Action.ERROR;
+    public String createPdf() throws Exception {
+        String fileName  = PdfUtil.convertUrlToPdf(AccountUtil.getCurrentOrg().getOrgId(), AccountUtil.getCurrentUser().getEmail(), getUrl());
+        downloadStream = new FileInputStream(new File(fileName));
+        setContentType("application/octet-stream");
+        return SUCCESS;
     }
 }
