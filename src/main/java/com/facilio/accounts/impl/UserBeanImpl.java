d39b9790dac4ff200b95ce2454bbaa2c5fa39c85
arunkumar <arunkumar@facilio.com>
2019-02-26 20:40:43 +0530

    invite user issue fix

--- src/main/java/com/facilio/accounts/impl/UserBeanImpl.java
+++ src/main/java/com/facilio/accounts/impl/UserBeanImpl.java
@@ -1283,7 +1283,7 @@ public class UserBeanImpl implements UserBean {
 				.table("Users")
 				.innerJoin("ORG_Users")
 				.on("Users.USERID = ORG_Users.USERID")
-				//.andCondition(CriteriaAPI.getCurrentOrgIdCondition(ModuleFactory.getOrgUserModule()))
+				.andCondition(CriteriaAPI.getCurrentOrgIdCondition(ModuleFactory.getOrgUserModule()))
 				;
 		
 		if (criteria != null && !criteria.isEmpty()) {
