a3f79c75f8dbf53edab04fad5842fd7450e56067
Tharani <tharani@facilio.com>
2019-03-04 19:51:04 +0530

    fix

--- src/main/java/com/facilio/unitconversion/Unit.java
+++ src/main/java/com/facilio/unitconversion/Unit.java
@@ -20,7 +20,7 @@ public enum Unit {
 	DECIWH(82,"Deciwatt Hour","DWh",Metric.ENERGY,"si*100","this/100"),
 	MWH(3,"Megawatt Hour","mWh",Metric.ENERGY,"si/1000","this*1000"),
 	
-	CELSIUS(4,"Celsius","&deg;C",Metric.TEMPERATURE),
+	CELSIUS(4,"Celsius","°C",Metric.TEMPERATURE),
 	FAHRENHEIT(5,"Fahrenheit","&deg;F",Metric.TEMPERATURE,"(si*1.8)+32","(this-32)/1.8"),
 	KELWIN(6,"Kelvin","K",Metric.TEMPERATURE,"si+273.15","this-273.15"),
 	
