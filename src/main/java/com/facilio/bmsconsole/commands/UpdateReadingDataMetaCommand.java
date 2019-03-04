62cbf34b445750fff7ed963f4888157cb6d78a48
Aravind Chintalapalli <aravind@facilio.com>
2019-03-05 03:09:48 +0530

    Adds Logging

--- src/main/java/com/facilio/bmsconsole/commands/UpdateReadingDataMetaCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/UpdateReadingDataMetaCommand.java
@@ -2,6 +2,8 @@ package com.facilio.bmsconsole.commands;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
@@ -12,11 +14,14 @@ import com.facilio.bmsconsole.util.ReadingsAPI;
 import com.facilio.constants.FacilioConstants;
 
 public class UpdateReadingDataMetaCommand implements Command {
-
+	private static final Logger LOGGER = Logger.getLogger(UpdateReadingDataMetaCommand.class.getName());
 	@Override
 	public boolean execute(Context context) throws Exception {
 		// TODO Auto-generated method stub
-		
+		if (context.get(FacilioConstants.ContextNames.MODULE_NAME) != null) {
+			LOGGER.log(Level.SEVERE, "context from update rdm" + context.get(FacilioConstants.ContextNames.MODULE_NAME));
+		}
+
 		List<ReadingDataMeta> metaList = (List<ReadingDataMeta>) context.get(FacilioConstants.ContextNames.READING_DATA_META_LIST);
 		if (metaList != null && !metaList.isEmpty()) {
 			ReadingInputType type = (ReadingInputType) context.get(FacilioConstants.ContextNames.READING_DATA_META_TYPE);
