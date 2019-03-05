35d9ba0283889ff2486b4e9a14e46b3e47ec1d8f
shaan <shaan@facilio.com>
2019-03-05 14:08:21 +0530

    removing maxlevel while getting parent ticket in task

--- src/main/java/com/facilio/bmsconsole/commands/UpdateTaskCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/UpdateTaskCommand.java
@@ -130,7 +130,8 @@ public class UpdateTaskCommand implements Command {
 				.module(woModule)
 				.beanClass(WorkOrderContext.class)
 				.andCondition(CriteriaAPI.getIdCondition(task.getParentTicketId(), woModule))
-				.maxLevel(1);
+//				.maxLevel(1)
+				;
 		
 		List<WorkOrderContext> tickets = builder.get();
 		if(tickets != null && !tickets.isEmpty()) {
