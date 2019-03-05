6376b90c11d9bbbcbf0e46f14ee2d57460bcd082
Manthosh <manthosh@facilio.com>
2019-03-05 13:58:26 +0530

    Updating geoLocation also in QR code scanning

--- src/main/java/com/facilio/bmsconsole/commands/UpdateGeoLocationCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/UpdateGeoLocationCommand.java
@@ -37,22 +37,43 @@ public class UpdateGeoLocationCommand implements Command {
 			String[] latLng = location.trim().split("\\s*,\\s*");
 			double lat = Double.parseDouble(latLng[0]);
 			double lng = Double.parseDouble(latLng[1]);
-			
-			if (asset.getIdentifiedLocation() == null || !isWithinLocation(asset.getIdentifiedLocation(), lat, lng)) {
-				SiteContext identifiedLocation = getNearestLocation(lat, lng);
+			SiteContext identifiedLocation = null;
+			String newGeoLocation = null;
+			JSONObject json = null;
+			if (!isSameAsPrevGeoLocation(asset.getGeoLocation(), lat, lng)) {
+				newGeoLocation = location;
+				asset.setGeoLocation(newGeoLocation);
+				json = json == null ? new JSONObject() : json;
+				json.put("geoLocation", newGeoLocation);
+			}
+			if (!isWithinLocation(asset.getIdentifiedLocation(), lat, lng)) {
+				identifiedLocation = getNearestLocation(lat, lng);
 				if (identifiedLocation == null) {
 					identifiedLocation = new SiteContext();
 					identifiedLocation.setId(-1);
 					identifiedLocation.setName("Unknown Location");
 				}
 				asset.setIdentifiedLocation(identifiedLocation);
-				updateGeoLocation(asset.getId(), identifiedLocation);
-				JSONObject info = new JSONObject();
-				info.put("value", identifiedLocation.getName());
-				CommonCommandUtil.addActivityToContext(asset.getId(), -1, AssetActivityType.LOCATION, info, (FacilioContext) context);
+				json = json == null ? new JSONObject() : json;
+				json.put("identifiedLocation", identifiedLocation.getName());
+			}
+			if (json != null) {
+				updateGeoLocation(asset.getId(), identifiedLocation, newGeoLocation);
+				CommonCommandUtil.addActivityToContext(asset.getId(), -1, AssetActivityType.LOCATION, json, (FacilioContext) context);
 			}
 		}
 		return false;
+	}
+	
+	private static final int MAX_DISTANCE_FOR_GEO_LOCATION = 10; //meter;
+	private boolean isSameAsPrevGeoLocation (String location, double lat, double lng) {
+		if (StringUtils.isEmpty(location)) {
+			return false;
+		}
+		String[] latLng = location.trim().split("\\s*,\\s*");
+		double prevLat = Double.parseDouble(latLng[0]);
+		double prevLng = Double.parseDouble(latLng[1]);
+		return FacilioUtil.calculateHaversineDistance(prevLat, prevLng, lat, lng) <= MAX_DISTANCE_FOR_GEO_LOCATION;
 	}
 	
 	private SiteContext getNearestLocation(double lat, double lng) throws Exception {
@@ -72,15 +93,16 @@ public class UpdateGeoLocationCommand implements Command {
 	
 	private static final int MAX_DISTANCE = 1000; //meter 
 	private boolean isWithinLocation (SiteContext site, double lat, double lng) {
-		if (site.getLocation() == null || site.getLocation().getLat() == -1 || site.getLocation().getLng() == -1) {
+		if (site == null || site.getLocation() == null || site.getLocation().getLat() == -1 || site.getLocation().getLng() == -1) {
 			return false;
 		}
 		return FacilioUtil.calculateHaversineDistance(site.getLocation().getLat(), site.getLocation().getLng(), lat, lng) <= MAX_DISTANCE;
 	}
 	
-	private void updateGeoLocation(long id, SiteContext location) throws Exception {
+	private void updateGeoLocation(long id, SiteContext identifiedLocation, String location) throws Exception {
 		AssetContext asset = new AssetContext();
-		asset.setIdentifiedLocation(location);
+		asset.setIdentifiedLocation(identifiedLocation);
+		asset.setGeoLocation(location);
 		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
 		FacilioModule module = modBean.getModule(FacilioConstants.ContextNames.ASSET);
 		UpdateRecordBuilder<AssetContext> updateBuilder = new UpdateRecordBuilder<AssetContext>()
