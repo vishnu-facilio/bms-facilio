2921f3066800774499a9e016f9febeb4a2563e67
Prashanth <prashanth.balaji@facilio.com>
2019-03-13 17:56:28 +0530

    fix

--- src/main/java/com/facilio/bmsconsole/context/AssetContext.java
+++ src/main/java/com/facilio/bmsconsole/context/AssetContext.java
@@ -261,14 +261,11 @@ public class AssetContext extends ResourceContext {
 		this.identifiedLocation = identifiedLocation;
 	}
 	
-	private String url;
 	
 	public String getUrl() {
 		return "app/at/asset/all/" + getId() + "/overview";
 	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
+
 
 	public static enum AssetState {
 		ACTIVE(1, "Active"),
