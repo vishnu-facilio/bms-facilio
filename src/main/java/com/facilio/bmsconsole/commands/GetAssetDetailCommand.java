d597bbf4eae775dbd645be971326ac10f5c748a5
shaan <shaan@facilio.com>
2019-03-15 14:20:46 +0530

    logger added while setting module name in asset detail

--- src/main/java/com/facilio/bmsconsole/commands/GetAssetDetailCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetAssetDetailCommand.java
@@ -3,14 +3,19 @@ package com.facilio.bmsconsole.commands;
 import java.util.Collections;
 
 import org.apache.commons.chain.Context;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.context.AssetContext;
+import com.facilio.bmsconsole.modules.FacilioModule;
 import com.facilio.bmsconsole.util.AssetsAPI;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.fw.BeanFactory;
 
 public class GetAssetDetailCommand extends GenericGetModuleDataDetailCommand {
+	
+	private static final Logger LOGGER = LogManager.getLogger(GetAssetDetailCommand.class.getName());
 
 	@Override
 	public boolean execute(Context context) throws Exception {
@@ -27,7 +32,20 @@ public class GetAssetDetailCommand extends GenericGetModuleDataDetailCommand {
 				AssetsAPI.loadAssetsLookups(Collections.singletonList(assetContext));
 			}
 			ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
-			assetContext.setModuleName(modBean.getModule(assetContext.getModuleId()).getName());
+			if (assetContext.getModuleId() != -1) {
+				FacilioModule module = modBean.getModule(assetContext.getModuleId());
+				if (module != null) {
+					assetContext.setModuleName(module.getName());
+				}
+				else {
+					assetContext.setModuleName(FacilioConstants.ContextNames.ASSET);
+					LOGGER.info("No module for asset" + assetContext.getId() + ", moduleId:" + assetContext.getModuleId());
+				}
+			}
+			else {
+				assetContext.setModuleName(FacilioConstants.ContextNames.ASSET);
+				LOGGER.info("No module ID for asset" + assetContext.getId());
+			}
 			context.put(FacilioConstants.ContextNames.ASSET, assetContext);
 		}
 		return false;
