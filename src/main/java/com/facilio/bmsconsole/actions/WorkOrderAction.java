f0e34631d68c6bf63ee2bc6904af2d69291679bb
Aravind Chintalapalli <aravind@facilio.com>
2019-03-20 21:12:48 +0530

    Shows all work orders

--- src/main/java/com/facilio/bmsconsole/actions/WorkOrderAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/WorkOrderAction.java
@@ -1574,7 +1574,7 @@ public class WorkOrderAction extends FacilioAction {
 		context.put(FacilioConstants.ContextNames.CV_NAME, getViewName());
 		context.put(FacilioConstants.ContextNames.WO_DUE_STARTTIME, getStartTime());
 		context.put(FacilioConstants.ContextNames.WO_DUE_ENDTIME, getEndTime());
-		context.put(FacilioConstants.ContextNames.IS_APPROVAL, isApproval());
+		context.put(FacilioConstants.ContextNames.IS_APPROVAL, true);
 		if (getCount() != null) {	// only count
 			context.put(FacilioConstants.ContextNames.WO_LIST_COUNT, getCount());
 		}
