b334ddd5172afba809cfe9a8c29f63bb3a1496de
Tharani <tharani@facilio.com>
2019-03-26 10:57:51 +0530

    code Refactoring

--- src/main/java/com/facilio/bmsconsole/util/NotesAPI.java
+++ src/main/java/com/facilio/bmsconsole/util/NotesAPI.java
@@ -1,5 +1,7 @@
 package com.facilio.bmsconsole.util;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
@@ -25,6 +27,24 @@ public class NotesAPI {
 	
 	public static List<NoteContext> fetchNotes(long parentId, String moduleName) throws Exception {
 		List<NoteContext> noteListContext = getListBuilder(parentId, moduleName).get();
+		return getNotes(Collections.singletonList(parentId), moduleName, noteListContext);
+		
+	}
+	
+	
+	public static long fetchNotesCount(long parentId, String moduleName) throws Exception {
+		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+		SelectRecordsBuilder<NoteContext> builder = getListBuilder(parentId, moduleName);
+		builder.select(FieldFactory.getCountField(modBean.getModule(moduleName)));
+		List<Map<String, Object>> props = builder.getAsProps();
+		long count = 0;
+		if (props != null && !props.isEmpty()) {
+			count = (long) props.get(0).get("count");
+		}
+		return count;
+	}
+	
+	public static List<NoteContext> getNotes (List<Long> parentIds, String moduleName, List<NoteContext> noteListContext) throws Exception {
 		List<Long> ids = noteListContext.stream().map(note -> note.getCreatedBy().getId()).collect(Collectors.toList());
 		if (ids.size() > 0) {
 			List<User> userList = AccountUtil.getUserBean().getUsers(null, ids);
@@ -39,19 +59,10 @@ public class NotesAPI {
 				notess.setCreatedBy(userMap.get(notess.getCreatedBy().getId()));
 			}
 		}
-		return noteListContext;
-	}
-	
-	public static long fetchNotesCount(long parentId, String moduleName) throws Exception {
-		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
-		SelectRecordsBuilder<NoteContext> builder = getListBuilder(parentId, moduleName);
-		builder.select(FieldFactory.getCountField(modBean.getModule(moduleName)));
-		List<Map<String, Object>> props = builder.getAsProps();
-		long count = 0;
-		if (props != null && !props.isEmpty()) {
-			count = (long) props.get(0).get("count");
-		}
-		return count;
+		
+		
+		return noteListContext;	
+		
 	}
 	
 	private static SelectRecordsBuilder<NoteContext> getListBuilder (long parentId, String moduleName) throws Exception {
@@ -93,21 +104,8 @@ public class NotesAPI {
 				.andCondition(CriteriaAPI.getCondition(fieldMap.get("parentId"), ids.toString(), NumberOperators.EQUALS));
 		
 		List<NoteContext> props = selectBuilder.get();
-		List<Long> userIds = props.stream().map(note -> note.getCreatedBy().getId()).collect(Collectors.toList());
-		if (userIds.size() > 0) {
-			List<User> userList = AccountUtil.getUserBean().getUsers(null, userIds);
-			Map<Long, User> userMap = userList.stream().collect(Collectors.toMap(User::getId, Function.identity(), 
-											(prevValue, curValue) -> { return prevValue; }));
-			if (userList != null) {
-				for (NoteContext notess : props) {
-					notess.setCreatedBy(userMap.get(notess.getCreatedBy().getId()));
-				}
-			}
-			for (NoteContext notess : props) {
-				notess.setCreatedBy(userMap.get(notess.getCreatedBy().getId()));
-			}
-		}
-		return props;
+
+		return getNotes(parentIds, moduleName, props);
 	}
 
 }
