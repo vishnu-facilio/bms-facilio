de3c23823dfe58a3f9125bfccbe2d694bab9ac87
Krishna <krishnan.e@facilio.com>
2019-03-30 15:15:19 +0530

    fix

--- src/main/java/com/facilio/bmsconsole/commands/GetReadingRuleDetailsCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetReadingRuleDetailsCommand.java
@@ -67,6 +67,12 @@ public class GetReadingRuleDetailsCommand implements Command {
 								
 								rcaIds.add(rcaRule.getId());
 							}
+							for(Long versionId :alarmRuleContext.getAlarmRCARulesVersionHistory().keySet()) {
+								
+								for(ReadingRuleContext rcaRule : alarmRuleContext.getAlarmRCARulesVersionHistory().get(versionId)) {
+									rcaIds.add(rcaRule.getId());
+								}
+							}
 							List<FacilioField> eventFields = EventConstants.EventFieldFactory.getEventFields();
 							Map<String, FacilioField> eventFieldsMap = FieldFactory.getAsMap(eventFields);
 							List<FacilioField> fields = new ArrayList<>();
