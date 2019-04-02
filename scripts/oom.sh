d94bffabb4348f4741b9c9ada20e9e8e6b774413
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2019-04-02 15:45:14 +0530

    added command to send email

--- scripts/oom.sh
+++ scripts/oom.sh
@@ -5,5 +5,5 @@ servername=`grep "app.domain" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 environment=`grep "environment" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 schedulerServer=`grep "schedulerServer" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 messageProcessor=`grep "messageProcessor" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
-#echo "scheduler=$schedulerServer  kinesis=$messageProcessor" | mail -s "OOM Occurred in $environment $ipAddress" alerts@facilio.com
+aws ses send-email --region us-west-2 --from bhuvaneswaran.g@facilio.com --to alerts@facilio.com --subject "OOM Occurred in $environment $ipAddress" --text "scheduler=$schedulerServer  kinesis=$messageProcessor"
 sh /home/ubuntu/start_server.sh
