9ca4514fb4768b38ef423f4c0a1c453c499c45fd
Krishna <krishnan.e@facilio.com>
2019-04-10 10:36:58 +0530

    enabling new flow to stage

--- src/main/java/com/facilio/bmsconsole/commands/AddTaskSectionTriggersCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/AddTaskSectionTriggersCommand.java
@@ -1,5 +1,6 @@
 package com.facilio.bmsconsole.commands;
 
+import com.facilio.aws.util.AwsUtil;
 import com.facilio.bmsconsole.context.PMTaskSectionTemplateTriggers;
 import com.facilio.bmsconsole.context.PMTriggerContext;
 import com.facilio.bmsconsole.context.PreventiveMaintenance;
@@ -42,6 +43,9 @@ public class AddTaskSectionTriggersCommand implements Command {
 		List<Map<String, Object>> props = new ArrayList<>();
 		
 		boolean isNewFlow = false;
+		if(!AwsUtil.isProduction()) {
+			isNewFlow = true;
+		}
 		if(isNewFlow) {
 			for (int i = 0; i < sectionTemplates.size(); i++) {
 				List<PMTaskSectionTemplateTriggers> pmTaskSectionTemplateTriggers = sectionTemplates.get(i).getPmTaskSectionTemplateTriggers();
