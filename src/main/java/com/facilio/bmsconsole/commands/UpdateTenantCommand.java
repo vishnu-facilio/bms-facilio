9c109f254d254cf57a770ce8ce2634bbce56373b
Vandhana Sridhar <vandhana@facilio.com>
2019-04-10 14:43:31 +0530

    tenant module related changes

--- src/main/java/com/facilio/bmsconsole/commands/UpdateTenantCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/UpdateTenantCommand.java
@@ -3,6 +3,10 @@ package com.facilio.bmsconsole.commands;
 import com.facilio.bmsconsole.tenant.TenantContext;
 import com.facilio.bmsconsole.util.TenantsAPI;
 import com.facilio.constants.FacilioConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 
@@ -11,10 +15,12 @@ public class UpdateTenantCommand implements Command {
 	@Override
 	public boolean execute(Context context) throws Exception {
 		TenantContext tenant = (TenantContext) context.get(TenantsAPI.TENANT_CONTEXT);
+		List<Long> spaceIds = (ArrayList<Long>)context.get(FacilioConstants.ContextNames.RECORD_ID_LIST);
+		
 		if(tenant.getStatusEnum() != TenantContext.Status.ACTIVE) {
 			throw new IllegalArgumentException("Cannot update an inactive tenant");
 		}
-		int rowsUpdated = TenantsAPI.updateTenant(tenant);
+		int rowsUpdated = TenantsAPI.updateTenant(tenant, spaceIds);
 		context.put(FacilioConstants.ContextNames.ROWS_UPDATED,rowsUpdated);
 		return false;
 	}
