3182fc1892c3bc1ca367c9bcebc56e8582d8aeff
javeed <javeed@192.168.1.46>
2019-04-19 12:51:35 +0530

    Modified to fit MLVariables modified columns

--- src/main/java/com/facilio/bmsconsole/commands/GetReadingsForMLCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetReadingsForMLCommand.java
@@ -10,6 +10,7 @@ import java.util.TreeMap;
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 
+import com.facilio.aws.util.AwsUtil;
 import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.context.MLContext;
 import com.facilio.bmsconsole.context.MLVariableContext;
@@ -29,9 +30,13 @@ public class GetReadingsForMLCommand implements Command {
 		MLContext mlContext = (MLContext) context.get(FacilioConstants.ContextNames.ML);
 		List<MLVariableContext> mlVariable = mlContext.getMLVariable();
 		
-		long maxSamplingPeriod = mlContext.getMaxSamplingPeriod();
 		long currentTime = System.currentTimeMillis();
-		long startTime = currentTime-maxSamplingPeriod;
+		if( AwsUtil.getConfig("environment").equals("development")) 
+		{
+			// for dev testing purpose time is moved back 
+			currentTime = 1536300000000L;
+		}
+		
 		SortedMap<Long,Hashtable<String,Object>> variableData = new TreeMap<Long,Hashtable<String,Object>>();
 		SortedMap<Long,Hashtable<String,Object>> criteriavariableData = new TreeMap<Long,Hashtable<String,Object>>();
 		
@@ -39,8 +44,10 @@ public class GetReadingsForMLCommand implements Command {
 		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
 		for(MLVariableContext variables:mlVariable)
 		{
-			
-            FacilioField variableField = modBean.getField(variables.getFieldid());
+			long startTime = currentTime-variables.getMaxSamplingPeriod();
+			SortedMap<Long,Object> data = new TreeMap<Long,Object>();
+            FacilioField variableField = modBean.getField(variables.getFieldID());
+            FacilioField parentField = modBean.getField(variables.getParentFieldID());
             FacilioField ttimeField = FieldFactory.getField("ttime", "TTIME", variableField.getModule(), FieldType.NUMBER);
             List<FacilioField> fieldList = new ArrayList<FacilioField>(2);
             fieldList.add(variableField);
@@ -50,62 +57,53 @@ public class GetReadingsForMLCommand implements Command {
 																	.module(variableField.getModule())
 																	.beanClass(ReadingContext.class)
 																	.orderBy("TTIME ASC")
-																	.andCustomWhere("TTIME >= ? AND TTIME < ? AND PARENT_ID=? ",
-																			startTime, currentTime, mlContext.getAssetContext().getAssetID());
+																	.andCustomWhere("TTIME >= ? AND TTIME < ? AND "+parentField.getColumnName()+"=? ",
+																			startTime, currentTime,variables.getParentID());
 			List<Map<String, Object>> props = selectBuilder.getAsProps();
 			for(Map<String,Object> prop : props)
 			{
-				long ttime = (long)prop.get(ttimeField.getName());
-				if(variableData.containsKey(ttime))
-				{
-					variableData.get(ttime).put(variableField.getName(), prop.get(variableField.getName()));
-				}
-				else
-				{
-					Hashtable<String,Object> data = new Hashtable<String,Object>();
-					data.put(variableField.getName(), prop.get(variableField.getName()));
-					variableData.put(ttime, data);
-				}
+				data.put((long)prop.get(ttimeField.getName()), prop.get(variableField.getName()));
 			}
-			
+			mlContext.setMlVariablesDataMap(variables.getParentID(), variableField.getName(), data);
 		}
 		
 		List<MLVariableContext> mlCriteriaVariable = mlContext.getMLCriteriaVariables();
-		for(MLVariableContext variables:mlCriteriaVariable)
+		if(mlCriteriaVariable!=null)
 		{
-			
-            FacilioField variableField = modBean.getField(variables.getFieldid());
-            FacilioField ttimeField = FieldFactory.getField("ttime", "TTIME", variableField.getModule(), FieldType.NUMBER);
-            List<FacilioField> fieldList = new ArrayList<FacilioField>(2);
-            fieldList.add(variableField);
-            fieldList.add(ttimeField);
-			SelectRecordsBuilder<ReadingContext> selectBuilder = new SelectRecordsBuilder<ReadingContext>()
-																	.select(fieldList)
-																	.module(variableField.getModule())
-																	.beanClass(ReadingContext.class)
-																	.orderBy("TTIME ASC")
-																	.andCustomWhere("TTIME >= ? AND TTIME < ? AND PARENT_ID=? ",
-																			startTime, currentTime, mlContext.getAssetContext().getAssetID());
-			List<Map<String, Object>> props = selectBuilder.getAsProps();
-			for(Map<String,Object> prop : props)
+			for(MLVariableContext variables:mlCriteriaVariable)
 			{
-				long ttime = (long)prop.get(ttimeField.getName());
-				if(criteriavariableData.containsKey(ttime))
+				long startTime = currentTime-variables.getMaxSamplingPeriod();
+	            FacilioField variableField = modBean.getField(variables.getFieldID());
+	            FacilioField ttimeField = FieldFactory.getField("ttime", "TTIME", variableField.getModule(), FieldType.NUMBER);
+	            List<FacilioField> fieldList = new ArrayList<FacilioField>(2);
+	            fieldList.add(variableField);
+	            fieldList.add(ttimeField);
+				SelectRecordsBuilder<ReadingContext> selectBuilder = new SelectRecordsBuilder<ReadingContext>()
+																		.select(fieldList)
+																		.module(variableField.getModule())
+																		.beanClass(ReadingContext.class)
+																		.orderBy("TTIME ASC")
+																		.andCustomWhere("TTIME >= ? AND TTIME < ? AND PARENT_ID=? ",
+																				startTime, currentTime, mlContext.getAssetContext().getAssetID());
+				List<Map<String, Object>> props = selectBuilder.getAsProps();
+				for(Map<String,Object> prop : props)
 				{
-					criteriavariableData.get(ttime).put(variableField.getName(), prop.get(variableField.getName()));
+					long ttime = (long)prop.get(ttimeField.getName());
+					if(criteriavariableData.containsKey(ttime))
+					{
+						criteriavariableData.get(ttime).put(variableField.getName(), prop.get(variableField.getName()));
+					}
+					else
+					{
+						Hashtable<String,Object> data = new Hashtable<String,Object>();
+						data.put(variableField.getName(), prop.get(variableField.getName()));
+						criteriavariableData.put(ttime, data);
+					}
 				}
-				else
-				{
-					Hashtable<String,Object> data = new Hashtable<String,Object>();
-					data.put(variableField.getName(), prop.get(variableField.getName()));
-					criteriavariableData.put(ttime, data);
-				}
+				
 			}
-			
+			mlContext.setMlCriteriaVariablesDataMap(criteriavariableData);
 		}
-		
-		mlContext.setMlVariablesDataMap(variableData);
-		mlContext.setMlCriteriaVariablesDataMap(criteriavariableData);
 		
 		return false;
 	}
