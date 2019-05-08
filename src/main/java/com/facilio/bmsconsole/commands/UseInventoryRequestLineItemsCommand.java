a012ed76c89eb580d2dd66dae04983003848cbf3
Vandhana Sridhar <vandhana@facilio.com>
2019-05-08 17:07:27 +0530

    parentid and status updated for no parentid lineitems

--- src/main/java/com/facilio/bmsconsole/commands/UseInventoryRequestLineItemsCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/UseInventoryRequestLineItemsCommand.java
@@ -28,6 +28,7 @@ public class UseInventoryRequestLineItemsCommand implements Command{
 		// TODO Auto-generated method stub
 		List<InventoryRequestLineItemContext> lineItems = (List<InventoryRequestLineItemContext>) context.get(FacilioConstants.ContextNames.INVENTORY_REQUEST_LINE_ITEMS);
 		Integer inventoryType = (Integer) context.get(FacilioConstants.ContextNames.INVENTORY_CATEGORY);
+		Long parentId = (Long) context.get(FacilioConstants.ContextNames.PARENT_ID);
 		
 		List<WorkorderItemContext> woItemList = new ArrayList<WorkorderItemContext>();
 		List<WorkorderToolsContext> woToolList = new ArrayList<WorkorderToolsContext>();
@@ -42,22 +43,28 @@ public class UseInventoryRequestLineItemsCommand implements Command{
 			}
 			lineItemIds.add(String.valueOf(lineItem.getId()));
 		}
-		updateInventoryRequestLineItemsStatus(lineItemIds.toString());
+		updateInventoryRequestLineItemsStatus(lineItemIds.toString(), parentId);
 		context.put(FacilioConstants.ContextNames.RECORD_LIST, inventoryType == InventoryType.ITEM.getValue() ? woItemList : woToolList);
 		
 		return false;
 	}
 	
-	private void updateInventoryRequestLineItemsStatus(String lineItemIds) throws Exception{
+	private void updateInventoryRequestLineItemsStatus(String lineItemIds, Long parentId) throws Exception {
 		
 		
 		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
 		FacilioModule inventoryRequestLineItemModule = modBean.getModule(FacilioConstants.ContextNames.INVENTORY_REQUEST_LINE_ITEMS);
 		Map<String, Object> updateMap = new HashMap<>();
 		FacilioField statusField = modBean.getField("status", inventoryRequestLineItemModule.getName());
+		FacilioField parentIdField = modBean.getField("parentId", inventoryRequestLineItemModule.getName());
+		
 		updateMap.put("status", InventoryRequestLineItemContext.Status.ISSUED);
+		updateMap.put("parentId", parentId);
+		
 		List<FacilioField> updatedfields = new ArrayList<FacilioField>();
 		updatedfields.add(statusField);
+		updatedfields.add(parentIdField);
+		
 		UpdateRecordBuilder<InventoryRequestLineItemContext> updateBuilder = new UpdateRecordBuilder<InventoryRequestLineItemContext>()
 						.module(inventoryRequestLineItemModule)
 						.fields(updatedfields)
