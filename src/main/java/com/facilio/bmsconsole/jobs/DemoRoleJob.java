dab455a4bfda8084144bd7537907d79b2f54ccb5
Manthosh <manthosh@facilio.com>
2019-05-09 15:58:14 +0530

    Adding try/catch for the entire job in DemoRoleJob

--- src/main/java/com/facilio/bmsconsole/jobs/DemoRoleJob.java
+++ src/main/java/com/facilio/bmsconsole/jobs/DemoRoleJob.java
@@ -31,52 +31,55 @@ public class DemoRoleJob extends FacilioJob{
 	public void execute(JobContext jc)throws Exception  {
 		// TODO Auto-generated method stub
 		long startTime = System.currentTimeMillis();
-		Map<String,List<String>> tableName=DemoRoleUtil.TABLES_WITH_COLUMN;
-		long orgId=AccountUtil.getCurrentOrg().getId();
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		boolean olderCommit = false;
-
 		try {
+			Map<String, List<String>> tableName = DemoRoleUtil.TABLES_WITH_COLUMN;
+			long orgId = AccountUtil.getCurrentOrg().getId();
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			boolean olderCommit = false;
 
-			conn = FacilioConnectionPool.INSTANCE.getConnectionFromPool();
-			olderCommit = conn.getAutoCommit();
-			conn.setAutoCommit(true);
-			 
-			for (Map.Entry<String, List<String>> tableList : tableName.entrySet()) {
+			try {
 
-				  String key = tableList.getKey();
-				  List<String> valueList = tableList.getValue();
-				  StringBuilder sql=new StringBuilder();
-				  sql.append("UPDATE").append(" ").append(key).append(" ").append("SET").append("  ");
-				  for (String columnName : valueList) {
-					  sql.append(columnName).append("=");
-					  sql.append(columnName).append("+").append(( 24 * 60 * 60 * 1000));
-					  sql.append(",");
-				  }		 
-				  sql.replace(sql.length()-1, sql.length(), " ");
-				  sql.append("WHERE ORGID=").append(orgId);
-				  pstmt = conn.prepareStatement(sql.toString());
-				  if(pstmt==null) {
-					  throw new RuntimeException("###update String Data is null");
-				  }
-				  try {
-					int count=  pstmt.executeUpdate();
-					System.out.println("###DemoRoleUp"+" "+count+" "+"of rows updated in"+key+"successfully");
-					LOGGER.info("###DemoRoleUp"+" "+count+" "+"of rows updated in"+key+"successfully");
-				  }
-				  catch(Exception e) {
-					  CommonCommandUtil.emailException("DemoRoleUp", "DemoRoleUp Failed - orgid -- "+AccountUtil.getCurrentOrg().getId(), e);
-				  }
+				conn = FacilioConnectionPool.INSTANCE.getConnectionFromPool();
+				olderCommit = conn.getAutoCommit();
+				conn.setAutoCommit(true);
+
+				for (Map.Entry<String, List<String>> tableList : tableName.entrySet()) {
+
+					String key = tableList.getKey();
+					List<String> valueList = tableList.getValue();
+					StringBuilder sql = new StringBuilder();
+					sql.append("UPDATE").append(" ").append(key).append(" ").append("SET").append("  ");
+					for (String columnName : valueList) {
+						sql.append(columnName).append("=");
+						sql.append(columnName).append("+").append((24 * 60 * 60 * 1000));
+						sql.append(",");
+					}
+					sql.replace(sql.length() - 1, sql.length(), " ");
+					sql.append("WHERE ORGID=").append(orgId);
+					pstmt = conn.prepareStatement(sql.toString());
+					if (pstmt == null) {
+						throw new RuntimeException("###update String Data is null");
+					}
+					try {
+						int count = pstmt.executeUpdate();
+						System.out.println("###DemoRoleUp" + " " + count + " " + "of rows updated in" + key + "successfully");
+						LOGGER.info("###DemoRoleUp" + " " + count + " " + "of rows updated in" + key + "successfully");
+					} catch (Exception e) {
+						CommonCommandUtil.emailException("DemoRoleUp", "DemoRoleUp Failed - orgid -- " + AccountUtil.getCurrentOrg().getId(), e);
+					}
+				}
+			} catch (Exception e) {
+				LOGGER.info("Exception occurred ", e);
+				throw e;
+			} finally {
+				conn.setAutoCommit(olderCommit);
+				DBUtil.closeAll(conn, pstmt);
 			}
 		}
-		catch(Exception e) {
-			CommonCommandUtil.emailException("DemoRoleUp", "DemoRoleUp Failed - orgid -- "+AccountUtil.getCurrentOrg().getId(), e);
+		catch (Exception e) {
+			CommonCommandUtil.emailException("DemoRoleUp", "DemoRoleUp Failed - orgid -- " + AccountUtil.getCurrentOrg().getId(), e);
 			LOGGER.info("Exception occurred ", e);
-		}
-		finally {
-			conn.setAutoCommit(olderCommit);
-			DBUtil.closeAll(conn, pstmt);
 		}
 		LOGGER.info("####DemoRoleUp Job completed time  is" + (System.currentTimeMillis()-startTime));
 	}
