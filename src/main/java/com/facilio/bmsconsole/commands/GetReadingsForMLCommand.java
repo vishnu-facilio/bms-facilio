6f28e41d3f16c649baf6e4f127e171716e78e628
javeed <javeed@192.168.1.49>
2019-05-13 13:20:46 +0530

    Added logging to debug an issue(Need to remove after fixing)

--- src/main/java/com/facilio/bmsconsole/commands/GetReadingsForMLCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetReadingsForMLCommand.java
@@ -9,6 +9,7 @@ import java.util.TreeMap;
 
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
+import org.apache.log4j.Logger;
 
 import com.facilio.aws.util.AwsUtil;
 import com.facilio.beans.ModuleBean;
@@ -24,6 +25,7 @@ import com.facilio.fw.BeanFactory;
 
 public class GetReadingsForMLCommand implements Command {
 
+	private static final Logger LOGGER = Logger.getLogger(GenerateMLModelCommand.class.getName());
 	@Override
 	public boolean execute(Context context) throws Exception 
 	{
@@ -59,7 +61,9 @@ public class GetReadingsForMLCommand implements Command {
 																	.orderBy("TTIME ASC")
 																	.andCustomWhere("TTIME >= ? AND TTIME < ? AND "+parentField.getColumnName()+"=? ",
 																			startTime, currentTime,variables.getParentID());
+			LOGGER.info(" select Builder query "+selectBuilder);
 			List<Map<String, Object>> props = selectBuilder.getAsProps();
+			LOGGER.info(" props are "+props);
 			for(Map<String,Object> prop : props)
 			{
 				data.put((long)prop.get(ttimeField.getName()), prop.get(variableField.getName()));
