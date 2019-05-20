9c5299d89d765f6bee5a69cf5c2bd3c761f74215
javeed <javeed@192.168.1.49>
2019-05-20 16:29:52 +0530

    added debug statements

--- src/main/java/com/facilio/bmsconsole/commands/ApplyRuleForMLCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/ApplyRuleForMLCommand.java
@@ -2,6 +2,7 @@ package com.facilio.bmsconsole.commands;
 
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
+import org.apache.log4j.Logger;
 
 import com.facilio.bmsconsole.context.MLContext;
 import com.facilio.bmsconsole.util.WorkflowRuleAPI;
@@ -11,18 +12,24 @@ import com.facilio.constants.FacilioConstants;
 
 public class ApplyRuleForMLCommand implements Command {
 
+	private static final Logger LOGGER = Logger.getLogger(ApplyRuleForMLCommand.class.getName());
+	
 	@Override
 	public boolean execute(Context context) throws Exception 
 	{
 		
 		MLContext mlContext = (MLContext) context.get(FacilioConstants.ContextNames.ML);
 		WorkflowRuleContext rule = WorkflowRuleAPI.getWorkflowRule(mlContext.getRuleID(),true,true,true);
+		LOGGER.info("MLContext for Rule id is "+mlContext.getRuleID()+":::"+mlContext.getId());
 		if (rule != null && rule.isActive()) 
 		{
+			LOGGER.info("Rule is active ");
 			String jobid = mlContext.getMLModelVariable("jobid");
 			FacilioContext ruleContext = new FacilioContext();
 			ruleContext.put("jobid", jobid);
+			LOGGER.info("Calling jobid "+jobid);
 			WorkflowRuleAPI.executeScheduledRule(rule, mlContext.getPredictionTime() * 1000, ruleContext);
+			LOGGER.info("After scheduling rule "+jobid);
 		}
 		return false;
 		
