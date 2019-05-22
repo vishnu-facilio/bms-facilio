aaa6f6436ea63c1f2224e894861d84e7338dd346
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2019-05-22 15:06:04 +0530

    monitoring via http prometheus

--- src/main/java/com/facilio/filters/HealthCheckFilter.java
+++ src/main/java/com/facilio/filters/HealthCheckFilter.java
@@ -3,10 +3,12 @@ package com.facilio.filters;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HealthCheckFilter implements Filter {
 
     private static int status = HttpServletResponse.SC_SERVICE_UNAVAILABLE;
+    private static String message = "app_server_running 0";
 
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -15,6 +17,14 @@ public class HealthCheckFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setStatus(status);
+        PrintWriter writer = response.getWriter();
+        try {
+            writer.println(message);
+        } catch (Exception e){
+
+        } finally {
+            writer.close();
+        }
     }
 
     public void destroy() {
@@ -23,5 +33,8 @@ public class HealthCheckFilter implements Filter {
 
     public static void setStatus(int status) {
         HealthCheckFilter.status = status;
+        if(status == 200) {
+            message = "app_server_running 1";
+        }
     }
 }
