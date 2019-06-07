b33b59b27b683e5defaa49c2c6349efd1b4937e9
Simran <gowtham341@gmail.com>
2019-06-07 12:02:43 +0530

    Attendance fix

--- src/main/java/com/facilio/bmsconsole/commands/AddAttendanceCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/AddAttendanceCommand.java
@@ -83,6 +83,7 @@ public class AddAttendanceCommand implements Command {
 					throw new IllegalArgumentException("The User has already checked out");
 				}
 				att.setCheckOutTime(attendanceTransaction.getTransactionTime());
+				att.setLastCheckInTime(-99);
 				long workhrs = att.getCheckOutTime() - att.getLastCheckInTime();
 				if (att.getWorkingHours() > 0) {
 					workhrs += att.getWorkingHours();
@@ -122,6 +123,7 @@ public class AddAttendanceCommand implements Command {
 		}
 		if (attendanceTransaction.getTransactionTypeEnum() == TransactionType.CHECKOUT) {
 			att.setCheckOutTime(attendanceTransaction.getTransactionTime());
+			att.setLastCheckInTime(-99);
 		}
 		UpdateRecordBuilder<AttendanceContext> updateBuilder = new UpdateRecordBuilder<AttendanceContext>()
 				.module(module).fields(fields).andCondition(CriteriaAPI.getIdCondition(att.getId(), module));
