743c7e7d75a2ac1e26e27cad2bee21b8fd3c9b29
Prasanth <prasanth.iot@gmail.com>
2019-06-18 15:29:57 +0530

    portal update fix

--- src/main/java/com/facilio/bmsconsole/commands/HandlePortalInfoCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/HandlePortalInfoCommand.java
@@ -3,6 +3,7 @@ package com.facilio.bmsconsole.commands;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.db.builder.GenericUpdateRecordBuilder;
 import com.facilio.db.criteria.CriteriaAPI;
+import com.facilio.db.criteria.operators.NumberOperators;
 import com.facilio.modules.FacilioModule;
 import com.facilio.modules.FieldFactory;
 import com.facilio.modules.ModuleFactory;
@@ -18,12 +19,21 @@ public class HandlePortalInfoCommand implements Command{
 	@Override
 	public boolean execute(Context context) throws Exception {
 		Map<String, Object> servicePortalMap  = (Map<String, Object>) context.get(FacilioConstants.ContextNames.PORTALINFO);
+
+		Long portalId = (Long) servicePortalMap.get("portalId");
+		if (portalId == null) {
+			throw new IllegalArgumentException("Invalid portal id during updation of portal settings");
+		}
+
 		FacilioModule module = ModuleFactory.getServicePortalModule();
 		
 		List<FacilioField> fields = FieldFactory.getServicePortalFields();
+		FacilioField portalIdField = FieldFactory.getAsMap(fields).get("portalId");
 		GenericUpdateRecordBuilder builder = new GenericUpdateRecordBuilder()
 												.table(module.getTableName())
-												.fields(fields);
+												.fields(fields)
+												.andCondition(CriteriaAPI.getCondition(portalIdField, portalId.toString(), NumberOperators.EQUALS))
+												;
 //												.andCondition(CriteriaAPI.getCurrentOrgIdCondition(module));
 		builder.update(servicePortalMap);
 		
