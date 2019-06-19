2228d89826e665e0d2393a5a50138a5deaa31d52
shaan <shaan@facilio.com>
2019-06-20 00:42:30 +0530

    Removing underscore check in filter

--- src/main/java/com/facilio/bmsconsole/commands/GenerateCriteriaFromFilterCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GenerateCriteriaFromFilterCommand.java
@@ -1,5 +1,17 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.chain.Command;
+import org.apache.commons.chain.Context;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import com.facilio.beans.ModuleBean;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.db.criteria.Condition;
@@ -10,17 +22,6 @@ import com.facilio.db.criteria.operators.Operator;
 import com.facilio.db.criteria.operators.PickListOperators;
 import com.facilio.fw.BeanFactory;
 import com.facilio.modules.fields.FacilioField;
-import org.apache.commons.chain.Command;
-import org.apache.commons.chain.Context;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
 public class GenerateCriteriaFromFilterCommand implements Command {
 
@@ -132,7 +133,8 @@ public class GenerateCriteriaFromFilterCommand implements Command {
 					else {
 						isFirst = false;
 					}
-					if (obj.indexOf("_") != -1 && !obj.equals(FacilioConstants.Criteria.LOGGED_IN_USER) && !obj.equals(FacilioConstants.Criteria.LOGGED_IN_USER_GROUP)) {
+					values.append(obj.trim());
+					/*if (obj.indexOf("_") != -1 && !obj.equals(FacilioConstants.Criteria.LOGGED_IN_USER) && !obj.equals(FacilioConstants.Criteria.LOGGED_IN_USER_GROUP)) {
 						try {
 							String filterValue = obj.split("_")[0];
 							values.append(filterValue);
@@ -143,7 +145,7 @@ public class GenerateCriteriaFromFilterCommand implements Command {
 					}
 					else {
 						values.append(obj.trim());
-					}
+					}*/
 				}
 				String valuesString = values.toString();
 				if (condition.getOperator() instanceof FieldOperator) {
