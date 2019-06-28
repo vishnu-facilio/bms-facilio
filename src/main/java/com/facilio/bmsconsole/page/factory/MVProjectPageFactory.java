50c05f82c9f5224fa4b18f46b983d04eb840b076
shaan <shaan@facilio.com>
2019-06-28 17:46:33 +0530

    mv project page changes

--- src/main/java/com/facilio/bmsconsole/page/factory/MVProjectPageFactory.java
+++ src/main/java/com/facilio/bmsconsole/page/factory/MVProjectPageFactory.java
@@ -34,8 +34,7 @@ public class MVProjectPageFactory {
 		Section tab2Sec1 = page.new Section();
 		tab2.addSection(tab2Sec1);
 		
-		addBaselineEquationWidget(tab2Sec1, false);
-		addBaselineEquationWidget(tab2Sec1, true);
+		addBaselineEquationWidget(tab2Sec1);
 		addBaselineEquationListWidget(tab2Sec1);
 		addAdjustmentWidget(tab2Sec1);
 		
@@ -52,27 +51,27 @@ public class MVProjectPageFactory {
 	
 	private static void addPrimaryDetailsWidget(Section section) {
 		PageWidget pageWidget = new PageWidget(WidgetType.MV_DETAILS_WIDGET);
-		pageWidget.addToLayoutParams(section, 24, 6);
+		pageWidget.addToLayoutParams(section, 24, 5);
 		section.addWidget(pageWidget);
 	}
 	
 	private static void addSavingsWidget(Section section) {
 		PageWidget cardWidget = new PageWidget(WidgetType.CARD);
-		cardWidget.addToLayoutParams(section, 10, 5);
+		cardWidget.addToLayoutParams(section, 9, 4);
 		cardWidget.addToWidgetParams("type", CardType.MV_SAVINGS.getName());
 		section.addWidget(cardWidget);
 	}
 	
 	private static void addEnergyWidget(Section section) {
 		PageWidget cardWidget = new PageWidget(WidgetType.CARD);
-		cardWidget.addToLayoutParams(section, 10, 5);
+		cardWidget.addToLayoutParams(section, 9, 4);
 		cardWidget.addToWidgetParams("type", CardType.MV_ENERGY.getName());
 		section.addWidget(cardWidget);
 	}
 	
 	private static void addCarbonWidget(Section section) {
 		PageWidget cardWidget = new PageWidget(WidgetType.CARD);
-		cardWidget.addToLayoutParams(section, 4, 5);
+		cardWidget.addToLayoutParams(section, 6, 4);
 		cardWidget.addToWidgetParams("type", CardType.CARBON_EMISSION.getName());
 		section.addWidget(cardWidget);
 	}
@@ -98,11 +97,10 @@ public class MVProjectPageFactory {
 		section.addWidget(cardWidget);
 	}
 	
-	private static void addBaselineEquationWidget(Section section, boolean isPostRetrofit) {
-		PageWidget cardWidget = new PageWidget(WidgetType.CARD, isPostRetrofit ? "baselineEquation" : "postRetrofitEquation");
-		cardWidget.addToLayoutParams(section, 12, 5);
+	private static void addBaselineEquationWidget(Section section) {
+		PageWidget cardWidget = new PageWidget(WidgetType.CARD,  "baselineEquation");
+		cardWidget.addToLayoutParams(section, 24, 5);
 		cardWidget.addToWidgetParams("type", CardType.BASELINE_EQUATION.getName());
-		cardWidget.addToWidgetParams("isPostRetrofit", isPostRetrofit);
 		section.addWidget(cardWidget);
 	}
 	
