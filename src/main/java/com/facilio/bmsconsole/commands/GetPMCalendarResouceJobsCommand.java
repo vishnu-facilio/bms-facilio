b78ba4298ec5d38a3243e2901fc859b25a8d381c
shaan <shaan@facilio.com>
2019-07-02 02:41:24 +0530

    adding more fields in pm job api and refactoring

--- src/main/java/com/facilio/bmsconsole/commands/GetPMCalendarResouceJobsCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetPMCalendarResouceJobsCommand.java
@@ -41,39 +41,30 @@ public class GetPMCalendarResouceJobsCommand implements Command {
 	private static final String PLANNED = "Planned";
 	private static final String ACTUAL = "Actual";
 	
+	private static final String PLANNED_FIELD = "createdTime";
+	private static final String ACTUAL_FIELD = "actualWorkStart";
+	
 	
 	Map<Long, String> assetIdVsName;
-	String plannedField = null;
-	String actualField = null;
 	
+	// TODO get from planner settings
+	boolean showAssetCategory = true;
+	boolean showFrequency = true;
 	boolean showTimeMetric = true;
+	
+	int rowDefaultSpan;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public boolean execute(Context context) throws Exception {
 		
-		// TODO get from planner settings
-		boolean showAssetCategory = true;
-		boolean showFrequency = true;
-
-
 		List<List<Map<String, Object>>> titles = new ArrayList<>();
 		List<List<Map<String, Object>>> datas = new ArrayList<>();
 		
 		// header name should be fieldName
-		List<Map<String, Object>> headers = new ArrayList<>();
-		if (showAssetCategory) {
-			headers.add(Collections.singletonMap("name", CATEGORY_NAME));
-		}
-		headers.add(Collections.singletonMap("name", RESOURCE_NAME));
-		if (showFrequency) {
-			headers.add(Collections.singletonMap("name", FREQUENCY));
-		}
-		if (showTimeMetric) {
-			headers.add(Collections.singletonMap("name", TIME_METRIC));
-			plannedField = "createdTime";
-			actualField = "actualWorkStart";
-		}
+		List<Map<String, Object>> headers = getHeaders();
+		
+		rowDefaultSpan = showTimeMetric ? 2 : 1;
 
 		long siteId = (long) context.get(ContextNames.SITE_ID);
 		
@@ -130,23 +121,23 @@ public class GetPMCalendarResouceJobsCommand implements Command {
 		FacilioField triggerField = woFieldMap.get("trigger");
 		FacilioField resourceField = woFieldMap.get("resource");
 
-		StringBuilder orderBy = new StringBuilder();
-		if (showAssetCategory) {
-			orderBy.append(assetCategoryFieldMap.get("name").getCompleteColumnName()).append(",");
-		}
-		orderBy.append(resourceNameField.getCompleteColumnName());
-		if (showFrequency) {
-			orderBy.append(",").append(pmTriggerFieldMap.get("frequency").getCompleteColumnName());
-		// .append("FIELD(").append(pmTriggerFieldMap.get("frequency").getCompleteColumnName()).append(str).append(")")
-		}
 
-		StringBuilder groupBy = new StringBuilder();
-		groupBy.append(assetIdField.getCompleteColumnName());
+		SelectRecordsBuilder<ModuleBaseWithCustomFields> commonBuilder = new SelectRecordsBuilder<ModuleBaseWithCustomFields>()
+				.module(woModule)
+				.andCondition(CriteriaAPI.getCondition(woFieldMap.get("pm"), CommonOperators.IS_NOT_EMPTY))
+				.andCondition(CriteriaAPI.getCondition(FieldFactory.getSiteIdField(woModule), String.valueOf(siteId) , NumberOperators.EQUALS))
+				;
+		if (dateRange != null) {
+			commonBuilder.andCondition(CriteriaAPI.getCondition(woFieldMap.get("createdTime"), dateRange.toString(), DateOperators.BETWEEN));
+		}
+		else {
+			commonBuilder.andCondition(CriteriaAPI.getCondition(woFieldMap.get("createdTime"), operator));
+		}
 		if (showFrequency) {
-			groupBy.append(",").append(pmTriggerFieldMap.get("frequency").getColumnName());
+			commonBuilder.innerJoin(pmTriggerTable).on(triggerField.getCompleteColumnName() + "=" + pmTriggerTable + ".ID");
 		}
 		
-
+		
 		List<FacilioField> selectFields = new ArrayList<>();
 		selectFields.add(resourceNameField);
 		selectFields.add(assetIdField);
@@ -157,44 +148,42 @@ public class GetPMCalendarResouceJobsCommand implements Command {
 		if (showFrequency) {
 			selectFields.add(pmTriggerFieldMap.get("frequency"));
 		}
+		
+		StringBuilder orderBy = new StringBuilder();
+		if (showAssetCategory) {
+			orderBy.append(assetCategoryFieldMap.get("name").getCompleteColumnName()).append(",");
+		}
+		orderBy.append(resourceNameField.getCompleteColumnName());
+		if (showFrequency) {
+			orderBy.append(",").append(pmTriggerFieldMap.get("frequency").getCompleteColumnName());
+		}
 
-		SelectRecordsBuilder<ModuleBaseWithCustomFields> builder = new SelectRecordsBuilder<ModuleBaseWithCustomFields>()
+		StringBuilder groupBy = new StringBuilder();
+		groupBy.append(assetIdField.getCompleteColumnName());
+		if (showFrequency) {
+			groupBy.append(",").append(pmTriggerFieldMap.get("frequency").getColumnName());
+		}
+		
+		SelectRecordsBuilder<ModuleBaseWithCustomFields> groupBuilder = new SelectRecordsBuilder<ModuleBaseWithCustomFields>(commonBuilder)
 				.select(selectFields)
-				.module(woModule)
 				.innerJoin(assetTable).on(resourceField.getCompleteColumnName() + "=" + assetTable + ".ID")
 				.innerJoin(resourceTable).on(assetIdField.getCompleteColumnName() + "=" + resourceTable + ".ID")
-				.andCondition(CriteriaAPI.getCondition(woFieldMap.get("pm"), CommonOperators.IS_NOT_EMPTY))
-				.andCondition(CriteriaAPI.getCondition(FieldFactory.getSiteIdField(woModule), String.valueOf(siteId) , NumberOperators.EQUALS))
 				.groupBy(groupBy.toString())
 				.orderBy(orderBy.toString())
 				;
-		if (dateRange != null) {
-			builder.andCondition(CriteriaAPI.getCondition(woFieldMap.get("createdTime"), dateRange.toString(), DateOperators.BETWEEN));
-		}
-		else {
-			builder.andCondition(CriteriaAPI.getCondition(woFieldMap.get("createdTime"), operator));
-		}
 		
 		if (showAssetCategory) {
-			builder.innerJoin(assetCategoryTable).on(assetFieldMap.get("category").getCompleteColumnName() + "=" + assetCategoryTable + ".ID");
+			groupBuilder.innerJoin(assetCategoryTable).on(assetFieldMap.get("category").getCompleteColumnName() + "=" + assetCategoryTable + ".ID");
 		}
-		if (showFrequency) {
-			builder.innerJoin(pmTriggerTable).on(triggerField.getCompleteColumnName() + "=" + pmTriggerTable + ".ID");
-		}
-
-		List<Map<String, Object>> props = builder.getAsProps();
+		
+		List<Map<String, Object>> props = groupBuilder.getAsProps();
 		
 		List<Long> assetIds = null;
-		
-//		Map<String, List<Map<String, Object>>> resourceTree = new HashMap<>();
-
-		
 		if (CollectionUtils.isNotEmpty(props)) {
 			assetIds = new ArrayList<>();
 			assetIdVsName = new HashMap<>();
 			
 			List<Map<String, Object>> prevHeaderValues = new ArrayList<>();
-			int rowDefaultSpan = showTimeMetric ? 2 : 1;
 			
 			for(Map<String, Object> prop: props) {
 				
@@ -202,83 +191,26 @@ public class GetPMCalendarResouceJobsCommand implements Command {
 				assetIds.add(assetId);
 				assetIdVsName.put(assetId, (String) prop.get(resourceNameField.getName()));
 				
-				List<Map<String, Object>> row = new ArrayList<>();
-				titles.add(row);
-				
-				boolean parentValueChanged = false;
-				
-				for(int i = 0, size = headers.size(); i < size; i++) {
-					Map<String, Object> header = headers.get(i);
-					
-					String name = (String) header.get("name");
-					String value;
-					if (name.equals(FREQUENCY)) {
-						FacilioFrequency frequency = FacilioFrequency.valueOf((int) prop.get(name));
-						value = frequency.getName();
-					}
-					else {
-						value = String.valueOf(prop.get(name));
-					}
-					if (name.equals(TIME_METRIC)) {
-						
-						Map<String, Object> plannedObj = new HashMap<>();
-						plannedObj.put("name", PLANNED);
-						plannedObj.put("rowSpan", 1);
-						plannedObj.put("count", prop.get(countField.getName()));
-						row.add(plannedObj);
-						
-						List<Map<String, Object>> actualRow = new ArrayList<>();
-						Map<String, Object> actualObj = new HashMap<>();
-						actualObj.put("name", ACTUAL);
-						actualObj.put("rowSpan", 1);
-						actualObj.put("count", prop.get(countField.getName()));
-						actualRow.add(actualObj);
-						titles.add(actualRow);
-						continue;
-					}
-					
-					
-					Map<String, Object> prevHeader;
-					if (prevHeaderValues.size() < (i+1) ) {
-						prevHeader = new HashMap<>();
-						prevHeaderValues.add(prevHeader);
-					}
-					else {
-						prevHeader = prevHeaderValues.get(i);
-					}
-					
-					String prevValue = (String) prevHeader.getOrDefault("name", "");
-					if (!prevValue.equals(value) || parentValueChanged) {
-						prevHeader = new HashMap<>();
-						prevHeader.put("name", value);
-						prevHeader.put("rowSpan", rowDefaultSpan);
-						row.add(prevHeader);
-						prevHeaderValues.add(i,  prevHeader);
-						parentValueChanged = true;
-					}
-					else {
-						int rowSpan = (int) prevHeader.get("rowSpan");
-						prevHeader.put("rowSpan", rowSpan + rowDefaultSpan);
-					}
-					if (i + 1 == size) {
-						prevHeader.put("count", prop.get(countField.getName()));
-					}
-				}
+				addTitles(titles, headers, prop, prevHeaderValues, countField);
 			}
+			
 			
 			selectFields = new ArrayList<>();
 			selectFields.add(woFieldMap.get("subject"));
 			selectFields.add(woFieldMap.get("createdTime"));
 			selectFields.add(woFieldMap.get("assignedTo"));
 			selectFields.add(woFieldMap.get("assignmentGroup"));
+			selectFields.add(woFieldMap.get("priority"));
+			selectFields.add(woFieldMap.get("category"));
+			selectFields.add(woFieldMap.get("type"));
 			selectFields.add(resourceField);
 			selectFields.add(FieldFactory.getIdField(woModule));
 			if (showTimeMetric) {
-				if (!actualField.equals("createdTime")) {
-					selectFields.add(woFieldMap.get(actualField));
+				if (!ACTUAL_FIELD.equals("createdTime")) {
+					selectFields.add(woFieldMap.get(ACTUAL_FIELD));
 				}
-				if (!plannedField.equals("createdTime")) {
-					selectFields.add(woFieldMap.get(plannedField));
+				if (!PLANNED_FIELD.equals("createdTime")) {
+					selectFields.add(woFieldMap.get(PLANNED_FIELD));
 				}
 			}
 			if (showFrequency) {
@@ -295,29 +227,12 @@ public class GetPMCalendarResouceJobsCommand implements Command {
 //				.append(StringUtils.join(triggerIds, ",")).append(")");
 			}
 			
-			builder = new SelectRecordsBuilder<ModuleBaseWithCustomFields>()
-					.module(woModule)
+			SelectRecordsBuilder<ModuleBaseWithCustomFields> dataBuilder = new SelectRecordsBuilder<ModuleBaseWithCustomFields>(commonBuilder)
 					.select(selectFields)
-					.andCondition(CriteriaAPI.getCondition(woFieldMap.get("pm"), CommonOperators.IS_NOT_EMPTY))
-					.andCondition(CriteriaAPI.getCondition(FieldFactory.getSiteIdField(woModule), String.valueOf(siteId) , NumberOperators.EQUALS))
-					.andCondition(CriteriaAPI.getCondition(resourceField, assetIds, NumberOperators.EQUALS))
 					.orderBy(orderBy.toString())
 					;
 			
-			if (dateRange != null) {
-				builder.andCondition(CriteriaAPI.getCondition(woFieldMap.get("createdTime"), dateRange.toString(), DateOperators.BETWEEN));
-			}
-			else {
-				builder.andCondition(CriteriaAPI.getCondition(woFieldMap.get("createdTime"), operator));
-			}
-			
-			if (showFrequency) {
-//				builder.andCondition(CriteriaAPI.getCondition(triggerField, triggerIds, NumberOperators.EQUALS));
-				
-				builder.innerJoin(pmTriggerTable).on(triggerField.getCompleteColumnName() + "=" + pmTriggerTable + ".ID");
-			}
-			
-			props = builder.getAsProps();
+			props = dataBuilder.getAsProps();
 			
 			if (CollectionUtils.isNotEmpty(props)) {
 				List<Map<String, Object>> row;
@@ -336,14 +251,14 @@ public class GetPMCalendarResouceJobsCommand implements Command {
 						             .collect(Collectors.toList());
 					
 					if (showTimeMetric) {
-						addData(row, filteredList, plannedField, count, totalCount, false);
+						addData(row, filteredList, PLANNED_FIELD, count, totalCount, false);
 						sort(row);
 						
 						row = new ArrayList<>();
 						datas.add(row);
 						sort(row);
 						
-						addData(row, new ArrayList<>(filteredList), actualField, count, totalCount, true);
+						addData(row, new ArrayList<>(filteredList), ACTUAL_FIELD, count, totalCount, true);
 						sort(row);
 						i++;
 					}
@@ -367,6 +282,71 @@ public class GetPMCalendarResouceJobsCommand implements Command {
 		return false;
 	}
 	
+	private void addTitles(List<List<Map<String, Object>>> titles, List<Map<String, Object>> headers, Map<String, Object> prop, List<Map<String, Object>> prevHeaderValues, FacilioField countField) {
+		List<Map<String, Object>> row = new ArrayList<>();
+		titles.add(row);
+		
+		boolean parentValueChanged = false;
+		
+		for(int i = 0, size = headers.size(); i < size; i++) {
+			Map<String, Object> header = headers.get(i);
+			
+			String name = (String) header.get("name");
+			String value;
+			if (name.equals(FREQUENCY)) {
+				FacilioFrequency frequency = FacilioFrequency.valueOf((int) prop.get(name));
+				value = frequency.getName();
+			}
+			else {
+				value = String.valueOf(prop.get(name));
+			}
+			if (name.equals(TIME_METRIC)) {
+				
+				Map<String, Object> plannedObj = new HashMap<>();
+				plannedObj.put("name", PLANNED);
+				plannedObj.put("rowSpan", 1);
+				plannedObj.put("count", prop.get(countField.getName()));
+				row.add(plannedObj);
+				
+				List<Map<String, Object>> actualRow = new ArrayList<>();
+				Map<String, Object> actualObj = new HashMap<>();
+				actualObj.put("name", ACTUAL);
+				actualObj.put("rowSpan", 1);
+				actualObj.put("count", prop.get(countField.getName()));
+				actualRow.add(actualObj);
+				titles.add(actualRow);
+				continue;
+			}
+			
+			
+			Map<String, Object> prevHeader;
+			if (prevHeaderValues.size() < (i+1) ) {
+				prevHeader = new HashMap<>();
+				prevHeaderValues.add(prevHeader);
+			}
+			else {
+				prevHeader = prevHeaderValues.get(i);
+			}
+			
+			String prevValue = (String) prevHeader.getOrDefault("name", "");
+			if (!prevValue.equals(value) || parentValueChanged) {
+				prevHeader = new HashMap<>();
+				prevHeader.put("name", value);
+				prevHeader.put("rowSpan", rowDefaultSpan);
+				row.add(prevHeader);
+				prevHeaderValues.add(i,  prevHeader);
+				parentValueChanged = true;
+			}
+			else {
+				int rowSpan = (int) prevHeader.get("rowSpan");
+				prevHeader.put("rowSpan", rowSpan + rowDefaultSpan);
+			}
+			if (i + 1 == size) {
+				prevHeader.put("count", prop.get(countField.getName()));
+			}
+		}
+	}
+	
 	private void addData(List<Map<String, Object>> row, List<Map<String, Object>> props, String field, long count, int totalCount, boolean isClone) {
 		for(int j = 0; j < props.size(); j++) {
 			Map<String, Object> prop = props.get(j);
@@ -375,10 +355,10 @@ public class GetPMCalendarResouceJobsCommand implements Command {
 				prop.put("asset", assetIdVsName.get(resource.get("id")));
 			}
 			if (showTimeMetric) {
-				if (field.equals(plannedField)) {
+				if (field.equals(PLANNED_FIELD)) {
 					prop.put("time", PLANNED);
 				}
-				if (field.equals(actualField)) {
+				if (field.equals(ACTUAL_FIELD)) {
 					prop.put("time", ACTUAL);
 				}
 			}
@@ -391,6 +371,21 @@ public class GetPMCalendarResouceJobsCommand implements Command {
 			}
 			
 		}
+	}
+	
+	private List<Map<String, Object>> getHeaders() {
+		List<Map<String, Object>> headers = new ArrayList<>();
+		if (showAssetCategory) {
+			headers.add(Collections.singletonMap("name", CATEGORY_NAME));
+		}
+		headers.add(Collections.singletonMap("name", RESOURCE_NAME));
+		if (showFrequency) {
+			headers.add(Collections.singletonMap("name", FREQUENCY));
+		}
+		if (showTimeMetric) {
+			headers.add(Collections.singletonMap("name", TIME_METRIC));
+		}
+		return headers;
 	}
 	
 	private void sort(List<Map<String, Object>> props) {
