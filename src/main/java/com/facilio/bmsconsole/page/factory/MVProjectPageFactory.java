edff284aed28aab686bd5661621e020dec7cb24b
shaan <shaan@facilio.com>
2019-07-02 08:36:12 +0530

    reducing the height of chart in mv summary

--- src/main/java/com/facilio/bmsconsole/page/factory/MVProjectPageFactory.java
+++ src/main/java/com/facilio/bmsconsole/page/factory/MVProjectPageFactory.java
@@ -85,21 +85,21 @@ public class MVProjectPageFactory {
 	
 	private static void addBaselineVsActualWidget(Section section) {
 		PageWidget cardWidget = new PageWidget(WidgetType.CHART, "baselineVsActual");
-		cardWidget.addToLayoutParams(section, 24, 14);
+		cardWidget.addToLayoutParams(section, 24, 13);
 		cardWidget.addToWidgetParams("type", "baselineVsActual");
 		section.addWidget(cardWidget);
 	}
 	
 	private static void addCostTrendWidget(Section section) {
 		PageWidget cardWidget = new PageWidget(WidgetType.CHART, "costTrend");
-		cardWidget.addToLayoutParams(section, 24, 14);
+		cardWidget.addToLayoutParams(section, 24, 13);
 		cardWidget.addToWidgetParams("type", "costTrend");
 		section.addWidget(cardWidget);
 	}
 	
 	private static void addCumulativeSavingsWidget(Section section) {
 		PageWidget cardWidget = new PageWidget(WidgetType.CHART, "cumulativeSavings");
-		cardWidget.addToLayoutParams(section, 24, 14);
+		cardWidget.addToLayoutParams(section, 24, 13);
 		cardWidget.addToWidgetParams("type", "cumulativeSavings");
 		section.addWidget(cardWidget);
 	}
