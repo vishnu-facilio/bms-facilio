bff8b1ff3298c33ce52cf43270be494ba32c7e1b
shaan <shaan@facilio.com>
2019-07-14 13:40:23 +0530

    approval backward compatibility

--- src/main/java/com/facilio/bmsconsole/commands/BackwardCompatibleStateFlowUpdateCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/BackwardCompatibleStateFlowUpdateCommand.java
@@ -1,17 +1,21 @@
 package com.facilio.bmsconsole.commands;
 
-import com.facilio.beans.ModuleBean;
-import com.facilio.bmsconsole.context.WorkOrderContext;
-import com.facilio.bmsconsole.util.StateFlowRulesAPI;
-import com.facilio.constants.FacilioConstants;
-import com.facilio.fw.BeanFactory;
-import com.facilio.modules.FacilioStatus;
-import com.facilio.modules.fields.FacilioField;
+import java.util.List;
+
 import org.apache.commons.chain.Command;
 import org.apache.commons.chain.Context;
 import org.apache.commons.collections4.CollectionUtils;
 
-import java.util.List;
+import com.facilio.beans.ModuleBean;
+import com.facilio.bmsconsole.context.WorkOrderContext;
+import com.facilio.bmsconsole.util.StateFlowRulesAPI;
+import com.facilio.bmsconsole.util.TicketAPI;
+import com.facilio.bmsconsole.workflow.rule.ApprovalState;
+import com.facilio.constants.FacilioConstants;
+import com.facilio.fw.BeanFactory;
+import com.facilio.modules.FacilioModule;
+import com.facilio.modules.FacilioStatus;
+import com.facilio.modules.fields.FacilioField;
 
 // TODO to be removed once supported by every client
 public class BackwardCompatibleStateFlowUpdateCommand implements Command {
@@ -27,9 +31,20 @@ public class BackwardCompatibleStateFlowUpdateCommand implements Command {
 			
 			// check whether moduleState is found
 			if (field != null) {
+				FacilioModule module = modBean.getModule(moduleName);
 				FacilioStatus status = workOrder.getStatus();
+				if (status == null && workOrder.getApprovalStateEnum() != null) {
+					ApprovalState approveState = workOrder.getApprovalStateEnum();
+					if (approveState == ApprovalState.APPROVED) {
+						status = TicketAPI.getStatus(module, "Submitted");
+					}
+					else if (approveState == ApprovalState.REJECTED) {
+						status = TicketAPI.getStatus(module, "Rejected");
+					}
+					workOrder.setStatus(status);
+				}
 				for (WorkOrderContext wo : oldWos) {
-					StateFlowRulesAPI.updateState(wo, modBean.getModule(moduleName), status, false, context);
+					StateFlowRulesAPI.updateState(wo, module, status, false, context);
 				}
 			}
 		}
