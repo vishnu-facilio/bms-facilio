0b8ac7a29878087a03cb2bb1580199272f1f849c
Manthosh <manthosh@facilio.com>
2019-07-17 16:01:01 +0530

    Moving runtime log handling to a common file

--- src/main/java/com/facilio/logging/FacilioGelfAppender.java
+++ src/main/java/com/facilio/logging/FacilioGelfAppender.java
@@ -26,61 +26,12 @@ import java.util.Map;
 public class FacilioGelfAppender extends GelfAppender {
 
     private static final String DEFAULT_ORG_USER_ID = "-1";
-    private static final File ROOT_FILE = new File("/");
-    private static long lastFreeSpaceCheckedTime = System.currentTimeMillis();
-    private static final long FREE_SPACE_THRESHOLD = 10000000000L;
-    private static long freeSpace = ROOT_FILE.getFreeSpace();
 
     public void append(LoggingEvent event) {
-        if((lastFreeSpaceCheckedTime + 300_000L) < System.currentTimeMillis()) {
-            lastFreeSpaceCheckedTime = System.currentTimeMillis();
-            freeSpace = ROOT_FILE.getFreeSpace();
-        }
-        if(!(
-                (   (event.getLevel().toInt() > Level.DEBUG_INT)
-                        || (AccountUtil.getCurrentAccount() != null && (event.getLevel().toInt() >= AccountUtil.getCurrentAccount().getLevel().toInt()))
-                )
-                        && (freeSpace > FREE_SPACE_THRESHOLD)
-        )) {
+        if(!FacilioLogHandler.isLoggable(event)) {
             return;
         }
-
-        Organization org = AccountUtil.getCurrentOrg();
-        if(org != null) {
-            event.setProperty("orgId", String.valueOf(org.getOrgId()));
-        } else {
-            event.setProperty("orgId", DEFAULT_ORG_USER_ID);
-        }
-        User user = AccountUtil.getCurrentUser();
-        if (user != null) {
-            event.setProperty("userId", String.valueOf(user.getOuid()));
-        } else {
-            event.setProperty("userId", DEFAULT_ORG_USER_ID);
-        }
-
-        if( AccountUtil.getCurrentAccount() != null && AccountUtil.getCurrentAccount().getRequestUri() != null) {
-            event.setProperty("req_uri", AccountUtil.getCurrentAccount().getRequestUri());
-        } else {
-            event.setProperty("req_uri", "-");
-        }
-
-        try {
-            ThrowableInformation information = event.getThrowableInformation();
-            if (information != null) {
-                Throwable throwable = information.getThrowable();
-                String exceptionType = throwable.getClass().getName();
-                event.setProperty("exception", exceptionType);
-                if( AccountUtil.getCurrentAccount() != null && AccountUtil.getCurrentAccount().getRequestParams() != null) {
-                    event.setProperty("req_params", AccountUtil.getCurrentAccount().getRequestParams());
-                }
-            } else {
-                event.setProperty("exception", "-");
-                event.setProperty("req_params", "-");
-            }
-        } catch (Exception e) {
-            event.setProperty("exception", "LogAppenderException");
-        }
-        super.append(event);
+        super.append(FacilioLogHandler.addEventProps(event));
     }
 
     public FacilioGelfAppender() {
--- src/main/java/com/facilio/logging/FacilioLogHandler.java
+++ src/main/java/com/facilio/logging/FacilioLogHandler.java
@@ -1,9 +1,15 @@
 package com.facilio.logging;
 
+import com.facilio.accounts.dto.Organization;
+import com.facilio.accounts.dto.User;
+import com.facilio.accounts.util.AccountUtil;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Priority;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -41,5 +47,61 @@ public class FacilioLogHandler extends Handler {
 
     public void close() throws SecurityException {
 
+    }
+
+    private static final File ROOT_FILE = new File("/");
+    private static long lastFreeSpaceCheckedTime = System.currentTimeMillis();
+    private static final long FREE_SPACE_THRESHOLD = 10000000000L;
+    private static long freeSpace = ROOT_FILE.getFreeSpace();
+    private static final String DEFAULT_ORG_USER_ID = "-1";
+    public static boolean isLoggable(LoggingEvent event) {
+        if((lastFreeSpaceCheckedTime + 300_000L) < System.currentTimeMillis()) {
+            lastFreeSpaceCheckedTime = System.currentTimeMillis();
+            freeSpace = ROOT_FILE.getFreeSpace();
+        }
+
+        return (
+                (   (event.getLevel().toInt() > org.apache.log4j.Level.INFO_INT)
+                        && (AccountUtil.getCurrentAccount() == null || (event.getLevel().toInt() > AccountUtil.getCurrentAccount().getLevel().toInt()))
+                )
+                        && (freeSpace > FREE_SPACE_THRESHOLD)
+        );
+    }
+
+    public static LoggingEvent addEventProps(LoggingEvent event) {
+        Organization org = AccountUtil.getCurrentOrg();
+        if(org != null) {
+            event.setProperty("orgId", String.valueOf(org.getOrgId()));
+        } else {
+            event.setProperty("orgId", DEFAULT_ORG_USER_ID);
+        }
+        User user = AccountUtil.getCurrentUser();
+        if (user != null) {
+            event.setProperty("userId", String.valueOf(user.getOuid()));
+        } else {
+            event.setProperty("userId", DEFAULT_ORG_USER_ID);
+        }
+        if( AccountUtil.getCurrentAccount() != null && AccountUtil.getCurrentAccount().getRequestUri() != null) {
+            event.setProperty("req_uri", AccountUtil.getCurrentAccount().getRequestUri());
+        } else {
+            event.setProperty("req_uri", "-");
+        }
+        try {
+            ThrowableInformation information = event.getThrowableInformation();
+            if (information != null) {
+                Throwable throwable = information.getThrowable();
+                String exceptionType = throwable.getClass().getName();
+                event.setProperty("exception", exceptionType);
+                if( AccountUtil.getCurrentAccount() != null && AccountUtil.getCurrentAccount().getRequestParams() != null) {
+                    event.setProperty("req_params", AccountUtil.getCurrentAccount().getRequestParams());
+                }
+            } else {
+                event.setProperty("exception", "-");
+                event.setProperty("req_params", "-");
+            }
+        } catch (Exception e) {
+            event.setProperty("exception", "LogAppenderException");
+        }
+        return event;
     }
 }
