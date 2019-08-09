f474de13fcbe1542e8babe600dbd19cfeed02762
Vandhana Sridhar <vandhana@facilio.com>
2019-08-09 17:25:10 +0530

    adding status check

--- src/main/java/com/facilio/iam/accounts/impl/IAMUserBeanImpl.java
+++ src/main/java/com/facilio/iam/accounts/impl/IAMUserBeanImpl.java
@@ -500,6 +500,7 @@ public class IAMUserBeanImpl implements IAMUserBean {
 		selectBuilder.andCondition(CriteriaAPI.getCondition("Organizations.DELETED_TIME", "orgDeletedTime", "-1", NumberOperators.EQUALS));
 		selectBuilder.andCondition(CriteriaAPI.getCondition("Account_ORG_Users.DELETED_TIME", "orgUserDeletedTime", "-1", NumberOperators.EQUALS));
 		selectBuilder.andCondition(CriteriaAPI.getCondition("Account_ORG_Users.ISDEFAULT", "isDefault", "1", NumberOperators.EQUALS));
+		selectBuilder.andCondition(CriteriaAPI.getCondition("Account_ORG_Users.USER_STATUS", "userStatus", "1", NumberOperators.EQUALS));
 		
 		
 		List<Map<String, Object>> props = selectBuilder.get();
