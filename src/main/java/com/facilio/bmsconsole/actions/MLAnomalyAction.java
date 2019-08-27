29d33d64d09be6ba85882a7838ddba57c8320168
Benitta <benitta@facilio.com>
2019-08-27 20:41:08 +0530

    Rca insight api

--- src/main/java/com/facilio/bmsconsole/actions/MLAnomalyAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/MLAnomalyAction.java
@@ -4,8 +4,10 @@ import org.apache.commons.chain.Chain;
 
 import com.facilio.bmsconsole.commands.ReadOnlyChainFactory;
 import com.facilio.bmsconsole.context.MLContext;
+import com.facilio.bmsconsole.util.MLAPI;
 import com.facilio.chain.FacilioContext;
 import com.facilio.constants.FacilioConstants;
+import com.facilio.time.DateRange;
 
 public class MLAnomalyAction extends FacilioAction {
 
@@ -32,7 +34,7 @@ public class MLAnomalyAction extends FacilioAction {
 	
 	public String fetchSubMeterHierarchy() throws Exception {
 		FacilioContext context = new FacilioContext();
-		//MLAPI.getSubMeterDetails(mlAnomalyAlarmId);
+		MLAPI.getSubMeterDetails(mlAnomalyAlarmId);
 		return SUCCESS;
 	}
 	
@@ -43,5 +45,49 @@ public class MLAnomalyAction extends FacilioAction {
 		mlDetailsChain.execute(context);
 		setResult(FacilioConstants.ContextNames.ML_RCA_ALARMS, context.get(FacilioConstants.ContextNames.ML_RCA_ALARMS));
 		return SUCCESS;
+	}
+	
+	public String fetchInsight() throws Exception {
+		FacilioContext context = new FacilioContext();
+		context.put(FacilioConstants.ContextNames.ALARM_ID, alarmId);
+		context.put(FacilioConstants.ContextNames.DATE_RANGE, dateRange);
+		context.put(FacilioConstants.ContextNames.DATE_OPERATOR, dateOperator);
+		context.put(FacilioConstants.ContextNames.DATE_OPERATOR_VALUE, dateOperatorValue);
+		MLAPI.getAlarmInsight(context);
+		setResult(FacilioConstants.ContextNames.ALARM, MLAPI.getAlarmInsight(context));
+		return SUCCESS;
+	}
+	
+	private int dateOperator = -1;
+	public int getDateOperator() {
+		return dateOperator;
+	}
+	public void setDateOperator(int dateOperator) {
+		this.dateOperator = dateOperator;
+	}
+	
+	private String dateOperatorValue;
+	public String getDateOperatorValue() {
+		return dateOperatorValue;
+	}
+	public void setDateOperatorValue(String dateOperatorValue) {
+		this.dateOperatorValue = dateOperatorValue;
+	}
+	
+	private DateRange dateRange;
+	public DateRange getDateRange() {
+		return dateRange;
+	}
+	public void setDateRange(DateRange dateRange) {
+		this.dateRange = dateRange;
+	}
+	
+	private long alarmId = -1;
+	
+	public long getAlarmId() {
+		return alarmId;
+	}
+	public void setAlarmId(long alarmId) {
+		this.alarmId = alarmId;
 	}
 }
--- src/main/java/com/facilio/bmsconsole/util/MLAPI.java
+++ src/main/java/com/facilio/bmsconsole/util/MLAPI.java
@@ -1,8 +1,31 @@
 package com.facilio.bmsconsole.util;
 
-import com.facilio.bmsconsole.context.AlarmContext;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.chain.Context;
+import org.apache.commons.collections.CollectionUtils;
+
+import com.facilio.beans.ModuleBean;
+import com.facilio.bmsconsole.context.AlarmOccurrenceContext;
 import com.facilio.bmsconsole.context.MLContext;
-import com.facilio.bmsconsole.context.TicketContext.SourceType;
+import com.facilio.bmsconsole.context.ResourceContext;
+import com.facilio.constants.FacilioConstants;
+import com.facilio.constants.FacilioConstants.ContextNames;
+import com.facilio.db.criteria.CriteriaAPI;
+import com.facilio.db.criteria.operators.DateOperators;
+import com.facilio.db.criteria.operators.NumberOperators;
+import com.facilio.db.criteria.operators.Operator;
+import com.facilio.fw.BeanFactory;
+import com.facilio.modules.FacilioModule;
+import com.facilio.modules.FieldFactory;
+import com.facilio.modules.FieldType;
+import com.facilio.modules.SelectRecordsBuilder;
+import com.facilio.modules.fields.FacilioField;
+import com.facilio.time.DateRange;
 
 public class MLAPI {
 	public static MLContext getSubMeterDetails (long mlAnomalyAlarmId) throws Exception {
@@ -10,6 +33,82 @@ public class MLAPI {
 		
 		MLUtil.getMLContext(mlAnomalyAlarmId);
 		return null;
+		
+	}
+	
+	
+	public static List<Map<String, Object>> getAlarmInsight (Context context) throws Exception {
+		long alarmId = (long) context.get(ContextNames.ALARM_ID);
+		DateOperators operator = DateOperators.CURRENT_WEEK;
+		DateRange dateRange = (DateRange) context.get(FacilioConstants.ContextNames.DATE_RANGE);
+		if (dateRange == null) {
+			Integer dateOperatorInt = (Integer) context.get(FacilioConstants.ContextNames.DATE_OPERATOR);
+			if (dateOperatorInt != null && dateOperatorInt > -1) {
+				String dateOperatorValue = (String) context.get(FacilioConstants.ContextNames.DATE_OPERATOR_VALUE);
+				operator = (DateOperators) Operator.getOperator(dateOperatorInt);
+				dateRange = operator.getRange(dateOperatorValue);
+			}
+		}
+		
+		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+
+		FacilioModule baseAlarmModule = modBean.getModule(ContextNames.BASE_ALARM);
+		FacilioModule occurrenceModule = modBean.getModule(ContextNames.ALARM_OCCURRENCE);
+
+		List<FacilioField> baseAlarmFields = modBean.getAllFields(baseAlarmModule.getName());
+		List<FacilioField> occurrenceFields = modBean.getAllFields(occurrenceModule.getName());
+		Map<String, FacilioField> fieldMaps = FieldFactory.getAsMap(baseAlarmFields);
+		fieldMaps.putAll(FieldFactory.getAsMap(occurrenceFields));
+
+		List<FacilioField> selectFields = new ArrayList<>();
+		String clearedTimeFieldColumn = fieldMaps.get("clearedTime").getColumnName();
+		String createdTimeFieldColumn = fieldMaps.get("createdTime").getColumnName();
+		FacilioField resourceFieldColumn = fieldMaps.get("resource");
+
+		StringBuilder durationAggrColumn = new StringBuilder("SUM(COALESCE(")
+				.append(clearedTimeFieldColumn).append(",").append(System.currentTimeMillis()).append(") - ")
+				.append(createdTimeFieldColumn).append(")")
+				;
+		FacilioField durationField = FieldFactory.getField("duration", durationAggrColumn.toString(), FieldType.NUMBER);
+
+//		selectFields.add(ruleField);
+		selectFields.add(durationField);
+		selectFields.add(resourceFieldColumn);
+		selectFields.addAll(FieldFactory.getCountField(occurrenceModule));
+		FacilioField alarmField = fieldMaps.get("alarm");
+		SelectRecordsBuilder<AlarmOccurrenceContext> builder = new SelectRecordsBuilder<AlarmOccurrenceContext>()
+				.select(selectFields)
+				.beanClass(AlarmOccurrenceContext.class)
+				.module(baseAlarmModule)
+				.innerJoin(occurrenceModule.getTableName())
+				.on(occurrenceModule.getTableName() + ".ALARM_ID = " + baseAlarmModule.getTableName() + ".ID")
+				;
+		if (alarmId > 0) {
+			builder.andCondition(CriteriaAPI.getCondition(fieldMaps.get("alarm"), String.valueOf(alarmId), NumberOperators.EQUALS))
+			.groupBy(alarmField.getCompleteColumnName());
+		}
+
+		if (dateRange != null) {
+			builder.andCondition(CriteriaAPI.getCondition(fieldMaps.get("createdTime"), dateRange.toString(), DateOperators.BETWEEN));
+		}
+		else {
+			builder.andCondition(CriteriaAPI.getCondition(fieldMaps.get("createdTime"), operator));
+		}
+
+		List<Map<String, Object>> props = builder.getAsProps();
+		// backward-compatibility
+		if (CollectionUtils.isNotEmpty(props)) {
+			List<Long> resourcesId = props.stream().map(prop -> {
+				HashMap<Object,Object> hsp = (HashMap<Object, Object>) prop.get("resource");
+				return (long) hsp.get("id");
+				}).collect(Collectors.toList());
+			Map<Long, ResourceContext> resources = ResourceAPI.getResourceAsMapFromIds(resourcesId);
+			for (Map<String, Object> prop : props) {
+				HashMap<Object,Object> resourceId =  (HashMap<Object, Object>) prop.get("resource");
+				prop.put("subject", resources.get((long) resourceId.get("id")).getName());
+			}
+		}
+		return props;
 		
 	}
 }
