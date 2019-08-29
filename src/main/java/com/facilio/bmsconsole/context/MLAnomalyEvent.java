c6ff1299227b5f4fc54a3ab82504fe5d071dc432
javeed <javeed@192.168.1.148>
2019-08-29 19:52:07 +0530

    checking whether Anomaly type is set before clear event generation

--- src/main/java/com/facilio/bmsconsole/context/MLAnomalyEvent.java
+++ src/main/java/com/facilio/bmsconsole/context/MLAnomalyEvent.java
@@ -41,7 +41,7 @@ public class MLAnomalyEvent extends BaseEventContext{
     public BaseEventContext createAdditionClearEvent(AlarmOccurrenceContext alarmOccurrence) {
         if (alarmOccurrence instanceof MLAlarmOccurenceContext) {
         	MLAlarmOccurenceContext context =(MLAlarmOccurenceContext)alarmOccurrence;
-        	if(!context.getMLAnomalyTypeEnum().equals(mlAnomalyType))
+        	if(mlAnomalyType!=null && context!=null && !context.getMLAnomalyTypeEnum().equals(mlAnomalyType))
         	{
         		String message = "Anomaly Cleared";
 				MLAnomalyEvent event = new MLAnomalyEvent();
