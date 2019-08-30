651dbb1fed3ef07d802b109aaf32a35d6fe8bd1f
Benitta <benitta@facilio.com>
2019-08-30 14:02:20 +0530

    Fetch lookup resource

--- src/main/java/com/facilio/bmsconsole/commands/GetMLSummmaryDetail.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetMLSummmaryDetail.java
@@ -12,7 +12,12 @@ import com.facilio.db.criteria.CriteriaAPI;
 import com.facilio.db.criteria.operators.NumberOperators;
 import com.facilio.fw.BeanFactory;
 import com.facilio.modules.FacilioModule;
+import com.facilio.modules.FieldFactory;
+import com.facilio.modules.FieldType;
+import com.facilio.modules.ModuleFactory;
 import com.facilio.modules.SelectRecordsBuilder;
+import com.facilio.modules.fields.FacilioField;
+import com.facilio.modules.fields.LookupField;
 
 
 public class GetMLSummmaryDetail extends FacilioCommand {
@@ -23,8 +28,11 @@ public class GetMLSummmaryDetail extends FacilioCommand {
 		
 		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
 		FacilioModule module = modBean.getModule(FacilioConstants.ContextNames.ANOMALY_ALARM_OCCURRENCE);
+		LookupField resourceLookup = (LookupField) FieldFactory.getField("resource", "RESOURCE_ID", FieldType.LOOKUP);
+		resourceLookup.setLookupModule(ModuleFactory.getResourceModule());
 		SelectRecordsBuilder<MLAlarmOccurenceContext> builder = new SelectRecordsBuilder<MLAlarmOccurenceContext>().module(module)
 				.beanClass(MLAlarmOccurenceContext.class).select(modBean.getAllFields(module.getName()))
+				.fetchLookup(resourceLookup)
 				.andCondition(CriteriaAPI.getCondition("PARENTID", "parentID",String.valueOf(mlAnomalyId),  NumberOperators.EQUALS));
 		List<MLAlarmOccurenceContext> list = builder.get();
 		context.put(ContextNames.ML_RCA_ALARMS, list);
