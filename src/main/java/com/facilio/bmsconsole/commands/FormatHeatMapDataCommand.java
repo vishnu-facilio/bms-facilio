17a4b9aca98a99311a407c538bae61f4a3ef45e7
Ayshvarya <ayshvarya@facilio.com>
2019-09-05 16:11:14 +0530

    violated readings in heatmap command

--- src/main/java/com/facilio/bmsconsole/commands/FormatHeatMapDataCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/FormatHeatMapDataCommand.java
@@ -17,16 +17,30 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.apache.commons.chain.Context;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.math3.util.FastMath;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import com.facilio.bmsconsole.context.MarkedReadingContext;
+import com.facilio.accounts.util.AccountUtil;
+import com.facilio.bmsconsole.actions.*;
 import com.facilio.constants.FacilioConstants;
+import com.facilio.db.builder.GenericSelectRecordBuilder;
+import com.facilio.db.criteria.Condition;
+import com.facilio.db.criteria.Criteria;
+import com.facilio.db.criteria.CriteriaAPI;
 import com.facilio.db.criteria.operators.DateOperators;
+import com.facilio.db.criteria.operators.NumberOperators;
 import com.facilio.db.criteria.operators.Operator;
 import com.facilio.modules.FieldFactory;
 import com.facilio.modules.FieldType;
 import com.facilio.modules.FieldUtil;
+import com.facilio.modules.ModuleFactory;
+import com.facilio.modules.AggregateOperator.DateAggregateOperator;
+import com.facilio.modules.AggregateOperator.NumberAggregateOperator;
 import com.facilio.modules.fields.FacilioField;
+import com.facilio.report.context.ReadingAnalysisContext;
 import com.facilio.report.context.ReportContext;
 import com.facilio.report.context.ReportDataPointContext;
 import com.facilio.report.context.ReportFieldContext;
@@ -44,6 +58,7 @@ public class FormatHeatMapDataCommand extends FacilioCommand{
 		
 		JSONObject reportData = (JSONObject) context.get(FacilioConstants.ContextNames.REPORT_DATA);
 		ReportContext reportContext = (ReportContext) context.get(FacilioConstants.ContextNames.REPORT);
+ 
 		
 		if(reportContext.getAnalyticsType() == 3 && reportContext.getType() == 1) {
 			Collection<Map<String, Object>> data = (Collection<Map<String, Object>>) reportData.get(FacilioConstants.ContextNames.DATA_KEY);
@@ -52,6 +67,7 @@ public class FormatHeatMapDataCommand extends FacilioCommand{
 			SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH");
 			Long startTime=range.getStartTime();
 			Long endTime=range.getEndTime();
+			List<Map<String, Object>> violatedReadings = getViolatedReadings(reportContext,startTime,endTime);
 			List<Map<String, Object>> heatMapData = new ArrayList<>();
 			long timeStep = endTime-startTime;
 			if(reportContext.getxAggr() == 20) {
@@ -62,33 +78,128 @@ public class FormatHeatMapDataCommand extends FacilioCommand{
 				timeStep = 86400000;
 			}
 			
+			HashMap reportDataMap= new HashMap();
+			for(Map<String, Object> record : data) {
+				Date recordDate = new java.util.Date(((long) record.get(FacilioConstants.ContextNames.REPORT_DEFAULT_X_ALIAS)));
+				record.put("Y", record.get(FacilioConstants.ContextNames.REPORT_DEFAULT_X_ALIAS));
+				reportDataMap.put(sdf.format(recordDate), record);
+				}
+			
+			HashMap markedDataMap= new HashMap();
+			for(Map<String, Object> record : violatedReadings) {
+				Date recordDate = new java.util.Date(((long) record.get("ttime")));
+				record.put("X", record.get("ttime"));
+				record.put("Y", record.get("ttime"));
+				record.put("violated_value",record.get("actualValue"));
+				markedDataMap.put(sdf.format(recordDate), record);
+				}
+			
 			long loopstart=System.currentTimeMillis();
-				while(startTime<=endTime) {
-					HashMap mapData= new HashMap();
-					mapData.put("X", startTime);
-					mapData.put("Y",startTime);
-					Date date = new java.util.Date(startTime);
-					for(Map<String, Object> record : data) {
-						Date recordDate = new java.util.Date(((long) record.get(FacilioConstants.ContextNames.REPORT_DEFAULT_X_ALIAS)));
-						if((sdf.format(date)).equalsIgnoreCase(sdf.format(recordDate))) {
-							record.put("Y", record.get(FacilioConstants.ContextNames.REPORT_DEFAULT_X_ALIAS));
-							mapData= (HashMap) record;
-						}
-					}
-					startTime=startTime+timeStep;
-					heatMapData.add(mapData);
-					}
+			
+			while(startTime<=endTime) {
+				HashMap mapData= new HashMap();
+				mapData.put("X", startTime);
+				mapData.put("Y",startTime);
+				Date date = new java.util.Date(startTime);
+				if(reportDataMap.containsKey(sdf.format(date)) && !markedDataMap.containsKey(sdf.format(date))) {
+					mapData=(HashMap) reportDataMap.get(sdf.format(date));
+				}
+				else if(markedDataMap.containsKey(sdf.format(date))) {
+					mapData=(HashMap) markedDataMap.get(sdf.format(date));
+				}
+				startTime=startTime+timeStep;
+				heatMapData.add(mapData);
+				}
 			reportData.put("heatMapData", heatMapData);
 			
-		long loopend=System.currentTimeMillis()- loopstart;
-		long commandend=System.currentTimeMillis()-start;
+			long loopend=System.currentTimeMillis()- loopstart;
+			long commandend=System.currentTimeMillis()-start;
 		
-		LOGGER.info("time taken for loop  -->  "+loopend);
-		LOGGER.info("time taken for command  -->  "+commandend);
+			LOGGER.info("time taken for loop  -->  "+loopend);
+			LOGGER.info("time taken for command  -->  "+commandend);
 			
 		}
 		
 		
 		return false;
+	}
+	
+	private List<Map<String, Object>> getViolatedReadings(ReportContext reportContext, Long startTime, Long endTime) throws Exception {		// report functions
+		
+		
+		List<Integer> markType = new ArrayList<>();
+		if (reportContext.getxAggr() == DateAggregateOperator.DAYSOFMONTH.getValue() || reportContext.getxAggr() == DateAggregateOperator.FULLDATE.getValue()) {
+			markType.add(MarkedReadingContext.MarkType.HIGH_VALUE_DAILY_VIOLATION.getValue());
+			markType.add(MarkedReadingContext.MarkType.DECREMENTAL_VALUE.getValue());
+		}
+		else if (reportContext.getxAggr() == DateAggregateOperator.HOURSOFDAY.getValue() || reportContext.getxAggr() == DateAggregateOperator.HOURSOFDAYONLY.getValue()) {
+			markType.add(MarkedReadingContext.MarkType.HIGH_VALUE_HOURLY_VIOLATION.getValue());
+			markType.add(MarkedReadingContext.MarkType.HIGH_VALUE_DAILY_VIOLATION.getValue());
+			markType.add(MarkedReadingContext.MarkType.DECREMENTAL_VALUE.getValue());
+		}
+		
+		List<Long> timeRange = new ArrayList<>();
+		timeRange.add(startTime);
+		timeRange.add(endTime);
+		
+		Criteria criteria = new Criteria(); 
+		if(timeRange!=null && !timeRange.isEmpty()) {
+			if (timeRange.size() == 1) {
+				Integer operatorId = Integer.parseInt(timeRange.get(0).toString());
+				Condition condition = new Condition();
+				condition.setFieldName("TTIME");
+				condition.setColumnName("TTIME");
+				condition.setOperatorId(operatorId);
+				
+				criteria.addAndCondition(condition);
+			}
+			else {
+				criteria.addAndCondition(CriteriaAPI.getCondition("TTIME","TTIME", 
+						StringUtils.join(timeRange, ","), DateOperators.BETWEEN));
+			}
+		}
+		
+		if(markType!=null && !markType.isEmpty()) {
+			criteria.addAndCondition(CriteriaAPI.getCondition("MARK_TYPE","MARK_TYPE", 
+					StringUtils.join(markType,","), NumberOperators.EQUALS));
+		}
+		
+		List<ReportDataPointContext> dpList=reportContext.getDataPoints();
+		ReportDataPointContext dp=dpList.get(0);
+		List<Long> parentIds =(List<Long>) dp.getMeta("parentIds");
+		
+		List<Long> deviceList = new ArrayList<>();
+		deviceList.add(parentIds.get(0));
+		if(deviceList!=null && !deviceList.isEmpty()) {
+			criteria.addAndCondition(CriteriaAPI.getCondition("RESOURCE_ID","RESOURCE_ID", 
+					StringUtils.join(deviceList,","), NumberOperators.EQUALS));
+		}
+		
+		List<Long> fieldList = new ArrayList<>();
+		fieldList.add((Long) dp.getyAxis().getFieldId());
+		if(fieldList!=null && !fieldList.isEmpty()) {
+			criteria.addAndCondition(CriteriaAPI.getCondition("FIELD_ID","FIELD_ID", 
+					StringUtils.join(fieldList,","), NumberOperators.EQUALS));
+			}
+		
+		List<Long> moduleList = new ArrayList<>();
+		moduleList.add((Long) dp.getyAxis().getModuleId());
+		if(moduleList!=null && !moduleList.isEmpty()) {
+			criteria.addAndCondition(CriteriaAPI.getCondition("MODULEID","MODULEID", 
+					StringUtils.join(moduleList,","), NumberOperators.EQUALS));
+			}
+		
+		GenericSelectRecordBuilder builder = new GenericSelectRecordBuilder()
+				.select(FieldFactory.getMarkedReadingFields())
+				.table(ModuleFactory.getMarkedReadingModule().getTableName());
+		if(!criteria.isEmpty()) {
+			builder.andCriteria(criteria);
+		}
+		
+		List<Map<String, Object>> markedReadings = builder.get();
+		
+		LOGGER.severe("marked readings builder--->"+builder);
+		return markedReadings;
+		
 	}
 }
