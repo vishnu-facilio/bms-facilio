5b914a5f82ba6e1e4e69750854b437a9b4cec345
Krishna <krishnan.e@facilio.com>
2019-09-06 11:59:17 +0530

    fix

--- src/main/java/com/facilio/workflows/util/WorkflowUtil.java
+++ src/main/java/com/facilio/workflows/util/WorkflowUtil.java
@@ -363,7 +363,7 @@ public class WorkflowUtil {
 		paramMap = workflowContext.getVariableResultMap();
 		
 		workflowContext.setIgnoreNullParams(ignoreNullExpressions);
-		log.debug("param" + (paramMap != null ? paramMap : ""));
+		log.debug("param -- " + paramMap);
 		Object result = workflowContext.executeWorkflow();
 		
 		if(workflowContext.isLogNeeded()) {
