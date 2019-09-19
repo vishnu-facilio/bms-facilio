383bb780dab182c1a02fc8a48b07263bad137eff
Anand <anand.h@facilio.com>
2019-09-19 19:37:37 +0530

    removed filestore

--- src/main/java/com/facilio/services/factory/FacilioFactory.java
+++ src/main/java/com/facilio/services/factory/FacilioFactory.java
@@ -1,22 +1,8 @@
 package com.facilio.services.factory;
 
-import com.facilio.accounts.util.AccountUtil;
-import com.facilio.aws.util.FacilioProperties;
 import com.facilio.services.email.EmailClient;
 import com.facilio.services.email.EmailFactory;
-import com.facilio.services.filestore.*;
-import com.facilio.services.email.AWSemail;
-import com.facilio.services.iot.FacilioIot;
-import com.facilio.services.iot.FacilioIotFactory;
-import com.facilio.services.messageQueue.KinesisMessageQueue;
-import com.facilio.services.messageQueue.MessageQueueFactory;
-import com.facilio.services.queue.QueueFactory;
-import com.facilio.services.queue.SQSQueue;
-import com.facilio.services.queue.InMemoryQueueService;
-import com.facilio.services.email.FacilioEmail;
-import com.facilio.services.messageQueue.KafkaMessageQueue;
-import com.facilio.services.messageQueue.MessageQueue;
-import com.facilio.services.queue.FacilioQueue;
+
 
 import java.util.Objects;
 
@@ -26,18 +12,18 @@ public class FacilioFactory {
 
     private FacilioFactory(){}
 
-    public static FileStore getFileStore(){
+   /* public static FileStore getFileStore(){
 
         return FileStoreFactory.getInstance().getFileStore();
 
-    }
+    }*/
 
 
     public static EmailClient getEmailClient(){
         return EmailFactory.getEmailClient();
     }
 
-    public static MessageQueue getMessageQueue(){
+ /*   public static MessageQueue getMessageQueue(){
        return MessageQueueFactory.getMessageQueue();
     }
     public static FacilioQueue getQueue(){
@@ -45,6 +31,6 @@ public class FacilioFactory {
     }
     public static FacilioIot getIotClient(){
         return FacilioIotFactory.getIotClient();
-    }
+    }*/
 
 }
