13d0be933e936cbc13758720724bfc5cc279f11d
shaan <shaan@facilio.com>
2019-10-03 16:53:29 +0530

    enabling ping for all org

--- src/main/java/com/facilio/bmsconsole/util/IoTMessageAPI.java
+++ src/main/java/com/facilio/bmsconsole/util/IoTMessageAPI.java
@@ -360,7 +360,7 @@ public class IoTMessageAPI {
 		addPublishData(data);
 		addPublishMessage(data.getId(), data.getMessages());
 		
-		if (AccountUtil.getCurrentOrg().getOrgId() == 152l && data.getCommandEnum() != IotCommandType.GET) {
+		if (data.getCommandEnum() != IotCommandType.GET) {
 			// Pinging device to check if it is active
 			PublishData pingData = constructIotMessage(data.getControllerId(), IotCommandType.PING);
 			pingData.setId(data.getId());
