c8a9614fd07425f1690ad2bf04d95e21100e6d4a
shaan <shaan@facilio.com>
2019-10-29 12:37:08 +0530

    reverting dbqueue processor

--- src/main/java/com/facilio/fw/listener/FacilioContextListener.java
+++ src/main/java/com/facilio/fw/listener/FacilioContextListener.java
@@ -95,9 +95,11 @@ public class FacilioContextListener implements ServletContextListener {
 		}
 
 		timer.schedule(new TransactionMonitor(), 0L, 3000L);
-
-
+		
 		if(FacilioProperties.isScheduleServer() && FacilioProperties.isProduction()) {
+			timer.schedule(new FacilioExceptionProcessor(), 0L, 900000L); // 30 minutes
+		}
+		if(!FacilioProperties.isProduction()) {
 			LOGGER.info("##Facilio exception queue Pull method calling");
 			timer.schedule(new FacilioDBQueueExceptionProcessor(), 0L, 900000L); // 30 minutes
 		}
