2f5f1617ae802a2021fdd0ad785f87239ae20f78
Muhamed Aashiq <aashiq@facilio.com>
2019-10-30 12:45:16 +0530

    fix

--- src/main/java/com/facilio/modules/FieldFactory.java
+++ src/main/java/com/facilio/modules/FieldFactory.java
@@ -5601,7 +5601,7 @@ public class FieldFactory {
 		fields.add(getField("ttime","TTIME", module, FieldType.NUMBER));
 		fields.add(getField("importId" , "IMPORTID", module, FieldType.NUMBER));
 		fields.add(getField("templateId" , "TEMPLATEID", module, FieldType.NUMBER));
-		fields.add(getField("total_rows" , "ROW_TOTAL", module, FieldType.NUMBER));
+//		fields.add(getField("total_rows" , "ROW_TOTAL", module, FieldType.NUMBER));
 		fields.add(getField("rowContextString" , "GROUPED_ROWS", module, FieldType.STRING));
 		fields.add(getField("error_resolved" , "ERROR_RESOLVED", module, FieldType.NUMBER));
 		fields.add(getField("correctedRowString","CORRECTED_ROW", module, FieldType.STRING));
