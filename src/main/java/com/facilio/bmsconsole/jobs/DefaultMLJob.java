5447694f86d005fac7d78553e94e2c7396146cd8
Anupriya <anupriya@facilio.com>
2019-11-05 20:41:13 +0530

    Logger for DefaultMLJob

--- src/main/java/com/facilio/bmsconsole/jobs/DefaultMLJob.java
+++ src/main/java/com/facilio/bmsconsole/jobs/DefaultMLJob.java
@@ -3,6 +3,7 @@ package com.facilio.bmsconsole.jobs;
 import java.util.List;
 
 import org.apache.commons.chain.Context;
+import org.apache.log4j.Logger;
 
 import com.facilio.bmsconsole.commands.FacilioChainFactory;
 import com.facilio.bmsconsole.context.MLContext;
@@ -15,13 +16,15 @@ import com.facilio.tasker.job.JobContext;
 
 public class DefaultMLJob extends FacilioJob 
 {
-
+	private static final Logger LOGGER = Logger.getLogger(DefaultMLJob.class.getName());
 	@Override
 	public void execute(JobContext jc) throws Exception 
 	{	
+		LOGGER.info("Inside DefaultMLJob");
 		List<MLContext> mlContextList = MLUtil.getMlContext(jc);
 		for(MLContext mlContext:mlContextList)
 		{
+			LOGGER.info("mlContext"+mlContext.getId());
 			if(mlContext==null)
 			{
 				continue;
