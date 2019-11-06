5b3f490c36d9514e53e5454a4a848c41ea6bd6be
Aravind Chintalapalli <aravind@facilio.com>
2019-11-06 23:11:19 +0530

    Revert "Revert "Adds pm condition""
    This reverts commit a9082c200bb368095b4f93c86364c577fe365105.

--- src/main/java/com/facilio/bmsconsole/jobs/SPIPMNewScheduler.java
+++ src/main/java/com/facilio/bmsconsole/jobs/SPIPMNewScheduler.java
@@ -434,6 +434,7 @@ public class SPIPMNewScheduler extends FacilioJob {
 				.beanClass(WorkOrderContext.class)
 				.andCondition(CriteriaAPI.getCondition(fieldMap.get("jobStatus"), 1+"", NumberOperators.EQUALS))
 				.andCondition(CriteriaAPI.getCondition(fieldMap.get("createdTime"), 1572546600000L+"", NumberOperators.GREATER_THAN))
+				.andCondition(CriteriaAPI.getCondition(fieldMap.get("pm"), pmId+"", NumberOperators.EQUALS))
 				.orderBy("WorkOrders.CREATED_TIME ASC")
 				.limit(1);
 
