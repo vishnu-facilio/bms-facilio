0de3eda90a035ba690d97b9ac468ad7e9709bd31
Muhamed Aashiq <aashiq@facilio.com>
2019-11-13 14:48:02 +0530

    Visitor Form Factory fixes

--- src/main/java/com/facilio/bmsconsole/forms/FormFactory.java
+++ src/main/java/com/facilio/bmsconsole/forms/FormFactory.java
@@ -1255,7 +1255,7 @@ public class FormFactory {
 	private static List<FormField> getVisitorFormFields() {
 		List<FormField> fields = new ArrayList<>();
 		fields.add(new FormField("name", FieldDisplayType.TEXTBOX, "Enter your name", Required.REQUIRED, 1, 1));
-		fields.add(new FormField("phone", FieldDisplayType.NUMBER, "Phone", Required.REQUIRED, 2, 2));
+		fields.add(new FormField("phone", FieldDisplayType.TEXTBOX, "Phone", Required.REQUIRED, 2, 2));
 		fields.add(new FormField("email", FieldDisplayType.EMAIL, "What is your email address", Required.OPTIONAL, 2, 3));		
 		fields.add(new FormField("isBlocked", FieldDisplayType.DECISION_BOX, "Visitor Entry", Required.OPTIONAL, 4, 2));
 		fields.add(new FormField("isVip", FieldDisplayType.DECISION_BOX, "VIP", Required.OPTIONAL, 4, 3));
@@ -1388,7 +1388,7 @@ public class FormFactory {
 		List<FormField> fields = new ArrayList<>();
 		fields.add(new FormField("avatarId", FieldDisplayType.IMAGE, "Avatar", Required.OPTIONAL, 1, 1));
 		fields.add(new FormField("name", FieldDisplayType.TEXTBOX, "Name", Required.REQUIRED, 2, 1));
-		fields.add(new FormField("phone", FieldDisplayType.NUMBER, "Phone", Required.REQUIRED, 3, 2));
+		fields.add(new FormField("phone", FieldDisplayType.TEXTBOX, "Phone", Required.REQUIRED, 3, 2));
 		fields.add(new FormField("email", FieldDisplayType.TEXTBOX, "Email", Required.OPTIONAL, 4, 3));
 		fields.add(new FormField("isBlocked", FieldDisplayType.DECISION_BOX, "Visitor Entry", Required.OPTIONAL, 4, 2));
 		fields.add(new FormField("isVip", FieldDisplayType.DECISION_BOX, "VIP", Required.OPTIONAL, 5, 3));
@@ -1472,7 +1472,7 @@ public class FormFactory {
 	private static List<FormField> getContactFormFields() {
 		List<FormField> fields = new ArrayList<>();
 		fields.add(new FormField("name", FieldDisplayType.TEXTBOX, "Name", Required.REQUIRED, 1, 1));
-		fields.add(new FormField("phone", FieldDisplayType.NUMBER, "Phone", Required.OPTIONAL, 2, 2));
+		fields.add(new FormField("phone", FieldDisplayType.TEXTBOX, "Phone", Required.OPTIONAL, 2, 2));
 		fields.add(new FormField("email", FieldDisplayType.TEXTBOX, "Email", Required.OPTIONAL, 2, 3));
 		fields.add(new FormField("contactType", FieldDisplayType.SELECTBOX, "Contact Type", Required.OPTIONAL, 3, 1));
 		fields.add(new FormField("vendor", FieldDisplayType.LOOKUP_SIMPLE, "Vendor", Required.OPTIONAL, "vendors",4, 2));
