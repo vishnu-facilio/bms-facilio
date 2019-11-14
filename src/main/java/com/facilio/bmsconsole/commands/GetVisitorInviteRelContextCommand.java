9e7954e58564b1e8b3449b9c90afb5b4539aed09
Vandhana Sridhar <vandhana@facilio.com>
2019-11-14 12:00:08 +0530

    adding new command fr visitor invite rel

---
+++ src/main/java/com/facilio/bmsconsole/commands/GetVisitorInviteRelContextCommand.java
@@ -0,0 +1,28 @@
+package com.facilio.bmsconsole.commands;
+
+import org.apache.commons.chain.Context;
+
+import com.facilio.bmsconsole.context.InviteVisitorRelContext;
+import com.facilio.bmsconsole.context.VisitorInviteContext;
+import com.facilio.bmsconsole.util.VisitorManagementAPI;
+import com.facilio.constants.FacilioConstants;
+
+public class GetVisitorInviteRelContextCommand extends FacilioCommand{
+
+	@Override
+	public boolean executeCommand(Context context) throws Exception {
+		// TODO Auto-generated method stub
+		Long inviteId = (Long)context.get(FacilioConstants.ContextNames.VISITOR_INVITE_ID);
+		Long visitorId = (Long)context.get(FacilioConstants.ContextNames.VISITOR_ID);
+		
+		if(inviteId > 0 && visitorId > 0) {
+			VisitorInviteContext invite = VisitorManagementAPI.getVisitorInvite(inviteId);
+			if(invite != null) {
+				InviteVisitorRelContext inviteVisitorRel = VisitorManagementAPI.getVisitorInviteRel(inviteId, visitorId);
+				context.put(FacilioConstants.ContextNames.VISITOR_INVITE_REL, inviteVisitorRel);
+			}
+		}
+		return false;
+	}
+
+}
