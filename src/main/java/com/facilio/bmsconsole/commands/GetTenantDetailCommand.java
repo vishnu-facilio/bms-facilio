1e6f01cf7556296874b92fd466f34f366b65fea7
Vandhana Sridhar <vandhana@facilio.com>
2019-12-02 15:40:38 +0530

    adding spaces to tenant details

--- src/main/java/com/facilio/bmsconsole/commands/GetTenantDetailCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetTenantDetailCommand.java
@@ -1,10 +1,16 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.apache.commons.chain.Context;
 
+import com.facilio.bmsconsole.context.BaseSpaceContext;
 import com.facilio.bmsconsole.tenant.TenantContext;
+import com.facilio.bmsconsole.util.SpaceAPI;
 import com.facilio.bmsconsole.util.TenantsAPI;
 import com.facilio.constants.FacilioConstants;
+
 
 public class GetTenantDetailCommand extends FacilioCommand{
 	@Override
@@ -12,6 +18,10 @@ public class GetTenantDetailCommand extends FacilioCommand{
 		
 		if (context.get(FacilioConstants.ContextNames.ID) != null) {
 			TenantContext tenant = TenantsAPI.fetchTenant((Long)context.get(FacilioConstants.ContextNames.ID));
+			if(tenant != null && tenant.getZone() != null) {
+				List<BaseSpaceContext> spaces =	SpaceAPI.getZoneChildren(Collections.singletonList(tenant.getZone().getId()));
+				context.put(FacilioConstants.ContextNames.SPACE_LIST, spaces);
+			}
 			context.put(FacilioConstants.ContextNames.TENANT, tenant);
 		}
 		return false;
