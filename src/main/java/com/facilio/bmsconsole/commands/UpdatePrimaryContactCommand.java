7cc5514c860191ed674f7ed4a8cbe40559717f79
Vandhana Sridhar <vandhana@facilio.com>
2019-12-05 12:58:38 +0530

    fix

--- src/main/java/com/facilio/bmsconsole/commands/UpdatePrimaryContactCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/UpdatePrimaryContactCommand.java
@@ -4,6 +4,7 @@ import org.apache.commons.chain.Context;
 
 import com.facilio.bmsconsole.context.ContactsContext;
 import com.facilio.bmsconsole.util.ContactsAPI;
+import com.facilio.bmsconsole.util.RecordAPI;
 import com.facilio.constants.FacilioConstants;
 
 public class UpdatePrimaryContactCommand extends FacilioCommand {
@@ -12,6 +13,7 @@ public class UpdatePrimaryContactCommand extends FacilioCommand {
 	public boolean executeCommand(Context context) throws Exception {
 		
 		ContactsContext contact = (ContactsContext)context.get(FacilioConstants.ContextNames.CONTACT);
+		contact = (ContactsContext) RecordAPI.getRecord(FacilioConstants.ContextNames.CONTACT,contact.getId());
 		contact.setIsPrimaryContact(!contact.isPrimaryContact());
 		int rowsUpdated = ContactsAPI.updatePrimaryContact(contact);
 		context.put(FacilioConstants.ContextNames.ROWS_UPDATED,rowsUpdated);
