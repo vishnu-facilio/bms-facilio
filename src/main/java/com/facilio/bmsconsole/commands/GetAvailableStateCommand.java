00fc42e57f0ab957f6290f019807fd4d2078d0f3
Vandhana Sridhar <vandhana@facilio.com>
2019-12-07 14:21:10 +0530

    handling removal of unwanted state transitions based on portal booleans and normal types trasitions

--- src/main/java/com/facilio/bmsconsole/commands/GetAvailableStateCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetAvailableStateCommand.java
@@ -1,13 +1,19 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.chain.Context;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import com.facilio.accounts.dto.IAMUser.AppType;
+import com.facilio.accounts.util.AccountUtil;
 import com.facilio.bmsconsole.util.StateFlowRulesAPI;
+import com.facilio.bmsconsole.workflow.rule.StateflowTransitionContext;
 import com.facilio.bmsconsole.workflow.rule.WorkflowRuleContext;
+import com.facilio.bmsconsole.workflow.rule.StateflowTransitionContext.TransitionType;
 import com.facilio.chain.FacilioContext;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.modules.FacilioStatus;
@@ -27,6 +33,7 @@ public class GetAvailableStateCommand extends FacilioCommand {
 			if (currentState != null) {
 				long currentTime = System.currentTimeMillis();
 				List<WorkflowRuleContext> availableState = StateFlowRulesAPI.getAvailableState(moduleData.getStateFlowId(), currentState.getId(), moduleName, moduleData, (FacilioContext) context);
+				removeUnwantedTranstions(availableState);
 //				System.out.println("################### time taken: " + (System.currentTimeMillis() - currentTime));
 				LOGGER.debug("### time taken inside getAvailableState: " + this.getClass().getSimpleName() + ": " + (System.currentTimeMillis() - currentTime));
 				context.put("availableStates", availableState);
@@ -35,6 +42,34 @@ public class GetAvailableStateCommand extends FacilioCommand {
 			throw new IllegalArgumentException("Record not found");
 		}
 		return false;
+	}
+	
+	public static void removeUnwantedTranstions(List<WorkflowRuleContext> states) {
+		if (CollectionUtils.isEmpty(states)) {
+			return;
+		}
+		
+		Iterator<WorkflowRuleContext> iterator = states.iterator();
+		while (iterator.hasNext()) {
+			StateflowTransitionContext transition = (StateflowTransitionContext) iterator.next();
+			if (transition.getTypeEnum() != TransitionType.NORMAL) {
+				iterator.remove();
+				continue;
+			}
+			
+			if(AccountUtil.getCurrentUser().getAppTypeEnum() == AppType.TENANT_PORTAL) {
+				if (!transition.isShowInTenantPortal()) {
+					iterator.remove();
+					continue;
+				}
+			}
+			else if (AccountUtil.getCurrentUser().getAppTypeEnum() == AppType.VENDOR_PORTAL) {
+				if (!transition.isShowInVendorPortal()) {
+					iterator.remove();
+					continue;
+				}
+			}
+		}
 	}
 
 }
--- src/main/java/com/facilio/bmsconsole/commands/GetStateflowsForModuleDataListCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetStateflowsForModuleDataListCommand.java
@@ -1,6 +1,5 @@
 package com.facilio.bmsconsole.commands;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -8,11 +7,7 @@ import org.apache.commons.chain.Context;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
 
-import com.facilio.accounts.dto.IAMUser.AppType;
-import com.facilio.accounts.util.AccountUtil;
 import com.facilio.bmsconsole.util.StateFlowRulesAPI;
-import com.facilio.bmsconsole.workflow.rule.StateflowTransitionContext;
-import com.facilio.bmsconsole.workflow.rule.StateflowTransitionContext.TransitionType;
 import com.facilio.bmsconsole.workflow.rule.WorkflowRuleContext;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.modules.ModuleBaseWithCustomFields;
@@ -28,27 +23,7 @@ public class GetStateflowsForModuleDataListCommand extends FacilioCommand{
 			if (MapUtils.isNotEmpty(stateFlows)) {
 				for (String key : stateFlows.keySet()) {
 					List<WorkflowRuleContext> list = stateFlows.get(key);
-					Iterator<WorkflowRuleContext> iterator = list.iterator();
-					while (iterator.hasNext()) {
-						StateflowTransitionContext transition = (StateflowTransitionContext) iterator.next();
-						if (transition.getTypeEnum() != TransitionType.NORMAL) {
-							iterator.remove();
-							continue;
-						}
-						
-						if(AccountUtil.getCurrentUser().getAppTypeEnum() == AppType.TENANT_PORTAL) {
-							if (!transition.isShowInTenantPortal()) {
-								iterator.remove();
-								continue;
-							}
-						}
-						else if (AccountUtil.getCurrentUser().getAppTypeEnum() == AppType.VENDOR_PORTAL) {
-							if (!transition.isShowInVendorPortal()) {
-								iterator.remove();
-								continue;
-							}
-						}
-					}
+					GetAvailableStateCommand.removeUnwantedTranstions(list);
 				}
 			}
 		context.put("stateFlows", stateFlows);
