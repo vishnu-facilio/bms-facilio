6c6a21199fca7935e089d39a12752667815f0d48
Krishna <krishnan.e@facilio.com>
2019-12-12 20:34:13 +0530

    DateRange rel. functions

--- src/main/java/com/facilio/workflows/functions/FacilioDateRangeFunctions.java
+++ src/main/java/com/facilio/workflows/functions/FacilioDateRangeFunctions.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.facilio.bmsconsole.util.BaseLineAPI;
+import com.facilio.modules.BaseLineContext;
+import com.facilio.modules.BaseLineContext.AdjustType;
 import com.facilio.time.DateRange;
 import com.facilio.workflows.exceptions.FunctionParamException;
 
@@ -41,6 +44,41 @@ public enum FacilioDateRangeFunctions implements FacilioWorkflowFunctionInterfac
 			}
 			DateRange dateRange = (DateRange) objects[0];
 			return dateRange.getEndTime();
+		};
+		
+		public void checkParam(Object... objects) throws Exception {
+			if(objects.length <= 0) {
+				throw new FunctionParamException("Required Object is null");
+			}
+		}
+	},
+	
+	CREATE(3,"create") {
+		@Override
+		public Object execute(Object... objects) throws Exception {
+			
+			checkParam(objects);
+			
+			long startTime = (long) Double.parseDouble(objects[0].toString());
+			long endTime = (long) Double.parseDouble(objects[1].toString());
+			
+			DateRange dateRange = new DateRange(startTime, endTime);
+			if(objects.length > 2 && objects[2] != null) {
+				String baselineName = objects[2].toString();
+				BaseLineContext baseline = BaseLineAPI.getBaseLine(baselineName);
+				if(baseline != null) {
+					if(objects.length > 3 && objects[3] != null) {
+						String baselineAdjustmentType = objects[3].toString();
+						baseline.setAdjustType(BaseLineContext.AdjustType.getAllAdjustments().get(baselineAdjustmentType));
+					}
+					else {
+						baseline.setAdjustType(AdjustType.WEEK);
+					}
+					dateRange = baseline.calculateBaseLineRange(dateRange, baseline.getAdjustTypeEnum());
+				}
+			}
+			
+			return dateRange;
 		};
 		
 		public void checkParam(Object... objects) throws Exception {
