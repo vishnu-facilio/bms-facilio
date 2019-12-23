fdc7617ce341d000757a28372cd04291d6efb5f0
arunkumar <arunkumar@facilio.com>
2019-12-23 08:56:23 +0530

    handle migration for Import Points

--- src/main/java/com/facilio/bmsconsole/commands/UpdateImportPointsDataCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/UpdateImportPointsDataCommand.java
@@ -1,68 +1,100 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.commons.chain.Context;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import com.facilio.bacnet.BACNetUtil.InstanceType;
+import com.facilio.beans.ModuleBean;
+import com.facilio.bmsconsole.context.ReadingDataMeta;
+import com.facilio.bmsconsole.context.ReadingDataMeta.ReadingInputType;
+import com.facilio.bmsconsole.context.ReadingDataMeta.ReadingType;
+import com.facilio.bmsconsole.util.ReadingsAPI;
+import com.facilio.chain.FacilioContext;
+import com.facilio.constants.FacilioConstants;
 import com.facilio.db.builder.GenericUpdateRecordBuilder;
 import com.facilio.db.criteria.CriteriaAPI;
 import com.facilio.db.criteria.operators.StringOperators;
 import com.facilio.db.transaction.FacilioTransactionManager;
+import com.facilio.fw.BeanFactory;
 import com.facilio.modules.FieldFactory;
 import com.facilio.modules.ModuleFactory;
 import com.facilio.modules.fields.FacilioField;
+import com.facilio.tasker.FacilioTimer;
+import com.facilio.timeseries.TimeSeriesAPI;
 
-public class UpdateImportPointsDataCommand extends FacilioCommand {
-	
+public class UpdateImportPointsDataCommand extends FacilioCommand implements PostTransactionCommand {
+
 	private static final Logger LOGGER = LogManager.getLogger(UpdateImportPointsDataCommand.class.getName());
+	private static List<FacilioContext> unmodelledHistoricalContexts;
+
 	@Override
 	public boolean executeCommand(Context context) throws Exception {
 		// TODO Auto-generated method stub
-		
-		List<Map<String,Object>> finalList = (List<Map<String, Object>>) context.get("POINTS_LIST");
-		long controllerId = (long) context.get("CONTROLLER_ID");
-		
-		try {
-//			FacilioTransactionManager.INSTANCE.getTransactionManager().begin();
 
-			for(Map<String,Object> prop:finalList) {
-				if(prop.isEmpty()) {
+		List<Map<String, Object>> finalList = (List<Map<String, Object>>) context.get("POINTS_LIST");
+		long controllerId = (long) context.get("CONTROLLER_ID");
+
+		try {
+			// FacilioTransactionManager.INSTANCE.getTransactionManager().begin();
+
+			for (Map<String, Object> prop : finalList) {
+				if (prop.isEmpty()) {
 					continue;
 				}
-				Map<String ,Object> map = new HashMap<String, Object>();
+
 				String deviceName = (String) prop.get("Device");
 				String instanceName = (String) prop.get("Instance");
-				map.put("resourceId",prop.get("Assets"));
-				map.put("categoryId", prop.get("Asset Category")); 
-				map.put("fieldId", prop.get("Reading"));
-				map.put("mappedTime", System.currentTimeMillis());
-				List<FacilioField> field = FieldFactory.getPointsFields();
-				Map<String, FacilioField> fieldMaps = FieldFactory.getAsMap(field);
+				long assetId = Long.parseLong((String) prop.get("Assets"));
+				long categoryId = Long.parseLong((String) prop.get("Asset Category"));
+				long fieldId = Long.parseLong((String) prop.get("Reading")) ;
+				TimeSeriesAPI.insertInstanceAssetMapping(deviceName, assetId, categoryId, controllerId, instanceName,
+						fieldId, null);
 
-				GenericUpdateRecordBuilder builderPoints = new GenericUpdateRecordBuilder()
-						.fields(FieldFactory.getPointsFields())
-						.table(ModuleFactory.getPointsModule().getTableName())
-						.andCondition(CriteriaAPI.getCondition(fieldMaps.get("controllerId"), String.valueOf(controllerId), StringOperators.IS))
-						.andCondition(CriteriaAPI.getCondition(fieldMaps.get("device"),deviceName, StringOperators.IS))
-						.andCondition(CriteriaAPI.getCondition(fieldMaps.get("instance"),instanceName, StringOperators.IS))
-						;
-				builderPoints.update(map);
-//				FacilioTransactionManager.INSTANCE.getTransactionManager().commit();
-			
+				FacilioContext contextImport = new FacilioContext();
+				contextImport.put(FacilioConstants.ContextNames.DEVICE_DATA, deviceName);
+				contextImport.put(FacilioConstants.ContextNames.INSTANCE_INFO, instanceName);
+				contextImport.put(FacilioConstants.ContextNames.ASSET_ID, assetId);
+				contextImport.put(FacilioConstants.ContextNames.FIELD_ID, fieldId);
+				contextImport.put(FacilioConstants.ContextNames.CONTROLLER_ID, controllerId);
+
+				if (unmodelledHistoricalContexts == null) {
+					unmodelledHistoricalContexts = new ArrayList<>();
+				}
+				unmodelledHistoricalContexts.add(contextImport);
+
+				// FacilioTransactionManager.INSTANCE.getTransactionManager().commit();
+
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			LOGGER.error("Exception occured While updating Points Table   :", e);
-//			FacilioTransactionManager.INSTANCE.getTransactionManager().rollback();
+			// FacilioTransactionManager.INSTANCE.getTransactionManager().rollback();
 			throw e;
-	}
-		
-		
-		
+		}
+
 		return false;
 	}
 
+	@Override
+	public boolean postExecute() throws Exception {
+		// TODO Auto-generated method stub
+
+		if (CollectionUtils.isNotEmpty(unmodelledHistoricalContexts)) {
+			// TODO handle multiple mappings in ProcessUnmodelledHistoricalData
+			for (FacilioContext context : unmodelledHistoricalContexts) {
+				FacilioTimer.scheduleInstantJob("ProcessUnmodelledHistoricalData", context);
+			}
+		}
+
+		return false;
+	}
 }
