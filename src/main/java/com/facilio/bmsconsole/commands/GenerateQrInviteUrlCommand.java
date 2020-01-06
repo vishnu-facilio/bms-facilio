7ed3ea384bfa02d35196c69bb7dcfc723cdd0f3f
Vandhana Sridhar <vandhana@facilio.com>
2020-01-06 21:29:51 +0530

    temp fix

--- src/main/java/com/facilio/bmsconsole/commands/GenerateQrInviteUrlCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GenerateQrInviteUrlCommand.java
@@ -37,11 +37,11 @@ public class GenerateQrInviteUrlCommand extends FacilioCommand {
 				JSONObject size = new JSONObject();
 				size.put("width", 200);
 				size.put("height", 200);
-				String originalUrl = PdfUtil.exportUrlAsPublicFilePdf("https://app.facilio.com/app/qr?code=" + qrCode, true, null, size, -1,  FileFormat.IMAGE);
-				String baseUrl = FacilioProperties.getClientAppUrl();
-				if(StringUtils.isNotEmpty(originalUrl)) {
-					inviteVisitor.setQrUrl(baseUrl.concat(originalUrl));
-				}
+				String originalUrl = PdfUtil.exportUrlAsPdf("https://app.facilio.com/app/qr?code=" + qrCode, true, null, size, FileFormat.IMAGE);
+				//String baseUrl = FacilioProperties.getClientAppUrl();
+				//if(StringUtils.isNotEmpty(originalUrl)) {
+				inviteVisitor.setQrUrl(originalUrl);
+				//}
 				
 				inviteVisitor.setPassCode(passCode);
 				
