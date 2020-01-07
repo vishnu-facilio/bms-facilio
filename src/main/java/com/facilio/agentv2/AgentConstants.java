10e4a4029f6caed394b7b63fc80fcdf55ca591c7
vijayC <vijay.c@facilio.com>
2020-01-07 12:01:11 +0530

    AgentConstants check in

--- src/main/java/com/facilio/agentv2/AgentConstants.java
+++ src/main/java/com/facilio/agentv2/AgentConstants.java
@@ -137,4 +137,5 @@ public class AgentConstants
     public static final String AGENT_METRICS_MODULE = "agentMetricsV2";
     public static final String NUMBER_OF_MSGS = "numberOfMessages";
     public static final String SIZE = "size";
+    public static final String CHILDJSON = "childJson";
 }
