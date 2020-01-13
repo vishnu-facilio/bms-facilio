06317b97c129a9162600505a2aac1a9fcc42e878
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2020-01-13 14:36:31 +0530

    setting kafka as default message queue

--- src/main/java/com/facilio/services/messageQueue/MessageQueueFactory.java
+++ src/main/java/com/facilio/services/messageQueue/MessageQueueFactory.java
@@ -1,16 +1,8 @@
 package com.facilio.services.messageQueue;
 
-import com.facilio.aws.util.FacilioProperties;
-
 public class MessageQueueFactory {
 
     public static MessageQueue getMessageQueue(){
-        MessageQueue messageQueue = null;
-        String messageQueueProp = FacilioProperties.getMessageQueue();
-        switch (messageQueueProp){
-            case "kafka": messageQueue = KafkaMessageQueue.getClient(); break;
-            default: messageQueue = KinesisMessageQueue.getClient(); break;
-        }
-        return messageQueue;
+        return KafkaMessageQueue.getClient();
     }
 }
