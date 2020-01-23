bed5ddaee3ba57e67bab3dd12342c97c9936f7e2
Boopathy <boopathy@facilio.com>
2020-01-23 13:11:15 +0530

    requester can updated even if record is locked - WO

--- src/main/java/com/facilio/bmsconsole/commands/VerifyApprovalCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/VerifyApprovalCommand.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.facilio.accounts.dto.User;
 import org.apache.commons.chain.Context;
 
 import com.facilio.bmsconsole.context.WorkOrderContext;
@@ -32,8 +33,21 @@ public class VerifyApprovalCommand extends FacilioCommand {
 				if (wo.getModuleState() != null) {
 					FacilioStatus stateContext = StateFlowRulesAPI.getStateContext(wo.getModuleState().getId());
 					Long stateTransitionId = (Long) context.get(FacilioConstants.ContextNames.TRANSITION_ID);
-					if (stateContext.isRecordLocked() && workOrder.getApprovalState() == -1 && (stateTransitionId == null || stateTransitionId == -1)) {
-						throw new IllegalArgumentException("Workorder with lock cannot be updated");
+					if ((stateContext.isRecordLocked() && workOrder.getApprovalState() == -1)) {
+						boolean cannotEdit = false;
+						if ((stateTransitionId == null || stateTransitionId == -1)) {
+							cannotEdit = true;
+						}
+						if (workOrder.getRequester() != null) {
+							cannotEdit = false;
+							User requester = workOrder.getRequester();
+							workOrder = new WorkOrderContext();
+							workOrder.setRequester(requester);
+							context.put(FacilioConstants.ContextNames.WORK_ORDER, workOrder);
+						}
+						if (cannotEdit) {
+							throw new IllegalArgumentException("Workorder with lock cannot be updated");
+						}
 					}
 				}
 				else {
