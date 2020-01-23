2d4a22a8d4e974b54ddf37ac8520502cb662f7dc
Aravind Chintalapalli <aravind@facilio.com>
2020-01-23 10:32:08 +0530

    Fixes compilation issue

---
+++ src/main/java/com/facilio/bmsconsole/commands/GetModulesWithDateFieldCommand.java
@@ -0,0 +1,23 @@
+package com.facilio.bmsconsole.commands;
+
+import com.facilio.beans.ModuleBean;
+import com.facilio.constants.FacilioConstants;
+import com.facilio.fw.BeanFactory;
+import com.facilio.modules.FacilioModule;
+import com.facilio.modules.FacilioModule.ModuleType;
+import org.apache.commons.chain.Context;
+
+import java.util.List;
+
+public class GetModulesWithDateFieldCommand extends FacilioCommand {
+
+	@Override
+	public boolean executeCommand(Context context) throws Exception {
+		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+		List<FacilioModule> moduleList = modBean.getCustomModulesWithDateFieldList();
+
+		context.put(FacilioConstants.ContextNames.MODULE_LIST, moduleList);
+		return false;
+	}
+
+}
