0fa9951138275d7c64d99fa9bfc9d5a0011f3284
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2020-02-10 12:11:56 +0530

    added deployment group demo user for production demo

--- scripts/change_conf.sh
+++ scripts/change_conf.sh
@@ -48,3 +48,13 @@ if [ "$DEPLOYMENT_GROUP_NAME" = "production-kinesis" ]; then
     cp $UBUNTU_HOME/setenv.sh $APP_HOME/bin/setenv.sh
     echo "copied $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
 fi
+
+if [ "$DEPLOYMENT_GROUP_NAME" = "demo-user" ]; then
+    echo "copying $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
+    rm $META_INF_DIR/context.xml
+    cp $UBUNTU_HOME/deployment-files/log4j-user.properties $CLASSES_DIR/log4j.properties
+    cp $UBUNTU_HOME/setenv.sh $APP_HOME/bin/setenv.sh
+    cp $UBUNTU_HOME/deployment-files/awsprops-user.properties $CONF_DIR/awsprops.properties
+    cp $UBUNTU-HOME/deployment-files/agentIntegration-production.properties $CONF_DIR/agentIntegration.properties
+    echo "copied $DEPLOYMENT_GROUP_NAME context file" >> /home/ubuntu/deployment.log
+fi
