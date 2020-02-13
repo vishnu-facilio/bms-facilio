9e675004b11e2e0aaf2ef2a2d92ebcc94087dd02
Muhamed Aashiq <aashiq@facilio.com>
2020-02-13 19:39:43 +0530

    fix

--- src/main/java/com/facilio/bmsconsole/forms/FormFactory.java
+++ src/main/java/com/facilio/bmsconsole/forms/FormFactory.java
@@ -1835,7 +1835,7 @@ public class FormFactory {
 	private static List<FormField> getServiceRequestFormFields() {
 		List<FormField> fields = new ArrayList<>();
 		fields.add(new FormField("subject", FieldDisplayType.TEXTBOX, "Subject", Required.REQUIRED, 1, 1));
-		fields.add(new FormField("description", FieldDisplayType.TEXTBOX, "Description", Required.OPTIONAL, 2, 1));
+		fields.add(new FormField("description", FieldDisplayType.TEXTAREA, "Description", Required.OPTIONAL, 2, 1));
 		fields.add(new FormField("site", FieldDisplayType.LOOKUP_SIMPLE, "Site", Required.REQUIRED, "site", 3, 1));
 		fields.add(new FormField("resource", FieldDisplayType.WOASSETSPACECHOOSER, "Space/Asset", Required.OPTIONAL, 4, 1));
 		fields.add(new FormField("assignment", FieldDisplayType.TEAMSTAFFASSIGNMENT, "Team/Staff", Required.OPTIONAL, 5, 1));
