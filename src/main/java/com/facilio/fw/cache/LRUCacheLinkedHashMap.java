7c0f67f98a6ed026072ba168a42249353205dd76
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2020-02-21 15:42:21 +0530

    added synchronization for write methods on map

--- src/main/java/com/facilio/fw/cache/LRUCacheLinkedHashMap.java
+++ src/main/java/com/facilio/fw/cache/LRUCacheLinkedHashMap.java
@@ -15,4 +15,39 @@ public class LRUCacheLinkedHashMap<K,V> extends LinkedHashMap<K,V> {
     protected boolean removeEldestEntry(Map.Entry eldest) {
         return size() > threshold;
     }
+
+    @Override
+    public void clear() {
+        synchronized (this) {
+            super.clear();
+        }
+    }
+
+    @Override
+    public V put(K key, V value) {
+        synchronized (this) {
+            return super.put(key, value);
+        }
+    }
+
+    @Override
+    public V remove(Object key) {
+        synchronized (this) {
+            return super.remove(key);
+        }
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        synchronized (this) {
+            super.putAll(m);
+        }
+    }
+
+    @Override
+    public V putIfAbsent(K key, V value) {
+        synchronized (this) {
+            return super.putIfAbsent(key, value);
+        }
+    }
 }
