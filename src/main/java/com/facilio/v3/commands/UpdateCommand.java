16873c7541a2ec031dce4933f00139425caa3773
Aravind Chintalapalli <aravind@facilio.com>
2020-04-06 18:17:24 +0530

    Makes v3 update command to support bulk update

--- src/main/java/com/facilio/v3/commands/UpdateCommand.java
+++ src/main/java/com/facilio/v3/commands/UpdateCommand.java
@@ -7,10 +7,12 @@ import com.facilio.db.criteria.CriteriaAPI;
 import com.facilio.fw.BeanFactory;
 import com.facilio.modules.FacilioModule;
 import com.facilio.modules.ModuleBaseWithCustomFields;
+import com.facilio.modules.UpdateChangeSet;
 import com.facilio.modules.UpdateRecordBuilder;
 import com.facilio.modules.fields.FacilioField;
 import org.apache.commons.chain.Context;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,33 +28,42 @@ public class UpdateCommand extends FacilioCommand {
     public boolean executeCommand(Context context) throws Exception {
         //copied from GenericUpdateModuleDataCommand
         Map<String, List<ModuleBaseWithCustomFields>> recordMap = (Map<String, List<ModuleBaseWithCustomFields>>) context.get(FacilioConstants.ContextNames.RECORD_MAP);
-        ModuleBaseWithCustomFields record = recordMap.get(module.getName()).get(0);
 
-        long recordId = record.getId();
+        String moduleName = (String) context.get(FacilioConstants.ContextNames.MODULE_NAME);
+        ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+        FacilioModule module = modBean.getModule(moduleName);
 
-        if(record != null && recordId > 0) {
-            String moduleName = (String) context.get(FacilioConstants.ContextNames.MODULE_NAME);
-            ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
-            FacilioModule module = modBean.getModule(moduleName);
-            List<FacilioField> fields = (List<FacilioField>) context.get(FacilioConstants.ContextNames.EXISTING_FIELD_LIST);
-            if (fields == null) {
-                fields = modBean.getAllFields(moduleName);
+        List<FacilioField> fields = (List<FacilioField>) context.get(FacilioConstants.ContextNames.EXISTING_FIELD_LIST);
+        if (fields == null) {
+            fields = modBean.getAllFields(moduleName);
+        }
+
+        Map<Long, List<UpdateChangeSet>> allChangesets = new HashMap<>();
+        int totalCount = 0;
+
+        for (ModuleBaseWithCustomFields record: recordMap.get(module.getName())) {
+            if(record != null && record.getId() > 0) {
+                continue;
             }
+
+            long recordId = record.getId();
+
             UpdateRecordBuilder<ModuleBaseWithCustomFields> updateBuilder = new UpdateRecordBuilder<ModuleBaseWithCustomFields>()
                     .module(module)
                     .fields(fields)
                     .andCondition(CriteriaAPI.getIdCondition(recordId, module));
 
-            Boolean withChangeSet = (Boolean) context.get(FacilioConstants.ContextNames.WITH_CHANGE_SET);
-            if (withChangeSet != null && withChangeSet) {
-                updateBuilder.withChangeSet(ModuleBaseWithCustomFields.class);
-            }
+            updateBuilder.withChangeSet(ModuleBaseWithCustomFields.class);
 
-            context.put(FacilioConstants.ContextNames.ROWS_UPDATED, updateBuilder.update(record));
-            if (withChangeSet != null && withChangeSet) {
-                context.put(FacilioConstants.ContextNames.CHANGE_SET, updateBuilder.getChangeSet());
-            }
+            totalCount += updateBuilder.update(record);
+
+            Map<Long, List<UpdateChangeSet>> changeSet = updateBuilder.getChangeSet();
+            allChangesets.putAll(changeSet);
         }
+
+        context.put(FacilioConstants.ContextNames.ROWS_UPDATED, totalCount);
+        context.put(FacilioConstants.ContextNames.CHANGE_SET, allChangesets);
+
         return false;
     }
 }
