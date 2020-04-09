495325892cb367214592d1bf50b772e92cb7bf0a
Manthosh <manthosh@facilio.com>
2020-04-09 20:55:33 +0530

    Adding Csrf filter only for /api urls

--- src/main/java/com/facilio/fw/filter/SecurityFilter.java
+++ src/main/java/com/facilio/fw/filter/SecurityFilter.java
@@ -1,8 +1,6 @@
 package com.facilio.fw.filter;
 
 import com.facilio.auth.cookie.FacilioCookie;
-import com.facilio.aws.util.FacilioProperties;
-import com.facilio.fw.util.RequestUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -11,7 +9,9 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SecurityFilter implements Filter {
     private static final Logger LOGGER = LogManager.getLogger(SecurityFilter.class.getName());
@@ -21,13 +21,23 @@ public class SecurityFilter implements Filter {
 
     }
 
+    private static List<String> initWhiteListedUrls() {
+        List<String> urls = new ArrayList<>();
+        urls.add("/api/v2/files/");
+        urls.add("/api/v3/files/");
+        urls.add("/websocket/");
+        return urls;
+    }
+
+    private static final List<String> WHITE_LISTED_URIS = Collections.unmodifiableList(initWhiteListedUrls());
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
 
-        if (true /*FacilioProperties.isProduction()*/) {
-            HttpServletRequest request = (HttpServletRequest) req;
-            HttpServletResponse response = (HttpServletResponse) res;
-
+        String requestUri = request.getRequestURI();
+        if (true /*FacilioProperties.isProduction()*/ && !isWhiteListedUri(requestUri)) {
             String csrfCookieToken = FacilioCookie.getUserCookie(request, FacilioCookie.CSRF_TOKEN_COOKIE);
             if (StringUtils.isNotEmpty(csrfCookieToken)) {
                 String csrfHeaderToken = request.getHeader(CSRF_HEADER);
@@ -35,6 +45,7 @@ public class SecurityFilter implements Filter {
                     chain.doFilter(req, res);
                 } else {
                     handleInvalid(request, response);
+                    chain.doFilter(req, res);
                 }
             } else {
                 chain.doFilter(req, res);
@@ -45,11 +56,21 @@ public class SecurityFilter implements Filter {
         }
     }
 
+    private boolean isWhiteListedUri (String uri) {
+        for (String url : WHITE_LISTED_URIS) {
+            boolean isWhiteListed = uri.contains(url);
+            if (isWhiteListed) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private void handleInvalid(HttpServletRequest request, HttpServletResponse response) {
         LOGGER.info("send 403 for " + request.getServerName() + request.getRequestURI());
-        response.setContentType("text/plain");
-        response.setStatus(403);
-        response.resetBuffer();
+//        response.setContentType("text/plain");
+//        response.setStatus(403);
+//        response.resetBuffer();
     }
 
     @Override
