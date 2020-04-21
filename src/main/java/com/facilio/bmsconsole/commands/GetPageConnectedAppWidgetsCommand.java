9d8e358491e4c2f9119575e2ce52f32216b038f3
magesh <magesh@facilio.com>
2020-04-21 10:32:58 +0530

    connected app page tab command fix

--- src/main/java/com/facilio/bmsconsole/commands/GetPageConnectedAppWidgetsCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetPageConnectedAppWidgetsCommand.java
@@ -3,6 +3,8 @@ package com.facilio.bmsconsole.commands;
 import java.util.List;
 
 import org.apache.commons.chain.Context;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.context.ConnectedAppWidgetContext;
@@ -15,30 +17,40 @@ import com.facilio.modules.ModuleBaseWithCustomFields;
 
 public class GetPageConnectedAppWidgetsCommand extends FacilioCommand {
 
+	private static Logger LOGGER = LogManager.getLogger(GetPageConnectedAppWidgetsCommand.class.getName());
+	
 	@Override
 	public boolean executeCommand(Context context) throws Exception {
 		String moduleName = (String) context.get(FacilioConstants.ContextNames.MODULE_NAME);
 		
-		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
-		FacilioModule module = modBean.getModule(moduleName);
-		
-		if (module != null) {
-			List<Long> moduleIds = module.getExtendedModuleIds();
+		try {
+			ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+			FacilioModule module = modBean.getModule(moduleName);
 			
-			Page page = (Page) context.get(FacilioConstants.ContextNames.PAGE);
-			Object record = context.get(FacilioConstants.ContextNames.RECORD);
-			
-			long recordId = ((ModuleBaseWithCustomFields) record).getId();
-			
-			List<ConnectedAppWidgetContext> widgets = ConnectedAppAPI.getSummaryPageWidgets(moduleIds, recordId);
-			if (widgets != null && !widgets.isEmpty()) {
+			if (module != null) {
+				List<Long> moduleIds = module.getExtendedModuleIds();
 				
-				for (ConnectedAppWidgetContext widget : widgets) {
-					Page.Tab tab = page.new Tab(widget.getId()+"", "connectedapp_widget");
-					tab.setDisplayName(widget.getWidgetName());
-					page.addTab(tab);
+				Page page = (Page) context.get(FacilioConstants.ContextNames.PAGE);
+				Object record = context.get(FacilioConstants.ContextNames.RECORD);
+				
+				if (page != null && record != null && record instanceof ModuleBaseWithCustomFields) {
+				
+					long recordId = ((ModuleBaseWithCustomFields) record).getId();
+					
+					List<ConnectedAppWidgetContext> widgets = ConnectedAppAPI.getSummaryPageWidgets(moduleIds, recordId);
+					if (widgets != null && !widgets.isEmpty()) {
+						
+						for (ConnectedAppWidgetContext widget : widgets) {
+							Page.Tab tab = page.new Tab(widget.getId()+"", "connectedapp_widget");
+							tab.setDisplayName(widget.getWidgetName());
+							page.addTab(tab);
+						}
+					}
 				}
 			}
+		}
+		catch (Exception e) {
+			LOGGER.error("Exception while adding connected app widgets to page factory", e);
 		}
 		
 		return false;
