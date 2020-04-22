90e103589b28386dc2f852787c3fbe176c42501b
Seeni Mohamed <seenimohamed@facilio.com>
2020-04-22 12:14:31 +0530

    customised future date limit for ML readings in reports for demo account handled

--- src/main/java/com/facilio/report/util/DemoHelperUtil.java
+++ src/main/java/com/facilio/report/util/DemoHelperUtil.java
@@ -4,17 +4,33 @@ import org.apache.log4j.LogManager;
 
 import com.facilio.modules.FacilioModule;
 import com.facilio.time.DateRange;
+import com.facilio.time.DateTimeUtil;
+;
 
 public class DemoHelperUtil {
+
 	private static final org.apache.log4j.Logger LOGGER = LogManager.getLogger(FilterUtil.class.getName());
-	
+
 	public static Long getEndTime(FacilioModule module, DateRange dateRange) {
-		Long endTime = System.currentTimeMillis();
+		Long currentTime = System.currentTimeMillis();
+		
 		if(dateRange != null) {
-		if (endTime >= dateRange.getEndTime()) {
-			return dateRange.getEndTime();
+			Long endTime = 0L;
+			String moduleName = module.getName();
+			if("energypredictionmlreadings".equals(moduleName))  {
+				return DateTimeUtil.getMonthEndTimeOf(currentTime);
+			}
+			
+			if("loadpredictionmlreadings".equals(moduleName)) {
+				endTime = DateTimeUtil.getDayEndTimeOf(currentTime);
+				endTime += (24*60*60*1000);
+				return endTime;
+			}
+			// to handle past date as endTime
+			if (endTime >= dateRange.getEndTime()) {
+				return dateRange.getEndTime();
+			}
 		}
-		}
-		return endTime;
+		return currentTime;
 	}
 }
