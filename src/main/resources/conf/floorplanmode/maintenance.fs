20b2af31ef61fb92386dbfc41d82d4ba1979a5b5
Prasanth <prasanth.iot@gmail.com>
2020-04-28 16:20:35 +0530

    floorplan script update

--- src/main/resources/conf/floorplanmode/maintenance.fs
+++ src/main/resources/conf/floorplanmode/maintenance.fs
@@ -1,6 +1,34 @@
 Map floorPlanMode(Map params) {
-  result = {};
-  result["title"] = params.title;
-  result["value"] = params.url;
+floorId = params.floorId;
+spaceIds = params.spaceId;
+
+ areas = [];
+   for each index, spaceId in spaceIds {
+    area = {};
+      resource = BaseSpace().getSubordinates(spaceId);
+    log "" + resource;
+  db = {
+    criteria: [resource == resource],
+  };
+  workorder = Module("workorder").fetch(db);
+    
+    if (workorder != null) {
+      label = "" + workorder.size();
+            area["spaceId"] = spaceId;
+      area["label"] = label;
+      area["values"] = workorder;
+      areas.add(area);
+     }
+    else {
+      empty = [];
+            area["spaceId"] = spaceId;
+      area["label"] = "0";
+      area["values"] = empty;
+      areas.add(area);
+    }
+      }
+result = {};
+  result.areas = areas;
+  result.layer = "heatmap";
   return result;
 }
