764227cde9639b1e0f7ca04144d1808218cc5852
Vandhana Sridhar <vandhana@facilio.com>
2020-05-01 09:29:26 +0530

    adding site id to tenant contacts

--- src/main/java/com/facilio/bmsconsole/commands/CheckForPeopleDuplicationCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/CheckForPeopleDuplicationCommand.java
@@ -7,7 +7,10 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.facilio.bmsconsole.context.PeopleContext;
+import com.facilio.bmsconsole.context.TenantContactContext;
+import com.facilio.bmsconsole.tenant.TenantContext;
 import com.facilio.bmsconsole.util.PeopleAPI;
+import com.facilio.bmsconsole.util.RecordAPI;
 import com.facilio.constants.FacilioConstants;
 
 public class CheckForPeopleDuplicationCommand extends FacilioCommand {
@@ -20,6 +23,11 @@ public class CheckForPeopleDuplicationCommand extends FacilioCommand {
 			for (PeopleContext people : peopleList) {
 				if(StringUtils.isNotEmpty(people.getEmail()) && PeopleAPI.checkForDuplicatePeople(people)) {
 					throw new IllegalArgumentException("People with the same email id already exists");
+				}
+				//setting tenant's site to all the contacts
+				if(people instanceof TenantContactContext && ((TenantContactContext)people).getTenant() != null && ((TenantContactContext)people).getTenant().getId() > 0) {
+					TenantContext tenant = (TenantContext)RecordAPI.getRecord(FacilioConstants.ContextNames.TENANT, ((TenantContactContext)people).getTenant().getId());
+					people.setSiteId(tenant.getSiteId());
 				}
 			}
 		}
