71bb5a79e7ebf727df1d65e8bb55c6791ce9ef42
Boopathy <boopathy@facilio.com>
2020-05-04 15:25:46 +0530

    approval related list widget

--- src/main/java/com/facilio/bmsconsole/page/factory/ApprovalPageFactory.java
+++ src/main/java/com/facilio/bmsconsole/page/factory/ApprovalPageFactory.java
@@ -1,7 +1,12 @@
 package com.facilio.bmsconsole.page.factory;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.facilio.beans.ModuleBean;
+import com.facilio.fw.BeanFactory;
+import com.facilio.modules.fields.FacilioField;
+import com.facilio.modules.fields.LookupField;
 import org.apache.commons.collections4.CollectionUtils;
 
 import com.facilio.bmsconsole.commands.ReadOnlyChainFactory;
@@ -17,6 +22,10 @@ import com.facilio.chain.FacilioContext;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.modules.FacilioModule;
 import com.facilio.modules.ModuleBaseWithCustomFields;
+import org.apache.commons.lang3.StringUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class ApprovalPageFactory extends PageFactory {
 	
@@ -40,6 +49,7 @@ public class ApprovalPageFactory extends PageFactory {
         tab1.addSection(tab1Sec1);
         
         addApprovalFieldsWidget(tab1Sec1);
+        addApprovalRelatedModuleWidget(tab1Sec1, approvalMeta.getConfigJson(), record.getModuleId());
         if (CollectionUtils.isNotEmpty(approvers) && approvers.size() > 1) {
         		addApproversWidget(tab1Sec1);
         }
@@ -48,7 +58,41 @@ public class ApprovalPageFactory extends PageFactory {
 
         return page;
 	}
-	
+
+	private static void addApprovalRelatedModuleWidget(Section section, String configJson, long moduleId) {
+		if (StringUtils.isNotEmpty(configJson)) {
+			JSONParser parser = new JSONParser();
+			try {
+				JSONObject parse = (JSONObject) parser.parse(configJson);
+				JSONArray relatedModules = (JSONArray) parse.get("relatedModules");
+				ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+
+				FacilioModule module = modBean.getModule(moduleId);
+				List<Long> extendedModuleIds = module.getExtendedModuleIds();
+				for (Object relatedModuleId: relatedModules) {
+					if (relatedModuleId instanceof Long) {
+						FacilioModule subModule = modBean.getModule((Long) relatedModuleId);
+						List<FacilioField> allFields = modBean.getAllFields(subModule.getName());
+						List<FacilioField> fields = allFields.stream().filter(field -> (field instanceof LookupField && (extendedModuleIds.contains(((LookupField) field).getLookupModuleId())))).collect(Collectors.toList());
+						if (CollectionUtils.isNotEmpty(fields)) {
+							for (FacilioField field : fields) {
+								PageWidget relatedListWidget = new PageWidget(WidgetType.RELATED_LIST);
+								JSONObject relatedList = new JSONObject();
+								relatedList.put("module", subModule);
+								relatedList.put("field", field);
+								relatedListWidget.setRelatedList(relatedList);
+								relatedListWidget.addToLayoutParams(section, 24, 8);
+								section.addWidget(relatedListWidget);
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	private static void addApprovalFieldsWidget(Section section) {
 		PageWidget pageWidget = new PageWidget(WidgetType.APPROVAL_FIELDS_WIDGET);
 		pageWidget.addToLayoutParams(section, 24, 4);
