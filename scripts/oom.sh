5cd10c387b476dcd44d4c8e7296c9c7dc752eb90
Bhuvaneswaran <bhuvaneswaran.g@facililo.com>
2020-05-08 15:18:21 +0530

    handled app.domain property check

--- scripts/oom.sh
+++ scripts/oom.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 export CONF_DIR="/home/ubuntu/tomcat/webapps/ROOT/WEB-INF/classes/conf"
 ipAddress=`hostname -I|awk '{$1=$1};1'`
-servername=`grep "app.domain" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
+servername=`grep "app.domain=" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 environment=`grep "environment" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 schedulerServer=`grep "schedulerServer" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 messageProcessor=`grep "messageProcessor" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
--- scripts/threaddump.sh
+++ scripts/threaddump.sh
@@ -9,7 +9,7 @@ export APP_HOME="/home/ubuntu/tomcat"
 export CONF_DIR="$APP_HOME/webapps/ROOT/WEB-INF/classes/conf"
 ipAddress=`hostname -I|awk '{$1=$1};1'`
 logsBucket=facilio-server-logs
-servername=`grep "app.domain" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
+servername=`grep "app.domain=" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 logsBucket=`grep "logs.bucket" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 
 today=`date +%FT%H-%M`
