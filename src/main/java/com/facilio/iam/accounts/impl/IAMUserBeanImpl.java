fb8618f1d52df32e843a3db4b55b43b338b5a0ab
vandhana <vandhana@facilio.com>
2020-06-05 00:06:22 +0530

    error handling while resetting password and the user is inactive

--- src/main/java/com/facilio/iam/accounts/impl/IAMUserBeanImpl.java
+++ src/main/java/com/facilio/iam/accounts/impl/IAMUserBeanImpl.java
@@ -1439,6 +1439,8 @@ public class IAMUserBeanImpl implements IAMUserBean {
 
 		selectBuilder.andCondition(CriteriaAPI.getCondition("Organizations.DELETED_TIME", "orgDeletedTime", "-1", NumberOperators.EQUALS));
 		selectBuilder.andCondition(CriteriaAPI.getCondition("Account_ORG_Users.DELETED_TIME", "orgUserDeletedTime", "-1", NumberOperators.EQUALS));
+		selectBuilder.andCondition(CriteriaAPI.getCondition("Account_ORG_Users.USER_STATUS", "userStatus", "1", NumberOperators.EQUALS));
+
 		if(StringUtils.isNotEmpty(identifier)) {
 			selectBuilder.andCondition(CriteriaAPI.getCondition("Account_Users.IDENTIFIER", "identifier", identifier, StringOperators.IS));
 		}
