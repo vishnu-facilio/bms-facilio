e17fadf525d38faffa736ba54329e54b0311b044
Anupriya <anupriya@facilio.com>
2020-06-08 23:02:14 +0530

    Connected App delete

--- src/main/java/com/facilio/bmsconsole/commands/DeleteConnectedAppCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/DeleteConnectedAppCommand.java
@@ -6,6 +6,7 @@ import com.facilio.constants.FacilioConstants;
 import com.facilio.db.builder.GenericDeleteRecordBuilder;
 import com.facilio.db.criteria.CriteriaAPI;
 import com.facilio.db.criteria.operators.NumberOperators;
+import com.facilio.db.criteria.operators.StringOperators;
 import com.facilio.modules.ModuleFactory;
 
 public class DeleteConnectedAppCommand extends FacilioCommand {
@@ -19,7 +20,25 @@ public class DeleteConnectedAppCommand extends FacilioCommand {
 			GenericDeleteRecordBuilder builder = new GenericDeleteRecordBuilder()
 					.table(ModuleFactory.getConnectedAppSAMLModule().getTableName())
 					.andCondition(CriteriaAPI.getCondition("CONNECTEDAPP_ID","connectedAppId",String.valueOf(connectedAppId), NumberOperators.EQUALS));
+
+			builder.delete();
 			
+			builder = new GenericDeleteRecordBuilder()
+					.table(ModuleFactory.getConnectedAppWidgetsModule().getTableName())
+					.andCondition(CriteriaAPI.getCondition("CONNECTEDAPP_ID","connectedAppId",String.valueOf(connectedAppId), NumberOperators.EQUALS));
+
+			builder.delete();
+			
+			builder = new GenericDeleteRecordBuilder()
+					.table(ModuleFactory.getVariablesModule().getTableName())
+					.andCondition(CriteriaAPI.getCondition("CONNECTEDAPP_ID","connectedAppId",String.valueOf(connectedAppId), NumberOperators.EQUALS));
+
+			builder.delete();
+			
+			builder = new GenericDeleteRecordBuilder()
+					.table(ModuleFactory.getConnectedAppConnectorsModule().getTableName())
+					.andCondition(CriteriaAPI.getCondition("CONNECTEDAPP_ID","connectedAppId",String.valueOf(connectedAppId), NumberOperators.EQUALS));
+
 			builder.delete();
 			
 			builder = new GenericDeleteRecordBuilder()
