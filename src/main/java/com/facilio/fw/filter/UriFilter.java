806cbac56304fa12822a4ad9167375156fecb818
Manthosh <manthosh@facilio.com>
2020-06-08 17:50:59 +0530

    Making sure any api call that has struts entry will go through security filters except admin actions

--- src/main/java/com/facilio/fw/filter/UriFilter.java
+++ src/main/java/com/facilio/fw/filter/UriFilter.java
@@ -7,6 +7,9 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class UriFilter implements Filter {
     @Override
@@ -15,9 +18,30 @@ public class UriFilter implements Filter {
     }
 
     private static final String URL_PATTERN = "/api/";
-    private static final String AUTH_API = "/auth/api/";
-    private static final String IOT_API = "/iot/api/";
-    private static final String MIGRATION_URI = "/internal/";
+//    Shouldn't be needed as it doesn't match the url pattern
+//    private static final String MIGRATION_URI = "/internal/";
+
+    private static List<String> initWhiteListedUrls() {
+        //The following urls won't be excluded. They will forwarded without app name parsing
+        //If a url is added here, it needs to be handled in web.xml as well for struts2 to work. But that shouldn't be done.
+
+        List<String> urls = new ArrayList<>();
+        urls.add("/auth/api/");
+        urls.add("/iot/api/");
+        return urls;
+    }
+
+    private static final List<String> WHITE_LISTED_URIS = Collections.unmodifiableList(initWhiteListedUrls());
+
+    private boolean isWhiteListedUri (String uri) {
+        for (String url : WHITE_LISTED_URIS) {
+            boolean isWhiteListed = uri.contains(url);
+            if (isWhiteListed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
@@ -26,10 +50,13 @@ public class UriFilter implements Filter {
 
         if (StringUtils.isEmpty((CharSequence) request.getAttribute(RequestUtil.REQUEST_APP_NAME))) {
             String reqUri = request.getRequestURI();
-            if (!reqUri.startsWith(AUTH_API) && !reqUri.startsWith(IOT_API) && !reqUri.startsWith(MIGRATION_URI)) {
+            if (isWhiteListedUri(reqUri)) {
+                req.getRequestDispatcher(reqUri).forward(request, response); //Doing this to restrict direct hitting of struts2 urls
+            }
+            else {
                 int idx = reqUri.indexOf(URL_PATTERN);
                 if (idx == 0) { //Doing this to make APIs called without app name to go through the usual security/ validation filters since we have struts2 configured to handle index.jsp globally
-                    req.getRequestDispatcher(reqUri).forward(request, response);
+                    req.getRequestDispatcher(reqUri).forward(request, response); //Doing this to restrict direct hitting of struts2 urls
                 } else if (idx > 0) {
                     String appName = reqUri.substring(1, idx);
                     String newUri = reqUri.substring(idx);
@@ -40,9 +67,6 @@ public class UriFilter implements Filter {
                 } else {
                     chain.doFilter(request, response);
                 }
-            }
-            else {
-                chain.doFilter(request, response);
             }
         }
         else {
