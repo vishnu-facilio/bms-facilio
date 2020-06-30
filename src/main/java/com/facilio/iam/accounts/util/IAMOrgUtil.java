9aabfb38f3ece498d1c17bdcb8df40cd838e889b
simran <simran@facilio.com>
2020-06-30 20:47:10 +0530

    Increased expiry time for org logo url to 60 mins

--- src/main/java/com/facilio/iam/accounts/util/IAMOrgUtil.java
+++ src/main/java/com/facilio/iam/accounts/util/IAMOrgUtil.java
@@ -16,45 +16,45 @@ public class IAMOrgUtil {
 	public static IAMAccount signUpOrg(org.json.simple.JSONObject jObj, Locale locale) throws Exception {
 		return FacilioService.runAsServiceWihReturn(() -> IAMUtil.getOrgBean().signUpOrg(jObj, locale));
 	}
-	
+
 	public static boolean rollBackSignedUpOrg(long orgId, long superAdminId) throws Exception {
 		return FacilioService.runAsServiceWihReturn(() -> IAMUtil.getOrgBean().rollbackSignUpOrg(orgId, superAdminId));
 	}
-	
+
 	public static boolean updateOrg(long orgId, Organization org) throws Exception {
 		return FacilioService.runAsServiceWihReturn(() -> IAMUtil.getOrgBean().updateOrgv2(orgId, org));
 	}
-	
+
 	public static boolean deleteOrg(long orgId) throws Exception {
 		return FacilioService.runAsServiceWihReturn(() -> IAMUtil.getOrgBean().deleteOrgv2(orgId));
 	}
-	
+
 	public static Organization getOrg(long orgId) throws Exception {
 		return FacilioService.runAsServiceWihReturn(() -> IAMUtil.getOrgBean().getOrgv2(orgId));
 	}
-	
+
 	public static Organization getOrg(String orgDomain) throws Exception {
 		return FacilioService.runAsServiceWihReturn(() -> IAMUtil.getOrgBean().getOrgv2(orgDomain));
 	}
-	
+
 	public static List<Organization> getOrgs() throws Exception {
 		return FacilioService.runAsServiceWihReturn(() -> IAMUtil.getOrgBean().getOrgs());
 	}
-	
+
 	public static void updateLoggerLevel(int level, long orgId) throws Exception {
 		FacilioService.runAsService(() -> IAMUtil.getOrgBean().updateLoggerLevel(level, orgId));
 	}
-	
+
 	public static Organization createOrgFromProps(Map<String, Object> prop) throws Exception {
 		Organization org = FieldUtil.getAsBeanFromMap(prop, Organization.class);
 		if (org.getLogoId() > 0) {
 			FileStore fs = FacilioFactory.getFileStore();
 //			org.setLogoUrl(fs.getPrivateUrl(org.getLogoId(), false));
-			org.setLogoUrl(fs.newPreviewFileUrl("organization",org.getLogoId()));
+			org.setLogoUrl(fs.newPreviewFileUrl("organization", org.getLogoId(), 3600000));
 			org.setOriginalUrl(fs.orginalFileUrl(org.getLogoId()));
 		}
 		return org;
 	}
-	
-	
+
+
 }
