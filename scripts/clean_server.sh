93b5d5377341405cbce4b0585b0a615db8c13a9a
Bhuvaneswaran <bhuvaneswaran.g@facilio.com>
2020-07-02 22:21:40 +0530

    Added clean_server to clear webapps directory, kill_server will stop the server. Added BeforeInstall hook to run clean_server.

---
+++ scripts/clean_server.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+echo "stopping the server..."
+
+export APP_HOME="/home/facilio/tomcat"
+export CONF_DIR="$APP_HOME/webapps/ROOT/WEB-INF/classes/conf"
+
+ipAddress=`hostname -I|awk '{$1=$1};1'`
+servername=`grep "api.servername" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
+logsBucket=`grep "logs.bucket" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
+today=`date +%F-%H-%M-%S`
+
+cd $APP_HOME
+pid=`/home/facilio/jdk/bin/jps | grep Bootstrap| cut -d' ' -f1`
+if [ -z "$pid" ]; then
+    echo "Java process is not running"
+else
+    echo "Shutting down java process $pid"
+    kill -9 $pid
+fi
+# sh bin/shutdown.sh
+sh /home/facilio/move_logs.sh
+aws s3 mv $APP_HOME/logs/catalina.out s3://$logsBucket/$servername/$ipAddress/catalinaout.$today.log
+rm -rf $APP_HOME/logs/*
+rm -rf webapps/ROOT
+rm -rf webapps/ROOT.war
+
+echo "server stopped..."
--- scripts/kill_server.sh
+++ scripts/kill_server.sh
@@ -2,25 +2,14 @@
 echo "stopping the server..."
 
 export APP_HOME="/home/facilio/tomcat"
-export CONF_DIR="$APP_HOME/webapps/ROOT/WEB-INF/classes/conf"
-
-ipAddress=`hostname -I|awk '{$1=$1};1'`
-servername=`grep "api.servername" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
-logsBucket=`grep "logs.bucket" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
-today=`date +%F-%H-%M-%S`
 
 cd $APP_HOME
+
 pid=`/home/facilio/jdk/bin/jps | grep Bootstrap| cut -d' ' -f1`
 if [ -z "$pid" ]; then
     echo "Java process is not running"
     exit 0;
 fi
 kill -9 $pid
-# sh bin/shutdown.sh
-sh /home/facilio/move_logs.sh
-aws s3 mv $APP_HOME/logs/catalina.out s3://$logsBucket/$servername/$ipAddress/catalinaout.$today.log
-rm -rf $APP_HOME/logs/*
-rm -rf webapps/ROOT
-rm -rf webapps/ROOT.war
 
 echo "server stopped..."
