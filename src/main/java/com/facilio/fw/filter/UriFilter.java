7db9ac1f4f6eb658af1b9ee12af749f860b18f22
Manthosh <manthosh@facilio.com>
2020-07-02 14:57:33 +0530

    Throwing 404 all urls with extension except whitelisted ones

--- src/main/java/com/facilio/fw/filter/UriFilter.java
+++ src/main/java/com/facilio/fw/filter/UriFilter.java
@@ -1,47 +1,70 @@
 package com.facilio.fw.filter;
 
 import com.facilio.util.RequestUtil;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class UriFilter implements Filter {
+    private static final String URL_PATTERN = "/api/";
+
+    private List<String> whiteListedUris = null;
+    private List<String> allowedExtensions = null;
+
+    private List<String> parseConfig (String config) {
+        if (StringUtils.isNotEmpty(config)) {
+            List<String> values = new ArrayList<>();
+            String[] split = config.split("\\s*,\\s*");
+            for (String value : split) {
+                values.add(value);
+            }
+            return Collections.unmodifiableList(values);
+        }
+        return null;
+    }
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        String excludeUrls = filterConfig.getInitParameter("exclude");
+        this.whiteListedUris = parseConfig(excludeUrls);
+        String extensions = filterConfig.getInitParameter("allowed-extensions");
+        this.allowedExtensions = parseConfig(extensions);
     }
-
-    private static final String URL_PATTERN = "/api/";
-//    Shouldn't be needed as it doesn't match the url pattern
-//    private static final String MIGRATION_URI = "/internal/";
-
-    private static List<String> initWhiteListedUrls() {
-        //The following urls won't be excluded. They will forwarded without app name parsing
-        //If a url is added here, it needs to be handled in web.xml as well for struts2 to work. But that shouldn't be done.
-
-        List<String> urls = new ArrayList<>();
-        urls.add("/auth/api/");
-        urls.add("/iot/api/");
-        return urls;
-    }
-
-    private static final List<String> WHITE_LISTED_URIS = Collections.unmodifiableList(initWhiteListedUrls());
 
     private boolean isWhiteListedUri (String uri) {
-        for (String url : WHITE_LISTED_URIS) {
-            boolean isWhiteListed = uri.contains(url);
-            if (isWhiteListed) {
-                return true;
+        if (CollectionUtils.isNotEmpty(whiteListedUris)) {
+            for (String url : whiteListedUris) {
+                boolean isWhiteListed = uri.startsWith(url);
+                if (isWhiteListed) {
+                    return true;
+                }
             }
         }
         return false;
+    }
+
+    private boolean isAllowedExtension (String uri) {
+        if (CollectionUtils.isNotEmpty(allowedExtensions)) {
+            String extension = FilenameUtils.getExtension(uri);
+            String fullName = FilenameUtils.getName(uri);
+            String baseName = FilenameUtils.getBaseName(uri);
+
+            if (StringUtils.isEmpty(extension)) {
+                return fullName.equals(baseName); //Not allowing urls that end with .
+            }
+            else {
+                return allowedExtensions.contains(extension);
+            }
+        }
+        return true;
     }
 
     @Override
@@ -49,10 +72,14 @@ public class UriFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        if (StringUtils.isEmpty((CharSequence) request.getAttribute(RequestUtil.REQUEST_APP_NAME))) {
+        // Commenting out the if check because it shouldn't be called when app name is set
+//        if (StringUtils.isEmpty((CharSequence) request.getAttribute(RequestUtil.REQUEST_APP_NAME))) {
             String reqUri = RequestUtil.normalize(request.getRequestURI(), true);
             if (isWhiteListedUri(reqUri)) {
                 req.getRequestDispatcher(reqUri).forward(request, response); //Doing this to restrict direct hitting of struts2 urls
+            }
+            else if (!isAllowedExtension(reqUri)) {
+                send404(response);
             }
             else {
                 int idx = reqUri.indexOf(URL_PATTERN);
@@ -69,11 +96,17 @@ public class UriFilter implements Filter {
                     chain.doFilter(request, response);
                 }
             }
-        }
-        else {
-            System.out.println("Else of facilio filter : "+request.getAttribute(RequestUtil.REQUEST_APP_NAME));
-            chain.doFilter(request, response);
-        }
+//        }
+//        else {
+//            System.out.println("Else of facilio filter : "+request.getAttribute(RequestUtil.REQUEST_APP_NAME));
+//            chain.doFilter(request, response);
+//        }
+    }
+
+    private void send404(HttpServletResponse response) {
+        response.setContentType("text/plain");
+        response.setStatus(404);
+        response.resetBuffer();
     }
 
     @Override
