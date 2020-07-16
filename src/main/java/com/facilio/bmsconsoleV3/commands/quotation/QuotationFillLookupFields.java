381d2b934659bf3eaefb309b4ee0430035210878
Muhamed Aashiq <aashiq@facilio.com>
2020-07-16 15:43:30 +0530

    Quotation fill custom lookup fields

--- src/main/java/com/facilio/bmsconsoleV3/commands/quotation/QuotationFillLookupFields.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/quotation/QuotationFillLookupFields.java
@@ -5,14 +5,17 @@ import com.facilio.bmsconsole.commands.FacilioCommand;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.fw.BeanFactory;
 import com.facilio.modules.FieldFactory;
+import com.facilio.modules.FieldType;
 import com.facilio.modules.fields.FacilioField;
 import com.facilio.modules.fields.LookupField;
 import com.facilio.modules.fields.LookupFieldMeta;
 import org.apache.commons.chain.Context;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class QuotationFillLookupFields extends FacilioCommand {
 
@@ -28,15 +31,21 @@ public class QuotationFillLookupFields extends FacilioCommand {
         fetchLookupsList.add((LookupField) fieldsAsMap.get("billToAddress"));
         fetchLookupsList.add((LookupField) fieldsAsMap.get("client"));
         fetchLookupsList.add((LookupField) fieldsAsMap.get("tenant"));
-
-        LookupFieldMeta workorderField =  new LookupFieldMeta((LookupField) fieldsAsMap.get("workorder"));
+        LookupFieldMeta workorderField = new LookupFieldMeta((LookupField) fieldsAsMap.get("workorder"));
         LookupField pmTrigger = (LookupField) modBean.getField("trigger", FacilioConstants.ContextNames.WORK_ORDER);
-       workorderField.addChildLookupField(pmTrigger);
+        workorderField.addChildLookupField(pmTrigger);
         fetchLookupsList.add(workorderField);
-
         fetchLookupsList.add((LookupField) fieldsAsMap.get("moduleState"));
         fetchLookupsList.add((LookupField) fieldsAsMap.get("contact"));
         fetchLookupsList.add((LookupField) fieldsAsMap.get("tax"));
+
+        // TEMP Adding custom lookup fields until supported in v3
+        List<FacilioField> customLookupFields = fields.stream().filter(field -> (field.getDefault() != null && !field.getDefault()) && (field.getDataType() == FieldType.LOOKUP.getTypeAsInt())).collect(Collectors.toList());
+        if (CollectionUtils.isNotEmpty(customLookupFields)) {
+            customLookupFields.forEach(field -> fetchLookupsList.add((LookupField) field));
+        }
+
+
         context.put(FacilioConstants.ContextNames.FETCH_SUPPLEMENTS, fetchLookupsList);
 
         return false;
