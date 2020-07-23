37b32cd411e034e91547a1ad7a2ba5ec0f6188fd
benitta <benitta@facilio.com>
2020-07-23 23:32:22 +0530

    Logger for unable to connect

--- src/main/java/com/facilio/services/email/ImapsClient.java
+++ src/main/java/com/facilio/services/email/ImapsClient.java
@@ -32,7 +32,6 @@ public class ImapsClient {
             properties.setProperty("mail.imap.socketFactory.fallback", "false");
 
             Session session = Session.getDefaultInstance(properties, null);
-
             try {
                 store = session.getStore("imaps");
                 store.connect(mailDetails.getMailServer(), mailDetails.getPort(), mailDetails.getUserName(), mailDetails.getPassword());
@@ -65,11 +64,15 @@ public class ImapsClient {
 
     public void getMessageGtUID(long latestMessageUID) throws Exception {
         if (store != null) {
-            IMAPFolder inbox = null;
-            inbox = (IMAPFolder) store.getFolder(folderName);
-            inbox.open(Folder.READ_ONLY);
-            Message[] messages = inbox.getMessagesByUID(latestMessageUID + 1, UIDFolder.MAXUID);
-            parseMessage(messages, inbox, supportEmailContext);
+            if(store.isConnected()) {
+                IMAPFolder inbox = null;
+                inbox = (IMAPFolder) store.getFolder(folderName);
+                inbox.open(Folder.READ_ONLY);
+                Message[] messages = inbox.getMessagesByUID(latestMessageUID + 1, UIDFolder.MAXUID);
+                parseMessage(messages, inbox, supportEmailContext);
+            } else {
+                log.info("Unable to connet IMAP for support Mail " + supportEmailContext.getId());
+            }
         }
     }
 
@@ -107,27 +110,34 @@ public class ImapsClient {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        finally {
+            closeFolder(inbox);
+        }
     }
     public void getNDaysMails(int days) throws  Exception {
         if (store != null) {
             IMAPFolder inbox = null;
-            inbox = (IMAPFolder) store.getFolder(folderName);
-            inbox.open(Folder.READ_ONLY);
-            long hrsToCheckinMillis= days * 10 * 3600000;
-            long endDate = System.currentTimeMillis();
-            long startDate = System.currentTimeMillis() - hrsToCheckinMillis ;
-            SearchTerm olderThan = new ReceivedDateTerm(ComparisonTerm.LE, new Date(endDate));
-            SearchTerm newerThan = new ReceivedDateTerm(ComparisonTerm.GE, new Date(startDate));
-            SearchTerm andTerm = new AndTerm(olderThan, newerThan);
-            Message messages[] = inbox.search(andTerm);
-            log.info("Size" + messages.length);
-            parseMessage(messages, inbox, supportEmailContext);
+            if(store.isConnected()) {
+                inbox = (IMAPFolder) store.getFolder(folderName);
+                inbox.open(Folder.READ_ONLY);
+                long hrsToCheckinMillis= days * 10 * 3600000;
+                long endDate = System.currentTimeMillis();
+                long startDate = System.currentTimeMillis() - hrsToCheckinMillis ;
+                SearchTerm olderThan = new ReceivedDateTerm(ComparisonTerm.LE, new Date(endDate));
+                SearchTerm newerThan = new ReceivedDateTerm(ComparisonTerm.GE, new Date(startDate));
+                SearchTerm andTerm = new AndTerm(olderThan, newerThan);
+                Message messages[] = inbox.search(andTerm);
+                log.info("Size" + messages.length);
+                parseMessage(messages, inbox, supportEmailContext);
+            } else {
+                log.info("Unable to connet IMAP for support Mail " + supportEmailContext.getId());
+            }
         }
     }
     private void closeFolder(Folder inbox) {
         try {
             if (inbox != null)
-                inbox.close(false);
+                inbox.close(true);
         } catch (MessagingException e) {
             e.printStackTrace();
         }
