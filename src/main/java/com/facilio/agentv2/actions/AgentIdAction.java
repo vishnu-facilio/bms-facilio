8be6605aac53abda748ab1afd30ac8d3c25744b8
arunkumar <arunkumar@facilio.com>
2020-08-07 13:35:08 +0530

    temp fix

--- src/main/java/com/facilio/agentv2/actions/AgentIdAction.java
+++ src/main/java/com/facilio/agentv2/actions/AgentIdAction.java
@@ -143,6 +143,81 @@ public class AgentIdAction extends AgentActionV2 {
 
 		return SUCCESS;
 	}
+	public String devices() {
+        try {
+        	FacilioContext context = new FacilioContext();
+        	context.put(AgentConstants.AGENT_ID, getAgentId());
+        	context.put(AgentConstants.CONTROLLER_TYPE, getControllerType());
+        	context.put(FacilioConstants.ContextNames.PAGINATION, getPagination());
+        	List<Map<String, Object>> devices = FieldDeviceApi.getDevices(context);
+			setResult(AgentConstants.DATA, devices);
+        	 ok();
+        } catch (Exception e) {
+            LOGGER.info("Exception occurred while getting devices ", e);
+            setResult(AgentConstants.RESULT, ERROR);
+            setResult(AgentConstants.EXCEPTION, e.getMessage());
+            internalError();
+        }
+        return SUCCESS;
+    }
+	
+    public String devicesCount() {
+        try {
+        	FacilioContext context = new FacilioContext();
+            context.put(AgentConstants.AGENT_ID, getAgentId());
+            context.put(AgentConstants.CONTROLLER_TYPE, getControllerType());
+            context.put(FacilioConstants.ContextNames.FETCH_COUNT, true);
+            List<Map<String, Object>> devices = FieldDeviceApi.getDevices( context);
+            long count = 0;
+            if (CollectionUtils.isNotEmpty(devices)) {
+                count =(long) devices.get(0).get(AgentConstants.ID);
+            }
+            
+            setResult(AgentConstants.DATA, count);
+            ok();
+        } catch (Exception e) {
+            LOGGER.info("Exception occurred while getting devices ", e);
+            setResult(AgentConstants.RESULT, ERROR);
+            setResult(AgentConstants.EXCEPTION, e.getMessage());
+            internalError();
+        }
+        return SUCCESS;
+    }
+    
+    public String getControllerCount() {
+        try {
+        	FacilioContext context = new FacilioContext();
+			context.put(AgentConstants.AGENT_ID, getAgentId());
+			context.put(AgentConstants.SEARCH_KEY, getName());
+			context.put(AgentConstants.CONTROLLER_TYPE, getControllerType());
+            setResult(AgentConstants.RESULT, SUCCESS);
+            setResult(AgentConstants.DATA, ControllerApiV2.getControllersCount(context));
+            ok();
+            return SUCCESS;
+        } catch (Exception e) {
+            LOGGER.info("Exception occurred while getting controller count for agentId->" + getAgentId());
+            setResult(AgentConstants.EXCEPTION, e.getMessage());
+            internalError();
+        }
+        return SUCCESS;
+    }
+    public String getControllers() {
+        try {
+			FacilioContext context = new FacilioContext();
+			context.put(AgentConstants.SEARCH_KEY, getName());
+			context.put(AgentConstants.AGENT_ID, getAgentId());
+			context.put(AgentConstants.CONTROLLER_TYPE, getControllerType());
+			context.put(FacilioConstants.ContextNames.PAGINATION, getPagination());
+			List<Map<String, Object>> controllers = ControllerApiV2.getControllerDataForAgent(context);
+            setResult(AgentConstants.DATA, controllers);
+            ok();
+        } catch (Exception e) {
+            setResult(AgentConstants.EXCEPTION, e.getMessage());
+            internalError();
+            LOGGER.info("exception while fetching controller data fro agent " + agentId + " ", e);
+        }
+        return SUCCESS;
+    }
 
     public String pingAgent() throws Exception {
         try {
