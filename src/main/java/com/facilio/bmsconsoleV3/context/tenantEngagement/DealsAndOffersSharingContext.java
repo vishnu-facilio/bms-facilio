3df95a35663373b54a821dda4b4db381e4374a52
Muhamed Aashiq <aashiq@facilio.com>
2020-08-11 13:22:56 +0530

    Renaming sharing info column names

--- src/main/java/com/facilio/bmsconsoleV3/context/tenantEngagement/DealsAndOffersSharingContext.java
+++ src/main/java/com/facilio/bmsconsoleV3/context/tenantEngagement/DealsAndOffersSharingContext.java
@@ -3,5 +3,14 @@ package com.facilio.bmsconsoleV3.context.tenantEngagement;
 import com.facilio.bmsconsoleV3.context.CommunitySharingInfoContext;
 
 public class DealsAndOffersSharingContext extends CommunitySharingInfoContext {
+
     private DealsAndOffersContext deals;
+
+    public DealsAndOffersContext getDeals() {
+        return deals;
+    }
+
+    public void setDeals(DealsAndOffersContext deals) {
+        this.deals = deals;
+    }
 }
