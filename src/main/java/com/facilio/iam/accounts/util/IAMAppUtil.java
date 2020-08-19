2d21111736ca4e97c17826c33508733374a0200b
aravind c <aravind@facilio.com>
2020-08-19 10:21:38 +0530

    checks in missing files

--- src/main/java/com/facilio/iam/accounts/util/IAMAppUtil.java
+++ src/main/java/com/facilio/iam/accounts/util/IAMAppUtil.java
@@ -1,9 +1,14 @@
 package com.facilio.iam.accounts.util;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.facilio.accounts.dto.AppDomain;
 import com.facilio.accounts.dto.AppDomain.AppDomainType;
+import com.facilio.accounts.dto.Organization;
+import com.facilio.accounts.sso.AccountSSO;
+import com.facilio.accounts.sso.SSOUtil;
 import com.facilio.service.FacilioService;
 
 public class IAMAppUtil {
@@ -20,6 +25,25 @@ public class IAMAppUtil {
 	
 	public static AppDomain getAppDomain(String appDomain) throws Exception {
 		return FacilioService.runAsServiceWihReturn(() -> IAMUtil.getUserBean().getAppDomain(appDomain));
+	}
+
+	public static Map<String, Object> getAppDomainType(String serverName) throws Exception {
+		AppDomain appDomain = getAppDomain(serverName);
+		Map<String, Object> result = new HashMap<>();
+		boolean isCustomDomain = AppDomain.DomainType.valueOf(appDomain.getDomainType()) == AppDomain.DomainType.CUSTOM;
+		result.put("isCustomDomain", isCustomDomain);
+		if (isCustomDomain) {
+			long orgId = appDomain.getOrgId();
+			Organization org = IAMOrgUtil.getOrg(orgId);
+			AccountSSO sso = IAMOrgUtil.getAccountSSO(org.getDomain());
+			if (sso != null && sso.getIsActive()) {
+				result.put("isSSOEnabled", true);
+				String ssoEndpoint = SSOUtil.getSSOEndpoint(org.getDomain());
+				result.put("ssoEndPoint", ssoEndpoint);
+			}
+			result.put("logo_url", org.getLogoUrl());
+		}
+		return result;
 	}
 	
 	public static AppDomain getAppDomain(long appDomainId) throws Exception {
