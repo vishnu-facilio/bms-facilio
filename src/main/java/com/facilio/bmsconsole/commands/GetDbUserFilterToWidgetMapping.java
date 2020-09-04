f084d728c69248f30d9ca111ed6d888cba649796
Sainath SR <sainath@facilio.com>
2020-09-04 14:03:33 +0530

    custom script db filters plus auto format

--- src/main/java/com/facilio/bmsconsole/commands/GetDbUserFilterToWidgetMapping.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetDbUserFilterToWidgetMapping.java
@@ -35,116 +35,106 @@ public class GetDbUserFilterToWidgetMapping extends FacilioCommand {
 
 	@Override
 	public boolean executeCommand(Context context) throws Exception {
-		
-			Map<Long, List<Long>> widgetUserFiltersMap=new HashMap<>();
-			
-		DashboardFilterContext dbFilter=(DashboardFilterContext) context.get(FacilioConstants.ContextNames.DASHBOARD_FILTER);
-		if(dbFilter!=null) {
-			
-			List<DashboardUserFilterContext> userFilters=dbFilter.getDashboardUserFilters();
-			
-			 DashboardContext dashboard=(DashboardContext) context.get(FacilioConstants.ContextNames.DASHBOARD);
-			 
-			 DashboardTabContext dashboardTab=(DashboardTabContext) context.get(FacilioConstants.ContextNames.DASHBOARD_TAB);
-			 
-			 List<DashboardWidgetContext> widgets=new ArrayList<>();
-			 if(dashboard!=null)
-			 {
-				 	widgets=dashboard.getDashboardWidgets();
-			 }
-			 else if(dashboardTab!=null)
-			 {
-				 widgets=dashboardTab.getDashboardWidgets();
-			 }
-			 else {
-				 throw new Exception("NO dashboard or dashboard tab");
-			 }
-			 
-			//for each  filter , check which widgets have the same module as the filter.
-			 
-				 
-				 
-				 
-				
-				 			
-				 
-				 if(widgets!=null&&userFilters!=null)
-				 {
-					 
-					 for (DashboardWidgetContext widget : widgets) {
-					
-						 long widgetId=widget.getId();
-						 
-						 long widgetModuleId=DashboardFilterUtil.getModuleIdFromWidget(widget);
 
-						 
-						 
-						 
-						 for(DashboardUserFilterContext filter:userFilters)
-						 {
-							 
-							 
-							 							 
-							 FacilioModule  filterModule=filter.getField().getModule();
-							 						
-							 ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
-							 
-							 List<FacilioModule> filterChildModules=modBean.getChildModules(filterModule);
-							 
-							 List<Long> filterChildModuleIds=new ArrayList<Long>();
-							 
-							 if(filterChildModules!=null)
-							 {
-								  filterChildModuleIds=filterChildModules.stream().map((FacilioModule module)-> {return module.getModuleId();}).collect(Collectors.toList());
-							 }
-							 
-							 
-							 //must check if widget module is either same as filter module or one of its children
-							 //Ex , ticketCategory field has module='ticket' but report_chart corresponding to workorders has module='workorder'
-							 
-							 JSONObject widgetSettings=widget.getWidgetSettings();
-							 boolean isFilterExclude=(boolean)widgetSettings.get("excludeDbFilters");
-							 Map<Long,FacilioField> widgetFilterFieldMap=filter.getWidgetFieldMap();
-							 boolean isFilterFieldMappedToWidgetField=false;
-							 
-							 if(widgetFilterFieldMap!=null&&widgetFilterFieldMap.containsKey(widgetId))
-							 {
-								 isFilterFieldMappedToWidgetField=true;
-							 }
-							 
-							 if(!isFilterExclude &&(widgetModuleId==filterModule.getModuleId()||filterChildModuleIds.contains(widgetModuleId)||isFilterFieldMappedToWidgetField))
-							 	
-							 	{
-							 	
-								 
-								 if(!widgetUserFiltersMap.containsKey(widgetId))
-								 {
-									 widgetUserFiltersMap.put(widgetId,new ArrayList<Long>());
-								 }
-								 widgetUserFiltersMap.get(widgetId).add(filter.getId());
-								 
-							 		
-							 	}
-						 }
-						 
-						 
+		Map<Long, List<Long>> widgetUserFiltersMap = new HashMap<>();
+
+		DashboardFilterContext dbFilter = (DashboardFilterContext) context
+				.get(FacilioConstants.ContextNames.DASHBOARD_FILTER);
+		if (dbFilter != null) {
+
+			List<DashboardUserFilterContext> userFilters = dbFilter.getDashboardUserFilters();
+
+			DashboardContext dashboard = (DashboardContext) context.get(FacilioConstants.ContextNames.DASHBOARD);
+
+			DashboardTabContext dashboardTab = (DashboardTabContext) context
+					.get(FacilioConstants.ContextNames.DASHBOARD_TAB);
+
+			List<DashboardWidgetContext> widgets = new ArrayList<>();
+			if (dashboard != null) {
+				widgets = dashboard.getDashboardWidgets();
+			} else if (dashboardTab != null) {
+				widgets = dashboardTab.getDashboardWidgets();
+			} else {
+				throw new Exception("NO dashboard or dashboard tab");
+			}
+
+			// for each filter , check which widgets have the same module as the filter.
+
+			if (widgets != null && userFilters != null) {
+
+				for (DashboardWidgetContext widget : widgets) {
+
+					long widgetId = widget.getId();
+
+					long widgetModuleId = DashboardFilterUtil.getModuleIdFromWidget(widget);
+
+					boolean isCustomScript = DashboardFilterUtil.isCustomScriptWidget(widget);
+
+					for (DashboardUserFilterContext filter : userFilters) {
+						if (isCustomScript) {
+							if (!widgetUserFiltersMap.containsKey(widgetId)) {
+								widgetUserFiltersMap.put(widgetId, new ArrayList<Long>());
+							}
+							widgetUserFiltersMap.get(widgetId).add(filter.getId());
+
+						} 
+
+						// for all  other widget , map only the filters that apply to them
+
+						else {
+							FacilioModule filterModule = filter.getField().getModule();
+
+							ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+
+							List<FacilioModule> filterChildModules = modBean.getChildModules(filterModule);
+
+							List<Long> filterChildModuleIds = new ArrayList<Long>();
+
+							if (filterChildModules != null) {
+								filterChildModuleIds = filterChildModules.stream().map((FacilioModule module) -> {
+									return module.getModuleId();
+								}).collect(Collectors.toList());
+							}
+
+							// must check if widget module is either same as filter module or one of its
+							// children
+							// Ex , ticketCategory field has module='ticket' but report_chart corresponding
+							// to workorders has module='workorder'
+
+							JSONObject widgetSettings = widget.getWidgetSettings();
+							boolean isFilterExclude = (boolean) widgetSettings.get("excludeDbFilters");
+							Map<Long, FacilioField> widgetFilterFieldMap = filter.getWidgetFieldMap();
+							boolean isFilterFieldMappedToWidgetField = false;
+
+							if (widgetFilterFieldMap != null && widgetFilterFieldMap.containsKey(widgetId)) {
+								isFilterFieldMappedToWidgetField = true;
+							}
+
+							if (!isFilterExclude && (widgetModuleId == filterModule.getModuleId()
+									|| filterChildModuleIds.contains(widgetModuleId)
+									|| isFilterFieldMappedToWidgetField))
+
+							{
+
+								if (!widgetUserFiltersMap.containsKey(widgetId)) {
+									widgetUserFiltersMap.put(widgetId, new ArrayList<Long>());
+								}
+								widgetUserFiltersMap.get(widgetId).add(filter.getId());
+
+							}
+
+						}
+
 					}
-				 }
-				 
-				 
-				 
-				 
-				 
-				 dbFilter.setWidgetUserFiltersMap(widgetUserFiltersMap);
-			
-			
-			 
-			
-			
+
+				}
+			}
+
+			dbFilter.setWidgetUserFiltersMap(widgetUserFiltersMap);
+
 		}
 
 		return false;
 	}
-
 
 }
