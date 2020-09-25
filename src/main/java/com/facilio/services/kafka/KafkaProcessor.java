4c6ad3e9a54ac81663f6779ca05038da7049c815
arunkumar <arunkumar@facilio.com>
2020-09-25 12:20:38 +0530

    check if org topic enabled or not

--- src/main/java/com/facilio/services/kafka/KafkaProcessor.java
+++ src/main/java/com/facilio/services/kafka/KafkaProcessor.java
@@ -1,14 +1,27 @@
 package com.facilio.services.kafka;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 
+import com.facilio.agent.module.AgentFieldFactory;
+import com.facilio.agent.module.AgentModuleFactory;
+import com.facilio.agentv2.AgentConstants;
 import com.facilio.aws.util.FacilioProperties;
 import com.facilio.dataprocessor.DataProcessorUtil;
+import com.facilio.db.builder.GenericSelectRecordBuilder;
+import com.facilio.db.criteria.CriteriaAPI;
+import com.facilio.db.criteria.operators.BooleanOperators;
+import com.facilio.db.criteria.operators.StringOperators;
+import com.facilio.modules.FacilioModule;
+import com.facilio.modules.FieldFactory;
+import com.facilio.modules.fields.FacilioField;
 import com.facilio.server.ServerInfo;
 import com.facilio.services.procon.message.FacilioRecord;
 import com.facilio.services.procon.processor.FacilioProcessor;
@@ -18,7 +31,10 @@ public class KafkaProcessor extends FacilioProcessor {
 
 
     private static final Logger LOGGER = LogManager.getLogger(KafkaProcessor.class.getName());
-    private DataProcessorUtil dataProcessorUtil;
+    private static final List<FacilioField> MESSAGE_TOPIC_FIELDS = AgentFieldFactory.getMessageTopicFields();
+	private static final FacilioModule MESSAGE_TOIPC_MODULE = AgentModuleFactory.getMessageToipcModule();
+
+	private DataProcessorUtil dataProcessorUtil;
 
     public KafkaProcessor(long orgId, String topic) {
         super(orgId, topic);
@@ -34,21 +50,37 @@ public class KafkaProcessor extends FacilioProcessor {
 
 
     @Override
-    public void processRecords(List<FacilioRecord> records) {
-        for (FacilioRecord record : records) {
-           System.out.println(" getting messages via kafka Record Id is : "+record.getId());
-            try {
-                if (!dataProcessorUtil.processRecord(record)) {
-                    LOGGER.info("Exception while processing ->" + record.getData());
-                }
-                getConsumer().commit(record);
-            } catch (Exception e){
-                LOGGER.info("Exception occurred while processing  ",e);
-            }
-        }
-    }
+	public void processRecords(List<FacilioRecord> records) {
+		try {
+			if (!isTopicEnabled()) {
+				LOGGER.info("Org Topic is disabled : "+getTopic());
+				return;
+			}
+			for (FacilioRecord record : records) {
+				if (!dataProcessorUtil.processRecord(record)) {
+					LOGGER.error("Exception while processing ->" + record.getData());
+				}
+				getConsumer().commit(record);
+			}
+		} catch (Exception e) {
+			LOGGER.error("Exception occurred while processing  ", e);
+		}
+	}
     
-    public long send(String orgDomain,JSONObject data) {
+    private boolean isTopicEnabled() throws Exception {
+    	
+    	if(StringUtils.isEmpty(getTopic())) {
+    		return false;
+    	}
+    	GenericSelectRecordBuilder builder = new GenericSelectRecordBuilder().select(MESSAGE_TOPIC_FIELDS)
+				.table(MESSAGE_TOIPC_MODULE.getTableName())
+				.andCondition(CriteriaAPI.getCondition(FieldFactory.getAsMap(MESSAGE_TOPIC_FIELDS).get(AgentConstants.MESSAGE_TOPIC),getTopic(),StringOperators.IS))
+				.andCondition(CriteriaAPI.getCondition(FieldFactory.getAsMap(MESSAGE_TOPIC_FIELDS).get(AgentConstants.IS_DISABLE),String.valueOf(false), BooleanOperators.IS));
+    	return MapUtils.isEmpty(builder.fetchFirst())?false:true;
+	}
+
+
+	public long send(String orgDomain,JSONObject data) {
     	FacilioRecord record= new FacilioRecord(orgDomain, data);
     	RecordMetadata metaData = (RecordMetadata) getProducer().putRecord(record);
     	return metaData.offset();
