bdcca18aaaf4c1229be274c93137086715c8faa8
manthosh <manthosh@facilio.com>
2020-10-09 12:52:35 +0530

    Revert "Removing pubsub for sutherland"
    This reverts commit 7f2cdb9dcefe48f58739339b0862c9e0f17c85a4.

--- src/main/java/com/facilio/bmsconsole/commands/ReadingPostProcessingCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/ReadingPostProcessingCommand.java
@@ -31,7 +31,7 @@ public class ReadingPostProcessingCommand extends FacilioCommand {
 //            }
             executeFormulae(context);
         }
-        if (AccountUtil.getCurrentOrg() != null && (AccountUtil.getCurrentOrg().getId() == 146 || AccountUtil.getCurrentOrg().getId() == 155) ){
+        if (AccountUtil.getCurrentOrg() != null && (AccountUtil.getCurrentOrg().getId() == 146 || AccountUtil.getCurrentOrg().getId() == 155 || AccountUtil.getCurrentOrg().getId() == 343) ){
             publishReadingChangeMessage(context);
         }
         LOGGER.debug("Post processing completed");
