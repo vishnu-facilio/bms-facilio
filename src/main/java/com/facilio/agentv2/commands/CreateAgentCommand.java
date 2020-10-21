70fa3a46141e2c4a59943c3ac6d8686b8564efcd
anand <anand.h@facilio.com>
2020-10-21 15:00:13 +0530

    add cloud agent job

--- src/main/java/com/facilio/agentv2/commands/CreateAgentCommand.java
+++ src/main/java/com/facilio/agentv2/commands/CreateAgentCommand.java
@@ -5,8 +5,13 @@ import com.facilio.agentv2.AgentApiV2;
 import com.facilio.agentv2.AgentConstants;
 import com.facilio.agentv2.FacilioAgent;
 import com.facilio.aws.util.AwsUtil;
+import com.facilio.tasker.ScheduleInfo;
+import com.facilio.tasker.job.JobContext;
+import com.facilio.tasker.job.JobStore;
 import org.apache.commons.chain.Context;
 import org.apache.log4j.LogManager;
+
+import java.time.LocalTime;
 
 public class CreateAgentCommand extends AgentV2Command {
 
@@ -21,33 +26,77 @@ public class CreateAgentCommand extends AgentV2Command {
             String certFileDownloadUrl = "certDownloadUrl";
             long agentId = AgentApiV2.addAgent(agent);
             agent.setId(agentId);
-            AwsUtil.addClientToPolicy(agent.getName(), orgDomainname, "facilio");
-            if (certFileDownloadUrl != null) {
-                context.put(AgentConstants.CERT_FILE_DOWNLOAD_URL, certFileDownloadUrl);
-                // pack agent and give a download link
-                agentDownloadUrl = packAgent(agent);
-                if ((agentDownloadUrl != null) && (agent.getType().equals("facilio")) && (agent.getType().equals("niagara"))) {
-                    switch (agent.getType()) {
-                        case "facilio":
-                        case "niagara":
-                            context.put(AgentConstants.DOWNLOAD_AGENT, agentDownloadUrl);
-                            break;
-                        default:
-                    }
-                } else {
-                    LOGGER.info(" agentDownload link cant be null ");
-                }
-
-                return false;
+            String type = agent.getType();
+            if (type.equalsIgnoreCase("facilio")
+                    || type.equalsIgnoreCase("niagara")) {
+                return createPolicy(context, orgDomainname, agent, certFileDownloadUrl);
+            } else if (type.equalsIgnoreCase("rest")) {
+                return createJob(agent);
             } else {
-                throw new Exception(" certFile download url can't be null ");
+                return false;
             }
         } else {
             throw new Exception(" agent missing from context " + context);
         }
     }
 
+    private boolean createJob(FacilioAgent agent) throws Exception {
+
+        JobContext cloudAgentJob = getJobContext(agent);
+        JobStore.addJob(cloudAgentJob);
+        return false;
+    }
+
+    private boolean createPolicy(Context context, String orgDomainname, FacilioAgent agent, String certFileDownloadUrl) throws Exception {
+        String agentDownloadUrl;
+        AwsUtil.addClientToPolicy(agent.getName(), orgDomainname, "facilio");
+        if (certFileDownloadUrl != null) {
+            context.put(AgentConstants.CERT_FILE_DOWNLOAD_URL, certFileDownloadUrl);
+            // pack agent and give a download link
+            agentDownloadUrl = packAgent(agent);
+            if ((agentDownloadUrl != null) && (agent.getType().equals("facilio")) && (agent.getType().equals("niagara"))) {
+                switch (agent.getType()) {
+                    case "facilio":
+                    case "niagara":
+                        context.put(AgentConstants.DOWNLOAD_AGENT, agentDownloadUrl);
+                        break;
+                    default:
+                }
+            } else {
+                LOGGER.info(" agentDownload link cant be null ");
+            }
+
+            return false;
+        } else {
+            throw new Exception(" certFile download url can't be null ");
+        }
+    }
+
     private String packAgent(FacilioAgent agent) {
         return "no implementation yet"; //TODO implement agent packing.
+    }
+
+    private JobContext getJobContext(FacilioAgent agent) {
+        long interval = agent.getInterval();
+        JobContext cloudAgentJobContext = new JobContext();
+        ScheduleInfo scheduleInfo = new ScheduleInfo();
+        scheduleInfo.setFrequencyType(ScheduleInfo.FrequencyType.DAILY);
+        long totalMinutesInADay = 60 * 24;
+        LocalTime time = LocalTime.of(0, 0);
+        for (long frequency = totalMinutesInADay / interval; frequency > 0; frequency--) {
+            time = time.plusMinutes(interval);
+            scheduleInfo.addTime(time);
+        }
+        cloudAgentJobContext.setSchedule(scheduleInfo);
+        cloudAgentJobContext.setJobId(agent.getId());
+        cloudAgentJobContext.setIsPeriodic(true);
+        cloudAgentJobContext.setJobName("CloudAgent");
+        cloudAgentJobContext.setActive(true);
+        cloudAgentJobContext.setExecutorName("facilio");
+        cloudAgentJobContext.setTransactionTimeout(300000);
+        cloudAgentJobContext.setExecutionTime(System.currentTimeMillis());
+        cloudAgentJobContext.setOrgId(AccountUtil.getCurrentOrg().getOrgId());
+        LOGGER.info("Adding Cloud Agent : " + agent.getName() + " : " + cloudAgentJobContext.toString());
+        return cloudAgentJobContext;
     }
 }
--- src/main/java/com/facilio/bmsconsole/jobs/CloudAgent.java
+++ src/main/java/com/facilio/bmsconsole/jobs/CloudAgent.java
@@ -2,33 +2,61 @@ package com.facilio.bmsconsole.jobs;
 
 import com.facilio.agentv2.AgentApiV2;
 import com.facilio.agentv2.FacilioAgent;
+import com.facilio.agentv2.actions.AddAgentAction;
 import com.facilio.fw.FacilioException;
 import com.facilio.tasker.job.FacilioJob;
 import com.facilio.tasker.job.JobContext;
 import com.facilio.timeseries.TimeSeriesAPI;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CloudAgent extends FacilioJob {
+    private static final Logger LOGGER = LogManager.getLogger(AddAgentAction.class.getName());
     @Override
     public void execute(JobContext jc) throws Exception {
         long jobId = jc.getJobId();
         FacilioAgent agent = AgentApiV2.getAgent(jobId);
         if (agent != null && agent.getType().equals("rest")) {
             long workflowId = agent.getWorkflowId();
-            List<JSONObject> payloads = getPayloadsFromWorkflow(workflowId);
-            for (JSONObject payload : payloads) {
-                TimeSeriesAPI.processPayLoad(0, payload, null);
-            }
+            getPayloadsFromWorkflowAndPushToMessageQueue(workflowId, agent);
         } else {
+            if (agent != null) {
+                LOGGER.info("Agent Type : " + agent.getType());
+            } else {
+                LOGGER.info("Agent is null ");
+            }
             throw new FacilioException("agent is null or invalid agent type");
         }
     }
 
-    private List<JSONObject> getPayloadsFromWorkflow(long workflowId) {
-        //TODO
+    private void pushToMessageQueue(List<JSONObject> payloads) throws Exception {
+        for (JSONObject payload : payloads) {
+            TimeSeriesAPI.processPayLoad(0, payload, null);
+        }
+    }
+
+    private void getPayloadsFromWorkflowAndPushToMessageQueue(long workflowId, FacilioAgent agent) throws Exception {
+        long lastDataReceivedTime = agent.getLastDataReceivedTime();
+        LOGGER.info("Last received Time : " + lastDataReceivedTime);
+        long interval = agent.getInterval() * 60 * 1000;
+        long currentTime = System.currentTimeMillis();
+        for (long noOfDataMissingIntervals = ((currentTime - lastDataReceivedTime) / interval) + 1L;
+             noOfDataMissingIntervals > 0; noOfDataMissingIntervals--) {
+            long nextTimestampToGetData = ((lastDataReceivedTime + interval) / interval) * interval;
+            LOGGER.info("Next Timestamp " + nextTimestampToGetData);
+            List<JSONObject> results = runWorkflow(workflowId, nextTimestampToGetData);
+            Thread.sleep(2000);
+            pushToMessageQueue(results);
+            lastDataReceivedTime = lastDataReceivedTime + interval;
+        }
+    }
+
+    private List<JSONObject> runWorkflow(long workflowId, long nextTimestampToGetData) {
+        //TODO get data from workflow for the time 'nextTimestampToGetData'
         return new ArrayList<>();
     }
 }
