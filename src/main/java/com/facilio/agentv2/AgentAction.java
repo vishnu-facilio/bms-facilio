ea4c03e369c8f180a32821eb61787af5e865eb45
arunkumar <arunkumar@facilio.com>
2020-10-28 14:22:10 +0530

    Revert "cert url changes"
    This reverts commit 14c1e126953895328fe9e32df93f134b73d7f837.

--- src/main/java/com/facilio/agentv2/AgentAction.java
+++ src/main/java/com/facilio/agentv2/AgentAction.java
@@ -1,9 +1,5 @@
 package com.facilio.agentv2;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
@@ -549,26 +545,14 @@ public class AgentAction extends AgentActionV2 {
 		}
 		return SUCCESS;
 	}
-	FileInputStream fileInputStream ;
-    public FileInputStream getFileInputStream() {
-		return fileInputStream;
-	}
 
-	public void setFileInputStream(FileInputStream fileInputStream) {
-		this.fileInputStream = fileInputStream;
-	}
-
-	public String downloadCertificate(){
+    public String downloadCertificate(){
         try{
-            String orgMessageTopic = FacilioService.runAsServiceWihReturn(()->getMessageTopic());
+            String orgMessageTopic = getMessageTopic();
             LOGGER.info("download certificate current org domain is :"+orgMessageTopic);
             FacilioAgent agent = AgentApiV2.getAgent(agentId);
             String downloadCertificateLink = DownloadCertFile.downloadCertificate(orgMessageTopic, "facilio");
-            File file = new File(downloadCertificateLink);
-            fileInputStream = new FileInputStream(file);
-            String fileName = orgMessageTopic+"_"+"cert"+".zip";
-			setFilename(fileName);
-			setContentType("application/zip");
+            setResult(AgentConstants.DATA, downloadCertificateLink);
             Objects.requireNonNull(agent, "no such agent");
             try {
                 AwsUtil.addClientToPolicy(agent.getName(), orgMessageTopic, "facilio");
@@ -594,7 +578,7 @@ public class AgentAction extends AgentActionV2 {
     			.andCondition(CriteriaAPI.getCondition(FieldFactory.getAsMap(AgentFieldFactory.getMessageTopicFields()).get("orgId"), String.valueOf(currentOrg.getOrgId()), StringOperators.IS));
     	Map<String,Object> prop =	builder.fetchFirst();
     	if(MapUtils.isEmpty(prop)) {
-    		return 	FacilioService.runAsServiceWihReturn(()->MessageQueueTopic.addMsgTopic(currentOrg.getDomain(), currentOrg.getOrgId())) ? currentOrg.getDomain():null;
+    		return 	MessageQueueTopic.addMsgTopic(currentOrg.getDomain(), currentOrg.getOrgId()) ? currentOrg.getDomain():null;
     	}
     	return prop.get("topic").toString();
     }
