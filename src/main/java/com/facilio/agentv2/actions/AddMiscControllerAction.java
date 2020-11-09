76ffe72514e1ef5a77c995e6de85621225f9a9d2
arunkumar <arunkumar@facilio.com>
2020-11-09 11:54:59 +0530

    changes in misc controller for rest type

--- src/main/java/com/facilio/agentv2/actions/AddMiscControllerAction.java
+++ src/main/java/com/facilio/agentv2/actions/AddMiscControllerAction.java
@@ -1,12 +1,17 @@
 package com.facilio.agentv2.actions;
 
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import com.facilio.accounts.util.AccountUtil;
 import com.facilio.agentv2.AgentConstants;
-import com.facilio.agentv2.iotmessage.AgentMessenger;
+import com.facilio.agentv2.controller.ControllerApiV2;
+import com.facilio.agentv2.device.Device;
+import com.facilio.agentv2.device.FieldDeviceApi;
 import com.facilio.agentv2.misc.MiscController;
 
 public class AddMiscControllerAction extends AgentIdAction{
@@ -17,11 +22,27 @@ public class AddMiscControllerAction extends AgentIdAction{
 	public String addMiscController() {
 		
 		try {
-			MiscController context = new MiscController();
-			context.setAgentId(getAgentId());
-			context.setName(getName());
-			context.setControllerType(getControllerType());
-			if(AgentMessenger.sendAddControllerCommand(context)) {
+			Device fieldDevice = new Device();
+			fieldDevice.setName(getName());
+			fieldDevice.setOrgId(AccountUtil.getCurrentOrg().getOrgId());
+			fieldDevice.setAgentId(getAgentId());
+			fieldDevice.setControllerType(getControllerType());
+			fieldDevice.setCreatedTime(System.currentTimeMillis());
+			Long deviceId = FieldDeviceApi.addFieldDevice(fieldDevice);
+			Long controllerId = null;
+			if(deviceId != null && deviceId > 0) {
+				MiscController context = new MiscController();
+				context.setAgentId(getAgentId());
+				context.setActive(true);
+				context.setDataInterval(900000);
+				context.setName(getName());
+				context.setControllerType(getControllerType());
+				context.setDeviceId(deviceId);
+				controllerId = ControllerApiV2.addController(context);
+			}
+			if(controllerId != null && controllerId >0) {
+				FieldDeviceApi.updateDeviceConfigured(deviceId);
+				LOGGER.info("successfully marked a Misc Device - "+getName()+" as a Controller. ");
 				setResult(AgentConstants.RESULT, SUCCESS);
 	            setResponseCode(HttpURLConnection.HTTP_OK);
 			}else {
