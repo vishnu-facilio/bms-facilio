ee1706189aa351f4ff9c225f2082ab3238866bbb
vandhana <vandhana@facilio.com>
2020-11-16 19:59:08 +0530

    inv req related fixes using the new approval license check

--- src/main/java/com/facilio/bmsconsole/commands/ValidateInventoryRequestUpdateCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/ValidateInventoryRequestUpdateCommand.java
@@ -9,11 +9,19 @@ import org.apache.commons.chain.Context;
 public class ValidateInventoryRequestUpdateCommand extends FacilioCommand{
     @Override
     public boolean executeCommand(Context context) throws Exception {
+
+        if (!AccountUtil.isFeatureEnabled(AccountUtil.FeatureLicense.NEW_APPROVALS)) {
+            return false;
+        }
+
         InventoryRequestContext inventoryRequestContext = (InventoryRequestContext) context.get(FacilioConstants.ContextNames.RECORD);
         if(inventoryRequestContext != null && inventoryRequestContext.getId() > 0) {
             InventoryRequestContext inv = (InventoryRequestContext) RecordAPI.getRecord(FacilioConstants.ContextNames.INVENTORY_REQUEST, inventoryRequestContext.getId());
-            if(inv.getApprovalFlowId() <= 0){
+            //inventory request should not updated after approval state.
+            if (inv.getApprovalFlowId() <= 0) {
                 throw new IllegalArgumentException("The Inventory request cannot be updated");
+            } else {
+                context.put(FacilioConstants.ContextNames.SKIP_APPROVAL_CHECK, true);
             }
         }
         return false;
