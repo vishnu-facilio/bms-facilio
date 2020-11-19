abcf9ca48e5dd8c1c8895484fee22c754b1e3242
vandhana <vandhana@facilio.com>
2020-11-19 13:32:18 +0530

    adding current user role to fetchDetails response

--- src/main/java/com/facilio/bmsconsole/actions/ApplicationAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/ApplicationAction.java
@@ -2,6 +2,7 @@ package com.facilio.bmsconsole.actions;
 
 import java.util.List;
 
+import com.facilio.accounts.dto.Role;
 import com.facilio.accounts.dto.User;
 import com.facilio.accounts.util.AccountUtil;
 import com.facilio.accounts.util.AccountUtil.FeatureLicense;
@@ -19,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 public class ApplicationAction extends FacilioAction {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
@@ -52,7 +53,7 @@ public class ApplicationAction extends FacilioAction {
 	public void setAppId(long appId) {
 		this.appId = appId;
 	}
-	
+
 	private List<Long> appIds;
 	public List<Long> getAppIds() {
 		return appIds;
@@ -60,7 +61,7 @@ public class ApplicationAction extends FacilioAction {
 	public void setAppIds(List<Long> appIds) {
 		this.appIds = appIds;
 	}
-	
+
 	private User user;
 	public User getUser() {
 		return user;
@@ -69,7 +70,7 @@ public class ApplicationAction extends FacilioAction {
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
+
 	private Boolean fetchNonAppUsers;
 	public Boolean getFetchNonAppUsers() {
 		return fetchNonAppUsers;
@@ -130,7 +131,7 @@ public class ApplicationAction extends FacilioAction {
 		setResult(FacilioConstants.ContextNames.APPLICATION, context.get(FacilioConstants.ContextNames.APPLICATION));
 		return SUCCESS;
 	}
-	
+
 	public String markApplicationAsDefault() throws Exception {
 		FacilioChain chain = TransactionChainFactory.markApplicationAsDefault();
 		FacilioContext context = chain.getContext();
@@ -138,7 +139,7 @@ public class ApplicationAction extends FacilioAction {
 		chain.execute();
 		return SUCCESS;
 	}
-	
+
 	public String deleteApplication() throws Exception {
 		FacilioChain chain = TransactionChainFactory.getDeleteApplicationsChain();
 		FacilioContext context = chain.getContext();
@@ -146,7 +147,7 @@ public class ApplicationAction extends FacilioAction {
 		chain.execute();
 		return SUCCESS;
 	}
-	
+
 	public String getApplicationDetails() throws Exception {
 		FacilioChain chain = ReadOnlyChainFactory.getApplicationDetails();
 		FacilioContext context = chain.getContext();
@@ -161,23 +162,29 @@ public class ApplicationAction extends FacilioAction {
 		}
 		chain.execute();
 		setResult(FacilioConstants.ContextNames.APPLICATION, context.get(FacilioConstants.ContextNames.APPLICATION));
+		if(AccountUtil.getCurrentUser() != null){
+			Role currentUserRole = AccountUtil.getCurrentUser().getRole();
+			if(currentUserRole != null) {
+				setResult("currentUserRole", currentUserRole);
+			}
+		}
 		boolean isWebTabEnabled = AccountUtil.isFeatureEnabled(FeatureLicense.WEB_TAB);
 		setResult("isWebTabEnabled", isWebTabEnabled);
 		return SUCCESS;
 	}
-	
+
 	public String addApplicationUsers() throws Exception {
 		FacilioChain chain = TransactionChainFactory.addApplicationUsersChain();
 		FacilioContext context = chain.getContext();
 		context.put(FacilioConstants.ContextNames.APPLICATION_ID, appId);
 		context.put(FacilioConstants.ContextNames.USER, user);
-		
+
 		chain.execute();
 		setResult(FacilioConstants.ContextNames.ORG_USER_ID, context.get(FacilioConstants.ContextNames.ORG_USER_ID));
 		return SUCCESS;
-	
+
 	}
-	
+
 	public String getApplicationUsers() throws Exception {
 		FacilioChain chain = ReadOnlyChainFactory.getApplicationUsersChain();
 		FacilioContext context = chain.getContext();
@@ -187,22 +194,22 @@ public class ApplicationAction extends FacilioAction {
 		}
 		chain.execute();
 		setResult(FacilioConstants.ContextNames.USERS, context.get(FacilioConstants.ContextNames.USERS));
-		
+
 		return SUCCESS;
-	
+
 	}
-	
+
 	public String deleteApplicationUsers() throws Exception {
 		FacilioChain chain = TransactionChainFactory.deleteApplicationUsersChain();
 		chain.getContext().put(FacilioConstants.ContextNames.APPLICATION_ID, appId);
 		chain.getContext().put(FacilioConstants.ContextNames.USER, user);
-		
+
 		FacilioContext context = chain.getContext();
 		chain.execute();
 		setResult(FacilioConstants.ContextNames.ROWS_UPDATED, context.get(FacilioConstants.ContextNames.ROWS_UPDATED));
-		
+
 		return SUCCESS;
-	
+
 	}
 
 }
