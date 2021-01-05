e516386deeac4293b82c1c4d6a549f90299c5b8b
manthosh <manthosh@facilio.com>
2021-01-05 18:03:46 +0530

    Adding separate executors for hca

--- scripts/change_conf.sh
+++ scripts/change_conf.sh
@@ -25,14 +25,28 @@ if [ "$DEPLOYMENT_GROUP_NAME" = "production_deployment" ]; then
     cp $FACILIO_HOME/deployment-files/log4j-user.properties $CLASSES_DIR/log4j.properties
     cp $FACILIO_HOME/setenv.sh $APP_HOME/bin/setenv.sh
     cp $FACILIO_HOME/deployment-files/awsprops-user.properties $CONF_DIR/awsprops.properties
+    cp $FACILIO_HOME/deployment-files/executors-production.xml $CONF_DIR/executors.xml
+    cp $FACILIO_HOME/deployment-files/instantjobexecutors-production.yml $CONF_DIR/instantjobexecutors.yml
     cp $FACILIO-HOME/deployment-files/agentIntegration-production.properties $CONF_DIR/agentIntegration.properties
     echo "copied $DEPLOYMENT_GROUP_NAME context file" >> /home/facilio/deployment.log
 fi
 
-if [ "$DEPLOYMENT_GROUP_NAME" = "production-scheduler" ] || [ "$DEPLOYMENT_GROUP_NAME" = "production-scheduler-app" ]; then
+if [ "$DEPLOYMENT_GROUP_NAME" = "production-scheduler" ]; then
     echo "copying $DEPLOYMENT_GROUP_NAME context file" >> /home/facilio/deployment.log
     cp $FACILIO_HOME/deployment-files/log4j-scheduler.properties $CLASSES_DIR/log4j.properties
     cp $FACILIO_HOME/deployment-files/awsprops-scheduler.properties $CONF_DIR/awsprops.properties
+    cp $FACILIO_HOME/deployment-files/executors-production.xml $CONF_DIR/executors.xml
+    cp $FACILIO_HOME/deployment-files/instantjobexecutors-production.yml $CONF_DIR/instantjobexecutors.yml
+    cp $FACILIO_HOME/setenv.sh $APP_HOME/bin/setenv.sh
+    echo "copied $DEPLOYMENT_GROUP_NAME context file" >> /home/facilio/deployment.log
+fi
+
+if [ "$DEPLOYMENT_GROUP_NAME" = "production-scheduler-app" ]; then
+    echo "copying $DEPLOYMENT_GROUP_NAME context file" >> /home/facilio/deployment.log
+    cp $FACILIO_HOME/deployment-files/log4j-scheduler-app.properties $CLASSES_DIR/log4j.properties
+    cp $FACILIO_HOME/deployment-files/awsprops-scheduler-app.properties $CONF_DIR/awsprops.properties
+    cp $FACILIO_HOME/deployment-files/executors-org.xml $CONF_DIR/executors.xml
+    cp $FACILIO_HOME/deployment-files/instantjobexecutors-org.yml $CONF_DIR/instantjobexecutors.yml
     cp $FACILIO_HOME/setenv.sh $APP_HOME/bin/setenv.sh
     echo "copied $DEPLOYMENT_GROUP_NAME context file" >> /home/facilio/deployment.log
 fi
