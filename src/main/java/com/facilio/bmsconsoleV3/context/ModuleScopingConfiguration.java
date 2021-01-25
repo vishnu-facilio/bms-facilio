79ba41f9fa3daf8e4e4e3187e1b996e9aa2ba2b6
vandhana <vandhana@facilio.com>
2021-01-25 20:32:51 +0530

    renamed files

--- src/main/java/com/facilio/bmsconsoleV3/context/ModuleScopingConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.facilio.bmsconsoleV3.context;
-
-import com.facilio.v3.V3Builder.V3Config;
-
-public abstract class ModuleScopingConfiguration {
-    public abstract void addScopingConfiguration ();
-}
