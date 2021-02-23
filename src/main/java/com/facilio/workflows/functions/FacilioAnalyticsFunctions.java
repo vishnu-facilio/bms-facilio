0cad991e9e2b89b3f30ec72812b308882246d51b
Ayshvarya <ayshvarya@facilio.com>
2021-02-23 20:40:18 +0530

    get report props in script

--- src/main/java/com/facilio/workflows/functions/FacilioAnalyticsFunctions.java
+++ src/main/java/com/facilio/workflows/functions/FacilioAnalyticsFunctions.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.facilio.chain.FacilioContext;
+import com.facilio.constants.FacilioConstants;
 import com.facilio.modules.FieldUtil;
+import com.facilio.report.context.ReportContext;
 import com.facilio.report.util.ReportUtil;
 import com.facilio.workflows.exceptions.FunctionParamException;
 
@@ -17,11 +19,15 @@ public enum FacilioAnalyticsFunctions implements FacilioWorkflowFunctionInterfac
 		public Object execute(Map<String, Object> globalParam, Object... objects) throws Exception {
 			
 			HashMap<String,Object> dataObjectMap = (HashMap<String,Object>)objects[0];
-			
+
 			FacilioContext context =  ReportUtil.getReportData(dataObjectMap);
+			if(context.get(FacilioConstants.ContextNames.REPORT) != null) {
+				ReportContext reportContext = (ReportContext) context.get(FacilioConstants.ContextNames.REPORT);
+				context.put("report", FieldUtil.getAsProperties(reportContext));
+			}
+			FieldUtil.getAsProperties(context);
 			
-			return FieldUtil.getAsProperties(context);
-			
+			return context;
 		};
 		
 		public void checkParam(Object... objects) throws Exception {
