d633370e1d06e686e52322523125a7cf4d370bff
Krishna <krishnan.e@facilio.com>
2021-03-11 12:22:32 +0530

    logger added

--- src/main/java/com/facilio/bmsconsoleV3/commands/PlanControlGroupSlots.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/PlanControlGroupSlots.java
@@ -1,37 +1,36 @@
 package com.facilio.bmsconsoleV3.commands;
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.chain.Context;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
-import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.commands.FacilioCommand;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.control.ControlGroupContext;
-import com.facilio.control.ControlScheduleGroupedSlot;
 import com.facilio.control.ControlScheduleSlot;
 import com.facilio.control.util.ControlScheduleUtil;
-import com.facilio.controlaction.context.ControlActionCommandContext;
-import com.facilio.db.criteria.Criteria;
-import com.facilio.db.criteria.CriteriaAPI;
-import com.facilio.db.criteria.operators.DateOperators;
-import com.facilio.db.criteria.operators.NumberOperators;
-import com.facilio.fw.BeanFactory;
-import com.facilio.modules.DeleteRecordBuilder;
-import com.facilio.modules.FieldFactory;
-import com.facilio.modules.FieldUtil;
-import com.facilio.modules.fields.FacilioField;
+import com.facilio.db.util.DBConf;
 import com.facilio.time.DateTimeUtil;
 
 public class PlanControlGroupSlots extends FacilioCommand {
 
+	private static final Logger LOGGER = LogManager.getLogger(PlanControlGroupSlots.class.getName());
 	@Override
 	public boolean executeCommand(Context context) throws Exception {
 		// TODO Auto-generated method stub
+		
 		ControlGroupContext controlGroup = (ControlGroupContext) context.get(ControlScheduleUtil.CONTROL_GROUP_MODULE_NAME);
 		
 		long startTime = (long) context.getOrDefault(FacilioConstants.ContextNames.START_TIME, DateTimeUtil.getCurrenTime());
+		
+		
+		LOGGER.error("DateTimeUtil.getZonedDateTime(startTime) --> "+DateTimeUtil.getZonedDateTime(startTime));
+		
+		LOGGER.error("getCurrentZoneId --> "+DBConf.getInstance().getCurrentZoneId());
+		LOGGER.error("getCurrentLocale --> "+DBConf.getInstance().getCurrentLocale());
+		
 		
 		long endTime = (long) context.getOrDefault(FacilioConstants.ContextNames.END_TIME,DateTimeUtil.addMonths(startTime, 1));
 		
