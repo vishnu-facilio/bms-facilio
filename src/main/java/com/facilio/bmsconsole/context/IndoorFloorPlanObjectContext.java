87c88ed1465f5255de1015b3f34e891517826a06
Prasanth <prasanth.iot@gmail.com>
2021-03-11 19:20:10 +0530

    indoor floor plan first commit

---
+++ src/main/java/com/facilio/bmsconsole/context/IndoorFloorPlanObjectContext.java
@@ -0,0 +1,71 @@
+package com.facilio.bmsconsole.context;
+
+import com.facilio.modules.ModuleBaseWithCustomFields;
+
+public class IndoorFloorPlanObjectContext extends ModuleBaseWithCustomFields{
+	
+	private long objectId;
+	public long getObjectId() {
+		return objectId;
+	}
+	public void setObjectId(long objectId) {
+		this.objectId = objectId;
+	}
+	public long getLocalId() {
+		return localId;
+	}
+	public void setLocalId(long localId) {
+		this.localId = localId;
+	}
+	public long getFloorplanId() {
+		return floorplanId;
+	}
+	public void setFloorplanId(long floorplanId) {
+		this.floorplanId = floorplanId;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getGeometry() {
+		return geometry;
+	}
+	public void setGeometry(String geometry) {
+		this.geometry = geometry;
+	}
+	public String getProperties() {
+		return properties;
+	}
+	public void setProperties(String properties) {
+		this.properties = properties;
+	}
+	public long getSpaceId() {
+		return spaceId;
+	}
+	public void setSpaceId(long spaceId) {
+		this.spaceId = spaceId;
+	}
+	public long getAssetId() {
+		return assetId;
+	}
+	public void setAssetId(long assetId) {
+		this.assetId = assetId;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	private long localId; //geo json id
+	private long floorplanId;
+	private String type;
+	private String geometry;
+	private String properties;
+	private long spaceId;
+	private long assetId;
+	private long userId;
+	
+}
