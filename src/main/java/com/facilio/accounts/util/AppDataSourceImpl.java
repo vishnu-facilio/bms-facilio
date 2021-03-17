dc215a004fe22207d740475a6d9d03ef6c753b74
manthosh <manthosh@facilio.com>
2021-03-17 18:03:25 +0530

    Temp fix for app service handler

--- src/main/java/com/facilio/accounts/util/AppDataSourceImpl.java
+++ src/main/java/com/facilio/accounts/util/AppDataSourceImpl.java
@@ -3,24 +3,38 @@ package com.facilio.accounts.util;
 import com.facilio.accounts.dto.Organization;
 import com.facilio.db.util.DBConf;
 import com.facilio.service.DataSourceInterfaceHandler;
+import lombok.extern.log4j.Log4j;
 
 import java.util.Objects;
 
+@Log4j
 public class AppDataSourceImpl implements DataSourceInterfaceHandler {
 
     @Override
     public String getDataSource () {
         Organization org = AccountUtil.getCurrentOrg();
-        Objects.requireNonNull(org,"current org is null while fetching data source");
-        String ds = org.getDataSource();
-        return (ds != null && !ds.trim().isEmpty()) ? ds : DBConf.getInstance().getDefaultDataSource();
+//        Objects.requireNonNull(org,"current org is null while fetching data source");
+        if (org == null) { //Temp fix
+            LOGGER.error("current org is null while fetching data source. This is not supposed to happen.");
+            return DBConf.getInstance().getDefaultDataSource();
+        }
+        else {
+            String ds = org.getDataSource();
+            return (ds != null && !ds.trim().isEmpty()) ? ds : DBConf.getInstance().getDefaultDataSource();
+        }
     }
 
     @Override
     public String getDbName () {
         Organization org = AccountUtil.getCurrentOrg();
-        Objects.requireNonNull(org,"current org is null while fetching db");
-        String db = org.getDbName();
-        return (db != null && !db.trim().isEmpty()) ? db : DBConf.getInstance().getDefaultDB();
+//        Objects.requireNonNull(org,"current org is null while fetching db");
+        if (org == null) { //Temp Fix
+            LOGGER.error("current org is null while fetching db. This is not supposed to happen.");
+            return DBConf.getInstance().getDefaultDB();
+        }
+        else {
+            String db = org.getDbName();
+            return (db != null && !db.trim().isEmpty()) ? db : DBConf.getInstance().getDefaultDB();
+        }
     }
 }
