91338d04b900c6f878fa78067de984dc4b626b3a
Krishna <krishnan.e@facilio.com>
2021-03-18 23:04:39 +0530

    Form Rule chaining fix.

--- src/main/java/com/facilio/bmsconsole/commands/ExecuteFormActionRulesForNextRoundCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/ExecuteFormActionRulesForNextRoundCommand.java
@@ -1,30 +1,56 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.chain.Context;
 import org.json.simple.JSONArray;
 
+import com.facilio.beans.ModuleBean;
+import com.facilio.bmsconsole.forms.FacilioForm;
 import com.facilio.bmsconsole.forms.FormRuleContext;
 import com.facilio.bmsconsole.util.FormRuleAPI;
 import com.facilio.chain.FacilioChain;
 import com.facilio.constants.FacilioConstants;
+import com.facilio.fw.BeanFactory;
+import com.facilio.modules.FacilioModule;
+import com.facilio.modules.fields.FacilioField;
 
 public class ExecuteFormActionRulesForNextRoundCommand extends FacilioCommand {
 
 	@Override
 	public boolean executeCommand(Context facilioContext) throws Exception {
 		
+		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+		
 		List<Long> valueFilledFields = (List<Long>) facilioContext.get(FormRuleAPI.VALUE_FILLED_FIELD_IDS);
 		
 		Map<String,Object> formDataToBeAddedforNextRound = (Map<String,Object>) facilioContext.get(FormRuleAPI.FORM_DATA_FOR_NEXT_ROUND);
 		
+		FacilioForm form = (FacilioForm) facilioContext.get(FacilioConstants.ContextNames.FORM );
 		
 		if(valueFilledFields != null && !valueFilledFields.isEmpty()) {
 			
 			Map<String,Object> formData = (Map<String, Object>) facilioContext.get(FormRuleAPI.FORM_DATA);
-			formData.putAll(formDataToBeAddedforNextRound);
+			
+			FacilioModule module = form.getModule();
+			
+			for(String fieldName : formDataToBeAddedforNextRound.keySet()) {
+				
+				FacilioField field = modBean.getField(fieldName, module.getName());
+				
+				if(field.isDefault()) {
+					formData.put(fieldName, formDataToBeAddedforNextRound.get(fieldName));
+				}
+				else {
+					Map<String,Object>  data = (Map<String, Object>) formData.getOrDefault("data", new HashMap<String, Object>());
+					
+					data.put(fieldName, formDataToBeAddedforNextRound.get(fieldName));
+					
+					formData.put("data", data);
+				}
+			}
 			
 			JSONArray result = (JSONArray) facilioContext.get(FormRuleAPI.FORM_RULE_RESULT_JSON);
 			
