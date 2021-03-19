426839c8486a0050312e1cefab23086df1440fd0
manthosh <manthosh@facilio.com>
2021-03-19 21:03:35 +0530

    Stopping sending alert emails

--- src/main/java/com/facilio/bmsconsole/commands/util/CommonCommandUtil.java
+++ src/main/java/com/facilio/bmsconsole/commands/util/CommonCommandUtil.java
@@ -194,36 +194,36 @@ public class CommonCommandUtil {
 	}
 
 	public static void emailAlert (String subject, String msg) {
-		try {
-
-			if (FacilioProperties.isProduction()) {
-				Organization org = AccountUtil.getCurrentOrg();
-
-				JSONObject json = new JSONObject();
-				json.put("sender", "alert@facilio.com");
-				json.put("to", "error+alert@facilio.com");
-				json.put("subject", org.getOrgId()+" - "+subject);
-
-				StringBuilder body = new StringBuilder()
-						.append(msg)
-						.append("\n\nInfo : \n--------\n")
-						.append("\n Org Time : ").append(DateTimeUtil.getDateTime())
-						.append("\n Indian Time : ").append(DateTimeUtil.getDateTime(ZoneId.of("Asia/Kolkata")))
-						.append("\n\nMsg : ")
-						.append(msg)
-						.append("\n\nApp Url : ")
-						.append(FacilioProperties.getConfig("clientapp.url"))
-						.append("\n\nOrg Info : \n--------\n")
-						.append(org.toString())
-						;
-				json.put("message", body.toString());
-
-				FacilioFactory.getEmailClient().sendEmail(json);
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//
+//			if (FacilioProperties.isProduction()) {
+//				Organization org = AccountUtil.getCurrentOrg();
+//
+//				JSONObject json = new JSONObject();
+//				json.put("sender", "alert@facilio.com");
+//				json.put("to", "error+alert@facilio.com");
+//				json.put("subject", org.getOrgId()+" - "+subject);
+//
+//				StringBuilder body = new StringBuilder()
+//						.append(msg)
+//						.append("\n\nInfo : \n--------\n")
+//						.append("\n Org Time : ").append(DateTimeUtil.getDateTime())
+//						.append("\n Indian Time : ").append(DateTimeUtil.getDateTime(ZoneId.of("Asia/Kolkata")))
+//						.append("\n\nMsg : ")
+//						.append(msg)
+//						.append("\n\nApp Url : ")
+//						.append(FacilioProperties.getConfig("clientapp.url"))
+//						.append("\n\nOrg Info : \n--------\n")
+//						.append(org.toString())
+//						;
+//				json.put("message", body.toString());
+//
+//				FacilioFactory.getEmailClient().sendEmail(json);
+//			}
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
 	}
 
