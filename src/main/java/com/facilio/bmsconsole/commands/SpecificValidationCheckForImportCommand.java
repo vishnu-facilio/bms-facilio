ed632a78ad9137dc2a26b15c881d8d5b03fc7b0e
Krishna <krishnan.e@facilio.com>
2021-03-23 11:52:10 +0530

    duplicate check for multi site.

--- src/main/java/com/facilio/bmsconsole/commands/SpecificValidationCheckForImportCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/SpecificValidationCheckForImportCommand.java
@@ -1,12 +1,29 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.apache.commons.chain.Context;
+import org.apache.commons.lang.StringUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.actions.ImportProcessContext;
 import com.facilio.bmsconsole.context.BimImportProcessMappingContext;
 import com.facilio.bmsconsole.context.ImportRowContext;
 import com.facilio.bmsconsole.context.ReadingContext;
+import com.facilio.bmsconsole.context.SiteContext;
+import com.facilio.bmsconsole.exceptions.importExceptions.ImportLookupModuleValueNotFoundException;
+import com.facilio.bmsconsole.exceptions.importExceptions.ImportMandatoryFieldsException;
 import com.facilio.bmsconsole.util.BimAPI;
 import com.facilio.bmsconsole.util.ImportAPI;
+import com.facilio.bmsconsole.util.SpaceAPI;
 import com.facilio.db.criteria.CriteriaAPI;
 import com.facilio.db.criteria.operators.StringOperators;
 import com.facilio.fw.BeanFactory;
@@ -15,15 +32,6 @@ import com.facilio.modules.FieldFactory;
 import com.facilio.modules.ModuleFactory;
 import com.facilio.modules.SelectRecordsBuilder;
 import com.facilio.modules.fields.FacilioField;
-import org.apache.commons.chain.Context;
-import org.apache.commons.lang.StringUtils;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.logging.Logger;
 
 public class SpecificValidationCheckForImportCommand extends FacilioCommand {
 	
@@ -65,26 +73,100 @@ public class SpecificValidationCheckForImportCommand extends FacilioCommand {
 			fields.add(FieldFactory.getIdField(module));
 			fields.add(modBean.getField("name", "resource"));
 			Long siteId = importProcessContext.getSiteId();
-			if(siteId != null) {
+			if(siteId != null && siteId > 0) {
 				
-				SelectRecordsBuilder<ReadingContext> selectBuilder = new SelectRecordsBuilder<ReadingContext>()
-						.beanClass(ReadingContext.class).moduleName(module.getName()).select(fields)
-						.table(module.getTableName()).andCondition(
-								CriteriaAPI.getCondition("Resources.NAME", "name", nameQueryString, StringOperators.IS))
-						.andCondition(CriteriaAPI.getCondition("Resources.SITE_ID", "siteId", siteId.toString(), StringOperators.IS));
-				List<ReadingContext> readingContext = selectBuilder.get();
-				if (readingContext != null && readingContext.size() > 0) {
-					for (ReadingContext rContext : readingContext) {
-						String assetName = (String) rContext.getData().get("name");
-						groupedContext.get(assetName).get(0)
-								.setError_resolved(ImportProcessContext.ImportLogErrorStatus.FOUND_IN_DB.getValue());
+				siteWiseDuplicateMarking(groupedContext, module, fields, siteId, nameQueryString);
+			}
+			else {
+				
+				Map<String,List<ImportRowContext>> siteWiseList = getSiteWiseAssetList(groupedContext,importProcessContext);
+				
+				for(String siteName : siteWiseList.keySet()) {
+					
+					List<ImportRowContext> importRows = siteWiseList.get(siteName);
+					
+					List<String> assetNames = new ArrayList<String>();
+					for(ImportRowContext importRow : importRows) {
+						
+						String name = (String) importRow.getColVal().get(importProcessContext.getFieldMapping().get("resource__name"));
+						
+						assetNames.add(name);
 					}
+					
+					SiteContext site = SpaceAPI.getSite(siteName);
+					
+					if(site == null) {
+						throw new ImportLookupModuleValueNotFoundException("site", 0, siteName, new Exception());
+					}
+					String names = StringUtils.join(assetNames, ",");
+					
+					siteWiseDuplicateMarking(groupedContext, module, fields, site.getId(), names);
 				}
+				
 			}
 		}
 		context.put(ImportAPI.ImportProcessConstants.GROUPED_ROW_CONTEXT, groupedContext);
 		
 		return false;
 	}
+	
+	private void siteWiseDuplicateMarking(HashMap<String, List<ImportRowContext>> groupedContext,FacilioModule module,List<FacilioField> fields,Long siteId,String nameCommaSeperated) throws Exception {
+		
+		SelectRecordsBuilder<ReadingContext> selectBuilder = new SelectRecordsBuilder<ReadingContext>()
+				.beanClass(ReadingContext.class).moduleName(module.getName()).select(fields)
+				.table(module.getTableName()).andCondition(
+						CriteriaAPI.getCondition("Resources.NAME", "name", nameCommaSeperated, StringOperators.IS))
+				.andCondition(CriteriaAPI.getCondition("Resources.SITE_ID", "siteId", siteId.toString(), StringOperators.IS));
+		
+		List<ReadingContext> readingContext = selectBuilder.get();
+		
+		if (readingContext != null && readingContext.size() > 0) {
+			for (ReadingContext rContext : readingContext) {
+				String assetName = (String) rContext.getData().get("name");
+				groupedContext.get(assetName).get(0)
+						.setError_resolved(ImportProcessContext.ImportLogErrorStatus.FOUND_IN_DB.getValue());
+			}
+		}
+	}
 
+	private Map<String, List<ImportRowContext>> getSiteWiseAssetList(HashMap<String, List<ImportRowContext>> groupedContext,ImportProcessContext importProcessContext) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Collection<List<ImportRowContext>> importRows = groupedContext.values();
+		
+		Map<String,List<ImportRowContext>> siteWiseList = new HashMap<String,List<ImportRowContext>>();
+		
+		HashMap<String, String> fieldMapping = importProcessContext.getFieldMapping();
+		
+		String siteField = fieldMapping.get("asset__site");
+		
+		if(siteField == null) {
+			throw new ImportMandatoryFieldsException(null, (ArrayList<String>) Collections.singletonList("Site"), new Exception());
+		}
+		
+		for(List<ImportRowContext> importRowList : importRows) {
+			
+			for(ImportRowContext importRow : importRowList) {
+				
+				String siteName = (String) importRow.getColVal().get(siteField);
+				
+				if(siteName == null) {
+					throw new ImportMandatoryFieldsException(importRow.getRowNumber(), (ArrayList<String>) Collections.singletonList("Site"), new Exception());
+				}
+				else {
+					
+					
+					List<ImportRowContext> rowList = siteWiseList.getOrDefault(siteName, new ArrayList<ImportRowContext>());
+					
+					rowList.add(importRow);
+					
+					siteWiseList.put(siteName, rowList);
+				}
+			}
+		}
+		
+		return siteWiseList;
+	}
+
+	
 }
