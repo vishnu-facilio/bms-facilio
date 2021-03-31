0448669fc8bc85d5d9d22e40bc2f885ad35f7daf
vandhana <vandhana@facilio.com>
2021-03-31 06:49:45 +0530

    adding default criteria for quote list in module data list api

--- src/main/java/com/facilio/bmsconsole/commands/GenerateCriteriaFromClientCriteriaCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GenerateCriteriaFromClientCriteriaCommand.java
@@ -1,6 +1,11 @@
 package com.facilio.bmsconsole.commands;
 
+import com.facilio.db.criteria.operators.BooleanOperators;
+import com.facilio.modules.FacilioModule;
+import com.facilio.modules.FieldFactory;
+import com.facilio.modules.FieldType;
 import org.apache.commons.chain.Context;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -33,6 +38,25 @@ public class GenerateCriteriaFromClientCriteriaCommand extends FacilioCommand {
 				FacilioField field = modBean.getField(condition.getFieldName(), moduleName);
 				condition.setField(field);
 			}
+		}
+		if(StringUtils.isNotEmpty(moduleName) && moduleName.equals(FacilioConstants.ContextNames.QUOTE)) {
+
+			ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+			FacilioModule module = modBean.getModule(FacilioConstants.ContextNames.QUOTE);
+
+				Criteria nonRevisedCriteria = new Criteria();
+				Condition nonRevisedCondition = new Condition();
+				nonRevisedCondition.setColumnName("IS_QUOTATION_REVISED");
+				nonRevisedCondition.setFieldName("isQuotationRevised");
+				nonRevisedCondition.setOperator(BooleanOperators.IS);
+				nonRevisedCondition.setField(FieldFactory.getField("isQuotationRevised", "IS_QUOTATION_REVISED", module, FieldType.BOOLEAN));
+				nonRevisedCondition.setValue(String.valueOf(false));
+
+				nonRevisedCriteria.addAndCondition(nonRevisedCondition);
+				if(clientFilterCriteria == null) {
+					clientFilterCriteria = new Criteria();
+				}
+				clientFilterCriteria.andCriteria(nonRevisedCriteria);
 		}
 		
 		return false;
