271ede22f594621ec4715f6ffcd93448ea3da089
manthosh <manthosh@facilio.com>
2021-04-21 14:27:21 +0530

    Using latest fw version

--- src/main/java/com/facilio/v3/commands/SaveCommand.java
+++ src/main/java/com/facilio/v3/commands/SaveCommand.java
@@ -13,13 +13,16 @@ import com.facilio.modules.UpdateChangeSet;
 import com.facilio.modules.fields.FacilioField;
 import com.facilio.modules.fields.SupplementRecord;
 import com.facilio.v3.context.Constants;
+import lombok.extern.log4j.Log4j;
 import org.apache.commons.chain.Context;
 import org.apache.commons.collections4.CollectionUtils;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@Log4j
 public class SaveCommand extends FacilioCommand {
     private FacilioModule module;
 
@@ -44,47 +47,52 @@ public class SaveCommand extends FacilioCommand {
     }
 
     private void insertData(FacilioContext context, String moduleName, List<ModuleBaseWithCustomFields> records) throws Exception {
-        //Copied from genericaddmoduledatacommand
-        ModuleBean modBean = Constants.getModBean();
-        FacilioModule module = modBean.getModule(moduleName);
+        try {
+            //Copied from genericaddmoduledatacommand
+            ModuleBean modBean = Constants.getModBean();
+            FacilioModule module = modBean.getModule(moduleName);
 
-        List<FacilioField> fields = modBean.getAllFields(moduleName);
+            List<FacilioField> fields = modBean.getAllFields(moduleName);
 
-        InsertRecordBuilder<ModuleBaseWithCustomFields> insertRecordBuilder = new InsertRecordBuilder<>()
-                .module(module)
-                .fields(fields)
-                ;
+            InsertRecordBuilder<ModuleBaseWithCustomFields> insertRecordBuilder = new InsertRecordBuilder<>()
+                    .module(module)
+                    .fields(fields);
 
-        Boolean setLocalId = (Boolean) context.get(FacilioConstants.ContextNames.SET_LOCAL_MODULE_ID);
-        if (setLocalId != null && setLocalId) {
-            insertRecordBuilder.withLocalId();
-        }
-
-        insertRecordBuilder.withChangeSet();
-        insertRecordBuilder.ignoreSplNullHandling();
-
-        if (module.isCustom()) {
-            for (ModuleBaseWithCustomFields record: records) {
-                CommonCommandUtil.handleLookupFormData(fields, record.getData());
+            Boolean setLocalId = (Boolean) context.get(FacilioConstants.ContextNames.SET_LOCAL_MODULE_ID);
+            if (setLocalId != null && setLocalId) {
+                insertRecordBuilder.withLocalId();
             }
+
+            insertRecordBuilder.withChangeSet();
+            insertRecordBuilder.ignoreSplNullHandling();
+
+            if (module.isCustom()) {
+                for (ModuleBaseWithCustomFields record : records) {
+                    CommonCommandUtil.handleLookupFormData(fields, record.getData());
+                }
+            }
+
+            if (CollectionUtils.isEmpty(records)) {
+                throw new IllegalArgumentException("Record cannot be null during addition");
+            }
+
+            insertRecordBuilder.addRecords(records);
+
+            //inserting multi select field values
+            List<SupplementRecord> supplementFields = (List<SupplementRecord>) context.get(FacilioConstants.ContextNames.FETCH_SUPPLEMENTS);
+            if (CollectionUtils.isNotEmpty(supplementFields)) {
+                insertRecordBuilder.insertSupplements(supplementFields);
+            }
+
+            insertRecordBuilder.save();
+
+            Map<Long, List<UpdateChangeSet>> changeSet = insertRecordBuilder.getChangeSet();
+
+            CommonCommandUtil.appendChangeSetMapToContext(context, changeSet, moduleName);
         }
-
-        if (CollectionUtils.isEmpty(records)) {
-            throw new IllegalArgumentException("Record cannot be null during addition");
+        catch (Exception e) {
+            LOGGER.error(MessageFormat.format("Error occurred during save command for module : {0}", moduleName));
+            throw e;
         }
-
-        insertRecordBuilder.addRecords(records);
-
-        //inserting multi select field values
-        List<SupplementRecord> supplementFields = (List<SupplementRecord>) context.get(FacilioConstants.ContextNames.FETCH_SUPPLEMENTS);
-        if (CollectionUtils.isNotEmpty(supplementFields)) {
-            insertRecordBuilder.insertSupplements(supplementFields);
-        }
-
-        insertRecordBuilder.save();
-
-        Map<Long, List<UpdateChangeSet>> changeSet = insertRecordBuilder.getChangeSet();
-
-        CommonCommandUtil.appendChangeSetMapToContext(context,changeSet,moduleName);
     }
 }
