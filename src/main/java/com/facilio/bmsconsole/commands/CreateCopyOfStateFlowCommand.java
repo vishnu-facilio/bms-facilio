d7b9496c20f927bc2651ed7139bb717ad630bba9
Boopathy <boopathy@facilio.com>
2021-04-29 13:14:06 +0530

    update diagram state ids when cloning stateflow

--- src/main/java/com/facilio/bmsconsole/commands/CreateCopyOfStateFlowCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/CreateCopyOfStateFlowCommand.java
@@ -9,10 +9,20 @@ import com.facilio.chain.FacilioContext;
 import com.facilio.constants.FacilioConstants;
 import org.apache.commons.chain.Context;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CreateCopyOfStateFlowCommand extends FacilioCommand {
+
+    private static final Logger LOGGER = LogManager.getLogger(CreateCopyOfStateFlowCommand.class.getName());
 
     @Override
     public boolean executeCommand(Context context) throws Exception {
@@ -29,7 +39,9 @@ public class CreateCopyOfStateFlowCommand extends FacilioCommand {
 
             List<WorkflowRuleContext> allStateTransitionList = StateFlowRulesAPI.getAllStateTransitionList(originalStateFlowId);
             if (CollectionUtils.isNotEmpty(allStateTransitionList)) {
+                Map<Long, Long> oldVsNewTransitionIds = new HashMap<>();
                 for (WorkflowRuleContext workflowRuleContext : allStateTransitionList) {
+                    long oldTransitionId = workflowRuleContext.getId();
                     StateflowTransitionContext stateflowTransitionContext = (StateflowTransitionContext) workflowRuleContext;
                     stateflowTransitionContext.setStateFlowId(createdStateFlowId);
 
@@ -37,10 +49,55 @@ public class CreateCopyOfStateFlowCommand extends FacilioCommand {
                     ruleContext = ruleChain.getContext();
                     ruleContext.put(FacilioConstants.ContextNames.WORKFLOW_RULE, stateflowTransitionContext);
                     ruleChain.execute();
+
+                    oldVsNewTransitionIds.put(oldTransitionId, stateflowTransitionContext.getId());
                 }
+
+                updateStateflowDiagram(stateFlowContext, oldVsNewTransitionIds);
             }
             context.put(FacilioConstants.ContextNames.STATE_TRANSITION_LIST, allStateTransitionList);
         }
         return false;
+    }
+
+    private void updateStateflowDiagram(StateFlowRuleContext stateFlowContext, Map<Long, Long> oldVsNewTransitionIds) {
+        try {
+            String diagramJson = stateFlowContext.getDiagramJson();
+            if (StringUtils.isNotEmpty(diagramJson)) {
+                JSONParser parser = new JSONParser();
+                JSONObject jsonObject = (JSONObject) parser.parse(diagramJson);
+                if (jsonObject.containsKey("states")) {
+                    JSONArray states = (JSONArray) jsonObject.get("states");
+                    if (CollectionUtils.isNotEmpty(states)) {
+                        for (int i = 0; i < states.size(); i++) {
+                            JSONArray anchors = (JSONArray) ((JSONObject) states.get(i)).get("anchors");
+                            if (CollectionUtils.isEmpty(states)) {
+                                continue;
+                            }
+
+                            for (int j = 0; j < anchors.size(); j++) {
+                                Object o = anchors.get(j);
+                                if (o == null) {
+                                    continue;
+                                }
+                                Object oldTransitionId = anchors.remove(j);
+                                if (oldVsNewTransitionIds.containsKey(oldTransitionId)) {
+                                    anchors.add(j, oldVsNewTransitionIds.get(oldTransitionId));
+                                }
+                            }
+                        }
+                    }
+                }
+                stateFlowContext.setDiagramJson(jsonObject.toJSONString());
+
+                FacilioChain updateStateFlowDiagramChain = TransactionChainFactory.getUpdateStateFlowDiagramChain();
+                FacilioContext updateStateFlowDiagramContext = updateStateFlowDiagramChain.getContext();
+                updateStateFlowDiagramContext.put(FacilioConstants.ContextNames.RECORD_ID, stateFlowContext.getId());
+                updateStateFlowDiagramContext.put(FacilioConstants.ContextNames.STATEFLOW_DIAGRAM, jsonObject);
+                updateStateFlowDiagramChain.execute();
+            }
+        } catch (Exception ex) {
+            LOGGER.error("Error in updating diagram in stateflow", ex);
+        }
     }
 }
