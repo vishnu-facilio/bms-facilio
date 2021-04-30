8078dbdcd5175d8987782a93e67ca294ad4c0a81
manthosh <manthosh@facilio.com>
2021-04-30 16:38:54 +0530

    Debug

--- src/main/java/com/facilio/qa/command/FetchPagesFromTemplateCommand.java
+++ src/main/java/com/facilio/qa/command/FetchPagesFromTemplateCommand.java
@@ -19,6 +19,7 @@ public class FetchPagesFromTemplateCommand extends FacilioCommand {
     @Override
     public boolean executeCommand(Context context) throws Exception {
 
+        LOGGER.info("FetchPagesFromTemplateCommand is called");
         List<QAndATemplateContext> templates = Constants.getRecordList((FacilioContext) context);
         QAndAUtil.fetchChildrenFromParent(templates,
                                         FacilioConstants.QAndA.PAGE,
@@ -28,7 +29,7 @@ public class FetchPagesFromTemplateCommand extends FacilioCommand {
                                         PageContext::setParent,
                                         QAndATemplateContext::setPages,
                                         QAndAUtil::setDefaultPropsAsNullToReduceRespSize);
-        LOGGER.debug(MessageFormat.format("Fetched pages from template : {0}", (CollectionUtils.isEmpty(templates) ? "null" : templates.get(0).getPages())));
+        LOGGER.info(MessageFormat.format("Fetched pages from template : {0}", (CollectionUtils.isEmpty(templates) ? "null" : templates.get(0).getPages())));
 
         return false;
     }
