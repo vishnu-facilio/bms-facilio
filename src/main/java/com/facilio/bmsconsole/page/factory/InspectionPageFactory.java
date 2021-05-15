d9a7cca181c280d7950d68bc9f16348f57048f09
Krishna <krishnan.e@facilio.com>
2021-05-15 09:51:21 +0530

    page factory related changes

--- src/main/java/com/facilio/bmsconsole/page/factory/InspectionPageFactory.java
+++ src/main/java/com/facilio/bmsconsole/page/factory/InspectionPageFactory.java
@@ -1,13 +1,28 @@
 package com.facilio.bmsconsole.page.factory;
 
+import java.util.Map;
+
+import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.page.Page;
 import com.facilio.bmsconsole.page.PageWidget;
+import com.facilio.bmsconsoleV3.context.inspection.InspectionResponseContext;
 import com.facilio.bmsconsoleV3.context.inspection.InspectionTemplateContext;
+import com.facilio.constants.FacilioConstants;
+import com.facilio.db.criteria.Criteria;
+import com.facilio.db.criteria.CriteriaAPI;
+import com.facilio.db.criteria.operators.DateOperators;
+import com.facilio.db.criteria.operators.NumberOperators;
+import com.facilio.fw.BeanFactory;
+import com.facilio.modules.BmsAggregateOperators.DateAggregateOperator;
 import com.facilio.modules.FacilioModule;
+import com.facilio.modules.FieldFactory;
+import com.facilio.modules.FieldUtil;
+import com.facilio.modules.fields.FacilioField;
+import com.facilio.qa.context.ResponseContext;
 
 public class InspectionPageFactory extends PageFactory {
 
-	public static Page getInspectionTemplatePage(InspectionTemplateContext record, FacilioModule module) {
+	public static Page getInspectionTemplatePage(InspectionTemplateContext record, FacilioModule module) throws Exception {
 		
 		Page page = new Page();
 		
@@ -45,9 +60,39 @@ public class InspectionPageFactory extends PageFactory {
         Page.Tab insightTab = page.new Tab("insight");
         Page.Section insightTabSec = page.new Section();
         
-        PageWidget inspectionInsights = new PageWidget(PageWidget.WidgetType.INSPECTION_TEMPLATE_inspectionInsights);
-        inspectionInsights.addToLayoutParams(insightTabSec, 24, 8);
-        insightTabSec.addWidget(inspectionInsights);
+//        PageWidget inspectionInsights = new PageWidget(PageWidget.WidgetType.INSPECTION_TEMPLATE_inspectionInsights);
+//        inspectionInsights.addToLayoutParams(insightTabSec, 24, 8);
+//        insightTabSec.addWidget(inspectionInsights);
+        
+        ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+        
+        Map<String, FacilioField> fieldMap = FieldFactory.getAsMap(modBean.getAllFields(FacilioConstants.Inspection.INSPECTION_RESPONSE));
+        
+        PageWidget inspectionCountInsight = new PageWidget(PageWidget.WidgetType.CHART,"inspectionCountInsight");
+        inspectionCountInsight.addToLayoutParams(insightTabSec, 12, 8);
+        
+        Criteria criteria = new Criteria();
+        
+        criteria.addAndCondition(CriteriaAPI.getCondition(fieldMap.get("resStatus"), ResponseContext.ResponseStatus.DISABLED.getIndex()+"", NumberOperators.NOT_EQUALS));
+        criteria.addAndCondition(CriteriaAPI.getCondition(fieldMap.get("parent"), record.getId()+"", NumberOperators.EQUALS));
+        
+		addChartParams(inspectionCountInsight, "donut",null, "resStatus", null,  null , null, DateOperators.CURRENT_YEAR, null, criteria, "sysCreatedTime", FacilioConstants.Inspection.INSPECTION_RESPONSE);
+		
+		insightTabSec.addWidget(inspectionCountInsight);
+		
+		
+		PageWidget inspectionInsightByTime = new PageWidget(PageWidget.WidgetType.CHART,"inspectionInsightByTime");
+		inspectionInsightByTime.addToLayoutParams(insightTabSec, 12, 8);
+        
+        criteria = new Criteria();
+        
+        criteria.addAndCondition(CriteriaAPI.getCondition(fieldMap.get("resStatus"), ResponseContext.ResponseStatus.DISABLED.getIndex()+"", NumberOperators.NOT_EQUALS));
+        criteria.addAndCondition(CriteriaAPI.getCondition(fieldMap.get("parent"), record.getId()+"", NumberOperators.EQUALS));
+        
+		addChartParams(inspectionInsightByTime, "bar",DateAggregateOperator.MONTH, "sysCreatedTime", null,  null , null, DateOperators.CURRENT_YEAR, null, criteria, "sysCreatedTime", FacilioConstants.Inspection.INSPECTION_RESPONSE);
+		
+		insightTabSec.addWidget(inspectionInsightByTime);
+        
         
         PageWidget inspectionQuestions = new PageWidget(PageWidget.WidgetType.INSPECTION_TEMPLATE_inspectionQuestions);
         inspectionQuestions.addToLayoutParams(insightTabSec, 24, 6);
@@ -56,7 +101,7 @@ public class InspectionPageFactory extends PageFactory {
         insightTab.addSection(insightTabSec);
         page.addTab(insightTab);
 
-        Page.Tab historyTab = page.new Tab("history");
+        Page.Tab historyTab = page.new Tab("inspectionHistory");
         Page.Section historyTabSec = page.new Section();
         
         PageWidget history = new PageWidget(PageWidget.WidgetType.INSPECTION_TEMPLATE_history);
