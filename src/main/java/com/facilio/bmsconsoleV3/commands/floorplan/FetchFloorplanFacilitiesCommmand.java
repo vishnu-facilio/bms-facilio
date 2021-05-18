0e412ea6612fc3e32893dc3e50c1ed91921f9a6a
Ayshvarya <ayshvarya@facilio.com>
2021-05-18 08:33:44 +0530

    floorplan facilities list- added amenities detail

--- src/main/java/com/facilio/bmsconsoleV3/commands/floorplan/FetchFloorplanFacilitiesCommmand.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/floorplan/FetchFloorplanFacilitiesCommmand.java
@@ -1,6 +1,8 @@
 package com.facilio.bmsconsoleV3.commands.floorplan;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.chain.Context;
@@ -15,11 +17,16 @@ import com.facilio.constants.FacilioConstants;
 import com.facilio.db.criteria.CriteriaAPI;
 import com.facilio.db.criteria.operators.NumberOperators;
 import com.facilio.fw.BeanFactory;
+import com.facilio.modules.FacilioModule;
 import com.facilio.modules.FieldFactory;
+import com.facilio.modules.FieldType;
 import com.facilio.modules.SelectRecordsBuilder;
 import com.facilio.modules.fields.FacilioField;
+import com.facilio.modules.fields.MultiLookupField;
+import com.facilio.modules.fields.MultiLookupMeta;
+import com.facilio.modules.fields.SupplementRecord;
+import org.apache.commons.collections4.CollectionUtils;
 
-import io.jsonwebtoken.lang.Collections;
 
 public class FetchFloorplanFacilitiesCommmand extends FacilioCommand {
 
@@ -30,18 +37,32 @@ public class FetchFloorplanFacilitiesCommmand extends FacilioCommand {
 		long startTime =  (long) context.get(FacilioConstants.ContextNames.START_TIME);
 		long endTime =  (long) context.get(FacilioConstants.ContextNames.END_TIME);
 		
-		if(!Collections.isEmpty(spaceIds)) {
+		if(!CollectionUtils.isEmpty(spaceIds)) {
 		
 			ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
 	        String slots = FacilioConstants.ContextNames.FacilityBooking.FACILITY;
 	        List<FacilioField> fields = modBean.getAllFields(slots);
 	        Map<String, FacilioField> fieldsAsMap = FieldFactory.getAsMap(fields);
+	        FacilioModule amenitiesModule = modBean.getModule(FacilioConstants.ContextNames.FacilityBooking.AMENITY);
+	        List<SupplementRecord> fetchLookupsList = new ArrayList<>();
+	        SupplementRecord manager = (SupplementRecord) fieldsAsMap.get("manager");
+	        SupplementRecord location = (SupplementRecord) fieldsAsMap.get("location");
+
+	        MultiLookupMeta amenities = new MultiLookupMeta((MultiLookupField) fieldsAsMap.get("amenities"));
+
+	        FacilioField nameField = FieldFactory.getField("description", "DESCRIPTION", amenitiesModule, FieldType.STRING);
+	        amenities.setSelectFields(Collections.singletonList(nameField));
+
+	        fetchLookupsList.add(manager);
+	        fetchLookupsList.add(location);
+	        fetchLookupsList.add(amenities);
 	
 	        SelectRecordsBuilder<FacilityContext> builder = new SelectRecordsBuilder<FacilityContext>()
 	                .moduleName(slots)
 	                .select(fields)
 	                .beanClass(FacilityContext.class)
-	                .andCondition(CriteriaAPI.getCondition(fieldsAsMap.get("parentId"), StringUtils.join(spaceIds, ","), NumberOperators.EQUALS));
+	                .andCondition(CriteriaAPI.getCondition(fieldsAsMap.get("parentId"), StringUtils.join(spaceIds, ","), NumberOperators.EQUALS))
+	                .fetchSupplements(fetchLookupsList);
 	
 	        List<FacilityContext> list = builder.get();
 	        Map<Long, FacilityContext> FacilitiesMap = new HashMap<>();
