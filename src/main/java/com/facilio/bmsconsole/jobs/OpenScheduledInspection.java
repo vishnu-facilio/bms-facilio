2a88ba3d1f7569ed42618ae616aaf7d521b97d01
Krishna <krishnan.e@facilio.com>
2021-05-19 12:26:57 +0530

    adding skip module criteria on inspection status change.

--- src/main/java/com/facilio/bmsconsole/jobs/OpenScheduledInspection.java
+++ src/main/java/com/facilio/bmsconsole/jobs/OpenScheduledInspection.java
@@ -1,10 +1,16 @@
 package com.facilio.bmsconsole.jobs;
 
+import java.util.List;
+
 import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsoleV3.context.inspection.InspectionResponseContext;
 import com.facilio.bmsconsoleV3.util.V3RecordAPI;
 import com.facilio.constants.FacilioConstants;
+import com.facilio.db.criteria.CriteriaAPI;
+import com.facilio.db.criteria.operators.NumberOperators;
 import com.facilio.fw.BeanFactory;
+import com.facilio.modules.FacilioModule;
+import com.facilio.modules.SelectRecordsBuilder;
 import com.facilio.qa.context.ResponseContext;
 import com.facilio.tasker.job.FacilioJob;
 import com.facilio.tasker.job.JobContext;
@@ -19,12 +25,23 @@ public class OpenScheduledInspection extends FacilioJob {
 		
 		long inspectionResponseId = jc.getJobId();
 		
-		InspectionResponseContext inspectionResponse = (InspectionResponseContext)V3RecordAPI.getRecord(FacilioConstants.Inspection.INSPECTION_RESPONSE, inspectionResponseId, InspectionResponseContext.class);
+		FacilioModule module = modBean.getModule(FacilioConstants.Inspection.INSPECTION_RESPONSE);
 		
-		inspectionResponse.setStatus(InspectionResponseContext.Status.OPEN.getIndex());
-		inspectionResponse.setResStatus(ResponseContext.ResponseStatus.NOT_ANSWERED);
-		
-		V3RecordAPI.updateRecord(inspectionResponse, modBean.getModule(FacilioConstants.Inspection.INSPECTION_RESPONSE), modBean.getAllFields(FacilioConstants.Inspection.INSPECTION_RESPONSE));
+		SelectRecordsBuilder<InspectionResponseContext> selectRecordsBuilder = new SelectRecordsBuilder<InspectionResponseContext>();
+        selectRecordsBuilder.select(modBean.getAllFields(FacilioConstants.Inspection.INSPECTION_RESPONSE))
+                .module(module)
+                .beanClass(InspectionResponseContext.class)
+                .andCondition(CriteriaAPI.getIdCondition(inspectionResponseId, module))
+                .skipModuleCriteria();
+        
+        InspectionResponseContext inspectionResponse = selectRecordsBuilder.fetchFirst();
+        
+        if(inspectionResponse != null) {
+        	inspectionResponse.setStatus(InspectionResponseContext.Status.OPEN.getIndex());
+    		inspectionResponse.setResStatus(ResponseContext.ResponseStatus.NOT_ANSWERED);
+    		
+    		V3RecordAPI.updateRecord(inspectionResponse, modBean.getModule(FacilioConstants.Inspection.INSPECTION_RESPONSE), modBean.getAllFields(FacilioConstants.Inspection.INSPECTION_RESPONSE));
+        }
 	}
 
 }
