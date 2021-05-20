1c3e01ee7c33bf21106116509baaf1da7ee3db3d
Tharani <tharani@facilio.com>
2021-05-20 19:03:22 +0530

    Supporting Service request module in service catelog

--- src/main/java/com/facilio/bmsconsole/commands/GetServiceCatalogModulesCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetServiceCatalogModulesCommand.java
@@ -1,5 +1,6 @@
 package com.facilio.bmsconsole.commands;
 
+import com.facilio.accounts.util.AccountUtil;
 import com.facilio.beans.ModuleBean;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.fw.BeanFactory;
@@ -17,13 +18,16 @@ public class GetServiceCatalogModulesCommand extends FacilioCommand {
         List<FacilioModule> modules = new ArrayList<>();
 
         modules.add(modBean.getModule(FacilioConstants.ContextNames.WORK_ORDER));
-//        modules.add(modBean.getModule(FacilioConstants.ContextNames.SERVICE_REQUEST));
-//        modules.add(modBean.getModule(FacilioConstants.ContextNames.ASSET));
+//      modules.add(modBean.getModule(FacilioConstants.ContextNames.ASSET));
         modules.add(modBean.getModule(FacilioConstants.ContextNames.WorkPermit.WORKPERMIT));
         modules.add(modBean.getModule(FacilioConstants.ContextNames.VENDORS));
         modules.add(modBean.getModule(FacilioConstants.ContextNames.PURCHASE_CONTRACT));
 
         modules.add(modBean.getModule(FacilioConstants.ContextNames.VISITOR_LOGGING));
+        
+        if(AccountUtil.isFeatureEnabled(AccountUtil.FeatureLicense.SERVICE_REQUEST)) {
+            modules.add(modBean.getModule(FacilioConstants.ContextNames.SERVICE_REQUEST));
+        }
 
         context.put(FacilioConstants.ContextNames.MODULE_LIST, modules);
         return false;
