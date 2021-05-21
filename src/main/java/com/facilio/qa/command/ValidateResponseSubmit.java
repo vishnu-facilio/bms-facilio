e1bdb2898421e35f37d976aeec1e00ee7d9a90f6
manthosh <manthosh@facilio.com>
2021-05-21 19:49:08 +0530

    Bug fix in Response status update

--- src/main/java/com/facilio/qa/command/ValidateResponseSubmit.java
+++ src/main/java/com/facilio/qa/command/ValidateResponseSubmit.java
@@ -44,9 +44,9 @@ public class ValidateResponseSubmit extends FacilioCommand {
     }
 
     private void validateResponseComplete (ResponseContext response) throws Exception {
-        List<QuestionContext> questions = QAndAUtil.getChildRecordsFromTemplate(FacilioConstants.QAndA.QUESTION, response.getId());
+        List<QuestionContext> questions = QAndAUtil.getChildRecordsFromTemplate(FacilioConstants.QAndA.QUESTION, response.getParent().getId());
         if (CollectionUtils.isNotEmpty(questions)) {
-            List<AnswerContext> answers = QAndAUtil.getChildRecordsFromTemplate(FacilioConstants.QAndA.ANSWER, response.getId());
+            List<AnswerContext> answers = QAndAUtil.getChildRecordsFromTemplate(FacilioConstants.QAndA.ANSWER, response.getParent().getId());
             Map<Long, AnswerContext> questionVsAnswer = answers.stream().collect(Collectors.toMap(a -> a.getQuestion()._getId(), Function.identity()));
             for (QuestionContext question : questions) {
                 AnswerContext answer = questionVsAnswer.get(question._getId());
