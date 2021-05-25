75e6752fedf73ae0dc8dcdc08b99f98428cc33c4
Krishna <krishnan.e@facilio.com>
2021-05-25 20:39:49 +0530

    adding feature license

--- src/main/java/com/facilio/accounts/util/AccountUtil.java
+++ src/main/java/com/facilio/accounts/util/AccountUtil.java
@@ -423,6 +423,7 @@ public class AccountUtil {
 		CUSTOM_BUTTON(1125899906842624l),
 		MULTISITEPM(2251799813685248l), // 2 ^ 51
 		FACILITY_BOOKING(4503599627370496l, new String[] {ContextNames.FacilityBooking.FACILITY, ContextNames.FacilityBooking.FACILITY_BOOKING, ContextNames.FacilityBooking.AMENITY}),
+		INSPECTION(9007199254740992l),
 
 		;
     		// Add Module name if license is added for specific module
