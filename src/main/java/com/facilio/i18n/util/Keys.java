fc901ef19f6f23967b88dcffe4b9b6736a55add5
manthosh <manthosh@facilio.com>
2021-05-28 12:29:20 +0530

    Adding Keys file for using in i18n

---
+++ src/main/java/com/facilio/i18n/util/Keys.java
@@ -0,0 +1,6 @@
+package com.facilio.i18n.util;
+
+public class Keys {
+    public static String WO_DISPLAY_NAME = "workorder.name";
+    public static String TICKET_DISPLAY_NAME = "tickets.name";
+}
