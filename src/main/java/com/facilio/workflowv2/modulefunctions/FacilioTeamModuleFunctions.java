273d4d74a9e58f25f1d8259e9cdb48051ccd30cb
Krishna <krishnan.e@facilio.com>
2021-06-07 12:15:10 +0530

    team module related changes.

---
+++ src/main/java/com/facilio/workflowv2/modulefunctions/FacilioTeamModuleFunctions.java
@@ -0,0 +1,34 @@
+package com.facilio.workflowv2.modulefunctions;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.facilio.accounts.dto.Group;
+import com.facilio.accounts.dto.GroupMember;
+import com.facilio.accounts.dto.User;
+import com.facilio.accounts.util.AccountUtil;
+import com.facilio.modules.FieldUtil;
+
+public class FacilioTeamModuleFunctions extends FacilioModuleFunctionImpl {
+
+	
+	public Object getTeamUsers(Map<String,Object> globalParams,List<Object> objects) throws Exception{
+
+		if (objects.size() > 1) {
+			String teamName = objects.get(1).toString();
+			Group group = AccountUtil.getGroupBean().getGroup(teamName);
+			if(group.getMembers() != null && !group.getMembers().isEmpty()) {
+				
+				List<Long> ouids = group.getMembers().stream().map(GroupMember::getOuid).collect(Collectors.toList());
+				List<User> users = AccountUtil.getUserBean().getUsersAsMap(null, ouids).values().stream().collect(Collectors.toList());
+				
+				return FieldUtil.getAsMapList(users, User.class);
+			}
+			
+		}
+		
+		return Collections.EMPTY_LIST;
+	}
+}
