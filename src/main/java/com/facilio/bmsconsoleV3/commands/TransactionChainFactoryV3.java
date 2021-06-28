9d20e0810c48714f9b0d9243c28cdc2c2761c970
vandhana <vandhana@facilio.com>
2021-06-28 17:52:33 +0530

    fix for booking

--- src/main/java/com/facilio/bmsconsoleV3/commands/TransactionChainFactoryV3.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/TransactionChainFactoryV3.java
@@ -713,7 +713,7 @@ public class TransactionChainFactoryV3 {
     public static FacilioChain getCreateBookingBeforeEditChain() {
         FacilioChain c = getDefaultChain();
         //c.addCommand(new ValidateCanEditBookingCommand());
-        c.addCommand(new ValidateFacilityBookingCommandForEditV3());
+        //c.addCommand(new ValidateFacilityBookingCommandForEditV3());
         c.addCommand(new ValidateCancelBookingCommandV3());
         return c;
     }
