574e4e17d6297b039a41150176dfe4cbb0fc5bc5
vandhana <vandhana@facilio.com>
2021-07-09 18:01:34 +0530

    inv req related fix

--- src/main/java/com/facilio/bmsconsole/actions/InventoryRequestAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/InventoryRequestAction.java
@@ -341,6 +341,7 @@ public class InventoryRequestAction extends FacilioAction {
 	}
 	public String issueInventoryRequests() throws Exception {
 		FacilioContext context = new FacilioContext();
+		inventoryRequest.setIsIssued(true);
 		context.put(FacilioConstants.ContextNames.RECORD, inventoryRequest);
 		context.put(FacilioConstants.ContextNames.EVENT_TYPE,EventType.EDIT);
 		FacilioChain issueRequestListChain = TransactionChainFactory.getIssueInventoryRequestChain();
