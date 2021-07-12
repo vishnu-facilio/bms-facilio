990012cbf79e30313633771a3a7d64e1a296c935
Boopathy <boopathy@facilio.com>
2021-07-12 19:10:04 +0530

    adding version, whenever the layout is changed by any mean

--- src/main/java/com/facilio/bmsconsole/commands/AssociateTabGroupCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/AssociateTabGroupCommand.java
@@ -1,7 +1,9 @@
 package com.facilio.bmsconsole.commands;
 
-import com.facilio.command.FacilioCommand;
+import com.facilio.bmsconsole.context.WebTabGroupContext;
 import com.facilio.bmsconsole.context.WebtabWebgroupContext;
+import com.facilio.bmsconsole.util.ApplicationApi;
+import com.facilio.command.FacilioCommand;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.db.builder.GenericInsertRecordBuilder;
 import com.facilio.modules.FieldFactory;
@@ -10,6 +12,7 @@ import com.facilio.modules.ModuleFactory;
 import org.apache.commons.chain.Context;
 import org.apache.commons.collections4.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +30,14 @@ public class AssociateTabGroupCommand extends FacilioCommand {
 
            insertBuilder.addRecords(props);
            insertBuilder.save();
+
+            // Increment layout version
+            Long groupId = (Long) context.get(FacilioConstants.ContextNames.WEB_TAB_GROUP_ID);
+            WebTabGroupContext webTabGroup = ApplicationApi.getWebTabGroup(groupId);
+            if (webTabGroup == null) {
+                throw new IllegalArgumentException("Invalid web group");
+            }
+            ApplicationApi.incrementLayoutVersionByIds(Collections.singletonList(webTabGroup.getLayoutId()));
         }
 
         return false;
--- src/main/java/com/facilio/bmsconsole/commands/ReorderTabGroupCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/ReorderTabGroupCommand.java
@@ -1,7 +1,9 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.Collections;
 import java.util.List;
 
+import com.facilio.bmsconsole.util.ApplicationApi;
 import com.facilio.command.FacilioCommand;
 import org.apache.commons.chain.Context;
 
@@ -19,6 +21,7 @@ public class ReorderTabGroupCommand extends FacilioCommand {
     public boolean executeCommand(Context context) throws Exception {
 		List<WebTabGroupContext> tabGroupList = (List<WebTabGroupContext>) context.get(FacilioConstants.ContextNames.WEB_TAB_GROUPS);
 		if (!tabGroupList.isEmpty()) {
+			long tabGroupId = tabGroupList.get(0).getId();	// get any one tab group, assuming client will send only tabgroup ids of same layout
 			
 			for(WebTabGroupContext tabGroup:tabGroupList) {
 				GenericUpdateRecordBuilder builder = new GenericUpdateRecordBuilder()
@@ -28,6 +31,12 @@ public class ReorderTabGroupCommand extends FacilioCommand {
 				builder.update(FieldUtil.getAsProperties(tabGroup));
 			}
 
+			// increment layout version
+			WebTabGroupContext webTabGroup = ApplicationApi.getWebTabGroup(tabGroupId);
+			if (webTabGroup == null) {
+				throw new IllegalArgumentException("Invalid web group");
+			}
+			ApplicationApi.incrementLayoutVersionByIds(Collections.singletonList(webTabGroup.getLayoutId()));
 		}
         return false;
     }
