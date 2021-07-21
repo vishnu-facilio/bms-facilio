afe2ab733315aa5642da8a1ae6bc32ada2e4c4a2
Krishna <krishnan.e@facilio.com>
2021-07-21 11:47:45 +0530

    adding new method for item transactions module.

---
+++ src/main/java/com/facilio/workflowv2/modulefunctions/FacilioItemTransactionFunctions.java
@@ -0,0 +1,35 @@
+package com.facilio.workflowv2.modulefunctions;
+
+import java.util.List;
+import java.util.Map;
+
+import com.facilio.bmsconsole.commands.TransactionChainFactory;
+import com.facilio.bmsconsole.context.ItemTransactionsContext;
+import com.facilio.bmsconsole.workflow.rule.EventType;
+import com.facilio.chain.FacilioChain;
+import com.facilio.chain.FacilioContext;
+import com.facilio.constants.FacilioConstants;
+import com.facilio.modules.FieldUtil;
+
+public class FacilioItemTransactionFunctions extends FacilioModuleFunctionImpl {
+
+	public void addOrUpdateItemTransactions(Map<String,Object> globalParams,List<Object> objects) throws Exception {
+		
+		List<Map<String, Object>> itemsRaw = (List<Map<String,Object>>)objects.get(1);
+		
+		List<ItemTransactionsContext> itemTransaction = FieldUtil.getAsBeanListFromMapList(itemsRaw, ItemTransactionsContext.class);
+		
+		List<Long> purchasedItems = (List<Long>) objects.get(2);
+		
+		FacilioChain addWorkorderPartChain = TransactionChainFactory.getAddOrUpdateItemTransactionsChain();
+		
+		FacilioContext context = addWorkorderPartChain.getContext();
+		context.put(FacilioConstants.ContextNames.EVENT_TYPE, EventType.CREATE);
+		context.put(FacilioConstants.ContextNames.RECORD_LIST, itemTransaction);
+		context.put(FacilioConstants.ContextNames.PURCHASED_ITEM, purchasedItems);
+		context.put(FacilioConstants.ContextNames.CURRENT_ACTIVITY, FacilioConstants.ContextNames.ASSET_ACTIVITY);
+		
+		addWorkorderPartChain.execute();
+		
+	}
+}
