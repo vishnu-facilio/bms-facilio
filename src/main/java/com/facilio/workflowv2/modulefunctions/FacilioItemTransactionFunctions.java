c65fb94a6904e8352e11cf223e12d6b06a666c6f
vandhana <vandhana@facilio.com>
2021-07-21 13:46:27 +0530

    paramater change

--- src/main/java/com/facilio/workflowv2/modulefunctions/FacilioItemTransactionFunctions.java
+++ src/main/java/com/facilio/workflowv2/modulefunctions/FacilioItemTransactionFunctions.java
@@ -1,5 +1,7 @@
 package com.facilio.workflowv2.modulefunctions;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -15,15 +17,15 @@ public class FacilioItemTransactionFunctions extends FacilioModuleFunctionImpl {
 
 	public void addOrUpdateItemTransactions(Map<String,Object> globalParams,List<Object> objects) throws Exception {
 		
-		List<Map<String, Object>> itemsRaw = (List<Map<String,Object>>)objects.get(1);
+		Map<String, Object> itemsRaw = (Map<String, Object>) objects.get(1);
 		
-		List<ItemTransactionsContext> itemTransaction = FieldUtil.getAsBeanListFromMapList(itemsRaw, ItemTransactionsContext.class);
+		ItemTransactionsContext itemTransaction = FieldUtil.getAsBeanFromMap(itemsRaw, ItemTransactionsContext.class);
 		
 		FacilioChain addWorkorderPartChain = TransactionChainFactory.getAddOrUpdateItemTransactionsChain();
 		
 		FacilioContext context = addWorkorderPartChain.getContext();
 		context.put(FacilioConstants.ContextNames.EVENT_TYPE, EventType.CREATE);
-		context.put(FacilioConstants.ContextNames.RECORD_LIST, itemTransaction);
+		context.put(FacilioConstants.ContextNames.RECORD_LIST, Collections.singletonList(itemTransaction));
 		context.put(FacilioConstants.ContextNames.CURRENT_ACTIVITY, FacilioConstants.ContextNames.ASSET_ACTIVITY);
 		
 		addWorkorderPartChain.execute();
