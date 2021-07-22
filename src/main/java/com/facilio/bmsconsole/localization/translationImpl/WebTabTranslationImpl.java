d7c24db2bb2f48fef16698188d96c99eb0d42407
arunkumar <arunkumar@facilio.com>
2021-07-22 20:51:32 +0530

    webtab translation changes

--- src/main/java/com/facilio/bmsconsole/localization/translationImpl/WebTabTranslationImpl.java
+++ src/main/java/com/facilio/bmsconsole/localization/translationImpl/WebTabTranslationImpl.java
@@ -2,6 +2,7 @@ package com.facilio.bmsconsole.localization.translationImpl;
 
 import com.facilio.bmsconsole.localization.util.TranslationConstants;
 import com.facilio.translation.TranslationIfc;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.util.Properties;
@@ -9,20 +10,40 @@ import java.util.Properties;
 public class WebTabTranslationImpl implements TranslationIfc {
 
     public static final String WEB_TAB = "webTab";
+    public static final String WEB_TAB_GROUP = "webTabGroup";
+
+    public static String getTranslationKey ( String prefix,String value ) {
+        return prefix + "." + value + "." + TranslationConstants.DISPLAY_NAME;
+    }
 
     @Override
     public JSONObject translate ( JSONObject json,Properties translationFile ) throws Exception {
 
         JSONObject jsonObject = (JSONObject)json.get("result");
-        JSONObject webTabObject = (JSONObject)jsonObject.get("webTabs");
-        webTabObject.keySet().forEach(webTab -> {
-            String key = getTranslationKey((String)webTabObject.get("route"));
-            webTabObject.put(TranslationConstants.NAME,getTranslation(translationFile,key,(String)webTabObject.get(TranslationConstants.NAME)));
-        });
-        return json;
-    }
+        JSONObject application = (JSONObject)jsonObject.get("application");
+        JSONArray jsonArray = (JSONArray)application.get("layouts");
 
-    public static  String getTranslationKey ( String value ) {
-        return WEB_TAB + "." + value + "." + TranslationConstants.DISPLAY_NAME;
+        for (int i = 0; i < jsonArray.size(); i++) {
+
+            JSONObject webTabGroups = (JSONObject)jsonArray.get(i);
+            JSONArray webTabGroupList = (JSONArray)webTabGroups.get("webTabGroupList");
+
+            for (int k = 0; k < webTabGroupList.size(); k++) {
+
+                JSONObject webTabGroupObject = (JSONObject)webTabGroupList.get(k);
+                String webTabGroupkey = getTranslationKey(WEB_TAB_GROUP,(String)webTabGroupObject.get("route"));
+                webTabGroupObject.put(TranslationConstants.NAME,getTranslation(translationFile,webTabGroupkey,(String)webTabGroupObject.get(TranslationConstants.NAME)));
+
+                JSONArray webTabArray = (JSONArray)webTabGroupObject.get("webTabs");
+
+                for (int j = 0; j < webTabArray.size(); j++) {
+                    JSONObject webTabObject = (JSONObject)webTabArray.get(j);
+                    String key = getTranslationKey(WEB_TAB,(String)webTabObject.get("route"));
+                    webTabObject.put(TranslationConstants.NAME,getTranslation(translationFile,key,(String)webTabObject.get(TranslationConstants.NAME)));
+                }
+            }
+        }
+
+        return json;
     }
 }
