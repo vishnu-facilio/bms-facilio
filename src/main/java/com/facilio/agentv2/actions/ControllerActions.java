072e2ea94986538b24616ee7d1044cfd322bf3da
anand <anand.h@facilio.com>
2021-08-01 19:44:51 +0530

    added export points to CSV API

--- src/main/java/com/facilio/agentv2/actions/ControllerActions.java
+++ src/main/java/com/facilio/agentv2/actions/ControllerActions.java
@@ -1,5 +1,6 @@
 package com.facilio.agentv2.actions;
 
+import com.facilio.accounts.util.AccountUtil;
 import com.facilio.agentv2.AgentConstants;
 import com.facilio.agentv2.controller.ControllerApiV2;
 import com.facilio.agentv2.controller.ControllerUtilV2;
@@ -8,12 +9,19 @@ import com.facilio.agentv2.point.GetPointRequest;
 import com.facilio.chain.FacilioContext;
 import com.facilio.constants.FacilioConstants;
 
+import com.sun.net.httpserver.Authenticator;
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -24,6 +32,12 @@ public class ControllerActions extends AgentActionV2 {
     @NotNull
     @Min(value = 0, message = "Controller Id can't be less than 1")
     private Long controllerId;
+    @Getter
+    @Setter
+    private InputStream downloadStream;
+    @Getter
+    @Setter
+    String fileName;
 
     public Long getControllerId() {
         return controllerId;
@@ -146,6 +160,49 @@ public class ControllerActions extends AgentActionV2 {
             setResult(AgentConstants.EXCEPTION,e.getMessage());
             internalError();
         }
+        return SUCCESS;
+    }
+
+    public String exportPoints() {
+
+        try {
+            GetPointRequest getPointRequest = new GetPointRequest().withControllerId(controllerId);
+            List<Map<String, Object>> points = getPointRequest.getPointsData();
+            if (points.size() > 0) {
+                String dirPath = System.getProperties().getProperty("java.io.tmpdir") + File.separator + AccountUtil.getCurrentOrg().getOrgId() + File.separator + "points";
+                String path = dirPath + File.separator + controllerId + ".csv";
+                setFileName(controllerId + ".csv");
+                File file = new File(path);
+                File dir = new File(dirPath);
+                boolean directoryExits = (dir.exists() && dir.isDirectory());
+                if (!directoryExits) {
+                    dir.mkdirs();
+                }
+                file.createNewFile();
+                FileOutputStream fos = new FileOutputStream(file);
+                StringBuilder content = new StringBuilder();
+                points.get(0).keySet().forEach(key -> {
+                    content.append(key);
+                    content.append(",");
+                });
+                content.append("\n");
+                points.forEach(row -> {
+                    row.forEach((colName, value) -> {
+                        content.append(value);
+                        content.append(",");
+                    });
+                    content.append("\n");
+                });
+                fos.write(content.toString().getBytes());
+                fos.close();
+                downloadStream = new FileInputStream(path);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return SUCCESS;
     }
 }
