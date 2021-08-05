cfd6f321dfaa588586ddb91cfc9da8ba49fd5a9b
Boopathy <boopathy@facilio.com>
2021-08-05 18:52:19 +0530

    add moduleState in supplement record in summary and list

--- src/main/java/com/facilio/v3/commands/FetchSysFields.java
+++ src/main/java/com/facilio/v3/commands/FetchSysFields.java
@@ -6,10 +6,12 @@ import com.facilio.constants.FacilioConstants;
 import com.facilio.fw.BeanFactory;
 import com.facilio.modules.FacilioModule;
 import com.facilio.modules.FieldFactory;
+import com.facilio.modules.fields.FacilioField;
 import com.facilio.modules.fields.LookupField;
 import com.facilio.modules.fields.SupplementRecord;
 import com.facilio.v3.context.Constants;
 import org.apache.commons.chain.Context;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,25 +23,31 @@ public class FetchSysFields extends FacilioCommand {
         if (isV4) {
             return false;
         }
+
         String moduleName = (String) context.get(FacilioConstants.ContextNames.MODULE_NAME);
         ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
 
         FacilioModule module = modBean.getModule(moduleName);
 
-        if (!module.isCustom()) {
-            return false;
-        }
-
         List<SupplementRecord> sysLookupFields = new ArrayList<>();
-        sysLookupFields.add((LookupField) FieldFactory.getSystemField("sysCreatedBy", module));
-        sysLookupFields.add((LookupField) FieldFactory.getSystemField("sysModifiedBy", module));
-
-        List<SupplementRecord> supplementFields = (List<SupplementRecord>) context.get(FacilioConstants.ContextNames.FETCH_SUPPLEMENTS);
-        if (supplementFields == null) {
-            supplementFields = new ArrayList<>();
-            context.put(FacilioConstants.ContextNames.FETCH_SUPPLEMENTS, supplementFields);
+        FacilioField moduleStateField = modBean.getField("moduleState", moduleName);
+        if (moduleStateField != null && moduleStateField instanceof LookupField) {
+            sysLookupFields.add((SupplementRecord) moduleStateField);
         }
-        supplementFields.addAll(sysLookupFields);
+
+        if (module.isCustom()) {
+            sysLookupFields.add((LookupField) FieldFactory.getSystemField("sysCreatedBy", module));
+            sysLookupFields.add((LookupField) FieldFactory.getSystemField("sysModifiedBy", module));
+        }
+
+        if (CollectionUtils.isNotEmpty(sysLookupFields)) {
+            List<SupplementRecord> supplementFields = (List<SupplementRecord>) context.get(FacilioConstants.ContextNames.FETCH_SUPPLEMENTS);
+            if (supplementFields == null) {
+                supplementFields = new ArrayList<>();
+                context.put(FacilioConstants.ContextNames.FETCH_SUPPLEMENTS, supplementFields);
+            }
+            supplementFields.addAll(sysLookupFields);
+        }
 
         return false;
     }
