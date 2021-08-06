d1001d7f48d3a0416aaa450785f10cb3ef9457dd
arunkumar <arunkumar@facilio.com>
2021-08-06 12:39:20 +0530

    added translation display columns for stateflow

--- src/main/java/com/facilio/bmsconsole/localization/translationImpl/StateFlowTranslationImplV3.java
+++ src/main/java/com/facilio/bmsconsole/localization/translationImpl/StateFlowTranslationImplV3.java
@@ -20,6 +20,8 @@ public class StateFlowTranslationImplV3 implements TranslationIfc {
             JSONObject state = (JSONObject)moduleState.get(stateKey);
             String key = StateFlowTranslationImpl.getTranslationKey(StateFlowTranslationImpl.STATE,String.valueOf(state.get("id")));
             state.put(TranslationConstants.DISPLAY_NAME,getTranslation(translationFile,key,(String)state.get(TranslationConstants.DISPLAY_NAME)));
+            state.put("primaryValue",getTranslation(translationFile,key,(String)state.get("primaryValue")));
+            state.put("status",getTranslation(translationFile,key,(String)state.get("status")));
         }
         return json;
     }
