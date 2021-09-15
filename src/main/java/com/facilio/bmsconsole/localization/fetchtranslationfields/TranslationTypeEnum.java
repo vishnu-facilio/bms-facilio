f0bc4424b672458c4985d8b52f89bff5e7b66806
arunkumar <arunkumar@facilio.com>
2021-09-15 16:54:49 +0530

    (Translation) : fetch details webtab column order changes.

--- src/main/java/com/facilio/bmsconsole/localization/fetchtranslationfields/TranslationTypeEnum.java
+++ src/main/java/com/facilio/bmsconsole/localization/fetchtranslationfields/TranslationTypeEnum.java
@@ -5,31 +5,30 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Getter
 @AllArgsConstructor
 public enum TranslationTypeEnum {
 
-    DETAILS(WebTabContext.Type.MODULE,new GetTranslationDetailFields()),
-    VIEWS(WebTabContext.Type.MODULE,new GetViewTranslationFields()),
-    VIEW_FOLDER(WebTabContext.Type.MODULE,new GetViewFolderTranslationFields()),
-    BUTTONS(WebTabContext.Type.MODULE, new GetButtonTranslationFields()),
-    STATES(WebTabContext.Type.MODULE,new GetStateTranslationFields()),
-    FORMS(WebTabContext.Type.MODULE,new GetFormTranslationFields()),
-    FORM_FIELDS(WebTabContext.Type.MODULE, new GetFormFieldTranslationFields()),
-    FIELDS(WebTabContext.Type.MODULE,new GetFieldTranslationFileds()),
-    WEB_TAB(WebTabContext.Type.MODULE,new GetWebTabTranslationFields()),
-    STATE_TRANSITION(WebTabContext.Type.MODULE,new GetStateTransitionTranslationFields()),
-    DASHBOARD_FOLDER(WebTabContext.Type.DASHBOARD,new GetDashboardFolderTranslationFields()),
-    DASHBOARD(WebTabContext.Type.DASHBOARD,new GetDashboardTabAndWidgetTransFields()),
-    REPORT_FOLDER(WebTabContext.Type.REPORT,new GetReportFolderTranslationFields()),
-    REPORT(WebTabContext.Type.REPORT,new GetReportTranslationFields())
+    DETAILS("DETAILS",WebTabContext.Type.MODULE,new GetTranslationDetailFields()),
+    FIELDS("FIELDS",WebTabContext.Type.MODULE,new GetFieldTranslationFileds()),
+    FORMS("FORMS",WebTabContext.Type.MODULE,new GetFormTranslationFields()),
+    VIEWS("VIEWS",WebTabContext.Type.MODULE,new GetViewTranslationFields()),
+    VIEW_FOLDER("VIEW FOLDER",WebTabContext.Type.MODULE,new GetViewFolderTranslationFields()),
+    STATES("STATES",WebTabContext.Type.MODULE,new GetStateTranslationFields()),
+    STATE_TRANSITION("STATE TRANSITION",WebTabContext.Type.MODULE,new GetStateTransitionTranslationFields()),
+    BUTTONS("BUTTONS",WebTabContext.Type.MODULE, new GetButtonTranslationFields()),
+    DASHBOARD_FOLDER("DASHBOARD FOLDER",WebTabContext.Type.DASHBOARD,new GetDashboardFolderTranslationFields()),
+    DASHBOARD("DASHBOARD",WebTabContext.Type.DASHBOARD,new GetDashboardTabAndWidgetTransFields()),
+    REPORT_FOLDER("REPORT FOLDER",WebTabContext.Type.REPORT,new GetReportFolderTranslationFields()),
+    REPORT("REPORT",WebTabContext.Type.REPORT,new GetReportTranslationFields())
     ;
 
     private static final Map<String, TranslationTypeEnum> TRANSLATION_TYPE_ENUM_MAP = initTranslationWiseType();
+    public static final Map<WebTabContext.Type, List<ClientColumnTypeEnum>> CLIENT_TRANSLATION_TYPE_ENUM= Collections.unmodifiableMap(init());
+
+    private String clientColumnName;
     private WebTabContext.Type type;
     private TranslationTypeInterface handler;
 
@@ -44,4 +43,35 @@ public enum TranslationTypeEnum {
     public static TranslationTypeEnum getTranslationTypeModule ( @NonNull String type ) {
         return TRANSLATION_TYPE_ENUM_MAP.get(type);
     }
+
+    private static Map<WebTabContext.Type, List<ClientColumnTypeEnum>> init () {
+        Map<WebTabContext.Type, List<ClientColumnTypeEnum>> columnVsType = new LinkedHashMap<>();
+        List<ClientColumnTypeEnum> moduleType = new ArrayList<>();
+        List<ClientColumnTypeEnum> dashboardType = new ArrayList<>();
+        List<ClientColumnTypeEnum> reportType = new ArrayList<>();
+        for (TranslationTypeEnum type : values()) {
+            switch (type.getType()) {
+                case MODULE:
+                    moduleType.add(new ClientColumnTypeEnum(type.name(),type.getClientColumnName()));
+                    break;
+                case DASHBOARD:
+                    dashboardType.add(new ClientColumnTypeEnum(type.name(),type.getClientColumnName()));
+                    break;
+                case REPORT:
+                    reportType.add(new ClientColumnTypeEnum(type.name(),type.getClientColumnName()));
+                    break;
+            }
+            columnVsType.put(WebTabContext.Type.MODULE,moduleType);
+            columnVsType.put(WebTabContext.Type.DASHBOARD,dashboardType);
+            columnVsType.put(WebTabContext.Type.REPORT,reportType);
+        }
+        return columnVsType;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class ClientColumnTypeEnum {
+        private String type, label;
+    }
 }
+
