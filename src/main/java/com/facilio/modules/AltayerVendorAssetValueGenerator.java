71eaa444f4b2295fd6c4d9a276e04620f2c6c734
Vandhana <vandhana@facilio.com>
2021-10-18 18:16:01 +0530

    adding vendor portal related scoping generators

---
+++ src/main/java/com/facilio/modules/AltayerVendorAssetValueGenerator.java
@@ -0,0 +1,88 @@
+package com.facilio.modules;
+
+import com.facilio.accounts.util.AccountUtil;
+import com.facilio.beans.ModuleBean;
+import com.facilio.bmsconsoleV3.context.V3VendorContext;
+import com.facilio.bmsconsoleV3.util.V3PeopleAPI;
+import com.facilio.db.criteria.CriteriaAPI;
+import com.facilio.db.criteria.operators.PickListOperators;
+import com.facilio.fw.BeanFactory;
+import com.facilio.modules.fields.FacilioField;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class AltayerVendorAssetValueGenerator extends ValueGenerator{
+    @Override
+    public Object generateValueForCondition(int appType) {
+        try {
+            V3VendorContext vendor = V3PeopleAPI.getVendorForUser(AccountUtil.getCurrentUser().getId());
+            if (vendor != null) {
+                List<Long> assetCategoryIds = getAssetCategoryData(vendor.getId());
+                if (CollectionUtils.isNotEmpty(assetCategoryIds)) {
+                    return StringUtils.join(assetCategoryIds, ",");
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Long> getAssetCategoryData(long vendorId) throws Exception {
+        List<Long> categoryIds = new ArrayList<>();
+        ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+        FacilioModule module = modBean.getModule("custom_vendormapping");
+        Map<String, FacilioField> fieldMap = FieldFactory.getAsMap(modBean.getAllFields(module.getName()));
+        SelectRecordsBuilder<ModuleBaseWithCustomFields> builder = new SelectRecordsBuilder<ModuleBaseWithCustomFields>()
+                .module(module)
+                .beanClass(ModuleBaseWithCustomFields.class)
+                .select(modBean.getAllFields(module.getName()))
+                .andCondition(CriteriaAPI.getCondition(fieldMap.get("vendor"), String.valueOf(vendorId), PickListOperators.IS))
+                ;
+        List<Map<String, Object>> props = builder.getAsProps();
+        if(CollectionUtils.isNotEmpty(props)) {
+            for(Map<String, Object> prop : props) {
+                Map<String, Object> category = (Map<String, Object>) prop.get("category");
+                Long categoryId = (Long)category.get("id");
+                if(!categoryIds.contains(categoryId)) {
+                    categoryIds.add(categoryId);
+                }
+            }
+            if(CollectionUtils.isNotEmpty(categoryIds)){
+                FacilioModule catModule = modBean.getModule("custom_workordercustomcategory");
+                Map<String, FacilioField> catFieldMap = FieldFactory.getAsMap(modBean.getAllFields(catModule.getName()));
+                SelectRecordsBuilder<ModuleBaseWithCustomFields> builderCategory = new SelectRecordsBuilder<ModuleBaseWithCustomFields>()
+                        .module(module)
+                        .beanClass(ModuleBaseWithCustomFields.class)
+                        .select(modBean.getAllFields(catModule.getName()))
+                        .andCondition(CriteriaAPI.getCondition(catFieldMap.get("workordercategory"), StringUtils.join(categoryIds, ","), PickListOperators.IS))
+                        ;
+                List<Map<String, Object>> catProps = builderCategory.getAsProps();
+                if(CollectionUtils.isNotEmpty(catProps)) {
+                    List<Long> assetCategoriesList = new ArrayList<>();
+                    for (Map<String, Object> prop : catProps) {
+                        List<Map<String, Object>> assetCategories = (List<Map<String, Object>>) prop.get("assetcategorynew");
+                        if(CollectionUtils.isNotEmpty(assetCategories)) {
+                            for(Map<String, Object> map : assetCategories) {
+                                Long categoryId = (Long) map.get("id");
+                                if (!assetCategoriesList.contains(categoryId)) {
+                                    assetCategoriesList.add(categoryId);
+                                }
+                            }
+                        }
+                    }
+                    if(CollectionUtils.isNotEmpty(assetCategoriesList)) {
+                        return assetCategoriesList;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
