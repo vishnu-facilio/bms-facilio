943df59f5c4bbfbd71757d93cbc26c001ade1fba
Anupriya <86521756+AnupriyaBhuvan@users.noreply.github.com>
2021-10-27 19:09:48 +0530

    Added loggers for debugging asset summary load issue

--- src/main/java/com/facilio/bmsconsoleV3/commands/asset/LoadAssetSummaryCommandV3.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/asset/LoadAssetSummaryCommandV3.java
@@ -89,11 +89,11 @@ public class LoadAssetSummaryCommandV3  extends FacilioCommand {
     private void getAssetLocation(V3AssetContext asset) throws Exception {
         V3BaseSpaceContext assetLocation = asset.getSpace();
 
-        LOGGER.info("Asset Summary ID ===>"+assetLocation.getId());
-        LOGGER.info("Asset Summary Site ID ===>"+assetLocation.getSiteId());
-        LOGGER.info("Asset Summary Building ID ===>"+assetLocation.getBuildingId());
-        LOGGER.info("Asset Summary Floor ID ===>"+assetLocation.getFloorId());
-        LOGGER.info("Asset Summary Space ID ===>"+assetLocation.getSpaceId());
+        LOGGER.info("Asset Summary location ID ===>"+assetLocation.getId());
+        LOGGER.info("Asset Summary Site ID ===>"+assetLocation.getSiteId() + " -- "+assetLocation.getSite());
+        LOGGER.info("Asset Summary Building ID ===>"+assetLocation.getBuildingId() + " -- "+assetLocation.getBuilding());
+        LOGGER.info("Asset Summary Floor ID ===>"+assetLocation.getFloorId() + " -- "+assetLocation.getFloor());
+        LOGGER.info("Asset Summary Space ID ===>"+assetLocation.getSpaceId() + " -- "+assetLocation.getSpace());
 
         if (assetLocation == null || assetLocation.getId() <= 0) {
             return;
