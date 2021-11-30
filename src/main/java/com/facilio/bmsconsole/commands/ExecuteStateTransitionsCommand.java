07a4e639b85a2948454cec6ef1c56ee49471c78b
Boopathy <boopathy@facilio.com>
2021-11-30 12:19:47 +0530

    optimizing state transition command

--- src/main/java/com/facilio/bmsconsole/commands/ExecuteStateTransitionsCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/ExecuteStateTransitionsCommand.java
@@ -1,7 +1,14 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import com.facilio.bmsconsole.workflow.rule.*;
+import com.facilio.chain.FacilioContext;
+import com.facilio.modules.FacilioModule;
 import org.apache.commons.chain.Context;
 import org.apache.commons.collections4.CollectionUtils;
 
@@ -33,5 +40,35 @@ public class ExecuteStateTransitionsCommand extends ExecuteAllWorkflowsCommand {
 		Criteria criteria = super.getCriteria(records);
 
 		return criteria;
+	}
+
+	@Override
+	protected List<WorkflowRuleContext> getWorkflowRules(FacilioModule module, List<EventType> activities, List<? extends ModuleBaseWithCustomFields> records, FacilioContext context) throws Exception {
+		List<WorkflowRuleContext> workflowRules = super.getWorkflowRules(module, activities, records, context);
+
+		Set<Long> stateFlowIds = new HashSet<>();
+		Set<Long> fromStateIds = new HashSet<>();
+		if (CollectionUtils.isNotEmpty(records)) {
+			for (ModuleBaseWithCustomFields record : records) {
+				stateFlowIds.add(record.getStateFlowId());
+				if (record.getModuleState() != null) {
+					fromStateIds.add(record.getModuleState().getId());
+				}
+			}
+			workflowRules = workflowRules.stream().filter(rule -> {
+				if (rule instanceof StateflowTransitionContext) {
+					StateflowTransitionContext stateflowTransitionContext = (StateflowTransitionContext) rule;
+					if (stateflowTransitionContext.getTypeEnum() != AbstractStateTransitionRuleContext.TransitionType.CONDITIONED) {
+						return false;
+					}
+					if (stateFlowIds.contains(stateflowTransitionContext.getStateFlowId()) && fromStateIds.contains(stateflowTransitionContext.getFromStateId())) {
+						return true;
+					}
+				}
+				return false;
+			}).collect(Collectors.toList());
+		}
+
+		return workflowRules;
 	}
 }
