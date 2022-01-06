9e54a24c23ab57eb582ffa72466371a3b65e46db
Krishna <krishnan.e@facilio.com>
2022-01-06 15:51:32 +0530

    supporting markAsDelete in script.

--- src/main/java/com/facilio/workflowv2/modulefunctions/FacilioModuleFunctionImpl.java
+++ src/main/java/com/facilio/workflowv2/modulefunctions/FacilioModuleFunctionImpl.java
@@ -338,7 +338,12 @@ public class FacilioModuleFunctionImpl implements FacilioModuleFunction {
 		
 		delete.andCriteria(criteria);
 		
-		delete.delete();
+		if(module.isTrashEnabled()) {
+			delete.markAsDelete();
+		}
+		else {
+			delete.delete();
+		}
 	}
 	
 	@Override
