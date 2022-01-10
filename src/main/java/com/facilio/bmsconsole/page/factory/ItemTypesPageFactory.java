e50fc5537735c7094a6b624c4d5fef128d2e873d
Yuvasree D <yuvasree@facilio.com>
2022-01-10 19:24:45 +0530

    removing redundant widget names and replacing it with one common name

--- src/main/java/com/facilio/bmsconsole/page/factory/ItemTypesPageFactory.java
+++ src/main/java/com/facilio/bmsconsole/page/factory/ItemTypesPageFactory.java
@@ -25,17 +25,17 @@ public class ItemTypesPageFactory extends PageFactory{
         }
         Page.Section tab1Sec2 = page.new Section();
         tab1.addSection(tab1Sec2);
-        PageWidget card1= new PageWidget(PageWidget.WidgetType.ITEM_TYPES_CARD);
+        PageWidget card1= new PageWidget(PageWidget.WidgetType.INVENTORY_CARD);
         card1.addToLayoutParams(tab1Sec2, 8, 5);
         card1.addToWidgetParams("card","itemtypescard1");
         tab1Sec2.addWidget(card1);
 
-        PageWidget card2= new PageWidget(PageWidget.WidgetType.ITEM_TYPES_CARD);
+        PageWidget card2= new PageWidget(PageWidget.WidgetType.INVENTORY_CARD);
         card2.addToLayoutParams(tab1Sec2, 8, 5);
         card2.addToWidgetParams("card","itemtypescard2");
         tab1Sec2.addWidget(card2);
 
-        PageWidget card3= new PageWidget(PageWidget.WidgetType.ITEM_TYPES_CARD);
+        PageWidget card3= new PageWidget(PageWidget.WidgetType.INVENTORY_CARD);
         card3.addToLayoutParams(tab1Sec2, 8, 5);
         card3.addToWidgetParams("card","itemtypescard3");
         tab1Sec2.addWidget(card3);
