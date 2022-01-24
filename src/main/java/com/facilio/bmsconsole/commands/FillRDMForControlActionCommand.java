ef961085566fb9b5e9bc4c3117a594cb24d32cf4
Mohammed Shaan Sameem <shaan@facilio.com>
2022-01-24 13:05:23 +0530

    supporting multiple control in script

--- src/main/java/com/facilio/bmsconsole/commands/FillRDMForControlActionCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/FillRDMForControlActionCommand.java
@@ -1,9 +1,15 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.facilio.command.FacilioCommand;
 import org.apache.commons.chain.Context;
+import org.apache.commons.lang3.tuple.Pair;
 
 import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.context.ReadingDataMeta;
@@ -22,13 +28,25 @@ public class FillRDMForControlActionCommand extends FacilioCommand {
 		ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
 		
 		if(commands != null) {
+			List<Pair<Long, FacilioField>> rdmPairs = new ArrayList<>();
 			for(ControlActionCommandContext command :commands) {
-				FacilioField field = modBean.getField(command.getFieldId());
 				if(command.getRdm() == null) {
-					ReadingDataMeta rdm = ReadingsAPI.getReadingDataMeta(command.getResource().getId(), field);
-					command.setRdm(rdm);
+					FacilioField field = modBean.getField(command.getFieldId());
+					command.setField(field);
+					rdmPairs.add(Pair.of(command.getResource().getId(), field));
 				}
-				
+			}
+			
+			if (!rdmPairs.isEmpty()) {
+				Map<String, ReadingDataMeta> rdmMap = new HashMap<>();
+				List<ReadingDataMeta> rdmList = ReadingsAPI.getReadingDataMetaList(rdmPairs);
+				rdmMap.putAll(rdmList.stream().collect(Collectors.toMap(rdm -> ReadingsAPI.getRDMKey(rdm), Function.identity())));
+				for(ControlActionCommandContext command :commands) {
+					if(command.getRdm() == null) {
+						ReadingDataMeta rdm = rdmMap.get(ReadingsAPI.getRDMKey(command.getResource().getId(), command.getField()));
+						command.setRdm(rdm);
+					}
+				}
 			}
 		}
 		else {
