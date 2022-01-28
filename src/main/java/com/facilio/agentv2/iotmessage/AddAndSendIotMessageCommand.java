62e8a5435a62a3af89385e0678dceb33019543bd
Mohammed Shaan Sameem <shaan@facilio.com>
2022-01-28 19:00:24 +0530

    debug logs for checking transaction in post execute

--- src/main/java/com/facilio/agentv2/iotmessage/AddAndSendIotMessageCommand.java
+++ src/main/java/com/facilio/agentv2/iotmessage/AddAndSendIotMessageCommand.java
@@ -3,6 +3,13 @@ package com.facilio.agentv2.iotmessage;
 import com.facilio.agentv2.AgentConstants;
 import com.facilio.command.FacilioCommand;
 import com.facilio.command.PostTransactionCommand;
+import com.facilio.db.transaction.FTransactionManager;
+import com.facilio.db.transaction.FacilioTransaction;
+import com.facilio.db.transaction.FacilioTransactionManager;
+
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
 
 import org.apache.commons.chain.Context;
 import org.apache.log4j.LogManager;
@@ -23,6 +30,16 @@ public class AddAndSendIotMessageCommand extends FacilioCommand implements PostT
 
 	@Override
 	public boolean postExecute() throws Exception {
+		try {
+			TransactionManager tm = FTransactionManager.getTransactionManager();
+			Transaction t = tm.getTransaction();
+			if(t != null ) {
+			   LOGGER.info("Transaction id in post execute - " + ((FacilioTransaction)t).getTransactionId());
+			}
+		} catch (SystemException e) {
+			LOGGER.info("Exception occurred while getting transaction", e);
+		}
+		
         IotMessageApiV2.publishIotMessage(data);
 		return false;
 	}
