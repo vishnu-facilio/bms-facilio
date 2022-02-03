fb4490277273de78310dcb32bd616524ab7e97eb
Yuvasree D <yuvasree@facilio.com>
2022-02-03 17:41:46 +0530

    adding long desc field to fetchSupplements

---
+++ src/main/java/com/facilio/bmsconsoleV3/commands/termsandconditions/LoadTermsLookupCommandV3.java
@@ -0,0 +1,33 @@
+package com.facilio.bmsconsoleV3.commands.termsandconditions;
+
+import com.facilio.beans.ModuleBean;
+import com.facilio.command.FacilioCommand;
+import com.facilio.constants.FacilioConstants;
+import com.facilio.fw.BeanFactory;
+import com.facilio.modules.FieldFactory;
+import com.facilio.modules.fields.FacilioField;
+import com.facilio.modules.fields.SupplementRecord;
+import org.apache.commons.chain.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class LoadTermsLookupCommandV3 extends FacilioCommand{
+    @Override
+    public boolean executeCommand(Context context) throws Exception {
+        String moduleName = (String) context.get(FacilioConstants.ContextNames.MODULE_NAME);
+        ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+        List<FacilioField> fields = modBean.getAllFields(moduleName);
+        Map<String, FacilioField> fieldsAsMap = FieldFactory.getAsMap(fields);
+        List<SupplementRecord> fetchLookupsList = new ArrayList<>();
+        SupplementRecord longDesc = (SupplementRecord) fieldsAsMap.get("longDesc");
+
+        fetchLookupsList.add(longDesc);
+
+        context.put(FacilioConstants.ContextNames.FETCH_SUPPLEMENTS, fetchLookupsList);
+        return false;
+    }
+}
+
+
