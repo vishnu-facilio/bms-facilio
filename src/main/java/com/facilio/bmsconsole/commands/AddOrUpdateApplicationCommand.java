5af77e8e51230e39e5a6c619dc1ec0be62d7529d
Vandhana <vandhana@facilio.com>
2022-02-17 19:09:31 +0530

    add default scoping when adding new app

--- src/main/java/com/facilio/bmsconsole/commands/AddOrUpdateApplicationCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/AddOrUpdateApplicationCommand.java
@@ -43,6 +43,7 @@ public class AddOrUpdateApplicationCommand extends FacilioCommand {
 					ApplicationApi.addApplicationLayout(layout);
 				}
 				addAdminRoleForApp(app);
+				ApplicationApi.addDefaultScoping(app.getId());
 			}
 
 		}
