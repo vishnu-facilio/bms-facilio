e9b8614377ceb186edf18588f9ac2e260619258a
Yuvasree D <yuvasree@facilio.com>
2022-03-03 13:18:04 +0530

    adding custom fields for storeroom module

--- src/main/java/com/facilio/apiv3/APIv3Config.java
+++ src/main/java/com/facilio/apiv3/APIv3Config.java
@@ -450,7 +450,7 @@ public class APIv3Config {
         }
     @Module("storeRoom")
     public static Supplier<V3Config> getStoreRoom() {
-        return () -> new V3Config(V3StoreRoomContext.class, null)
+        return () -> new V3Config(V3StoreRoomContext.class, new ModuleCustomFieldCount30())
                 .create()
                     .afterSave(new UpdateServingSitesinStoreRoomCommandV3())
                 .update()
