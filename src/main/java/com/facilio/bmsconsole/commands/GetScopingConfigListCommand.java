b026e144eaeec0580fcb7d4c71cab63860adcb21
Vandhana <vandhana@facilio.com>
2022-03-15 17:44:17 +0530

    changes to scoping summary api

--- src/main/java/com/facilio/bmsconsole/commands/GetScopingConfigListCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetScopingConfigListCommand.java
@@ -2,11 +2,15 @@ package com.facilio.bmsconsole.commands;
 
 import com.facilio.accounts.util.AccountUtil;
 import com.facilio.bmsconsole.context.ScopingConfigContext;
+import com.facilio.bmsconsole.context.ScopingContext;
 import com.facilio.bmsconsole.util.ApplicationApi;
 import com.facilio.command.FacilioCommand;
 import com.facilio.constants.FacilioConstants;
 import org.apache.commons.chain.Context;
+import org.apache.commons.collections4.MapUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class GetScopingConfigListCommand extends FacilioCommand {
@@ -17,8 +21,12 @@ public class GetScopingConfigListCommand extends FacilioCommand {
         if(scopingId == null || scopingId <= 0){
             throw new IllegalArgumentException("Scoping Id is invalid");
         }
+        ScopingContext scoping = ApplicationApi.getScoping(scopingId);
         Map<Long, ScopingConfigContext> scopingMap = ApplicationApi.getScopingMapForApp(scopingId);
-        context.put(FacilioConstants.ContextNames.SCOPING_CONTEXT, scopingMap.values());
+        if(MapUtils.isNotEmpty(scopingMap)) {
+            scoping.setScopingConfigList(new ArrayList<>(scopingMap.values()));
+        }
+        context.put(FacilioConstants.ContextNames.SCOPING_CONTEXT, scoping);
         return false;
     }
 }
