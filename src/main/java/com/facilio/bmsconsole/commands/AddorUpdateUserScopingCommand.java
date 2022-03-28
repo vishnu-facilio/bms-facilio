f1297624e704387297fb29e188f0265796d2f13b
Kamala Kannan <kamalakannan@facilio.com>
2022-03-28 13:54:02 +0530

    User scoping validation is removed - until ui is live

--- src/main/java/com/facilio/bmsconsole/commands/AddorUpdateUserScopingCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/AddorUpdateUserScopingCommand.java
@@ -4,7 +4,6 @@ import com.facilio.accounts.dto.User;
 import com.facilio.bmsconsole.util.ApplicationApi;
 import com.facilio.command.FacilioCommand;
 import com.facilio.constants.FacilioConstants;
-import com.facilio.util.FacilioUtil;
 import org.apache.commons.chain.Context;
 
 public class AddorUpdateUserScopingCommand extends FacilioCommand {
@@ -12,12 +11,14 @@ public class AddorUpdateUserScopingCommand extends FacilioCommand {
     public boolean executeCommand(Context context) throws Exception {
         User user = (User) context.get(FacilioConstants.ContextNames.USER);
         //commenting until ui is live
-        //FacilioUtil.throwIllegalArgumentException(user.getScoping() == null, "Invalid scoping");
-        Long appId = (Long)context.getOrDefault(FacilioConstants.ContextNames.APPLICATION_ID, -1l);
-        if(appId == null || appId <= 0) {
-            appId = ApplicationApi.getApplicationIdForLinkName(FacilioConstants.ApplicationLinkNames.FACILIO_MAIN_APP);
+        if(user.getScoping() != null) {
+            //FacilioUtil.throwIllegalArgumentException(user.getScoping() == null, "Invalid scoping");
+            Long appId = (Long) context.getOrDefault(FacilioConstants.ContextNames.APPLICATION_ID, -1l);
+            if (appId == null || appId <= 0) {
+                appId = ApplicationApi.getApplicationIdForLinkName(FacilioConstants.ApplicationLinkNames.FACILIO_MAIN_APP);
+            }
+            ApplicationApi.updateScopingForUser(user.getScoping().getId(), appId, user.getOuid());
         }
-        ApplicationApi.updateScopingForUser(user.getScoping().getId(),appId,user.getOuid());
         return false;
     }
 }
