4e3bd1149554b850c414afc4eb515ed33c122ec5
Mohammed Shaan Sameem <shaan@facilio.com>
2022-04-05 05:48:56 +0000

    Merged in auto_points_addition_custom (pull request #1151)
    auto controller and point addition for custom agent
    
    * auto controller and point addition for custom agent
    * auto addition of points and controllers based on agent type
    
    Approved-by: anand h
    Approved-by: Mukundhan R

--- src/main/java/com/facilio/agent/AgentType.java
+++ src/main/java/com/facilio/agent/AgentType.java
@@ -6,31 +6,37 @@ import java.util.Map;
 
 public enum AgentType {
 	CUSTOM(0, "custom"),
-	FACILIO(1, "facilio"),
-	NIAGARA(2, "niagara"),
+	FACILIO(1, "facilio", false),
+	NIAGARA(2, "niagara", false),
     CLOUD(3,"cloud"),
     REST(4,"rest"),
     WATTSENSE(5, "wattsense"),
-    RDM(6, "RDM", true),
-    CLOUD_ON_SERVICE(7, "cloud-on-service", true),
-    MQTT(8, "mqtt", true)
+    RDM(6, "RDM", false, true),
+    CLOUD_ON_SERVICE(7, "cloud-on-service", true, true),
+    MQTT(8, "mqtt", true, true)
     ;
 
 
 	private int key;
     private String label;
     private boolean agentService;
+    private boolean allowAutoAddition;
 
     AgentType(int key, String label) {
-        this(key, label, false);
+        this(key, label, true);
     }
     
-    AgentType(int key, String label, boolean agentService) {
+    AgentType(int key, String label, boolean allowAutoAddition) {
+    	this(key, label, allowAutoAddition, false);
+    }
+    
+    AgentType(int key, String label, boolean allowAutoAddition, boolean agentService) {
         this.key = key;
         this.label = label;
         this.agentService = agentService;
+        this.allowAutoAddition = allowAutoAddition;
     }
-
+    
     public  int getKey(){
         return key;
     }
@@ -41,6 +47,10 @@ public enum AgentType {
     
     public boolean isAgentService() {
     	return agentService;
+    }
+    
+    public boolean allowAutoAddition() {
+    	return allowAutoAddition;
     }
 
     public static AgentType valueOf(int value) {
