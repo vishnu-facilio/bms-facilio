8f46858179b93d57251cd43626b6f82bb228d10b
Krishna <krishnan.e@facilio.com>
2022-04-28 13:13:43 +0530

    adding a hack for service request form add.

--- src/main/java/com/facilio/bmsconsoleV3/signup/serviceRequest/AddServiceRequestForm.java
+++ src/main/java/com/facilio/bmsconsoleV3/signup/serviceRequest/AddServiceRequestForm.java
@@ -1,5 +1,7 @@
 package com.facilio.bmsconsoleV3.signup.serviceRequest;
 
+import java.util.List;
+
 import org.apache.commons.chain.Context;
 import com.facilio.bmsconsole.commands.TransactionChainFactory;
 import com.facilio.bmsconsole.context.ApplicationContext;
@@ -9,14 +11,22 @@ import com.facilio.bmsconsoleV3.signup.SignUpData;
 import com.facilio.chain.FacilioChain;
 import com.facilio.chain.FacilioContext;
 import com.facilio.constants.FacilioConstants;
+import com.facilio.modules.FacilioModule;
+import com.facilio.v3.context.Constants;
 
 public class AddServiceRequestForm extends SignUpData {
 
+	
 	public void addData() throws Exception {
 		ApplicationContext appContext = ApplicationApi.getApplicationForLinkName(FacilioConstants.ApplicationLinkNames.FACILIO_MAIN_APP);
 		
 		FacilioChain addform = TransactionChainFactory.getAddFormCommand();
 		FacilioContext context=addform.getContext();
+		
+		// -- DO NOT REMOVE THESE LINES  
+		FacilioModule srattachments = Constants.getModBean().getModule("servicerequestsattachments");
+		Constants.getModBean().getModule(srattachments.getModuleId());
+		// -- DO NOT REMOVE THESE LINES
 		
 		context.put(FacilioConstants.ContextNames.MODULE_NAME, "serviceRequest");
 		FacilioForm form = new FacilioForm();
