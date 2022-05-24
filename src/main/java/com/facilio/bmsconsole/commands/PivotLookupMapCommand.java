1c9642222041ab73218f19f1d493576b22accd06
harish-facilio <harish@facilio.com>
2022-05-24 11:34:18 +0530

    pivot lookup drill down fix

---
+++ src/main/java/com/facilio/bmsconsole/commands/PivotLookupMapCommand.java
@@ -0,0 +1,185 @@
+package com.facilio.bmsconsole.commands;
+
+import com.facilio.beans.ModuleBean;
+import com.facilio.bmsconsole.util.LookupSpecialTypeUtil;
+import com.facilio.command.FacilioCommand;
+import com.facilio.constants.FacilioConstants;
+import com.facilio.db.criteria.CriteriaAPI;
+import com.facilio.db.criteria.operators.*;
+import com.facilio.fw.BeanFactory;
+import com.facilio.modules.FacilioModule;
+import com.facilio.modules.FieldFactory;
+import com.facilio.modules.ModuleBaseWithCustomFields;
+import com.facilio.modules.SelectRecordsBuilder;
+import com.facilio.modules.fields.FacilioField;
+import com.facilio.modules.fields.LookupField;
+import com.facilio.report.context.PivotRowColumnContext;
+import org.apache.commons.chain.Context;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PivotLookupMapCommand extends FacilioCommand {
+
+    @Override
+    public boolean executeCommand(Context context) throws Exception {
+        List<PivotRowColumnContext> rowColumns = (List<PivotRowColumnContext>) context.get(FacilioConstants.Reports.ROWS);
+        ArrayList<HashMap<String, Object>> pivotTableData = (ArrayList<HashMap<String, Object>>) context.get(FacilioConstants.ContextNames.PIVOT_TABLE_DATA);
+
+        ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+
+        Map<String, Object> pivotLookupMap = new HashMap<>();
+
+        for(PivotRowColumnContext row: rowColumns){
+            FacilioField facilioField = getField(row, modBean);
+            LookupField lookupField = getLookupField(row, modBean);
+            if(facilioField instanceof LookupField) {
+                List<Long> ids = getColumnArray(row.getAlias(), pivotTableData);
+                pivotLookupMap.put(row.getAlias(), getLookUpMap(lookupField.getSpecialType(), lookupField.getLookupModule(), ids));
+            } else if(lookupField != null){
+                String valueString = getColumnValues(row.getAlias(), pivotTableData);
+                pivotLookupMap.put(row.getAlias(), getLookUpMap(lookupField.getLookupModule(), facilioField, valueString));
+            }
+        }
+
+        context.put(FacilioConstants.ContextNames.PIVOT_LOOKUP_MAP, pivotLookupMap);
+
+        return false;
+    }
+
+    private FacilioField getField(PivotRowColumnContext rowColumn, ModuleBean modBean) throws Exception {
+        return modBean.getField(rowColumn.getField().getId()).clone();
+    }
+
+    private LookupField getLookupField(PivotRowColumnContext rowColumn, ModuleBean modBean) throws Exception {
+        if(rowColumn.getLookupFieldId() > 0){
+            return (LookupField) modBean.getField(rowColumn.getLookupFieldId()).clone();
+        }
+        return null;
+    }
+
+
+    private String getColumnValues(String alias, ArrayList<HashMap<String, Object>> pivotTableData){
+        StringJoiner stringJoiner = new StringJoiner(",");
+        List<String> valueArray = new ArrayList<>();
+        for (HashMap<String, Object> data: pivotTableData) {
+            Map<String, Object> rows = (Map<String, Object>) data.get("rows");
+            Object val = rows.get(alias);
+            if (val == null) continue;
+            valueArray.add(val.toString());
+        }
+
+        valueArray = valueArray.stream().distinct().collect(Collectors.toList());
+
+        for (String value: valueArray) {
+            stringJoiner.add(value);
+        }
+        return stringJoiner.toString();
+    }
+
+    private List<Long> getColumnArray(String alias, ArrayList<HashMap<String, Object>> pivotTableData){
+        List<Long> idsArray = new ArrayList<>();
+        List<Object> valueArray = new ArrayList<>();
+        for (HashMap<String, Object> data: pivotTableData) {
+            Map<String, Object> rows = (Map<String, Object>) data.get("rows");
+            Object val = rows.get(alias);
+            if (val == null) continue;
+            idsArray.add(Long.parseLong(val.toString()));
+        }
+
+        idsArray = idsArray.stream().distinct().collect(Collectors.toList());
+
+        return idsArray;
+    }
+
+    private Map<Object, Long>getLookUpMap(FacilioModule lookupModule, FacilioField facilioField, String data) throws Exception {
+        Map<Object, Long> lookupMap = new HashMap<>();
+
+        List<FacilioField> selectFields = new ArrayList<>();
+        selectFields.add(FieldFactory.getIdField(lookupModule));
+        selectFields.add(facilioField);
+
+        SelectRecordsBuilder<? extends ModuleBaseWithCustomFields> builder = new SelectRecordsBuilder()
+                .beanClass(FacilioConstants.ContextNames.getClassFromModule(lookupModule, false))
+                .select(selectFields)
+                .module(lookupModule);
+
+        builder.andCondition(CriteriaAPI.getCondition(facilioField, data, getOperatorId(facilioField)));
+
+        List<Map<String, Object>> valueProps = builder.getAsProps();
+        for (Map<String, Object> row : valueProps){
+            lookupMap.put(row.get(facilioField.getName()), Long.parseLong(row.get("id").toString()));
+        }
+        return lookupMap;
+    }
+
+    private Operator getOperatorId(FacilioField field) {
+        switch (field.getDataTypeEnum()) {
+            case DECIMAL:
+            case NUMBER:
+                return NumberOperators.EQUALS;
+            case STRING:
+                return StringOperators.IS;
+            case BOOLEAN:
+                return BooleanOperators.IS;
+            case ENUM:
+            case SYSTEM_ENUM:
+                return EnumOperators.IS;
+            case MULTI_ENUM:
+                return MultiFieldOperators.CONTAINS;
+            case DATE:
+            case DATE_TIME:
+                return DateOperators.BETWEEN;
+            case LOOKUP:
+                return PickListOperators.IS;
+        }
+        return null;
+    }
+
+
+    private Map<Long, Object> getLookUpMap(String specialType, FacilioModule lookupModule, List<Long> ids) throws Exception {
+        Map<Long, Object> lookupMap;
+
+        String moduleName;
+        if (LookupSpecialTypeUtil.isSpecialType(specialType)) {
+            moduleName = specialType;
+        } else {
+            moduleName = lookupModule.getName();
+        }
+
+        ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
+        FacilioField mainField = modBean.getPrimaryField(moduleName);
+
+        List<FacilioField> selectFields = new ArrayList<>();
+        selectFields.add(mainField);
+        selectFields.add(FieldFactory.getIdField(lookupModule));
+        SelectRecordsBuilder<? extends ModuleBaseWithCustomFields> builder = new SelectRecordsBuilder()
+                .beanClass(FacilioConstants.ContextNames.getClassFromModule(lookupModule, false))
+                .select(selectFields)
+                .module(lookupModule);
+        if (CollectionUtils.isNotEmpty(ids)) {
+            builder.andCondition(CriteriaAPI.getIdCondition(ids, lookupModule));
+        }
+
+        List<Map<String, Object>> asProps = builder.getAsProps();
+
+        Map<Long, Object> lookupValueMap = null;
+        if (mainField instanceof LookupField) {
+            LookupField lookupField = (LookupField) mainField;
+            lookupValueMap = getLookUpMap(lookupField.getSpecialType(), lookupField.getLookupModule(), null);
+        }
+
+        lookupMap = new HashMap<>();
+        for (Map<String, Object> map : asProps) {
+            if (mainField instanceof LookupField) {
+                long id = (Long) map.get("id");
+                long lookupFieldId = (Long) ((Map<String, Object>) map.get(mainField.getName())).get("id");
+                lookupMap.put(id, (String) lookupValueMap.get(lookupFieldId));
+            } else {
+                lookupMap.put((Long) map.get("id"), (String) map.get(mainField.getName()));
+            }
+        }
+        return lookupMap;
+    }
+}
