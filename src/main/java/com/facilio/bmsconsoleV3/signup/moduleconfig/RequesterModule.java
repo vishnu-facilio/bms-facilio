5899bc77433d4e5714a3533fde08c60726bd33a5
kirubanandhan <kirubanandhan@facilio.com>
2022-05-25 23:07:39 +0530

    Add Modules Files

---
+++ src/main/java/com/facilio/bmsconsoleV3/signup/moduleconfig/RequesterModule.java
@@ -0,0 +1,11 @@
+package com.facilio.bmsconsoleV3.signup.moduleconfig;
+
+public class RequesterModule extends BaseModuleConfig{
+    @Override
+    protected void addForms() throws Exception {
+
+    }
+
+    public void migration() throws Exception {
+    }
+}
---
+++ src/main/java/com/facilio/bmsconsoleV3/signup/moduleconfig/WorkPermitModule.java
@@ -0,0 +1,11 @@
+package com.facilio.bmsconsoleV3.signup.moduleconfig;
+
+public class WorkPermitModule extends BaseModuleConfig{
+    @Override
+    protected void addForms() throws Exception {
+
+    }
+
+    public void migration() throws Exception {
+    }
+}
