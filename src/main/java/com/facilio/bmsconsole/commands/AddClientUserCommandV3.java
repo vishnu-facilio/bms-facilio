11a16fafd971016e7fb84b51b858530480790429
mohammed sahil <mohammedsahil@facilio.com>
2022-06-02 19:16:27 +0530

    Removed insertion of client in contact table
    Removed insertion of client in contact table

--- src/main/java/com/facilio/bmsconsole/commands/AddClientUserCommandV3.java
+++ src/main/java/com/facilio/bmsconsole/commands/AddClientUserCommandV3.java
@@ -46,33 +46,9 @@ public class AddClientUserCommandV3 extends FacilioCommand {
 					{
 						V3ClientContext client = (V3ClientContext)record;
 						ModuleBean modBean = (ModuleBean) BeanFactory.lookup("ModuleBean");
-						FacilioModule module = modBean.getModule(FacilioConstants.ContextNames.CONTACT);
-						List<FacilioField> fields = modBean.getAllFields(module.getName());
-						
 						FacilioModule ccModule = modBean.getModule(FacilioConstants.ContextNames.CLIENT_CONTACT);
 						List<FacilioField> ccFields = modBean.getAllFields(ccModule.getName());
-					
-						EventType eventType = (EventType) context.getOrDefault(FacilioConstants.ContextNames.EVENT_TYPE,
-								EventType.CREATE);
 
-						if (eventType == EventType.CREATE) {
-							V3ContactsContext primarycontact = addDefaultClientPrimaryContact(client);
-							V3RecordAPI.addRecord(true, Collections.singletonList(primarycontact), module, fields);
-						} else {
-							if (StringUtils.isNoneEmpty(client.getPrimaryContactPhone())) {
-								V3ContactsContext existingcontactForPhone = V3ContactsAPI
-										.getContactforPhone(client.getPrimaryContactPhone(), client.getId(), false);
-								if (existingcontactForPhone == null) {
-									existingcontactForPhone = addDefaultClientPrimaryContact(client);
-									V3RecordAPI.addRecord(true, Collections.singletonList(existingcontactForPhone), module,
-											fields);
-								} else {
-									existingcontactForPhone.setName(client.getPrimaryContactName());
-									existingcontactForPhone.setEmail(client.getPrimaryContactEmail());
-									V3RecordAPI.updateRecord(existingcontactForPhone, module, fields);
-								}
-							}
-						}
 						if(AccountUtil.isFeatureEnabled(FeatureLicense.PEOPLE_CONTACTS)) {
 							V3ClientContactContext cc = getDefaultClientContact(client);
 							List<V3ClientContactContext> primarycontatsIfAny = V3PeopleAPI.getClientContacts(cc.getClient().getId(), true);
@@ -82,37 +58,22 @@ public class AddClientUserCommandV3 extends FacilioCommand {
 							}
 							V3PeopleAPI.addParentPrimaryContactAsPeople(cc, ccModule, ccFields, cc.getClient().getId(), clientPrimaryContact);
 						}
-		
 					}
 				}
 			}
 		}
 		return false;
 	}
-	
-	private V3ContactsContext addDefaultClientPrimaryContact(V3ClientContext client) throws Exception {
-		V3ContactsAPI.unMarkPrimaryContact(-1, client.getId(), V3ContactsContext.ContactType.CLIENT);
-		V3ContactsContext contact = new V3ContactsContext();
-		contact.setName(client.getPrimaryContactName() != null ? client.getPrimaryContactName() : client.getName());
-		contact.setContactType(V3ContactsContext.ContactType.CLIENT.getIndex());
-		contact.setClient(client);
-		contact.setEmail(client.getPrimaryContactEmail());
-		contact.setPhone(client.getPrimaryContactPhone());
-		contact.setIsPrimaryContact(true);
-		contact.setIsPortalAccessNeeded(false);
-		return contact;
-	}
-
 	private V3ClientContactContext getDefaultClientContact(V3ClientContext client) throws Exception {
-		V3ClientContactContext tc = new V3ClientContactContext();
-		tc.setName(client.getPrimaryContactName());
-		tc.setEmail(client.getPrimaryContactEmail());
-		tc.setPhone(client.getPrimaryContactPhone());
-		tc.setPeopleType(V3PeopleContext.PeopleType.CLIENT_CONTACT.getIndex());
-		tc.setClient(client);
-		tc.setIsPrimaryContact(true);
+		V3ClientContactContext cc = new V3ClientContactContext();
+		cc.setName(client.getPrimaryContactName());
+		cc.setEmail(client.getPrimaryContactEmail());
+		cc.setPhone(client.getPrimaryContactPhone());
+		cc.setPeopleType(V3PeopleContext.PeopleType.CLIENT_CONTACT.getIndex());
+		cc.setClient(client);
+		cc.setIsPrimaryContact(true);
 
-		return tc;
+		return cc;
 	}
 }
 
