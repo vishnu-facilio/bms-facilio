7c3d97f994ed22528f54ec58971fc067ee5a5f95
Arun Kumar R <arunkumarr@facilio.com>
2022-06-07 19:03:19 +0530

    align CommissioningTabs

--- src/main/java/com/facilio/bmsconsole/commands/GetCommissioningLogsCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/GetCommissioningLogsCommand.java
@@ -1,5 +1,6 @@
 package com.facilio.bmsconsole.commands;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.facilio.command.FacilioCommand;
@@ -16,10 +17,29 @@ public class GetCommissioningLogsCommand extends FacilioCommand {
 	public boolean executeCommand(Context context) throws Exception {
 		
 		JSONObject pagination = (JSONObject) context.get(FacilioConstants.ContextNames.PAGINATION);
+		String status = (String) context.get("status");
+		List<CommissioningLogContext>published = new ArrayList<>();
+		List<CommissioningLogContext>draft = new ArrayList<>();
 		
-		List<CommissioningLogContext> commissioniongList = CommissioningApi.commissioniongList(null, true, pagination);
-		context.put("logs", commissioniongList);
-		
+		List<CommissioningLogContext> commissioningList = CommissioningApi.commissioniongList(null, true, pagination);
+		for (CommissioningLogContext commissioning : commissioningList ){
+			long publishedTime = commissioning.getPublishedTime();
+			if (publishedTime==-1){
+				draft.add(commissioning);
+			}
+			else{
+				published.add(commissioning);
+			}
+		}
+		if (status.equals("draft")){
+			context.put("logs", draft);
+		} else if (status.equals("published")) {
+			context.put("logs", published);
+		}
+		else {
+			context.put("logs", commissioningList);
+		}
+
 		return false;
 	}
 
