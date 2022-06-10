dfbb5d1241fb1c56a0dd59ddff88b8ef70ac28bb
KeerthanaM02 <mkeerthana2974@gmail.com>
2022-06-10 20:45:50 +0530

    Revert "filtered result of approval_process"
    This reverts commit 8b9612d9a68dd90f73e1a276dca3043c3ad1c5b9.

--- src/main/java/com/facilio/bmsconsole/actions/ModuleWorkflowRuleAction.java
+++ src/main/java/com/facilio/bmsconsole/actions/ModuleWorkflowRuleAction.java
@@ -1,5 +1,7 @@
 package com.facilio.bmsconsole.actions;
 
+import java.util.List;
+
 import com.facilio.bmsconsole.commands.ReadOnlyChainFactory;
 import com.facilio.bmsconsole.commands.TransactionChainFactory;
 import com.facilio.bmsconsole.workflow.rule.WorkflowRuleContext;
@@ -9,11 +11,6 @@ import com.facilio.chain.FacilioContext;
 import com.facilio.constants.FacilioConstants;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class ModuleWorkflowRuleAction extends FacilioAction {
 
@@ -52,17 +49,8 @@ public class ModuleWorkflowRuleAction extends FacilioAction {
         constructListContext(context);
         c.execute();
 
-        List<WorkflowRuleContext>workFlowRuleList= (List<WorkflowRuleContext>) context.get(FacilioConstants.ContextNames.WORKFLOW_RULE_LIST);
-        List<Map<String,Object>> approvalWorkFlowRuleList = new ArrayList<>();
-        for(WorkflowRuleContext approvalWorkFlowRule:workFlowRuleList){
-            Map<String,Object>approvalWorkflowRuleMap=new HashMap<>();
-            approvalWorkflowRuleMap.put("name",approvalWorkFlowRule.getName());
-            approvalWorkflowRuleMap.put("description",approvalWorkFlowRule.getDescription());
-            approvalWorkflowRuleMap.put("active",approvalWorkFlowRule.getStatus()!=null ? approvalWorkFlowRule.getStatus():false);
-            approvalWorkflowRuleMap.put("id",approvalWorkFlowRule.getId());
-            approvalWorkFlowRuleList.add(approvalWorkflowRuleMap);
-        }
-        setResult(FacilioConstants.ContextNames.WORKFLOW_RULE_LIST, approvalWorkFlowRuleList);
+        setResult(FacilioConstants.ContextNames.WORKFLOW_RULE_LIST, context.get(FacilioConstants.ContextNames.WORKFLOW_RULE_LIST));
+
         return SUCCESS;
     }
 
