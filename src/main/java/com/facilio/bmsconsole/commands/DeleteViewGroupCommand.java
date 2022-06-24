67635ee5a7b5d6cf6b2b891a232df99c09e0e875
Manoj Kannan D <manojkannan@facilio.com>
2022-06-24 06:28:53 +0000

    Merged in views_admin_edit_access (pull request #1723)
    Views admin edit access
    
    * added edit access to Administrator
    
    * rearranged viewGroup delete conditions and changed datatype to Long
    
    
    Approved-by: Logesh Kannan K
    Approved-by: Mohammed Jamaludeen

--- src/main/java/com/facilio/bmsconsole/commands/DeleteViewGroupCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/DeleteViewGroupCommand.java
@@ -17,18 +17,15 @@ public class DeleteViewGroupCommand extends FacilioCommand {
 		long viewGroupId = (long) context.get(FacilioConstants.ContextNames.VIEW_GROUP_ID);
 		ViewGroups viewGroup = ViewAPI.getGroup(viewGroupId);
 
-		if (viewGroup != null) {
-			
-			GenericDeleteRecordBuilder deleteRecordBuilder = new GenericDeleteRecordBuilder();
-			
-			deleteRecordBuilder.table(ModuleFactory.getViewGroupsModule().getTableName())
-			.andCondition(CriteriaAPI.getIdCondition(viewGroupId, ModuleFactory.getViewGroupsModule()));
-			
-			deleteRecordBuilder.delete();
-
-		} else {
+		if (viewGroup == null) {
 			throw new IllegalArgumentException("ViewGroup not found");
 		}
+
+		GenericDeleteRecordBuilder deleteRecordBuilder = new GenericDeleteRecordBuilder();
+		deleteRecordBuilder.table(ModuleFactory.getViewGroupsModule().getTableName())
+				.andCondition(CriteriaAPI.getIdCondition(viewGroupId, ModuleFactory.getViewGroupsModule()));
+
+		deleteRecordBuilder.delete();
 
 		context.put(FacilioConstants.ContextNames.VIEW_GROUP, viewGroup);
 		return false;
