06161f2b6559ff97b4e9cbe0fdc78487f9b24d2c
Kavin Raju Sridhar <kavinraju@facilio.com>
2022-07-18 09:40:09 +0000

    Merged in feat/jobPlan_taskSection_migration (pull request #1942)
    feat/jobPlan taskSection migration
    
    * feat(jobplan-tasksection): Added InputType, Additional Info properties in JobPlanTaskSectionContext.java
    
    * feat(jobTaskSection-migration): Added INPUT_TYPE, ADDITIONAL_INFO, fields for jobplansection module.
    
    * feat(jobTaskSection-migration): Added INPUT_TYPE, ADDITIONAL_INFO columns in Create table query of JobPlan_Task_Section
    
    * feat(jobTaskSection-migration): fixed typo
    
    
    Approved-by: Aruna Velayutham
    Approved-by: Vignesh Rajkumar
    Approved-by: Vandhana sridhar

--- src/main/java/com/facilio/bmsconsoleV3/context/jobplan/JobPlanTaskSectionContext.java
+++ src/main/java/com/facilio/bmsconsoleV3/context/jobplan/JobPlanTaskSectionContext.java
@@ -2,6 +2,9 @@ package com.facilio.bmsconsoleV3.context.jobplan;
 
 import com.facilio.modules.FacilioIntEnum;
 import com.facilio.v3.context.V3Context;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.util.List;
 import java.util.Map;
@@ -119,5 +122,77 @@ public class JobPlanTaskSectionContext extends V3Context {
         this.tasks = tasks;
     }
 
+    // declarations for InputType
+    private JobPlanTaskSectionContext.InputType inputType;
+    public int getInputType() {
+        if(inputType != null) {
+            return inputType.getVal();
+        }
+        return -1;
+    }
+    public void setInputType(int inputType) {
+        this.inputType = JobPlanTaskSectionContext.InputType.valueOf(inputType);
+    }
+    public void setInputType(JobPlanTaskSectionContext.InputType inputType) {
+        this.inputType = inputType;
+    }
+    public JobPlanTaskSectionContext.InputType getInputTypeEnum() {
+        return inputType;
+    }
 
+    public enum InputType {
+        NONE,
+        READING,
+        TEXT,
+        NUMBER,
+        RADIO,
+        //CHECKBOX,
+        BOOLEAN
+        ;
+
+        public int getVal() {
+            return ordinal()+1;
+        }
+
+        public static JobPlanTaskSectionContext.InputType valueOf(int val) {
+            if(val > 0 && val <= values().length) {
+                return values()[val - 1];
+            }
+            return null;
+        }
+    }
+
+    // declarations for additionInfo
+    private JSONObject additionInfo;
+    public JSONObject getAdditionInfo() {
+        return additionInfo;
+    }
+    public void setAdditionInfo(JSONObject additionInfo) {
+        this.additionInfo = additionInfo;
+    }
+
+    public void addAdditionInfo(String key, Object value) {
+        if(this.additionInfo == null) {
+            this.additionInfo =  new JSONObject();
+        }
+        this.additionInfo.put(key,value);
+    }
+
+    public String getAdditionalInfoJsonStr() {
+        if(additionInfo != null) {
+            return additionInfo.toJSONString();
+        }
+        return null;
+    }
+    public void setAdditionalInfoJsonStr(String jsonStr) throws ParseException {
+        if(jsonStr != null) {
+            JSONParser parser = new JSONParser();
+            additionInfo = (JSONObject) parser.parse(jsonStr);
+        }
+    }
+    public void setOptions(List<String> options) {
+        if(options != null && !options.isEmpty()) {
+            addAdditionInfo("options",options);
+        }
+    }
 }
