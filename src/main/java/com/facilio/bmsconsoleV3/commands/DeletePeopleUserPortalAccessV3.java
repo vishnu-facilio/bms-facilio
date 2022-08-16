37f385494e5cda9e916e2d83383793e46f66a61a
Kamala Kannan R <kamalakannan@facilio.com>
2022-08-16 06:21:54 +0000

    Merged in fix/v3-people-user-delete (pull request #2209)
    Fixed v3 people user deletion command
    
    * Fixed v3 people user deletion command
    
    * People deletion v3 fix
    
    
    Approved-by: Aruna Velayutham
    Approved-by: Vandhana sridhar

--- src/main/java/com/facilio/bmsconsoleV3/commands/DeletePeopleUserPortalAccessV3.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/DeletePeopleUserPortalAccessV3.java
@@ -1,37 +1,22 @@
 package com.facilio.bmsconsoleV3.commands;
 
-import com.facilio.bmsconsoleV3.context.V3PeopleContext;
-import com.facilio.bmsconsoleV3.context.V3TenantContactContext;
-import com.facilio.bmsconsoleV3.context.V3VendorContactContext;
 import com.facilio.bmsconsoleV3.util.V3PeopleAPI;
 import com.facilio.command.FacilioCommand;
 import com.facilio.v3.context.Constants;
 import org.apache.commons.chain.Context;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.util.List;
-import java.util.Map;
 
 public class DeletePeopleUserPortalAccessV3 extends FacilioCommand {
     @Override
     public boolean executeCommand(Context context) throws Exception {
-        String moduleName = Constants.getModuleName(context);
-        Map<String, List> recordMap = (Map<String, List>) context.get(Constants.RECORD_MAP);
-        List<V3PeopleContext> peopleList = recordMap.get(moduleName);
-        for (V3PeopleContext people : peopleList) {
-            List records = recordMap.get(moduleName);
-            long id =0;
-            if (records != null && !records.isEmpty()) {
-                for (int i = 0; i < records.size(); i++) {
-                    if (people.getPeopleType() == V3PeopleContext.PeopleType.TENANT_CONTACT.getIndex()) {
-                        V3TenantContactContext tc = (V3TenantContactContext) records.get(i);
-                        id = tc.getId();
-                    } else if (people.getPeopleType() == V3PeopleContext.PeopleType.VENDOR_CONTACT.getIndex()) {
-                        V3VendorContactContext vc = (V3VendorContactContext) records.get(i);
-                        id = vc.getId();
-                    }
-                }
+
+        List<Long> peopleIds = Constants.getRecordIds(context);
+        if(CollectionUtils.isNotEmpty(peopleIds)) {
+            for (Long id : peopleIds) {
+                V3PeopleAPI.deletePeopleUsers(id);
             }
-            V3PeopleAPI.deletePeopleUsers(id);
         }
         return false;
     }
--- src/main/java/com/facilio/bmsconsoleV3/commands/PeopleUserDeletionV3.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/PeopleUserDeletionV3.java
@@ -11,7 +11,6 @@ public class PeopleUserDeletionV3 extends FacilioCommand {
     @Override
     public boolean executeCommand(Context context) throws Exception {
         List records = (List) context.get("deletedRecords");
-        long id = 0;
         if (records != null && !records.isEmpty()) {
             for(int  i=0; i< records.size(); i++){
                 if(records.get(0) instanceof V3TenantContext) {
@@ -20,7 +19,7 @@ public class PeopleUserDeletionV3 extends FacilioCommand {
                         long tenant_id = tenant.getId();
                         List<V3TenantContactContext> tenantContacts = V3PeopleAPI.getTenantContacts(tenant_id, false, false, true);
                         for (V3TenantContactContext tc : tenantContacts) {
-                            id = tc.getId();
+                            V3PeopleAPI.deletePeopleUsers(tc.getId());
                         }
                     }
                 }
@@ -28,17 +27,16 @@ public class PeopleUserDeletionV3 extends FacilioCommand {
                     V3VendorContext vendor = (V3VendorContext) records.get(i);
                     long vendor_id = vendor.getId();
                     List<V3VendorContactContext> vendorContacts = V3PeopleAPI.getVendorContacts(vendor_id, false, true);
-                        for(V3VendorContactContext vc : vendorContacts){
-                            id = vc.getId();
-                        }
+                    for(V3VendorContactContext vc : vendorContacts){
+                        V3PeopleAPI.deletePeopleUsers(vc.getId());
                     }
+                }
                 else if(records.get(0) instanceof V3EmployeeContext){
                     V3EmployeeContext employee = (V3EmployeeContext) records.get(i);
-                    id = employee.getId();
+                    V3PeopleAPI.deletePeopleUsers(employee.getId());
                 }
             }
         }
-        V3PeopleAPI.deletePeopleUsers(id);
         return false;
     }
 }
