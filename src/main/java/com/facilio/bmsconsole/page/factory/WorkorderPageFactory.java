813980d12d0cfa4c7ff584fe497663cd4ba2731b
Magesh V <magesh@facilio.com>
2022-08-22 21:23:54 +0530

    sla remaining time field changed

--- src/main/java/com/facilio/bmsconsole/page/factory/WorkorderPageFactory.java
+++ src/main/java/com/facilio/bmsconsole/page/factory/WorkorderPageFactory.java
@@ -109,7 +109,7 @@ public class WorkorderPageFactory extends PageFactory {
         int widgetHeight = 3;
         PageWidget slaRemainingDuration = new PageWidget(PageWidget.WidgetType.SLA_REMAINING_TIME);
         slaRemainingDuration.addToLayoutParams(xOffset, yOffset, widgetWidth, widgetHeight);
-        slaRemainingDuration.addToWidgetParams("dateField", "sla_current_time_workorder");
+        slaRemainingDuration.addToWidgetParams("dateField", "responseDueDate");
         section.addWidget(slaRemainingDuration);
         yOffset += widgetHeight;
 
