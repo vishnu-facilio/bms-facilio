f5569281ff84a5175d72c4d17f58ca6ec2b92f38
Aravindh Vasu <aravindhvasu@facilio.com>
2022-08-26 19:01:04 +0530

    minor fix for faults log

--- src/main/java/com/facilio/bmsconsole/commands/HandleV3AlarmListLookupCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/HandleV3AlarmListLookupCommand.java
@@ -3,16 +3,15 @@ package com.facilio.bmsconsole.commands;
 import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.context.BaseAlarmContext;
 import com.facilio.bmsconsole.context.ReadingAlarm;
-import com.facilio.bmsconsole.context.ReadingAlarmContext;
 import com.facilio.bmsconsole.util.NewAlarmAPI;
 import com.facilio.bmsconsole.util.WorkflowRuleAPI;
-import com.facilio.bmsconsole.workflow.rule.ReadingRuleContext;
 import com.facilio.bmsconsole.workflow.rule.ReadingRuleInterface;
 import com.facilio.bmsconsole.workflow.rule.WorkflowRuleContext;
 import com.facilio.command.FacilioCommand;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.fw.BeanFactory;
 import com.facilio.modules.ModuleBaseWithCustomFields;
+import com.facilio.readingrule.context.NewReadingRuleContext;
 import com.facilio.readingrule.util.NewReadingRuleAPI;
 import lombok.extern.log4j.Log4j;
 import org.apache.commons.chain.Context;
@@ -23,7 +22,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Log4j
 public class HandleV3AlarmListLookupCommand extends FacilioCommand {
@@ -68,7 +66,7 @@ public class HandleV3AlarmListLookupCommand extends FacilioCommand {
 //            List<Long> newRuleIds = newReadingAlarms.stream().map(el -> el.getRule().getId()).collect(Collectors.toList());
             List<Long> newRuleIds = new ArrayList<>();
             for(ReadingAlarm newReadingAlarm:newReadingAlarms){
-                ReadingRuleContext newReadingRule = (ReadingRuleContext) newReadingAlarm.getRule();
+                NewReadingRuleContext newReadingRule = (NewReadingRuleContext) newReadingAlarm.getRule();
                 if(newReadingRule==null){
                     LOGGER.info("Id of New Alarm that doesn't have a Rule Id " + newReadingAlarm.getId());
                 } else {
