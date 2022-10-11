233f56c688c16212727923b3c0efc8e2a028f9f4
vishnu-facilio <87803555+vishnu-facilio@users.noreply.github.com>
2022-10-11 13:09:44 +0530

    Revert "removed props files"

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -28,6 +28,7 @@ else
 fi
 chmod 644 $APP_HOME/logs/*
 sh $APP_HOME/bin/startup.sh
+rm -rf $FACILIO_HOME/deployment-files/*
 
 #remove build_update file so that monit will monitor and start the server if it quits
 if [ "$LIFECYCLE_EVENT" = "ApplicationStart" ];
