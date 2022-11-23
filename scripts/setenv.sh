355aaf88d5f66fb49955455c495cca8a2984d534
Logesh kannan K <logeshkannan@facilio.com>
2022-11-23 12:58:25 +0530

    Revert "Update setenv.sh"
    This reverts commit 7fbab99d554cd28d1d73ae72186c1cd6b4653f37.

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -6,22 +6,3 @@ JVM_OOM_OPTS="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/home/facilio/tom
 DATE=`date +%Y-%m-%d`
 JAVA_GC_OPTS="-XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=10M -Xloggc:/home/facilio/tomcat/logs/gc.%t.log -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
 JAVA_OPTS="$FACILIO_OPTS $JMX_OPTS $JMX_EXPORTER_OPTS $JVM_OPTS $JAVA_GC_OPTS $JVM_OOM_OPTS"
-
-if [ "$DEPLOYMENT_GROUP_NAME" = "pre_production" ]; then
-   export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/home/facilio/opentelemetry-javaagent.jar"
-   export OTEL_METRICS_EXPORTER=none
-   export OTEL_EXPORTER_OTLP_ENDPOINT=http://172.31.72.166:4317
-   export OTEL_RESOURCE_ATTRIBUTES=service.name=stage_app
-fi
-if [ "$DEPLOYMENT_GROUP_NAME" = "production_deployment" ]; then
-   export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/home/facilio/opentelemetry-javaagent.jar"
-   export OTEL_METRICS_EXPORTER=none
-   export OTEL_EXPORTER_OTLP_ENDPOINT=http://172.31.72.166:4317
-   export OTEL_RESOURCE_ATTRIBUTES=service.name=production_app
-fi
-if [ "$DEPLOYMENT_GROUP_NAME" = "production-scheduler" ]; then
-   export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/home/facilio/opentelemetry-javaagent.jar"
-   export OTEL_METRICS_EXPORTER=none
-   export OTEL_EXPORTER_OTLP_ENDPOINT=http://172.31.72.166:4317
-   export OTEL_RESOURCE_ATTRIBUTES=service.name=production_scheduler
-fi
