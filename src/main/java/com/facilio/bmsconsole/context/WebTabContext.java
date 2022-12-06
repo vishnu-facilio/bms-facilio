5682c77938799059379d215fc2b420ed3e6055b7
Aravind Pughal <aravindpughal@facilio.com>
2022-12-06 12:08:18 +0530

    removing duplicate enum

--- src/main/java/com/facilio/bmsconsole/context/WebTabContext.java
+++ src/main/java/com/facilio/bmsconsole/context/WebTabContext.java
@@ -234,8 +234,7 @@ public class WebTabContext implements Serializable {
         AUDIT_LOGS("Audit Logs",false,TabType.SETUP),
         BOOKING_POLICY("Booking Policy",false,TabType.SETUP),
         PIVOT("Pivot",false,TabType.NORMAL),
-        SCOPING("Scoping",false,TabType.SETUP),
-        WEATHER_STATION("Weather Station",false,TabType.SETUP);
+        SCOPING("Scoping",false,TabType.SETUP);
 
 
 
