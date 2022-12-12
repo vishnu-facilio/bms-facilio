63ac6bcf9c0ae72a392fe17e8e14a46d1f4e9680
Keerthanasundarraj <108932259+Keerthanasundarraj@users.noreply.github.com>
2022-12-12 14:45:49 +0530

    Adding License for Verify QR in WO

--- src/main/java/com/facilio/bmsconsole/commands/VerifyQrCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/VerifyQrCommand.java
@@ -1,5 +1,6 @@
 package com.facilio.bmsconsole.commands;
 
+import com.facilio.accounts.util.AccountUtil;
 import com.facilio.command.FacilioCommand;
 import org.apache.commons.chain.Context;
 
@@ -12,6 +13,10 @@ public class VerifyQrCommand extends FacilioCommand {
 	@Override
 	public boolean executeCommand(Context context) throws Exception {
 		// TODO Auto-generated method stub
+		if(AccountUtil.isFeatureEnabled(AccountUtil.FeatureLicense.DONT_VERIFY_QR))
+		{
+			return false;
+		}
 		Long resourceId = (Long) context.get(FacilioConstants.ContextNames.RESOURCE_ID);
 		Boolean bool = (Boolean) context.get(FacilioConstants.ContextNames.SHOULD_VERIFY_QR);
 		String qrVal = (String) context.get(FacilioConstants.ContextNames.QR_VALUE);
