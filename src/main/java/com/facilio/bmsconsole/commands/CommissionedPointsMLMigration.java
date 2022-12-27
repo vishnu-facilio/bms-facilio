be83f8ba98c4082cefeb6fb8e29892e2637b9eac
Akash Kannan <akash@facilio.com>
2022-12-27 13:13:29 +0000

    Merged in commissioning_page (pull request #3265)
    no controllerName in Point object fixed
    
    * using resource util method instead of getControllerRequest
    
    
    * no controllerName in Point object fixed
    
    
    
    Approved-by: Aruna Velayutham
    Approved-by: Mohammed Shaan Sameem

--- src/main/java/com/facilio/bmsconsole/commands/CommissionedPointsMLMigration.java
+++ src/main/java/com/facilio/bmsconsole/commands/CommissionedPointsMLMigration.java
@@ -2,11 +2,14 @@ package com.facilio.bmsconsole.commands;
 
 import com.facilio.agentv2.AgentConstants;
 import com.facilio.agentv2.commands.AgentV2Command;
+import com.facilio.agentv2.controller.Controller;
 import com.facilio.agentv2.controller.ControllerApiV2;
+import com.facilio.agentv2.controller.GetControllerRequest;
 import com.facilio.agentv2.point.GetPointRequest;
 import com.facilio.agentv2.point.Point;
 import com.facilio.agentv2.point.PointEnum.ConfigureStatus;
 import com.facilio.agentv2.point.PointsAPI;
+import com.facilio.bmsconsole.util.CommissioningApi;
 import com.facilio.db.criteria.Criteria;
 import com.facilio.db.criteria.CriteriaAPI;
 import com.facilio.db.criteria.operators.NumberOperators;
@@ -18,6 +21,7 @@ import com.facilio.modules.fields.FacilioField;
 import org.apache.commons.chain.Context;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class CommissionedPointsMLMigration extends AgentV2Command {
     @Override
@@ -29,9 +33,16 @@ public class CommissionedPointsMLMigration extends AgentV2Command {
                 .filterConfigurePoints()
                 .limit(-1)
                 ;
+
         List<Point>points = getPointRequest.getPoints();
+
         context.put(AgentConstants.POINT,points);
         List<Map<String, Object>> pointsMapList = FieldUtil.getAsMapList(points,Point.class);
+        Map<Long, String> controllerIdVSNameMap = CommissioningApi.getResources(pointsMapList.stream().map(x -> (Long) x.get("controllerId")).filter(Objects::nonNull).collect(Collectors.toSet()));
+
+        for (Map<String,Object>point : pointsMapList){
+            point.put("controllerName",controllerIdVSNameMap.get(point.get("controllerId")));
+        }
         context.put(AgentConstants.POINTS,pointsMapList);
         return false;
     }
