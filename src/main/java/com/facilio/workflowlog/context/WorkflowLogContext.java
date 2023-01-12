855d9a6fd7af6e4c44f25105568e00d7eb3abc29
Amal <amalhadeez@facilio.com>
2023-01-12 11:37:16 +0530

    perf : Scriplog Logtypes are updated

--- src/main/java/com/facilio/workflowlog/context/WorkflowLogContext.java
+++ src/main/java/com/facilio/workflowlog/context/WorkflowLogContext.java
@@ -1,29 +1,16 @@
 package com.facilio.workflowlog.context;
 
+import com.facilio.bmsconsole.workflow.rule.WorkflowRuleContext.RuleType;
+import com.facilio.modules.FacilioIntEnum;
+import com.facilio.v3.context.V3Context;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.facilio.accounts.dto.Group;
-import com.facilio.accounts.dto.User;
-import com.facilio.bmsconsole.context.ResourceContext;
-import com.facilio.bmsconsole.context.VendorContext;
-import com.facilio.bmsconsole.tenant.TenantContext;
-import com.facilio.bmsconsole.workflow.rule.WorkflowRuleContext.RuleType;
-import com.facilio.bmsconsoleV3.context.inspection.InspectionCategoryContext;
-import com.facilio.bmsconsoleV3.context.inspection.InspectionPriorityContext;
-import com.facilio.bmsconsoleV3.context.inspection.InspectionResponseContext;
-import com.facilio.bmsconsoleV3.context.inspection.InspectionTemplateContext;
-import com.facilio.bmsconsoleV3.context.inspection.InspectionResponseContext.SourceType;
-import com.facilio.constants.FacilioConstants;
-import com.facilio.modules.FacilioIntEnum;
-import com.facilio.v3.context.V3Context;
-import com.facilio.workflowlog.context.WorkflowLogContext.WorkflowLogStatus;
-import com.facilio.workflowlog.context.WorkflowLogContext.WorkflowLogType;
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.extern.log4j.Log4j;
 @Getter
 @Setter
 @Log4j
@@ -76,7 +63,10 @@ public class WorkflowLogContext extends V3Context {
 		MODULE_RULE(3,RuleType.MODULE_RULE),
 		STATE_RULE(4,RuleType.STATE_RULE),
 		WORKFLOW_RULE_EVALUATION(5),
-		Q_AND_A_RULE(6)
+		Q_AND_A_RULE(6),
+		CUSTOM_BUTTON_RULE(7, RuleType.CUSTOM_BUTTON),
+		APPROVAL_STATE_FLOW(8,RuleType.APPROVAL_STATE_FLOW),
+		APPROVAL_STATE_TRANSITION(9,RuleType.APPROVAL_STATE_TRANSITION)
 		;
 		
 		WorkflowLogType(int i) {
