11711f69d235a006839e69d532bf2e8928c6e26b
kavinrajus <kavinraju@facilio.com>
2023-02-01 10:47:41 +0530

    feat: Enabled WO_STATE_TRANSITION_V3 license by default on signup

--- src/main/java/com/facilio/bmsconsole/commands/AddDefaultLicenseCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/AddDefaultLicenseCommand.java
@@ -40,7 +40,8 @@ public class AddDefaultLicenseCommand extends FacilioCommand {
             AccountUtil.FeatureLicense.NEW_SITE_SUMMARY,
             AccountUtil.FeatureLicense.WEATHER_INTEGRATION,
             AccountUtil.FeatureLicense.MAINTENANCE_APP_SIGNUP,
-            AccountUtil.FeatureLicense.NEW_SETUP
+            AccountUtil.FeatureLicense.NEW_SETUP,
+            AccountUtil.FeatureLicense.WO_STATE_TRANSITION_V3
             // Add new default licenses here
     )));
 
