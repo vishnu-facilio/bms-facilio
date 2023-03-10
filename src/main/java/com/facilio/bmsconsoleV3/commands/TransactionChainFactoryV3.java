fa0de1d46df142c2e08808da77e25c9d2746e05e
Keerthanasundarraj <108932259+Keerthanasundarraj@users.noreply.github.com>
2023-03-10 13:08:11 +0530

    Adding routes entry for wo genartion via PM

--- src/main/java/com/facilio/bmsconsoleV3/commands/TransactionChainFactoryV3.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/TransactionChainFactoryV3.java
@@ -631,6 +631,7 @@ public class TransactionChainFactoryV3 {
         c.addCommand(new GetRecordIdsFromRecordMapCommandV3());
         c.addCommand(new AddPrerequisiteApproversCommandV3());
         c.addCommand(new UpdateJobStatusForWorkOrderCommand());
+        c.addCommand(new AddOrUpdateMultiResourceForWorkorderCommandV3());
         c.addCommand(new AddActivityForWoPostCreateCommand());
         c.addCommand(new AddActivitiesCommand(FacilioConstants.ContextNames.WORKORDER_ACTIVITY));
         return c;
