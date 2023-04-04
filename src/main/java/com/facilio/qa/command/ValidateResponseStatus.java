d9717c83ee629c1c97ae0592623bd8b6f8298c89
Abishek <abishek@facilio.com>
2023-04-04 11:38:25 +0530

    validation added properly

--- src/main/java/com/facilio/qa/command/ValidateResponseStatus.java
+++ src/main/java/com/facilio/qa/command/ValidateResponseStatus.java
@@ -6,6 +6,7 @@ import com.facilio.constants.FacilioConstants;
 import com.facilio.qa.QAndAUtil;
 import com.facilio.qa.context.QAndATemplateContext;
 import com.facilio.qa.context.ResponseContext;
+import com.facilio.qa.displaylogic.util.DisplayLogicUtil;
 import com.facilio.v3.exception.ErrorCode;
 import com.facilio.v3.util.ExtendedModuleUtil;
 import com.facilio.v3.util.V3Util;
@@ -19,6 +20,8 @@ import java.util.Collections;
 public class ValidateResponseStatus extends FacilioCommand {
     @Override
     public boolean executeCommand(Context context) throws Exception {
+
+        Boolean isDisplyLogicExecutionOnPageLoad = (Boolean) context.getOrDefault(DisplayLogicUtil.IS_DISPLAY_LOGIC_EXECUTION_ON_PAGE_LOAD, false);
         JSONObject answerData = (JSONObject) context.get(FacilioConstants.QAndA.Command.ANSWER_DATA);
         V3Util.throwRestException(MapUtils.isEmpty(answerData), ErrorCode.VALIDATION_ERROR, "Data cannot be null for add or updating answers");
         Long responseId = (Long) answerData.get("response");
@@ -33,7 +36,7 @@ public class ValidateResponseStatus extends FacilioCommand {
 //		QAndAUtil.validateResponseExpiry(response);
 
         QAndATemplateContext parent = V3RecordAPI.getRecord(response.getQAndAType().getTemplateModule(), response.getParent().getId(),null,true);
-        V3Util.throwRestException(parent == null, ErrorCode.VALIDATION_ERROR, "Cannot add/ update answer when the template is deleted");
+        V3Util.throwRestException(parent == null && !isDisplyLogicExecutionOnPageLoad, ErrorCode.VALIDATION_ERROR, "Cannot add/ update answer when the template is deleted");
         context.put(FacilioConstants.ContextNames.RECORD_LIST, Collections.singletonList(response));
         context.put(FacilioConstants.QAndA.RESPONSE, response);
         return false;
