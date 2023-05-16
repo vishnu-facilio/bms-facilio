4eb217227c8f86217821343bce7bcb259f86af90
Seeni Mohamed <seenimohamed@facilio.com>
2023-05-16 19:51:22 +0530

    pom updated to use transaction timeout handling in wms
    .

--- src/main/java/com/facilio/wmsv2/handler/PingHandler.java
+++ src/main/java/com/facilio/wmsv2/handler/PingHandler.java
@@ -1,13 +1,17 @@
 package com.facilio.wmsv2.handler;
 
-import com.facilio.wmsv2.endpoint.WmsBroadcaster;
+import com.facilio.db.util.DBConf;
 import com.facilio.wmsv2.message.Message;
 
 public class PingHandler extends BaseHandler {
 
     @Override
     public void processIncomingMessage(Message message) {
-        WmsBroadcaster.getBroadcaster().sendMessage(message);
+        message = Processor.getInstance().filterOutgoingMessage(message);
+        BaseHandler handler = Processor.getInstance().getHandler(message.getTopic());
+        handler.processOutgoingMessage(message);
+        if(handler.isLiveMessage()) {
+            DBConf.getInstance().pushToLiveSession(message);
+        }
     }
-
 }
