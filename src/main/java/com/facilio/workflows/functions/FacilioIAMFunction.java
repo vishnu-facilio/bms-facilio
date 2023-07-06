b38eb894ddf20fdbc08f67bcdce154116cd1cf61
santhosh <santhosh@facilio.com>
2023-07-06 14:32:23 +0530

    Workflow Module Rel Tracking Table Entry

--- src/main/java/com/facilio/workflows/functions/FacilioIAMFunction.java
+++ src/main/java/com/facilio/workflows/functions/FacilioIAMFunction.java
@@ -2,20 +2,21 @@ package com.facilio.workflows.functions;
 
 import com.facilio.bmsconsole.util.PeopleAPI;
 import com.facilio.scriptengine.annotation.ScriptNameSpace;
+import com.facilio.scriptengine.context.ScriptContext;
 import com.facilio.scriptengine.exceptions.FunctionParamException;
 import com.facilio.scriptengine.systemfunctions.FacilioNameSpaceConstants;
 
 import java.util.Map;
 @ScriptNameSpace(nameSpace = FacilioNameSpaceConstants.IAM_FUNCTION)
 public class FacilioIAMFunction {
-    public Object allowAppAccess(Map<String, Object> map, Object... objects) throws Exception {
+    public Object allowAppAccess(ScriptContext scriptContext, Map<String, Object> map, Object... objects) throws Exception {
         if (objects.length < 4) {
             throw new FunctionParamException("Required arguments is missing. Expected arguments => (peopleId, appId, roleId, sendInvitation)");
         }
         PeopleAPI.addApplicationUsersByPeopleId((Long) objects[0], (Long) objects[1], (Long) objects[2], (boolean) objects[3]);
         return true;
     }
-    public Object changeAppRole(Map<String, Object> map, Object... objects) throws Exception {
+    public Object changeAppRole(ScriptContext scriptContext , Map<String, Object> map, Object... objects) throws Exception {
         if (objects.length < 3) {
             throw new FunctionParamException("Required arguments is missing. Expected arguments => (peopleId, appId, roleId)");
         }
@@ -23,7 +24,7 @@ public class FacilioIAMFunction {
         return true;
     }
 
-    public Object revokeAppAccess(Map<String, Object> map, Object... objects) throws Exception {
+    public Object revokeAppAccess(ScriptContext scriptContext, Map<String, Object> map, Object... objects) throws Exception {
         if (objects.length < 2) {
             throw new FunctionParamException("Required arguments is missing. Expected arguments => (peopleId, appId)");
         }
