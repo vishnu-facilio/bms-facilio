eefe3d36b8650d95685d4eacc9ed29d794ad147f
Tharun S N <tharun@facilio.com>
2023-07-10 17:43:23 +0530

    fix(rfq): compute and set tax amount before summary

--- src/main/java/com/facilio/bmsconsoleV3/commands/requestForQuotation/SetRequestForQuotationLineItemsCommandV3.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/requestForQuotation/SetRequestForQuotationLineItemsCommandV3.java
@@ -1,8 +1,12 @@
 package com.facilio.bmsconsoleV3.commands.requestForQuotation;
 
 import com.facilio.beans.ModuleBean;
+import com.facilio.bmsconsoleV3.context.quotation.TaxContext;
 import com.facilio.bmsconsoleV3.context.requestforquotation.V3RequestForQuotationContext;
 import com.facilio.bmsconsoleV3.context.requestforquotation.V3RequestForQuotationLineItemsContext;
+import com.facilio.bmsconsoleV3.context.vendorquotes.V3VendorQuotesLineItemsContext;
+import com.facilio.bmsconsoleV3.util.QuotationAPI;
+import com.facilio.bmsconsoleV3.util.V3InventoryUtil;
 import com.facilio.command.FacilioCommand;
 import com.facilio.constants.FacilioConstants;
 import com.facilio.db.criteria.CriteriaAPI;
@@ -15,10 +19,14 @@ import com.facilio.modules.fields.LookupField;
 import com.facilio.v3.context.Constants;
 import com.facilio.v3.util.CommandUtil;
 import org.apache.commons.chain.Context;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SetRequestForQuotationLineItemsCommandV3  extends FacilioCommand {
     @Override
@@ -38,8 +46,31 @@ public class SetRequestForQuotationLineItemsCommandV3  extends FacilioCommand {
                 .andCondition(CriteriaAPI.getCondition("RFQ_ID", "requestForQuotation", String.valueOf(id), NumberOperators.EQUALS))
                 .fetchSupplements(Arrays.asList((LookupField) fieldsAsMap.get("itemType"), (LookupField) fieldsAsMap.get("toolType"), (LookupField) fieldsAsMap.get("service"), (LookupField) fieldsAsMap.get("awardedTo")));
         List<V3RequestForQuotationLineItemsContext> list = builder.get();
+        setTaxAmount(list);
         requestForQuotation.setRequestForQuotationLineItems(list);
 
         return false;
+    }
+
+    private static void setTaxAmount(List<V3RequestForQuotationLineItemsContext> lineItems) throws Exception {
+        List<Long> uniqueTaxIds = lineItems.stream().filter(lineItem -> V3InventoryUtil.lookupValueIsNotEmpty(lineItem.getTax())).map(lineItem -> lineItem.getTax().getId()).distinct().collect(Collectors.toList());
+        List<TaxContext> taxList = QuotationAPI.getTaxesForIdList(uniqueTaxIds);
+        Map<Long, Double> taxIdVsRateMap = new HashMap<>();
+        if (CollectionUtils.isNotEmpty(taxList)) {
+            taxList.forEach(tax -> taxIdVsRateMap.put(tax.getId(), tax.getRate()));
+        }
+        if(CollectionUtils.isNotEmpty(lineItems)){
+            for (V3RequestForQuotationLineItemsContext lineItem: lineItems) {
+                if(!ObjectUtils.allNotNull(lineItem,lineItem.getTax(),lineItem.getQuantity(),lineItem.getAwardedPrice(),taxIdVsRateMap)){
+                    continue;
+                }
+                Double taxRate = taxIdVsRateMap.get(lineItem.getTax().getId());
+                if(taxRate == null) {
+                    continue;
+                }
+                Double taxAmount = taxRate * (lineItem.getQuantity() * lineItem.getAwardedPrice())/100;
+                lineItem.setTaxAmount(taxAmount);
+            }
+        }
     }
 }
