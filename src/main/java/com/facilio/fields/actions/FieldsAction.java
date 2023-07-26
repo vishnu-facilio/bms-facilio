d721454f0b9b0cf7d0ed6089d06e54b74a26478e
Rahul <ragul@facilio.com>
2023-07-26 12:10:47 +0530

    lookUpFields advanced search handled with permission

--- src/main/java/com/facilio/fields/actions/FieldsAction.java
+++ src/main/java/com/facilio/fields/actions/FieldsAction.java
@@ -28,6 +28,16 @@ public class FieldsAction extends FacilioAction {
 
     private boolean isOneLevelFields;
 
+    public boolean isOneLevelFields() {
+        return isOneLevelFields;
+    }
+
+    public void setOneLevelFields(boolean oneLevelFields) {
+        isOneLevelFields = oneLevelFields;
+    }
+
+    private boolean isOneLevelFields;
+
 
     public String fetchFilterFields() throws Exception {
         FacilioChain chain = ReadOnlyChainFactory.getAdvancedFilterFieldsChain();
