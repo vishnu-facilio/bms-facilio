ec83a1dade5d07a7aaaddb89a4780454f6c9f8a2
vishnu-facilio <vishnu.r@facilio.com>
2023-08-04 17:44:53 +0530

    Update disk_alert.sh

--- scripts/disk_alert.sh
+++ scripts/disk_alert.sh
@@ -10,7 +10,7 @@ available=$(df -Ph | grep '/dev/nvme0n1p1' | awk '{ print $4}')
 standardvalue=1500000
 used=$(df -Ph | grep '/dev/nvme0n1p1' | awk '{ print $3}')
 cal=`expr "$currentvalue" - "$lastusage"`
-region_name=`grep "region=" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
+region_name=`grep "\bregion=" $CONF_DIR/awsprops.properties | cut -d'=' -f 2`
 
 echo "Last 3hours logs size = $cal kb"
 
@@ -24,6 +24,15 @@ fi
 
 if [ "$dusage" = "1" ]; then
     echo "$dusage" | aws ses send-email --region us-west-2 --from alerts@facilio.com --to devops@facilio.com --subject "Disk space Alert on $ipAddress" --text "Disk Space Alert  - $usedper of disk space has been used IP = $ipAddress AvailableSpace =$available Used = $used ($usedper)"
+    curl -X POST -H "Content-Type: application/json" -d '{
+      "recipient":{
+        "thread_key":"6859821310705272"
+      },
+      "messaging_type": "RESPONSE",
+      "message":{
+        "text":"Disk Space Alert - '$usedper' of disk space has been used \nIP = '$ipAddress' \nRegion = '$region_name' \nAvailableSpace ='$available' \nUsed = '$used' ('$usedper')"
+      }
+    }' "https://graph.workplace.com/me/messages?access_token=DQVJ1OUE5TVlDZAlMxYlhnTy1qREhHd0p1S0tVTkR1MDMzMFdOd1JudklmbFJMd0dMbW5ObmFGdXZAkRFNHS2RCS2o0V2ZAva2kyOGFzYXotZAWNUZAEtBSFhoSk9ENjVhNGt0OTB1R0RqbTVWTmJiWnZAjYlZADa1VGR1hocWE5eDVuRzBDOEFfdWFzVFphbWtQUWlWSEhrT1BXeWJnZAlRVdF9wSmJpTjU1X1g5LU1Tc2xRZA24tbHE1OGw0SDlfQi1ESW11ZA2p3Q3VB"
     sh move_logs.sh
 else
     echo "Disk usage is in under threshold"
