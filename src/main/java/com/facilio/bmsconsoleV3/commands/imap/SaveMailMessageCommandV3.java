4fe6f7257c3cee5364e1a501820b3c32b54eadb8
subashini <subashini@facilio.com>
2023-08-22 16:51:20 +0530

    SR tomail data too long fix

--- src/main/java/com/facilio/bmsconsoleV3/commands/imap/SaveMailMessageCommandV3.java
+++ src/main/java/com/facilio/bmsconsoleV3/commands/imap/SaveMailMessageCommandV3.java
@@ -1,6 +1,7 @@
 package com.facilio.bmsconsoleV3.commands.imap;
 
 import com.facilio.bmsconsole.util.MailMessageUtil;
+import com.facilio.bmsconsoleV3.context.BaseMailMessageContext;
 import com.facilio.command.FacilioCommand;
 import com.facilio.chain.FacilioChain;
 import com.facilio.chain.FacilioContext;
@@ -22,20 +23,24 @@ import org.apache.log4j.Logger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 @Log4j
 public class SaveMailMessageCommandV3 extends FacilioCommand {
     @Override
     public boolean executeCommand(Context context) throws Exception {
         List<ModuleBaseWithCustomFields> messages = (List<ModuleBaseWithCustomFields>) context.get(FacilioConstants.ContextNames.MESSAGES);
-        if(CollectionUtils.isNotEmpty(messages)) {
+        if (CollectionUtils.isNotEmpty(messages)) {
             Map<String, List<ModuleBaseWithCustomFields>> recordMap = new HashMap<>();
             FacilioChain createRecordChain = ChainUtil.getCreateChain(FacilioConstants.ContextNames.BASE_MAIL_MESSAGE);
             FacilioContext createContext = createRecordChain.getContext();
             recordMap.put(FacilioConstants.ContextNames.BASE_MAIL_MESSAGE, messages);
+
+            trimEmailAddressesToMaxLength(messages);
+
             Constants.setRecordMap(createContext, recordMap);
             createContext.put(FacilioConstants.ContextNames.SUPPORT_EMAIL, context.get(FacilioConstants.ContextNames.SUPPORT_EMAIL));
             createContext.put(FacilioConstants.ContextNames.EVENT_TYPE, com.facilio.bmsconsole.workflow.rule.EventType.CREATE);
-            createContext.put(FacilioConstants.ContextNames.REQUEST_EMAIL_ID,context.get(FacilioConstants.ContextNames.REQUEST_EMAIL_ID));
+            createContext.put(FacilioConstants.ContextNames.REQUEST_EMAIL_ID, context.get(FacilioConstants.ContextNames.REQUEST_EMAIL_ID));
             Constants.setModuleName(createContext, FacilioConstants.ContextNames.BASE_MAIL_MESSAGE);
             FacilioModule module = ChainUtil.getModule(FacilioConstants.ContextNames.BASE_MAIL_MESSAGE);
             Class beanClass = FacilioConstants.ContextNames.getClassFromModule(module);
@@ -49,4 +54,46 @@ public class SaveMailMessageCommandV3 extends FacilioCommand {
         }
         return false;
     }
+
+    private void trimEmailAddressesToMaxLength(List<ModuleBaseWithCustomFields> messages) {
+        try {
+            for (int i = 0; i < messages.size(); i++) {
+                BaseMailMessageContext message = (BaseMailMessageContext) messages.get(i);
+                int maxLength = 999;
+                if (message != null) {
+                    String originalToAddress = message.getTo();
+                    if (originalToAddress != null && !originalToAddress.isEmpty()) {
+                        String trimmedToAddress = trimAddressToMaxLength(originalToAddress, maxLength);
+                        ((BaseMailMessageContext) messages.get(i)).setTo(trimmedToAddress);
+                    }
+                    String originalBccAddress = message.getBcc();
+                    if (originalBccAddress != null && !originalBccAddress.isEmpty()) {
+                        String trimmedBccAddress = trimAddressToMaxLength(originalBccAddress, maxLength);
+                        ((BaseMailMessageContext) messages.get(i)).setBcc(trimmedBccAddress);
+                    }
+                    String originalCcAddress = message.getCc();
+                    if (originalCcAddress != null && !originalCcAddress.isEmpty()) {
+                        String trimmedCcAddress = trimAddressToMaxLength(originalCcAddress, maxLength);
+                        ((BaseMailMessageContext) messages.get(i)).setCc(trimmedCcAddress);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.info("Exception Occured :: " + e);
+        }
+    }
+
+    public String trimAddressToMaxLength(String address, int maxLength) {
+        LOGGER.info("Original Email Address :: " + address);
+        if (address.length() >= maxLength) {
+            address = address.substring(0, maxLength);
+            int lastIndex = address.lastIndexOf(",");
+            if (lastIndex != -1) {
+                address = address.substring(0, lastIndex);
+                LOGGER.info("Trimmed Email Address :: " + address);
+            }
+        }
+        return address;
+    }
+
 }
