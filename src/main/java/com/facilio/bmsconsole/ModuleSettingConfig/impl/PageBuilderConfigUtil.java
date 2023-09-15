9674b9849e71d3b973e355c3f69a6d72ca316dcc
Jaisakthi K <jaisakthik@Jaisakthis-MacBook-Pro.local>
2023-09-15 16:49:09 +0530

    meters energy

--- src/main/java/com/facilio/bmsconsole/ModuleSettingConfig/impl/PageBuilderConfigUtil.java
+++ src/main/java/com/facilio/bmsconsole/ModuleSettingConfig/impl/PageBuilderConfigUtil.java
@@ -31,6 +31,7 @@ public class PageBuilderConfigUtil {
             moduleList.add(modBean.getModule(FacilioConstants.Meter.VIRTUAL_METER_TEMPLATE));
             moduleList.add(modBean.getModule(FacilioConstants.Calendar.CALENDAR_MODULE_NAME));
             moduleList.add(modBean.getModule(FacilioConstants.Control_Action.CONTROL_ACTION_MODULE_NAME));
+            moduleList.add(modBean.getModule(FacilioConstants.Control_Action.CONTROL_ACTION_TEMPLATE_MODULE_NAME));
 
             if (CollectionUtils.isNotEmpty(moduleList)) {
                 List<Long> moduleIds = moduleList.stream().filter(Objects::nonNull).map(FacilioModule::getModuleId).collect(Collectors.toList());
