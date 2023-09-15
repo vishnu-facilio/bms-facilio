cf3c6a7cdaacde943b593be747d2e11d0233968e
barath <barathraj@facilio.com>
2023-09-15 11:53:00 +0530

    emailTemplate
    emailTemplate

--- src/main/java/com/facilio/workflows/functions/FacilioNotificationFunctions.java
+++ src/main/java/com/facilio/workflows/functions/FacilioNotificationFunctions.java
@@ -3,11 +3,17 @@ package com.facilio.workflows.functions;
 
 import com.facilio.aws.util.AwsUtil;
 import com.facilio.beans.ModuleBean;
+import com.facilio.bmsconsole.templates.Template;
+import com.facilio.bmsconsole.templates.TemplateAttachment;
 import com.facilio.bmsconsole.util.NotificationAPI;
+import com.facilio.bmsconsole.util.TemplateAPI;
+import com.facilio.bmsconsole.util.WorkflowRuleAPI;
+import com.facilio.bmsconsole.workflow.rule.ActionContext;
 import com.facilio.bmsconsole.workflow.rule.ActionType;
 import com.facilio.bmsconsoleV3.context.EmailFromAddress;
 import com.facilio.chain.FacilioContext;
 import com.facilio.constants.FacilioConstants;
+import com.facilio.emailtemplate.context.EMailStructure;
 import com.facilio.fw.BeanFactory;
 import com.facilio.mailtracking.MailConstants;
 import com.facilio.mailtracking.context.MailSourceType;
@@ -22,17 +28,19 @@ import com.facilio.services.factory.FacilioFactory;
 import com.facilio.tasker.FacilioTimer;
 import com.facilio.v3.context.Constants;
 import com.facilio.v3.util.V3Util;
+import com.facilio.workflows.context.WorkflowContext;
 import com.facilio.workflowv2.util.WorkflowV2Util;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.chain.Context;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 import com.facilio.scriptengine.context.ScriptContext;
 
 @ScriptNameSpace(nameSpace = FacilioNameSpaceConstants.NOTIFICATION_FUNCTION)
@@ -65,6 +73,73 @@ public class FacilioNotificationFunctions {
 		}
 		scriptContext.incrementTotalEmailCount();
 		return null;
+	}
+	public Object sendEmail(ScriptContext scriptContext, Map<String, Object> globalParam, Object... objects) throws Exception{
+
+		checkParam(objects);
+		if(MapUtils.isEmpty((Map<String, Object>) objects[0])|| objects.length<=1 || MapUtils.isEmpty((Map<String, Object>) objects[1])){
+			StringBuilder logStr=new StringBuilder();
+			logStr.append(scriptContext.getLogString()+" paramsMap or recordMap cannot be null while sending Email from script ");
+			scriptContext.setLogString(logStr);
+			return null;
+		}
+
+		Map<String,Object> paramsMap= (Map<String, Object>) objects[0];
+		Map<String,Object> record= (Map<String, Object>) objects[1];
+
+		Long emailTemplateId= (Long) paramsMap.get("templateId");
+
+        String fromAddress=null;
+		if(paramsMap.containsKey("from") && paramsMap.get("from") != null) {
+			fromAddress = EmailFactory.getEmailClient().getNotificationFromAddressEmailFromName((String)paramsMap.get("from"));
+		}
+		if(fromAddress == null) {
+			fromAddress = EmailFactory.getEmailClient().getSystemFromAddress(EmailFromAddress.SourceType.NOTIFICATION);
+		}
+
+
+		String recordModuleName=Constants.getModBean().getModule((Long)record.get("moduleId")).getName();
+		Template template= TemplateAPI.getTemplate(emailTemplateId);
+
+		if(template!=null && template instanceof EMailStructure) {
+
+			EMailStructure emailStructure = (EMailStructure) template;
+			Map<String, Object> placeHolders = WorkflowRuleAPI.getOrgPlaceHolders();
+			Map<String, Object> recordPlaceHolders = WorkflowRuleAPI.getRecordPlaceHolders(recordModuleName, record, placeHolders);
+			JSONObject actionObj = template.getTemplate(recordPlaceHolders);
+			paramsMap.put("sender", fromAddress);
+			paramsMap.put("subject", actionObj.get("subject"));
+			paramsMap.put("message", actionObj.get("message"));
+			paramsMap.put("html", emailStructure.isHtml());
+			paramsMap.put(MailConstants.Params.SOURCE_TYPE, MailSourceType.SCRIPT.name());
+
+			paramsMap.put("isAttachmentAdded", emailStructure.getIsAttachmentAdded());
+			if (emailStructure.isHtml()) {
+				paramsMap.put("mailType", "html");
+			}
+
+			Map<String, String> attachmentMap = new HashMap<String, String>();
+			if (emailStructure.getIsAttachmentAdded()) {
+				if (CollectionUtils.isNotEmpty(template.getAttachments())) {
+					for (TemplateAttachment attachment : template.getAttachments()) {
+						String url = attachment.fetchFileUrl(record);
+						if (url != null) {
+							attachmentMap.put(attachment.getFileName(), url);
+						}
+					}
+				}
+			}
+			if(MapUtils.isNotEmpty(attachmentMap)) {
+				FacilioFactory.getEmailClient().sendEmailWithActiveUserCheck(WorkflowV2Util.getAsJSONObject(paramsMap), attachmentMap);
+			}else {
+				FacilioFactory.getEmailClient().sendEmailWithActiveUserCheck(WorkflowV2Util.getAsJSONObject(paramsMap));
+			}
+		}else{
+			StringBuilder logStr=new StringBuilder();
+			logStr.append(scriptContext.getLogString()+"  template not available while sending mail from script for TemplateId -- "+emailTemplateId);
+			scriptContext.setLogString(logStr);
+		}
+	return null;
 	}
 
 	public Object sendSms(ScriptContext scriptContext, Map<String, Object> globalParam, Object... objects) throws Exception {
