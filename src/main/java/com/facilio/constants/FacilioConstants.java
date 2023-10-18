797eb56bac345da6c7a5c9a2c120611265348bbc
Kirubanandhan <kirubanandhan@facilio.com>
2023-10-18 11:28:29 +0530

    change email regex

--- src/main/java/com/facilio/constants/FacilioConstants.java
+++ src/main/java/com/facilio/constants/FacilioConstants.java
@@ -362,7 +362,7 @@ public class FacilioConstants {
 		public static final String FORM_SECTION_SEQUENCE_NUMBER = "formSectionSequenceNumber";
 		public static final String NEXT_FORM_SECTION_ID = "nextFormSectionId";
 		public static final String SEQUENCE_NUMBER = "sequenceNumber";
-		public static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+		public static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_+.-]+(\\.[A-Za-z0-9_+.-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 	}
 
 
