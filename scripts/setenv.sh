24f6e13b494af289b2f07b311c27572b865ddc38
vishnu-facilio <vishnu.r@facilio.com>
2023-10-27 15:10:29 +0530

    added tyke.ai java opts to setenv

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -7,15 +7,9 @@ DATE=`date +%Y-%m-%d`
 JAVA_GC_OPTS="-XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=10M -Xloggc:/home/facilio/tomcat/logs/gc.%t.log -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
 JAVA_OPTS="$FACILIO_OPTS $JMX_OPTS $JMX_EXPORTER_OPTS $JVM_OPTS $JAVA_GC_OPTS $JVM_OOM_OPTS"
 
-if [ "$DEPLOYMENT_GROUP_NAME" = "pre_production" ]; then
-   export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/home/facilio/opentelemetry-javaagent.jar"
-   export OTEL_METRICS_EXPORTER=none
-   export OTEL_EXPORTER_OTLP_ENDPOINT=http://172.31.72.166:4317
-   export OTEL_RESOURCE_ATTRIBUTES=service.name=stage_app
+if [ "$DEPLOYMENT_GROUP_NAME" = "preprod" ]; then
+   export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/home/facilio/tykeagent.jar -Dtyke.endpoint=http://172.31.72.56:4317  -Dtyke.attributes=app.name=pre-production,service.name=preprod-user,service.identifier=preprod-user"
 fi
-if [ "$DEPLOYMENT_GROUP_NAME" = "production_deployment" ]; then
-   export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/home/facilio/opentelemetry-javaagent.jar"
-   export OTEL_METRICS_EXPORTER=none
-   export OTEL_EXPORTER_OTLP_ENDPOINT=http://172.31.72.166:4317
-   export OTEL_RESOURCE_ATTRIBUTES=service.name=production_app
+if [ "$DEPLOYMENT_GROUP_NAME" = "preprod-scheduler" ]; then
+   export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/home/facilio/tykeagent.jar -Dtyke.endpoint=http://172.31.72.56:4317  -Dtyke.attributes=app.name=pre-production,service.name=preprod-scheduler,service.identifier=preprod-scheduler"
 fi
