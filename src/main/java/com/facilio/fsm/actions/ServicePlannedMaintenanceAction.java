c80c68ace893471b2b046607f4ba77af2cdaa45d
kavinrajus <kavinraju@facilio.com>
2023-11-08 10:40:08 +0530

    feat(trigger_API_nextExecutionTimes): API for fetching nextExecutionTimes based on Trigger schedule info. This will be modified by Yuvasree later, I am adding it as it's required for PM creation flow.

---
+++ src/main/java/com/facilio/fsm/actions/ServicePlannedMaintenanceAction.java
@@ -0,0 +1,49 @@
+package com.facilio.fsm.actions;
+
+import com.facilio.constants.FacilioConstants;
+import com.facilio.modules.FieldUtil;
+import com.facilio.taskengine.ScheduleInfo;
+import com.facilio.v3.V3Action;
+import com.facilio.v3.util.V3Util;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.tuple.Pair;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter @Setter
+public class ServicePlannedMaintenanceAction  extends V3Action {
+    private static final long serialVersionUID = 1L;
+
+    private Long startTime;
+    private String scheduleInfo;
+    private Long recordId;
+
+    public String getNextExecutionDates() throws Exception {
+        JSONParser parser = new JSONParser();
+        List<Long> nextExecutionTimes = new ArrayList<>();
+        ScheduleInfo schedule = FieldUtil.getAsBeanFromJson((JSONObject)parser.parse(this.scheduleInfo), ScheduleInfo.class);
+        Pair<Long, Integer> result = schedule.nextExecutionTime(Pair.of(startTime - 300, 0));
+        long currentTime = System.currentTimeMillis()/1000;
+        while (result.getLeft() < currentTime) {
+            result = schedule.nextExecutionTime(result);
+        }
+        long endDate = schedule.getEndDate();
+        nextExecutionTimes.add(result.getLeft());
+        for (int i = 0; i < 9; i++) {
+            result = schedule.nextExecutionTime(result);
+            long nextExecutionTime = result.getLeft();
+            if(endDate > 0 && nextExecutionTime * 1000 > endDate){
+                break;
+            }
+            nextExecutionTimes.add(nextExecutionTime);
+        }
+        setData("nextExecutionDates",nextExecutionTimes);
+        return V3Action.SUCCESS;
+    }
+}
