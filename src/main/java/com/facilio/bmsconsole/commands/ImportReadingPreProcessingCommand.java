c34ccd587604a8b1c5f6a74056c867dacffe9158
HARIPRASAD <hariprasad@facilio.com>
2024-01-29 17:04:42 +0530

    Converted to SI unit Instead of app unit

--- src/main/java/com/facilio/bmsconsole/commands/ImportReadingPreProcessingCommand.java
+++ src/main/java/com/facilio/bmsconsole/commands/ImportReadingPreProcessingCommand.java
@@ -1,48 +1,45 @@
 package com.facilio.bmsconsole.commands;
 
-import com.facilio.beans.ModuleBean;
 import com.facilio.bmsconsole.commands.util.CommonCommandUtil;
 import com.facilio.bmsconsole.context.ReadingContext;
 import com.facilio.chain.FacilioContext;
 import com.facilio.command.FacilioCommand;
-import com.facilio.fw.BeanFactory;
-import com.facilio.modules.FieldFactory;
-import com.facilio.modules.fields.FacilioField;
-import com.facilio.modules.fields.NumberField;
 import com.facilio.unitconversion.Unit;
 import com.facilio.unitconversion.UnitsUtil;
 import org.apache.commons.chain.Context;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 import java.util.Map;
 
 public class ImportReadingPreProcessingCommand extends FacilioCommand {
+    private static final Logger LOGGER = LogManager.getLogger(ImportReadingPreProcessingCommand.class.getName());
     @Override
     public boolean executeCommand(Context context) throws Exception {
         convertSIUnits((FacilioContext) context);
         return false;
     }
 
-    private void convertSIUnits(FacilioContext context) throws Exception {
+    private void convertSIUnits(FacilioContext context) {
         Map<String, List<ReadingContext>> readingMap = CommonCommandUtil.getReadingMap(context);
-        ModuleBean bean = (ModuleBean) BeanFactory.lookup("ModuleBean");
         for (Map.Entry<String, List<ReadingContext>> entry : readingMap.entrySet()) {
-            String moduleName = entry.getKey();
             List<ReadingContext> entryValue = entry.getValue();
-            List<FacilioField> allFields= bean.getAllFields(moduleName);
-            Map<String, FacilioField> fieldMap = FieldFactory.getAsMap(allFields);
             for(ReadingContext readingContext : entryValue){
                 Map<String, Integer> unitMap = readingContext.getUnits();
                 if(!unitMap.isEmpty()) {
                     Map<String, Object> readings = readingContext.getReadings();
                     for (Map.Entry<String, Object> reading : readings.entrySet()) {
-
-                        String fieldName = reading.getKey();
-                        Integer fromUnit = unitMap.get(fieldName);
-                        if (fromUnit != null) {
-                            FacilioField field = fieldMap.get(fieldName);
-                            Object convertedValue = UnitsUtil.convert(reading.getValue(), getUnitEnum(fromUnit), ((NumberField) field).getUnitEnum());
-                            reading.setValue(convertedValue);
+                        try {
+                            String fieldName = reading.getKey();
+                            Integer fromUnit = unitMap.get(fieldName);
+                            if (fromUnit != null) {
+                                Object convertedValue = UnitsUtil.convertToSiUnit(reading.getValue(), getUnitEnum(fromUnit));
+                                reading.setValue(convertedValue);
+                            }
+                        }catch (Exception e) {
+                            LOGGER.error("Error occurred during readings import unit conversion" + e);
+                            throw e;
                         }
                     }
                 }
